548b879f298613d14d5d29d3fdb89827a0874bca
damien <devnull@localhost>
2013-11-19 14:49:18 +0100

    add setup script for running/developement environment setup

---
+++ bin/setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+POD_BIN_PATH=`dirname $0`
+POD_INSTALL_PATH=`dirname ${POD_BIN_PATH}`
+POD_INSTALL_FULL_PATH=`realpath ${POD_INSTALL_PATH}`
+
+echo $POD_BIN_PATH
+echo $POD_INSTALL_PATH
+echo $POD_INSTALL_FULL_PATH
+
+OLD_PATH=`pwd`
+
+
+cd ${POD_INSTALL_FULL_PATH}
+# virtualenv tg2env
+echo
+echo "-------------------------"
+echo "- initializes virtualenv"
+echo "-------------------------"
+echo "-> path:        tg2env/"
+echo "-> interpreter: python3"
+echo
+echo
+virtualenv -p /usr/bin/python3 tg2env
+
+echo
+echo
+echo "-------------------------"
+echo "- activates virtualenv"
+echo "-------------------------"
+source tg2env/bin/activate
+echo
+echo
+
+echo
+echo
+echo "-------------------------"
+echo "- installing turbogears"
+echo "-------------------------"
+pip install -f http://tg.gy/230 tg.devtools
+
+echo
+echo
+
+echo
+echo
+echo "-------------------------"
+echo "- setup project"
+echo "-------------------------"
+cd pboard/
+python setup.py develop
+echo
+echo
+
+# pip install psycopg2
+# pip install pillow
+
+cd ${OLD_PATH}
