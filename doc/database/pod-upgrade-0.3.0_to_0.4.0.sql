2421406aa605dca4d355757b8a16ee2e94b8eb36
Damien Accorsi <daccorsi@mozart>
2014-05-06 17:23:37 +0200

    make the update also work with full history tracking

--- doc/database/pod-upgrade-0.3.0_to_0.4.0.sql
+++ doc/database/pod-upgrade-0.3.0_to_0.4.0.sql
@@ -11,4 +11,25 @@ CREATE OR REPLACE VIEW pod_nodes AS
     FROM pod_nodes_history
     ORDER BY node_id, updated_at DESC;
 
+CREATE OR REPLACE RULE pod_insert_new_node AS ON INSERT
+TO pod_nodes
+DO INSTEAD INSERT INTO pod_nodes_history (node_id, parent_id, node_order, node_type, created_at, updated_at, 
+       data_label, data_content, data_datetime, node_status, data_reminder_datetime, 
+       data_file_name, data_file_content, data_file_mime_type, parent_tree_path, 
+       node_depth, owner_id, version_id, is_shared, is_public, public_url_key) VALUES (nextval('pod_nodes__node_id__sequence'), NEW.parent_id, NEW.node_order, NEW.node_type, NEW.created_at, NEW.updated_at, NEW.data_label, NEW.data_content, NEW.data_datetime, NEW.node_status, NEW.data_reminder_datetime, NEW.data_file_name, NEW.data_file_content, NEW.data_file_mime_type, NEW.parent_tree_path, NEW.node_depth, NEW.owner_id, nextval('pod_nodes_version_id_sequence'), NEW.is_shared, NEW.is_public, NEW.public_url_key)
+RETURNING node_id, parent_id, node_order, node_type, created_at, updated_at, 
+       data_label, data_content, data_datetime, node_status, data_reminder_datetime, 
+       data_file_name, data_file_content, data_file_mime_type, parent_tree_path, 
+       node_depth, owner_id, is_shared, is_public, public_url_key;
 
+CREATE OR REPLACE FUNCTION pod_update_node() RETURNS trigger AS $$
+BEGIN
+INSERT INTO pod_nodes_history (node_id, parent_id, node_order, node_type, created_at, updated_at, 
+       data_label, data_content, data_datetime, node_status, data_reminder_datetime, 
+       data_file_name, data_file_content, data_file_mime_type, parent_tree_path, 
+       node_depth, owner_id, version_id, is_shared, is_public, public_url_key) VALUES (NEW.node_id, NEW.parent_id, NEW.node_order, NEW.node_type, NEW.created_at, NEW.updated_at, NEW.data_label, NEW.data_content, NEW.data_datetime, NEW.node_status, NEW.data_reminder_datetime, NEW.data_file_name, NEW.data_file_content, NEW.data_file_mime_type, NEW.parent_tree_path, NEW.node_depth, NEW.owner_id, nextval('pod_nodes_version_id_sequence'), NEW.is_shared, NEW.is_public, NEW.public_url_key);
+return new;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER pod_update_node_tg INSTEAD OF UPDATE ON pod_nodes FOR EACH ROW EXECUTE PROCEDURE pod_update_node();
