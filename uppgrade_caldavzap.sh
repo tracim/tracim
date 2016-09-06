dc07fbfaec4c88f33e674e1e7184a58e34dc45c8
Bastien Sevajol (Algoo) <bastien.sevajol@algoo.fr>
2016-09-06 12:19:16 +0200

    caldavzap uppgrade script

---
+++ uppgrade_caldavzap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+read -p "Delete actual tracim/tracim/public/caldavzap folder? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -r tracim/tracim/public/caldavzap
+fi
+git clone https://github.com/algoo/caldavzap.git tracim/tracim/public/caldavzap
+rm -r tracim/tracim/public/caldavzap/.git
