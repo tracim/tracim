ec9f711d8d6a617bb3c05cb8f1d2b428f8ba48d0
Bastien Sevajol (Algoo) <bastien.sevajol@algoo.fr>
2016-12-14 16:37:30 +0100

    prepare working with volumes

--- docker/Debian_Uwsgi_PostgreSQL/entrypoint.sh
+++ docker/Debian_Uwsgi_PostgreSQL/entrypoint.sh
@@ -5,6 +5,7 @@
 # TODO: Supporter le changement des variables d'environnement ? (actuellement utilisé pour générer le .ini)
 # TODO: Fichiers de config link ls -s dans un dossier pour VOLUME
 # TODO: README QQCH pour les ports ? 80, 3060 et 5333
+# TODO: generate cookie secrent (if not yet done)
 
 # Start PostgreSQL
 service postgresql start
@@ -17,16 +18,25 @@ if ! [ "$( su - postgres -s /bin/bash -c "psql -tAc \"SELECT 1 FROM pg_database 
 fi
 
 # Create config.ini file if no exist
-if [ ! -f /tracim/tracim/config.ini ]; then
-    cp /tracim/tracim/development.ini.base /tracim/tracim/config.ini
-    sed -i "s/\(sqlalchemy.url *= *\).*/\1postgresql:\/\/tracim:tracim@127.0.0.1:5432\/tracim?client_encoding=utf8/" /tracim/tracim/config.ini
-    # TODO: ENV VAR
+if [ ! -f /etc/tracim/config.ini ]; then
+    cp /tracim/tracim/development.ini.base /etc/tracim/config.ini
 fi
+ln -s /etc/tracim/config.ini /tracim/tracim/config.ini
 
 # Create wsgidav.conf file if no exist
-if [ ! -f /tracim/tracim/wsgidav.conf ]; then
-    cp /tracim/tracim/wsgidav.conf.sample /tracim/tracim/wsgidav.conf
+if [ ! -f /etc/tracim/wsgidav.conf ]; then
+    cp /tracim/tracim/wsgidav.conf.sample /etc/tracim/wsgidav.conf
+fi
+ln -s /etc/tracim/wsgidav.conf /tracim/tracim/wsgidav.conf
+
+# Create uwsgi file if no exist
+if [ ! -f /etc/tracim/uwsgi.ini ]; then
+    cp /tracim/tracim/uwsgi.ini /etc/tracim/uwsgi.ini
 fi
 
+# Update config with ENV
+# TODO Manage other ENV vars
+sed -i "s/\(sqlalchemy.url *= *\).*/\1postgresql:\/\/tracim:tracim@127.0.0.1:5432\/tracim?client_encoding=utf8/" /etc/tracim/config.ini
+
 # Start with uwsgi
-uwsgi --http-socket 0.0.0.0:80 /etc/uwsgi/apps-available/tracim.ini
+uwsgi --http-socket 0.0.0.0:80 /etc/tracim/uwsgi.ini
