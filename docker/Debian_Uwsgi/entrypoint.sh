2282a7384e829072c1aa2da078e8e5bc083c0217
Bastien Sevajol (Algoo) <bastien.sevajol@algoo.fr>
2016-12-19 12:09:17 +0100

    docker: std image working with pgsql

--- docker/Debian_Uwsgi/entrypoint.sh
+++ docker/Debian_Uwsgi/entrypoint.sh
@@ -13,35 +13,14 @@
 #
 # * DATABASE_TYPE (values: postgresql, mysql, sqlite)
 # * DATABASE_USER
-# * DATABASE PASSWORD
+# * DATABASE_PASSWORD
 # * DATABASE_HOST
+# * DATABASE_PORT
 # * DATABASE_NAME
 #
 
 # Check environment variables
-./check_env_vars.sh
-
-
-
-
-# MySQL case: engine is mysql+oursql
-if [ "$DATABASE_TYPE" = mysql ] ; then
-    DATABASE_TYPE=mysql+oursql
-fi
-
-# Script preparation
-FRESH_INSTALL=false
-
-# Start PostgreSQL
-service postgresql start  # Broken if volume empty ... ???
-
-# Init database if needed
-if ! [ "$( su - postgres -s /bin/bash -c "psql -tAc \"SELECT 1 FROM pg_database WHERE datname='tracim'\"" )" = '1' ]; then
-    FRESH_INSTALL=true
-    su - postgres -s /bin/bash -c "psql -c \"CREATE DATABASE tracim;\""
-    su - postgres -s /bin/bash -c "psql -c \"CREATE USER tracim WITH PASSWORD 'tracim';\""
-    su - postgres -s /bin/bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE tracim TO tracim;\""
-fi
+/tracim/check_env_vars.sh
 
 # Create config.ini file if no exist
 if [ ! -f /etc/tracim/config.ini ]; then
@@ -60,13 +39,37 @@ if [ ! -f /etc/tracim/uwsgi.ini ]; then
     cp /tracim/tracim/uwsgi.ini.template /etc/tracim/uwsgi.ini
 fi
 
-# Update config with ENV
-# TODO Manage other ENV vars
-sed -i "s/\(sqlalchemy.url *= *\).*/\1postgresql:\/\/tracim:tracim@127.0.0.1:5432\/tracim?client_encoding=utf8/" /etc/tracim/config.ini
+# MySQL case
+if [ "$DATABASE_TYPE" = mysql ] ; then
+    # Ensure DATABASE_PORT is set
+    if ! [ -n "$DATABASE_PORT" ]; then
+        DATABASE_PORT=3306
+    fi
+    # engine is mysql+oursql
+    DATABASE_TYPE=mysql+oursql
+fi
+
+# PostgreSQL case
+if [ "$DATABASE_TYPE" = postgresql ] ; then
+    # Ensure DATABASE_PORT is set
+    if ! [ -n "$DATABASE_PORT" ]; then
+        DATABASE_PORT=5432
+    fi
+    DATABASE_SUFFIX="?client_encoding=utf8"
+fi
+
+# Update sqlalchemy.url
+sed -i "s/\(sqlalchemy.url *= *\).*/\\sqlalchemy.url = $DATABASE_TYPE:\/\/$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT\/$DATABASE_NAME$DATABASE_SUFFIX/" /etc/tracim/config.ini
 
 # Initialize database if it is a fresh install
-if [ "$FRESH_INSTALL" = true ] ; then
-    echo "Init tracim database"
+if [ "$DATABASE_TYPE" = postgresql ] ; then
+    TEST_TABLE=$(PGPASSWORD="$DATABASE_PASSWORD" psql -U ${DATABASE_USER} -h ${DATABASE_HOST} -d ${DATABASE_NAME} -t -c "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'content' );")
+    if [ ${TEST_TABLE} = f ] ; then
+        INIT_DATABASE=true
+    fi
+fi
+
+if [ "$INIT_DATABASE" = true ] ; then
     cd /tracim/tracim/ && gearbox setup-app -c config.ini
 fi
 
