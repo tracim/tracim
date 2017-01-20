8cdd01b00ed4ce7a451b2b0d7b42a267f3b4f594
Bastien Sevajol (Algoo) <bastien.sevajol@algoo.fr>
2017-01-20 09:33:51 +0100

    Use algoo version of tgapp-resetpassword

--- tracim/tracim/tests/library/test_resetpassword.py
+++ tracim/tracim/tests/library/test_resetpassword.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+from nose.tools import assert_raises
+from resetpassword.lib import _plain_send_mail
+
+from tracim.tests import TestStandard
+
+
+class TestSerializers(TestStandard):
+    application_under_test = 'nosmtp'
+
+    def test_unit__plain_send_mail__ok(self):
+        assert_raises(
+            ConnectionRefusedError,
+            _plain_send_mail,
+            'Name of sender <email@sender.local>',
+            'Recipient name <recipient@recipient.local>',
+            'hello',
+            'How are you ?',
+        )
