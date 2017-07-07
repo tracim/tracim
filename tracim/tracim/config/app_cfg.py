80dbaa4d1638288e0275a75d2558cdaea2884077
Adrien Panay <algooapy@users.noreply.github.com>
2017-07-07 12:34:32 +0200

    Parameters the storage path of files depot.

--- tracim/tracim/config/app_cfg.py
+++ tracim/tracim/config/app_cfg.py
@@ -13,10 +13,10 @@ convert them into boolean, for example, you should use the
 
 """
 import imp
-import importlib
 import os
 from urllib.parse import urlparse
 
+from depot.manager import DepotManager
 import tg
 from paste.deploy.converters import asbool
 from tg.configuration.milestones import environment_loaded
@@ -28,7 +28,6 @@ from tracim.lib.system import InterruptManager
 from tracim.lib.utils import lazy_ugettext as l_
 
 import tracim
-from tracim import model
 from tracim.config import TracimAppConfig
 from tracim.lib.base import logger
 from tracim.lib.daemons import DaemonsManager
@@ -38,11 +37,6 @@ from tracim.lib.daemons import WsgiDavDaemon
 from tracim.model.data import ActionDescription
 from tracim.model.data import ContentType
 
-from depot.manager import DepotManager
-DepotManager.configure(
-    'default',
-    {'depot.storage_path': '/tmp/depot_storage_path/'}
-)
 
 base_config = TracimAppConfig()
 base_config.renderers = []
@@ -51,25 +45,28 @@ base_config.use_toscawidgets2 = True
 
 base_config.package = tracim
 
-#Enable json in expose
+# Enable json in expose
 base_config.renderers.append('json')
-#Enable genshi in expose to have a lingua franca for extensions and pluggable apps
-#you can remove this if you don't plan to use it.
+# Enable genshi in expose to have a lingua franca for extensions and pluggable
+# apps
+# you can remove this if you don't plan to use it.
 base_config.renderers.append('genshi')
 
-#Set the default renderer
+# Set the default renderer
 base_config.default_renderer = 'mako'
 base_config.renderers.append('mako')
-#Configure the base SQLALchemy Setup
+# Configure the base SQLALchemy Setup
 base_config.use_sqlalchemy = True
 base_config.model = tracim.model
 base_config.DBSession = tracim.model.DBSession
 
-# This value can be modified by tracim.lib.auth.wrapper.AuthConfigWrapper but have to be specified before
+# This value can be modified by tracim.lib.auth.wrapper.AuthConfigWrapper but
+# have to be specified before
 base_config.auth_backend = 'sqlalchemy'
 
 # base_config.flash.cookie_name
-# base_config.flash.default_status -> Default message status if not specified (ok by default)
+# base_config.flash.default_status -> Default message status if not specified
+# (ok by default)
 base_config['flash.template'] = '''
 <div class="alert alert-${status}" style="margin-top: 1em;">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -79,9 +76,20 @@ base_config['flash.template'] = '''
     </div>
 </div>
 '''
-# -> string.Template instance used as the flash template when rendered from server side, will receive $container_id, $message and $status variables.
-# flash.js_call -> javascript code which will be run when displaying the flash from javascript. Default is webflash.render(), you can use webflash.payload() to retrieve the message and show it with your favourite library.
-# flash.js_template -> string.Template instance used to replace full javascript support for flash messages. When rendering flash message for javascript usage the following code will be used instead of providing the standard webflash object. If you replace js_template you must also ensure cookie parsing and delete it for already displayed messages. The template will receive: $container_id, $cookie_name, $js_call variables.
+
+# -> string.Template instance used as the flash template when rendered from
+# server side, will receive $container_id, $message and $status variables.
+
+# flash.js_call -> javascript code which will be run when displaying the flash
+# from javascript. Default is webflash.render(), you can use webflash.payload()
+# to retrieve the message and show it with your favourite library.
+
+# flash.js_template -> string.Template instance used to replace full javascript
+# support for flash messages. When rendering flash message for javascript usage
+# the following code will be used instead of providing the standard webflash
+# object. If you replace js_template you must also ensure cookie parsing and
+# delete it for already displayed messages. The template will receive:
+# $container_id, $cookie_name, $js_call variables.
 
 base_config['templating.genshi.name_constant_patch'] = True
 
@@ -91,11 +99,18 @@ base_config['templating.genshi.name_constant_patch'] = True
 base_config.sa_auth.cookie_secret = "3283411b-1904-4554-b0e1-883863b53080"
 
 # INFO - This is the way to specialize the resetpassword email properties
-# plug(base_config, 'resetpassword', None, mail_subject=reset_password_email_subject)
+# plug(base_config,
+#      'resetpassword',
+#      None,
+#      mail_subject=reset_password_email_subject)
 plug(base_config, 'resetpassword', 'reset_password')
 
-replace_template(base_config, 'resetpassword.templates.index', 'tracim.templates.reset_password_index')
-replace_template(base_config, 'resetpassword.templates.change_password', 'mako:tracim.templates.reset_password_change_password')
+replace_template(base_config,
+                 'resetpassword.templates.index',
+                 'tracim.templates.reset_password_index')
+replace_template(base_config,
+                 'resetpassword.templates.change_password',
+                 'mako:tracim.templates.reset_password_change_password')
 
 daemons = DaemonsManager()
 
@@ -116,10 +131,25 @@ def start_daemons(manager: DaemonsManager):
     if cfg.EMAIL_PROCESSING_MODE == CFG.CST.ASYNC:
         manager.run('mail_sender', MailSenderDaemon)
 
+
+def configure_depot():
+    """
+    Configure Depot
+    """
+    depot_storage_name = 'default'
+    depot_storage_path = CFG.get_instance().DEPOT_STORAGE_PATH
+    depot_storage_settings = {'depot.storage_path': depot_storage_path}
+    DepotManager.configure(depot_storage_name,
+                           depot_storage_settings)
+
+
 environment_loaded.register(lambda: start_daemons(daemons))
+environment_loaded.register(lambda: configure_depot())
+
 interrupt_manager = InterruptManager(os.getpid(), daemons_manager=daemons)
 
-# Note: here are fake translatable strings that allow to translate messages for reset password email content
+# Note: here are fake translatable strings that allow to translate messages for
+# reset password email content
 duplicated_email_subject = l_('Password reset request')
 duplicated_email_body = l_('''
 We've received a request to reset the password for this account.
@@ -129,11 +159,21 @@ Please click this link to reset your password:
 
 If you no longer wish to make the above change, or if you did not initiate this request, please disregard and/or delete this e-mail.
 ''')
+duplicated_email_body = l_('''
+We've received a request to reset the password for this account.
+Please click this link to reset your password:
+
+%(password_reset_link)s
+
+If you no longer wish to make the above change, or if you did not initiate this request, please disregard and/or delete this e-mail.
+''')
+import pudb; pu.db
 
 #######
 #
 # INFO - D.A. - 2014-10-31
-# The following code is a dirty way to integrate translation for resetpassword tgapp in tracim
+# The following code is a dirty way to integrate translation for resetpassword
+# tgapp in tracim
 # TODO - Integrate these translations into tgapp-resetpassword
 #
 
@@ -142,7 +182,6 @@ l_('Confirm new password')
 l_('Save new password')
 l_('Email address')
 l_('Send Request')
-
 
 l_('Password reset request sent')
 l_('Invalid password reset request')
@@ -159,6 +198,7 @@ Please click this link to reset your password:
 If you no longer wish to make the above change, or if you did not initiate this request, please disregard and/or delete this e-mail.
 ''')
 
+
 class CFG(object):
     """
     Singleton used for easy access to config file parameters
@@ -174,7 +214,8 @@ class CFG(object):
 
     def __setattr__(self, key, value):
         """
-        Log-ready setter. this is used for logging configuration (every parameter except password)
+        Log-ready setter. this is used for logging configuration (every
+        parameter except password)
         :param key:
         :param value:
         :return:
@@ -183,7 +224,8 @@ class CFG(object):
                 ('URL' not in key or type(value) == str) and \
                 'CONTENT' not in key:
             # We do not show PASSWORD for security reason
-            # we do not show URL because the associated config uses tg.lurl() which is evaluated when at display time.
+            # we do not show URL because the associated config uses tg.lurl()
+            # which is evaluated when at display time.
             # At the time of configuration setup, it can't be evaluated
             # We do not show CONTENT in order not to pollute log files
             logger.info(self, 'CONFIG: [ {} | {} ]'.format(key, value))
@@ -194,14 +236,19 @@ class CFG(object):
 
     def __init__(self):
 
-        self.PREVIEW_CACHE = str(tg.config.get('preview_cache_dir'))
+        self.DEPOT_STORAGE_PATH = tg.config.get('depot_storage_path')
+        self.PREVIEW_CACHE = tg.config.get('preview_cache_dir')
 
-        self.DATA_UPDATE_ALLOWED_DURATION = int(tg.config.get('content.update.allowed.duration', 0))
+        self.DATA_UPDATE_ALLOWED_DURATION = \
+            int(tg.config.get('content.update.allowed.duration', 0))
 
         self.WEBSITE_TITLE = tg.config.get('website.title', 'TRACIM')
-        self.WEBSITE_HOME_TITLE_COLOR = tg.config.get('website.title.color', '#555')
-        self.WEBSITE_HOME_IMAGE_URL = tg.lurl('/assets/img/home_illustration.jpg')
-        self.WEBSITE_HOME_BACKGROUND_IMAGE_URL = tg.lurl('/assets/img/bg.jpg')
+        self.WEBSITE_HOME_TITLE_COLOR = \
+            tg.config.get('website.title.color', '#555')
+        self.WEBSITE_HOME_IMAGE_URL = \
+            tg.lurl('/assets/img/home_illustration.jpg')
+        self.WEBSITE_HOME_BACKGROUND_IMAGE_URL = \
+            tg.lurl('/assets/img/bg.jpg')
         self.WEBSITE_BASE_URL = tg.config.get('website.base_url', '')
         self.WEBSITE_SERVER_NAME = tg.config.get('website.server_name', None)
 
@@ -216,7 +263,8 @@ class CFG(object):
 
         self.WEBSITE_HOME_TAG_LINE = tg.config.get('website.home.tag_line', '')
         self.WEBSITE_SUBTITLE = tg.config.get('website.home.subtitle', '')
-        self.WEBSITE_HOME_BELOW_LOGIN_FORM = tg.config.get('website.home.below_login_form', '')
+        self.WEBSITE_HOME_BELOW_LOGIN_FORM = \
+            tg.config.get('website.home.below_login_form', '')
 
         if tg.config.get('email.notification.from'):
             raise Exception(
@@ -229,8 +277,10 @@ class CFG(object):
             tg.config.get('email.notification.from.email')
         self.EMAIL_NOTIFICATION_FROM_DEFAULT_LABEL = \
             tg.config.get('email.notification.from.default_label')
-        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_TEMPLATE_HTML = tg.config.get('email.notification.content_update.template.html')
-        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_TEMPLATE_TEXT = tg.config.get('email.notification.content_update.template.text')
+        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_TEMPLATE_HTML = \
+            tg.config.get('email.notification.content_update.template.html')
+        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_TEMPLATE_TEXT = \
+            tg.config.get('email.notification.content_update.template.text')
         self.EMAIL_NOTIFICATION_CREATED_ACCOUNT_TEMPLATE_HTML = tg.config.get(
             'email.notification.created_account.template.html',
             './tracim/templates/mail/created_account_body_html.mak',
@@ -239,24 +289,32 @@ class CFG(object):
             'email.notification.created_account.template.text',
             './tracim/templates/mail/created_account_body_text.mak',
         )
-        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_SUBJECT = tg.config.get('email.notification.content_update.subject')
+        self.EMAIL_NOTIFICATION_CONTENT_UPDATE_SUBJECT = \
+            tg.config.get('email.notification.content_update.subject')
         self.EMAIL_NOTIFICATION_CREATED_ACCOUNT_SUBJECT = tg.config.get(
             'email.notification.created_account.subject',
             '[{website_title}] Created account',
         )
-        self.EMAIL_NOTIFICATION_PROCESSING_MODE = tg.config.get('email.notification.processing_mode')
+        self.EMAIL_NOTIFICATION_PROCESSING_MODE = \
+            tg.config.get('email.notification.processing_mode')
 
-
-        self.EMAIL_NOTIFICATION_ACTIVATED = asbool(tg.config.get('email.notification.activated'))
-        self.EMAIL_NOTIFICATION_SMTP_SERVER = tg.config.get('email.notification.smtp.server')
-        self.EMAIL_NOTIFICATION_SMTP_PORT = tg.config.get('email.notification.smtp.port')
-        self.EMAIL_NOTIFICATION_SMTP_USER = tg.config.get('email.notification.smtp.user')
-        self.EMAIL_NOTIFICATION_SMTP_PASSWORD = tg.config.get('email.notification.smtp.password')
+        self.EMAIL_NOTIFICATION_ACTIVATED = \
+            asbool(tg.config.get('email.notification.activated'))
+        self.EMAIL_NOTIFICATION_SMTP_SERVER = \
+            tg.config.get('email.notification.smtp.server')
+        self.EMAIL_NOTIFICATION_SMTP_PORT = \
+            tg.config.get('email.notification.smtp.port')
+        self.EMAIL_NOTIFICATION_SMTP_USER = \
+            tg.config.get('email.notification.smtp.user')
+        self.EMAIL_NOTIFICATION_SMTP_PASSWORD = \
+            tg.config.get('email.notification.smtp.password')
 
         self.TRACKER_JS_PATH = tg.config.get('js_tracker_path')
-        self.TRACKER_JS_CONTENT = self.get_tracker_js_content(self.TRACKER_JS_PATH)
+        self.TRACKER_JS_CONTENT = \
+            self.get_tracker_js_content(self.TRACKER_JS_PATH)
 
-        self.WEBSITE_TREEVIEW_CONTENT = tg.config.get('website.treeview.content')
+        self.WEBSITE_TREEVIEW_CONTENT = \
+            tg.config.get('website.treeview.content')
 
         self.EMAIL_NOTIFICATION_NOTIFIED_EVENTS = [
             ActionDescription.COMMENT,
@@ -274,12 +332,13 @@ class CFG(object):
             # ContentType.Folder -- Folder is skipped
         ]
 
-        self.RADICALE_SERVER_HOST = tg.config.get('radicale.server.host', '0.0.0.0')
-        self.RADICALE_SERVER_PORT = int(
-            tg.config.get('radicale.server.port', 5232)
-        )
+        self.RADICALE_SERVER_HOST = \
+            tg.config.get('radicale.server.host', '0.0.0.0')
+        self.RADICALE_SERVER_PORT = \
+            int(tg.config.get('radicale.server.port', 5232))
         # Note: Other parameters needed to work in SSL (cert file, etc)
-        self.RADICALE_SERVER_SSL = asbool(tg.config.get('radicale.server.ssl', False))
+        self.RADICALE_SERVER_SSL = \
+            asbool(tg.config.get('radicale.server.ssl', False))
         self.RADICALE_SERVER_FILE_SYSTEM_FOLDER = tg.config.get(
             'radicale.server.filesystem.folder',
             './radicale/collections',
@@ -337,7 +396,9 @@ class CFG(object):
             'wsgidav.config_path',
             'wsgidav.conf',
         )
-        # TODO: Convert to importlib (cf http://stackoverflow.com/questions/41063938/use-importlib-instead-imp-for-non-py-file)
+
+        # TODO: Convert to importlib
+        # http://stackoverflow.com/questions/41063938/use-importlib-instead-imp-for-non-py-file
         self.wsgidav_config = imp.load_source(
             'wsgidav_config',
             self.WSGIDAV_CONFIG_PATH,
@@ -395,17 +456,16 @@ class CFG(object):
             0,
         ))
 
-    def get_tracker_js_content(self, js_tracker_file_path = None):
+    def get_tracker_js_content(self, js_tracker_file_path=None):
+        result = ''
         js_tracker_file_path = tg.config.get('js_tracker_path', None)
         if js_tracker_file_path:
-            logger.info(self, 'Reading JS tracking code from file {}'.format(js_tracker_file_path))
-            with open (js_tracker_file_path, 'r') as js_file:
+            info_log = 'Reading JS tracking code from file {}'
+            logger.info(self, info_log.format(js_tracker_file_path))
+            with open(js_tracker_file_path, 'r') as js_file:
                 data = js_file.read()
-            return data
-        else:
-            return ''
-
-
+            result = data
+        return result
 
     class CST(object):
         ASYNC = 'ASYNC'
@@ -413,6 +473,7 @@ class CFG(object):
 
         TREEVIEW_FOLDERS = 'folders'
         TREEVIEW_ALL = 'all'
+
 
 #######
 #
