68f77bb0d37ed3e13ffb3acb2434bf0dd652687b
Damien Accorsi <damien@christophecolomb>
2017-08-04 18:17:05 +0200

    fixes reset password test

--- tracim/tracim/tests/library/test_resetpassword.py
+++ tracim/tracim/tests/library/test_resetpassword.py
@@ -3,14 +3,14 @@ from nose.tools import assert_raises
 from resetpassword.lib import _plain_send_mail
 
 from tracim.tests import TestStandard
-
+from smtplib import SMTPNotSupportedError
 
 class TestSerializers(TestStandard):
     application_under_test = 'nosmtp'
 
     def test_unit__plain_send_mail__ok(self):
         assert_raises(
-            ConnectionRefusedError,
+            (ConnectionRefusedError, SMTPNotSupportedError),
             _plain_send_mail,
             'Name of sender <email@sender.local>',
             'Recipient name <recipient@recipient.local>',
