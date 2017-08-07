3340e9ce610bd490dcdb30b0fec2b83b8103dd06
Adrien Panay <algooapy@users.noreply.github.com>
2017-08-07 10:14:29 +0200

    Tests getting notifiable roles
    Function tested:
    tracim/tracim/lib/workspace.py:WorkspaceApi.get_notifiable_roles
    
    lebouquetin@8c23f0a has closed #168

--- tracim/tracim/tests/library/test_workspace.py
+++ tracim/tracim/tests/library/test_workspace.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-import transaction
 from nose.tools import eq_
 
 from tracim.lib.content import ContentApi
-from tracim.model import DBSession, Content, User
+from tracim.lib.user import UserApi
+from tracim.lib.userworkspace import RoleApi
+from tracim.lib.workspace import WorkspaceApi
+from tracim.model import Content
+from tracim.model import DBSession
+from tracim.model import User
+from tracim.model.data import UserRoleInWorkspace
 from tracim.model.data import Workspace
-from tracim.tests import BaseTestThread, TestStandard
+from tracim.tests import BaseTestThread
+from tracim.tests import TestStandard
 
 
 class TestThread(BaseTestThread, TestStandard):
@@ -21,3 +27,17 @@ class TestThread(BaseTestThread, TestStandard):
         workspace = DBSession.query(Workspace).filter(Workspace.label == 'workspace_1').one()
         folder = ContentApi.get_canonical_query().filter(Content.label == 'folder_1').one()
         eq_([folder, ], list(workspace.get_valid_children()))
+
+    def test_get_notifiable_roles(self):
+        admin = DBSession.query(User) \
+            .filter(User.email == 'admin@admin.admin').one()
+        wapi = WorkspaceApi(admin)
+        w = wapi.create_workspace(label='workspace w', save_now=True)
+        uapi = UserApi(admin)
+        u = uapi.create_user(email='u.u@u.u', save_now=True)
+        eq_([], wapi.get_notifiable_roles(workspace=w))
+        rapi = RoleApi(u)
+        r = rapi.create_one(u, w, UserRoleInWorkspace.READER, with_notif='on')
+        eq_([r, ], wapi.get_notifiable_roles(workspace=w))
+        u.is_active = False
+        eq_([], wapi.get_notifiable_roles(workspace=w))
