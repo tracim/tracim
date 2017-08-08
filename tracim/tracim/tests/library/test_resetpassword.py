b8858b1bef0e47768ac59770ff71d96e3eeaac36
Damien Accorsi <damien@christophecolomb>
2017-08-08 16:50:03 +0200

    fixes test resetpassword (different smtp exception raised between python 3.4 and python 3.5)

--- tracim/tracim/tests/library/test_resetpassword.py
+++ tracim/tracim/tests/library/test_resetpassword.py
@@ -3,17 +3,30 @@ from nose.tools import assert_raises
 from resetpassword.lib import _plain_send_mail
 
 from tracim.tests import TestStandard
-from smtplib import SMTPNotSupportedError
 
 class TestSerializers(TestStandard):
     application_under_test = 'nosmtp'
 
     def test_unit__plain_send_mail__ok(self):
-        assert_raises(
-            (ConnectionRefusedError, SMTPNotSupportedError),
-            _plain_send_mail,
-            'Name of sender <email@sender.local>',
-            'Recipient name <recipient@recipient.local>',
-            'hello',
-            'How are you ?',
-        )
+        import sys
+        if sys.version_info >= (3, 5):
+            from smtplib import SMTPNotSupportedError
+
+            assert_raises(
+                (ConnectionRefusedError, SMTPNotSupportedError),
+                _plain_send_mail,
+                'Name of sender <email@sender.local>',
+                'Recipient name <recipient@recipient.local>',
+                'hello',
+                'How are you ?',
+            )
+        else:
+            from smtplib import SMTPException
+            assert_raises(
+                (SMTPException, ConnectionRefusedError),
+                _plain_send_mail,
+                'Name of sender <email@sender.local>',
+                'Recipient name <recipient@recipient.local>',
+                'hello',
+                'How are you ?',
+            )
