3f3937c0394c6440cbb22ec30c2774f21ed7e6b8
Adrien Panay <algooapy@users.noreply.github.com>
2017-09-11 14:39:39 +0200

    Fixes 'all files only on disk' migration downgrade
    Closes #325

--- tracim/migration/versions/f3852e1349c4_all_files_only_on_disk.py
+++ tracim/migration/versions/f3852e1349c4_all_files_only_on_disk.py
@@ -1,4 +1,4 @@
-"""all files only on disk
+"""all files only on disk.
 
 Revision ID: f3852e1349c4
 Revises: 913efdf409e5
@@ -8,10 +8,11 @@ Create Date: 2017-07-24 17:15:54.278141
 
 from alembic import context
 from alembic import op
+
 from depot.fields.sqlalchemy import UploadedFileField
 from depot.manager import DepotManager
+
 import sqlalchemy as sa
-from sqlalchemy.sql.expression import func
 
 # revision identifiers, used by Alembic.
 revision = 'f3852e1349c4'
@@ -33,9 +34,6 @@ revision_helper = sa.Table(
     'content_revisions',
     sa.MetaData(),
     sa.Column('revision_id', sa.Integer, primary_key=True),
-    sa.Column('label', sa.String(1024), nullable=False),
-    sa.Column('file_extension', sa.String(255), nullable=False),
-    sa.Column('file_mimetype', sa.String(255), nullable=False),
     sa.Column('file_content', sa.LargeBinary),
     sa.Column('depot_file', UploadedFileField, nullable=True),
     sa.Column('type', sa.String(32), nullable=False),
@@ -43,13 +41,13 @@ revision_helper = sa.Table(
 
 
 def upgrade():
-    """Drops the file content from revision."""
+    """Drop the file content from revision."""
     with op.batch_alter_table('content_revisions') as batch_op:
         batch_op.drop_column('file_content')
 
 
 def downgrade():
-    """Adds the file content in revision."""
+    """Add the file content in revision."""
     with op.batch_alter_table('content_revisions') as batch_op:
         batch_op.add_column(sa.Column('file_content', sa.LargeBinary))
 
@@ -58,11 +56,10 @@ def downgrade():
     connection = op.get_bind()
     select_query = revision_helper.select() \
         .where(revision_helper.c.type == 'file') \
-        .where(revision_helper.c.depot_file.isnot(None)) \
-        .where(func.length(revision_helper.c.depot_file) > 0)
+        .where(revision_helper.c.depot_file.isnot(None))
     files = connection.execute(select_query).fetchall()
     for file in files:
-        depot_file_content = depot.get(file.depot_file)
+        depot_file_content = depot.get(file.depot_file).read()
         update_query = revision_helper.update() \
             .where(revision_helper.c.revision_id == file.revision_id) \
             .values(file_content=depot_file_content)
