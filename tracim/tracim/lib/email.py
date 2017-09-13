4651cacc4679f73b03a190cfec942e9a7a91050d
guillaume chanaud <g.chanaud@regence-production.ch>
2017-09-13 22:05:22 +0200

    Separate if statements for starttls() and login() calls

--- tracim/tracim/lib/email.py
+++ tracim/tracim/lib/email.py
@@ -73,10 +73,16 @@ class EmailSender(object):
                 try:
                     starttls_result = self._smtp_connection.starttls()
                     logger.debug(self, 'SMTP start TLS result: {}'.format(starttls_result))
+                except Exception as e:
+                    logger.debug(self, 'SMTP start TLS error: {}'.format(e.__str__()))
+                    
+            if self._smtp_config.login:
+                try:
                     login_res = self._smtp_connection.login(self._smtp_config.login, self._smtp_config.password)
                     logger.debug(self, 'SMTP login result: {}'.format(login_res))
                 except Exception as e:
-                    logger.debug(self, 'SMTP start TLS error: {}'.format(e.__str__()))
+                    logger.debug(self, 'SMTP login error: {}'.format(e.__str__()))
+                
             logger.info(self, 'Connection OK')
 
     def disconnect(self):
