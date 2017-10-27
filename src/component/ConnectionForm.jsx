544961620eec4e1ee4ce3862ab02f07af460eb77
Skylsmoi <come.huguies@gmail.com>
2017-10-27 16:01:34 +0200

    missing name refactored

--- src/component/ConnectionForm.jsx
+++ src/component/ConnectionForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Connection = props => {
+export const ConnectionForm = props => {
   return (
     <div>
       <input type='text' onChange={props.onChangeLogin} placeholder='Login' />
