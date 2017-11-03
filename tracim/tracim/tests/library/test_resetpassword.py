85fbb13c9e2ceccf5c1462526f221b09828eb5cf
philippe <philippe.accorsi@algoo.fr>
2017-11-03 09:41:34 +0100

    deactivate test_resetpassword for good

--- tracim/tracim/tests/library/test_resetpassword.py
+++ tracim/tracim/tests/library/test_resetpassword.py
@@ -16,25 +16,25 @@ class TestResetPassword(TestStandard):
     # [1] no admin calendar: https://github.com/tracim/tracim/issues/274
     # Deactivating it solves this odd issue, probably due to improper
     # individual test environment setup in tests sequence.
-    def deactivated_test_unit__plain_send_mail__ok(self):
-        if sys.version_info >= (3, 5):
-            from smtplib import SMTPNotSupportedError
+    #def deactivated_test_unit__plain_send_mail__ok(self):
+    #    if sys.version_info >= (3, 5):
+    #        from smtplib import SMTPNotSupportedError
 
-            assert_raises(
-                (ConnectionRefusedError, SMTPNotSupportedError),
-                _plain_send_mail,
-                'Name of sender <email@sender.local>',
-                'Recipient name <recipient@recipient.local>',
-                'hello',
-                'How are you ?',
-            )
-        else:
-            from smtplib import SMTPException
-            assert_raises(
-                (SMTPException, ConnectionRefusedError),
-                _plain_send_mail,
-                'Name of sender <email@sender.local>',
-                'Recipient name <recipient@recipient.local>',
-                'hello',
-                'How are you ?',
-            )
+    #        assert_raises(
+    #            (ConnectionRefusedError, SMTPNotSupportedError),
+    #            _plain_send_mail,
+    #            'Name of sender <email@sender.local>',
+    #            'Recipient name <recipient@recipient.local>',
+    #            'hello',
+    #            'How are you ?',
+    #        )
+    #    else:
+    #        from smtplib import SMTPException
+    #        assert_raises(
+    #            (SMTPException, ConnectionRefusedError),
+    #            _plain_send_mail,
+    #            'Name of sender <email@sender.local>',
+    #            'Recipient name <recipient@recipient.local>',
+    #            'hello',
+    #            'How are you ?',
+    #        )
