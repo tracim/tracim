5b7bfb6dc67ca5569b61da471ac56b81ff896418
AlexiCauvin <cauvin.alexi@gmail.com>
2018-01-04 16:25:08 +0100

    Fixe the page file without the fixe of the sidebar's transition

--- src/component/Workspace/FileType/File.jsx
+++ src/component/Workspace/FileType/File.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
 import imgPDF from '../../../img/pdf.jpg'
-// import imgExcel from '../img/excel.png'
+// import imgExcel from '../../../img/excel.png'
 
 class File extends Component {
   constructor (props) {
@@ -15,82 +15,68 @@ class File extends Component {
 
   render () {
     return (
-      <div className={classnames('wsFileFile__contentpage__visualizer', {'activesidebar': this.state.activesidebar})}>
-        <div className='wsFileFile__contentpage__visualizer__dloption'>
-          <div className='wsFileFile__contentpage__visualizer__dloption__icon'>
+      <div className={classnames('wsFileFile__contentpage__preview', {'activesidebar': this.state.activesidebar})}>
+        <div className='wsFileFile__contentpage__preview__dloption'>
+          <div className='wsFileFile__contentpage__preview__dloption__icon'>
             <i className='fa fa-download' />
           </div>
-          <div className='wsFileFile__contentpage__visualizer__dloption__icon'>
+          <div className='wsFileFile__contentpage__preview__dloption__icon'>
             <i className='fa fa-file-pdf-o' />
           </div>
-          <div className='wsFileFile__contentpage__visualizer__dloption__icon'>
+          <div className='wsFileFile__contentpage__preview__dloption__icon'>
             <i className='fa fa-files-o' />
           </div>
         </div>
-        <div className='wsFileFile__contentpage__visualizer__slidecontainer'>
-          <div className='wsFileFile__contentpage__visualizer__slidecontainer__chevron'>
+        <div className='wsFileFile__contentpage__preview__slider'>
+          <div className='wsFileFile__contentpage__preview__slider__icon'>
             <i className='fa fa-chevron-left' />
           </div>
-          <div className='wsFileFile__contentpage__visualizer__slidecontainer__fileimg'>
-            <img src={imgPDF} alt='fichier pdf' className='d-block img-thumbnail rounded m-auto' />
+          <div className='wsFileFile__contentpage__preview__slider__fileimg'>
+            <img src={imgPDF} alt='fichier pdf' className='img-thumbnail mx-auto' />
           </div>
-          <div className='wsFileFile__contentpage__visualizer__slidecontainer__chevron'>
+          <div className='wsFileFile__contentpage__preview__slider__icon'>
             <i className='fa fa-chevron-right' />
           </div>
         </div>
-        <div className='wsFileFile__contentpage__visualizer__sidebar' onClick={this.handleClickSidebar}>
-          <div className='wsFileFile__contentpage__visualizer__sidebar__visiblepart'>
-            <div className='wsFileFile__contentpage__visualizer__sidebar__visiblepart__icon'>
+        <div className='wsFileFile__contentpage__preview__sidebar' onClick={this.handleClickSidebar}>
+          <div className='wsFileFile__contentpage__preview__sidebar__button'>
+            <div className='wsFileFile__contentpage__preview__sidebar__button__icon'>
               <i className='fa fa-gear' />
             </div>
-            <div className='wsFileFile__contentpage__visualizer__sidebar__visiblepart__title'>
+            <div className='wsFileFile__contentpage__preview__sidebar__button__title'>
               Propriétés
             </div>
           </div>
-          <div className='wsFileFile__contentpage__visualizer__sidebar__propertydetail'>
-            Fichier : Planning Intégration de Tracim
-            <br />
-            Description : Fichier excel et pdf du planning
-            <br />
-            Taille : 50ko
-            <br />
-            Utilisateurs : Damien, Bastien, Côme, Phillipe
-            <br />
-            Date de Création : 08/07/17
-            <br />
-            Commentaire principale : Ce planning est voué à être modifié régulièrement par l’équipe.
-            <br />
-            Paramètres utilisés (optionnelle) :
-            Aliqua mollit nulla velit magna velit adipisicing culpa ex dolor cupidatat eu commodo.
-            <br />
-            <br />
-            Description : Fichier excel et pdf du planning
-            <br />
-            Taille : 50ko
-            <br />
-            Utilisateurs : Damien, Bastien, Côme, Phillipe
-            <br />
-            Date de Création : 08/07/17
-            <br />
-            Commentaire principale : Ce planning est voué à être modifié régulièrement par l’équipe.
-            <br />
-            Paramètres utilisés (optionnelle) :
-            Aliqua mollit nulla velit magna velit adipisicing culpa ex dolor cupidatat eu commodo.
-            <br />
-            <br />
-            Description : Fichier excel et pdf du planning
-            <br />
-            Taille : 50ko
-            <br />
-            Utilisateurs : Damien, Bastien, Côme, Phillipe
-            <br />
-            Date de Création : 08/07/17
-            <br />
-            Commentaire principale : Ce planning est voué à être modifié régulièrement par l’équipe.
-            <br />
-            Paramètres utilisés (optionnelle) :
-            Aliqua mollit nulla velit magna velit adipisicing culpa ex dolor cupidatat eu commodo.
-            <br />
+          <div className='wsFileFile__contentpage__preview__sidebar__property'>
+            <div className='wsFileFile__contentpage__preview__sidebar__property__detail'>
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__label'>
+                  Fichier :
+              </div>
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__info'>
+                  Planning Intégration de Tracim
+              </div>
+
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__label'>
+                  Description :
+              </div>
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__info'>
+                  Fichier excel et pdf du planning
+              </div>
+
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__label'>
+                  Taille :
+              </div>
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__info'>
+                  500ko
+              </div>
+
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__label'>
+                  Equipe :
+              </div>
+              <div className='wsFileFile__contentpage__preview__sidebar__property__detail__info'>
+                  Bastien, Côme, Damien, Philippe.
+              </div>
+            </div>
           </div>
         </div>
       </div>
