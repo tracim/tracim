bcaea9f03f3496c828dee4c978d447bdfef0ea7d
Bastien Sevajol <bastien.sevajol@algoo.fr>
2018-01-12 09:52:05 +0100

    Add /assets/css/default_skin.css in main root template

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ const _srcdir = 'tracim/tracim/public/assets/'
 const _tpldir = 'tracim/tracim/templates/'
 
 const listCssFiles = [
-  _srcdir + 'css/default_skin.css',
+  // _srcdir + 'css/default_skin.css',
   // _srcdir + 'css/bootstrap.css', // Côme - 2018/01/11 - removed to allow easier configuration of theme builder
   _srcdir + 'font-awesome-4.2.0/css/font-awesome.css',
   _srcdir + 'select2-4.0.3/css/select2.min.css',
