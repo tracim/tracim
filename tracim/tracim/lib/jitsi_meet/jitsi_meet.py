9060535a9c6fa9584e03c3d54c8484702eb8e36b
Guénaël Muller <guenael.muller@algoo.fr>
2018-01-15 15:24:26 +0100

    POO refactoring

--- tracim/tracim/lib/jitsi_meet/jitsi_meet.py
+++ tracim/tracim/lib/jitsi_meet/jitsi_meet.py
@@ -1,54 +1,90 @@
-import jwt
-import json
 import datetime
-import time
+import typing
 
-JITSI_URL="https://prosody"
-JWT_APP_ID="test"
-JWT_SECRET="secret"
-JWT_ALG='HS256'
-JWT_DURATION=60*1 # duration in second
-JITSI_USE_TOKEN=True
+import jwt
 
-
-
-def _generate_token(room:str)->str:
-    '''
-    Create jwt token according to room name and config
-    :param room: room name
-    :return: jwt encoded token as string
-    '''
-    now = datetime.datetime.utcnow()
-    exp = now+datetime.timedelta(seconds=JWT_DURATION)
-    data = {
-        "iss":JWT_APP_ID, #Issuer
-        "room": room, # Custom-param for jitsi_meet
-        "aud": "*", # TODO: Understood this param
-        "exp": exp, # Expiration date
-        "nbf": now, # NotBefore
-        "iat": now  # IssuedAt
-    }
-    jwt
-    jwttoken= jwt.encode(data,
-                        JWT_SECRET,
-                        algorithm=JWT_ALG)
-    return jwttoken.decode("utf-8")
-
-def _generate_url(room:str)->str:
-    '''
-    Generate url with or without token
-    :param room: room name
-    :return: url as string
-    '''
-    if JITSI_USE_TOKEN:
-        token=_generate_token(room)
-        url="{}/{}?jwt={}".format(JITSI_URL,
-                              room
-                              ,token)
-    else:
-        url="{}/{}".format(JITSI_URL,
-                           room)
-    return url
+JITSI_URL = "https://prosody"
+JWT_APP_ID = "test"
+JWT_SECRET = "secret"
+JWT_ALG = 'HS256'
+JWT_DURATION = 60*1  # duration in second
+JITSI_USE_TOKEN = True
 
 
-print(_generate_url("test"))
+class JitsiTokenConfig:
+
+    def __init__(self,
+                 app_id: str,
+                 secret: str,
+                 alg: str,
+                 duration: int,
+                 )-> None:
+        self.app_id = app_id
+        self.secret = secret
+        self.alg = alg
+        self.duration = duration
+
+
+class JitsiMeetRoomHandler:
+
+    def __init__(self,
+                 base_url: str,
+                 token_config: typing.Optional[JitsiTokenConfig],
+                 ) -> None:
+        self.base_url = base_url
+        self.token_config = token_config
+
+    def generate_token(self, room: str)->str:
+        """
+        Create jwt token according to room name and config
+        :param room: room name
+        :return: jwt encoded token as string
+        """
+        assert self.token_config
+        now = datetime.datetime.utcnow()
+        exp = now+datetime.timedelta(seconds=self.token_config.duration)
+        data = {
+            "iss": self.token_config.app_id,  # Issuer
+            "room": room,  # Custom-param for jitsi_meet
+            "aud": "*",  # TODO: Understood this param
+            "exp": exp,  # Expiration date
+            "nbf": now,  # NotBefore
+            "iat": now,   # IssuedAt
+        }
+        jwt_token = jwt.encode(data,
+                               self.token_config.secret,
+                               algorithm=self.token_config.alg)
+        return jwt_token.decode("utf-8")
+
+    def generate_url(self, room: str)->str:
+        """
+        Generate url with or without token
+        :param room: room name
+        :return: url as string
+        """
+        if self.token_config:
+            token = self.generate_token(room)
+            url = "{}/{}?jwt={}".format(JITSI_URL,
+                                        room,
+                                        token,)
+        else:
+            url = "{}/{}".format(JITSI_URL,
+                                 room,)
+        return url
+
+if JITSI_USE_TOKEN:
+    defaultTokenConfig = JitsiTokenConfig(
+        app_id=JWT_APP_ID,
+        secret=JWT_SECRET,
+        alg=JWT_ALG,
+        duration=JWT_DURATION,
+    )
+else:
+    defaultTokenConfig = None
+
+JitsiMeetRoom = JitsiMeetRoomHandler(
+    base_url=JITSI_URL,
+    token_config= defaultTokenConfig
+)
+
+print(JitsiMeetRoom.generate_url('test'))
