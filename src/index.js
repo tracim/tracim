1ec8a204a8203fd8c400dd507a61e24a669fa63a
Skylsmoi <come.huguies@gmail.com>
2018-02-26 18:30:53 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function bonjour () {
+  console.log('bonjour ?!')
+}
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const webpack = require('webpack')
+const path = require('path')
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tracimLib.js',
+    pathinfo: !isProduction,
+    library: 'tracimLib',
+    libraryTarget: 'var'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist/'),
+    port: 8071,
+    hot: true,
+    noInfo: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    historyApiFallback: true
+    // headers: {
+    //   'Access-Control-Allow-Origin': '*'
+    // }
+  },
+  devtool: isProduction ? false : 'cheap-module-source-map',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      enforce: 'pre',
+      use: 'standard-loader',
+      exclude: [/node_modules/]
+    }, {
+      test: [/\.js$/, /\.jsx$/],
+      loader: 'babel-loader',
+      options: {
+        presets: ['env', 'react'],
+        plugins: ['transform-object-rest-spread', 'transform-class-properties', 'transform-object-assign']
+      },
+      exclude: [/node_modules/]
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, {
+      test: /\.styl$/,
+      use: ['style-loader', 'css-loader', 'stylus-loader']
+    }, {
+      test: /\.(jpg|png|svg)$/,
+      loader: 'url-loader',
+      options: {
+        limit: 25000
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins:[
+    ...[], // generic plugins always present
+    ...(isProduction
+      ? [ // production specific plugins
+        new webpack.DefinePlugin({
+          'process.env': { 'NODE_ENV': JSON.stringify('production') }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          compress: { warnings: false }
+        })
+      ]
+      : [] // development specific plugins
+    )
+  ]
+}
