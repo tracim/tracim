483e5c8f2d07a7e9e52ec5a18a6518974d1c4548
Skylsmoi <come.huguies@gmail.com>
2018-02-28 16:07:05 +0100

    added TextAreaPlugin integration + fixed url of tracim_lib dependency

--- src/component/PageHtml.jsx
+++ src/component/PageHtml.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { TextAreaPlugin } from 'tracim_lib'
 
 const PageHtml = props => {
   return (
     <div className='wsFilePageHtml__contentpage__textnote'>
       <div className='wsFilePageHtml__contentpage__textnote__latestversion' dangerouslySetInnerHTML={{__html: props.version}} />
       <div className='wsFilePageHtml__contentpage__textnote__text' dangerouslySetInnerHTML={{__html: props.text}} />
+
+      <TextAreaPlugin customClass={'wsFilePageHtml'} />
     </div>
   )
 }
