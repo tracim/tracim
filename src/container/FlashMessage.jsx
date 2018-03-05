e5760fd3584c26cd454ec8b219c1ae17f42a808d
AlexiCauvin <cauvin.alexi@gmail.com>
2018-03-05 15:58:31 +0100

    Integration of flash message

---
+++ src/container/FlashMessage.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class FlashMessage extends Component {
+  render() {
+    return(
+      <div className='flashmessage'>
+        <div className='flashmessage__wrapper card'>
+          <div className='flashmessage__header' />
+
+          <div className='card-body nopadding'>
+
+            <div className='flashmessage__close'>
+              <i className='fa fa-times' />
+            </div>
+
+            <div className='flashmessage__content'>
+              <div className='flashmessage__content__icon'>
+                <i className='fa fa-times-circle' />
+              </div>
+
+              <div className='flashmessage__content__text'>
+                <div className='flashmessage__content__text__title'>
+                  Sorry !
+                </div>
+                <div className='flashmessage__content__text__paragraph'>
+                  Reprehenderit reprehenderit veniam dolore velit dolor velit in occaecat dolor veniam nisi officia velit consequat amet cupidatat.
+                  Reprehenderit reprehenderit veniam dolore velit dolor velit in occaecat dolor veniam nisi officia velit consequat amet cupidatat.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FlashMessage
