7ebb9deb3d10dad4a40721c3f32a528b0d2eb826
Skylsmoi <come.huguies@gmail.com>
2018-03-08 17:21:03 +0100

    added translation

---
+++ src/component/Input/SelectStatus/SelectStatus.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { translate } from 'react-i18next'
+import i18n from '../../../i18n.js'
+
+translate.setI18n(i18n)
+
+export const SelectStatus = props =>
+  <div className='wsFileGeneric__option__menu__status dropdown'>
+    <button className='wsFileGeneric__option__menu__status__dropdownbtn check btn dropdown-toggle' type='button' id='dropdownMenu2' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+      {props.t('Input.SelectStatus.validated')}
+      <div className='wsFileGeneric__option__menu__status__dropdownbtn__icon'>
+        <i className='fa fa-check' />
+      </div>
+    </button>
+
+    <div className='wsFileGeneric__option__menu__status__submenu dropdown-menu' aria-labelledby='dropdownMenu2'>
+      <h6 className='dropdown-header'>{props.t('Input.SelectStatus.file_status')}</h6>
+
+      <div className='dropdown-divider' />
+
+      <button className='wsFileGeneric__option__menu__status__submenu__item current  dropdown-item' type='button'>
+        {props.t('Input.SelectStatus.ongoing')}
+        <div className='wsFileGeneric__option__menu__status__submenu__item__icon'>
+          <i className='fa fa-gears' />
+        </div>
+      </button>
+
+      <button className='wsFileGeneric__option__menu__status__submenu__item check dropdown-item' type='button'>
+        {props.t('Input.SelectStatus.validated')}
+        <div className='wsFileGeneric__option__menu__status__submenu__item__icon'>
+          <i className='fa fa-check' />
+        </div>
+      </button>
+
+      <button className='wsFileGeneric__option__menu__status__submenu__item invalid dropdown-item' type='button'>
+        {props.t('Input.SelectStatus.unvalidated')}
+        <div className='wsFileGeneric__option__menu__status__submenu__item__icon'>
+          <i className='fa fa-times' />
+        </div>
+      </button>
+
+      <button className='wsFileGeneric__option__menu__status__submenu__item ban dropdown-item' type='button'>
+        {props.t('Input.SelectStatus.obsolete')}
+        <div className='wsFileGeneric__option__menu__status__submenu__item__icon'>
+          <i className='fa fa-ban' />
+        </div>
+      </button>
+    </div>
+  </div>
+
+export default translate()(SelectStatus)
---
+++ src/translate/en.js
@@ -0,0 +1,18 @@
+const en = {
+  translation: { // 'en' in the namespace 'translation'
+    Input: {
+      SelectStatus: {
+        file_status: 'File status',
+        validated: 'Validated',
+        unvalidated: 'Unvalidated',
+        obsolete: 'Obsolete',
+        ongoing: 'Ongoing'
+      }
+    },
+    PopinFixedOption: {
+      new_version: 'New version'
+    }
+  }
+}
+
+export default en
