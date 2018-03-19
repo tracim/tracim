42e084bf08b8f7541e1a93ebb4ebddb19997d28c
Guénaël Muller <guenael.muller@algoo.fr>
2018-03-19 15:55:08 +0100

    more explicit return

--- tracim/tracim/lib/utils.py
+++ tracim/tracim/lib/utils.py
@@ -187,8 +187,7 @@ def current_date_for_filename() -> str:
     """
     ISO8601 current date, adapted to be used in filename (for
     webdav feature for example), with trouble-free characters.
-
-    :return: current date as string
+    :return: current date as string like "2018-03-19T15.49.27.246592"
     """
     # INFO - G.M - 19-03-2018 - As ':' is in transform_to_bdd method in
     # webdav utils, it may cause trouble. So, it should be replaced to
