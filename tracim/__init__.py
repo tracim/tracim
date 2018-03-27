368d9a9cb87d3689a8cc550a9b5d2e6e4060a1d7
Guénaël Muller <guenael.muller@algoo.fr>
2018-03-27 09:59:37 +0200

    Add Tracim CFG to Request object

--- tracim/__init__.py
+++ tracim/__init__.py
@@ -1,5 +1,7 @@
 from pyramid.config import Configurator
 
+from config import RequestWithCFG
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
@@ -8,5 +10,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
+    config.set_request_factory(RequestWithCFG)
     config.scan()
     return config.make_wsgi_app()
