4bde22aa1cbaa2e5be8fd295abe91189ad4b3c17
Guénaël Muller <guenael.muller@algoo.fr>
2018-04-03 15:11:47 +0200

    fix readme path change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
