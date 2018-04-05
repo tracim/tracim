4fa52aa36690e85d74c6d382cca42529137a4ea2
Guénaël Muller <guenael.muller@algoo.fr>
2018-04-05 14:39:27 +0200

    simple test for command line

---
+++ tracim/tests/commands/commands.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+
+import tracim
+
+
+class TestCommands(object):
+    def test_commands(self):
+        """
+        Test listing of tracimcli command: Tracim commands must be listed
+        :return:
+        """
+        os.chdir(os.path.dirname(tracim.__file__) + '/../')
+
+        output = subprocess.check_output(["tracimcli", "-h"])
+        output = output.decode('utf-8')
+
+        assert output.find('user create') > 0
+        assert output.find('user update') > 0
+        assert output.find('db init') > 0
