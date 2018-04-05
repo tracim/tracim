0af49844489a125501bced2073785595094b5b0c
Skylsmoi <come.huguies@gmail.com>
2018-04-05 11:16:44 +0200

    removed deprecated folder

--- src/app/README.md
+++
@@ -1,4 +0,0 @@
-# Deprecated
-this folder is deprecated due to the new way of handling apps as externals react apps.
-
-It will be deleted soon.
--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import pageHtml from './ContentType/PageHtml/index.js'
-import thread from './ContentType/Thread/index.js'
-
-const appDatabase = [pageHtml, thread]
-
-export default appDatabase
