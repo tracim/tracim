66cc93ffcf01691cbcf0aab510b0caa64e6bfa51
Guénaël Muller <guenael.muller@algoo.fr>
2018-04-24 14:24:23 +0200

    allow to return UserInContext object from user_lib + some cleaning/doc

--- tracim/lib/core/user.py
+++ tracim/lib/core/user.py
@@ -8,6 +8,7 @@ from tracim.models.auth import User
 from sqlalchemy.orm.exc import NoResultFound
 from tracim.exceptions import BadUserPassword
 from tracim.exceptions import AuthenticationFailed
+from tracim.models.context_models import UserInContext
 
 
 class UserApi(object):
@@ -20,17 +21,63 @@ class UserApi(object):
     def _base_query(self):
         return self._session.query(User)
 
-    def get_one(self, user_id: int) -> User:
-        return self._base_query().filter(User.user_id==user_id).one()
+    def _get_correct_user_type(
+            self,
+            user: User,
+            in_context: bool,
+    ) -> typing.Union[User, UserInContext]:
+        """
+        Choose user type object depending on in_context bool.
+        :param user:
+        :param in_context:
+        :return: user as User or UserInContext if in_context is True
+        """
+        if in_context:
+            user = UserInContext(
+                user=user,
+                dbsession=self._session,
+                config=self._config,
+            )
+        return user
 
-    def get_one_by_email(self, email: str) -> User:
-        return self._base_query().filter(User.email==email).one()
+    # Getters
 
-    def get_one_by_id(self, id: int) -> User:
-        return self._base_query().filter(User.user_id==id).one()
+    def get_one(
+            self,
+            user_id: int,
+            in_context: bool=False,
+    ) -> typing.Union[UserInContext, User]:
+        """
+        Get one user by user id
+        :param user_id:
+        :param in_context: Return User or UserInContext Object
+        :return: one user
+        """
+        user = self._base_query().filter(User.user_id == user_id).one()
+        return self._get_correct_user_type(user, in_context)
+
+    def get_one_by_email(
+            self,
+            email: str,
+            in_context: bool=False,
+    ) -> User:
+        """
+        Get one user by email
+        :param email: Email of the user
+        :param in_context: Return User or UserInContext Object
+        :return: one user
+        """
+        user = self._base_query().filter(User.email == email).one()
+        return self._get_correct_user_type(user, in_context)
+
+    # FIXME - G.M - 24-04-2018 - Duplicate method with get_one.
+    def get_one_by_id(self, id: int, in_context=False) -> User:
+        return self.get_one(user_id=id, in_context=in_context)
 
     def get_all(self) -> typing.Iterable[User]:
         return self._session.query(User).order_by(User.display_name).all()
+
+    # Check methods
 
     def user_with_email_exists(self, email: str):
         try:
@@ -56,6 +103,8 @@ class UserApi(object):
                 raise BadUserPassword()
         except (BadUserPassword, NoResultFound):
             raise AuthenticationFailed
+
+    # Actions
 
     def update(
             self,
