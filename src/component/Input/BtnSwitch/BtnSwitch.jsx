b86b8bd8fbcfbfd879af1f5e7d9b96a9ca613ef9
AlexiCauvin <cauvin.alexi@gmail.com>
2018-05-17 15:06:07 +0200

    theming tracim_lib

--- src/component/Input/BtnSwitch/BtnSwitch.jsx
+++ src/component/Input/BtnSwitch/BtnSwitch.jsx
@@ -9,7 +9,7 @@ export const BtnSwitch = props =>
       <span className='slider round' />
     </label>
     <div className='btnswitch__text'>
-      { props.checked ? 'Actif' : 'Inactif'}
+      { props.checked ? 'actif' : 'inactif'}
     </div>
   </div>
 
