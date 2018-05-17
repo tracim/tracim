fe004b945f14500f85826b5ce6640d3ef32cbe55
AlexiCauvin <cauvin.alexi@gmail.com>
2018-05-17 15:05:27 +0200

    fix fix css bugs on app's option bar

--- src/component/OptionComponent/EditContent.jsx
+++ src/component/OptionComponent/EditContent.jsx
@@ -4,11 +4,11 @@ class EditContent extends Component {
   render () {
     return (
       <div className='d-flex align-items-center'>
-        <div className='wsContentGeneric__option__menu__action d-none d-sm-block'>
-          <i className='fa fa-archive' />
+        <div className='wsContentGeneric__option__menu__action optionicon d-none d-sm-block'>
+          <i className='fa fa-fw fa-archive' />
         </div>
-        <div className='wsContentGeneric__option__menu__action d-none d-sm-block'>
-          <i className='fa fa-trash' />
+        <div className='wsContentGeneric__option__menu__action optionicon d-none d-sm-block'>
+          <i className='fa fa-fw fa-trash' />
         </div>
       </div>
     )
--- src/component/OptionComponent/NewVersionBtn.jsx
+++ src/component/OptionComponent/NewVersionBtn.jsx
@@ -4,7 +4,7 @@ class NewVersionBtn extends Component {
   render () {
     return (
       <div
-        className='wsContentGeneric__option__menu__addversion btn btn-outline-primary mr-auto'
+        className='wsContentGeneric__option__menu__addversion newversionbtn btn btn-outline-primary mr-auto'
         onClick={this.props.onClickNewVersionBtn}
       >
         Nouvelle version
