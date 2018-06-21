efe07b89105c40b4490b12542c352f8779a4abdb
Guénaël Muller <guenael.muller@algoo.fr>
2018-06-21 16:44:26 +0200

    fix endpoint descriptions with function.wraps methods in alls decorator

--- tracim/lib/utils/authorization.py
+++ tracim/lib/utils/authorization.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from typing import TYPE_CHECKING
-
+import functools
 from pyramid.interfaces import IAuthorizationPolicy
 from zope.interface import implementer
 try:
@@ -53,6 +53,7 @@ def require_same_user_or_profile(group: int):
     :return:
     """
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(self, context, request: 'TracimRequest'):
             auth_user = request.current_user
             candidate_user = request.candidate_user
@@ -73,6 +74,7 @@ def require_profile(group: int):
     :return:
     """
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(self, context, request: 'TracimRequest'):
             user = request.current_user
             if user.profile.id >= group:
@@ -91,7 +93,7 @@ def require_workspace_role(minimal_required_role: int):
     :return: decorator
     """
     def decorator(func):
-
+        @functools.wraps(func)
         def wrapper(self, context, request: 'TracimRequest'):
             user = request.current_user
             workspace = request.current_workspace
