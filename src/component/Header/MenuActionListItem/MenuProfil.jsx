87a62ad6ab203e10425397cf2e1c8c0f14ffda7e
AlexiCauvin <cauvin.alexi@gmail.com>
2018-07-05 14:40:51 +0200

    update the boutons design in header*

--- src/component/Header/MenuActionListItem/MenuProfil.jsx
+++ src/component/Header/MenuActionListItem/MenuProfil.jsx
@@ -8,7 +8,7 @@ const MenuProfil = props => {
     ? (
       <li className='header__menu__rightside__itemprofil'>
         <div className='profilgroup dropdown'>
-          <button className='profilgroup__name btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          <button className='profilgroup__name btn btn-outline-primary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
             <img className='profilgroup__name__imgprofil' src={props.user.avatar_url} />
             <div className='profilgroup__name__text'>
               {props.user.name}
