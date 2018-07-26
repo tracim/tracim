791d8b24f7385162d0eb19c0119e0e14387410e7
Skylsmoi <come.huguies@gmail.com>
2018-07-26 15:26:06 +0200

    added hours to log

--- bash_library.sh
+++ bash_library.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+YELLOW='\033[1;33m'
 BROWN='\033[0;33m'
 NC='\033[0m' # No Color
 
 function log {
-    echo -e "\n${BROWN}>> $ $1${NC}\n"
+    echo -e "\n${YELLOW}[$(date +'%H:%M:%S')]${BROWN} >> $ $1${NC}\n"
 }
