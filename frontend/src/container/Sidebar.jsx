fa53d0961f6f7478d32b443a263e5ebbff5054e4
AlexiCauvin <cauvin.alexi@gmail.com>
2018-08-07 15:44:03 +0200

    Fix the width and fix the scroll on the responsive mode of the sidebar

--- frontend/src/container/Sidebar.jsx
+++ frontend/src/container/Sidebar.jsx
@@ -106,49 +106,47 @@ class Sidebar extends React.Component {
 
     return (
       <div className={classnames('sidebar primaryColorBgDarken', {'sidebarclose': sidebarClose})}>
-        <div className='sidebarSticky'>
-          <div className='sidebar__expand primaryColorBg' onClick={this.handleClickToggleSidebar}>
-            <i className={classnames('fa fa-chevron-left', {'fa-chevron-right': sidebarClose, 'fa-chevron-left': !sidebarClose})} />
+
+        <div className='sidebar__expand primaryColorBg' onClick={this.handleClickToggleSidebar}>
+          <i className={classnames('fa fa-chevron-left', {'fa-chevron-right': sidebarClose, 'fa-chevron-left': !sidebarClose})} />
+        </div>
+
+        <div className='sidebar__content'>
+          <nav className={classnames('sidebar__content__navigation', {'sidebarclose': sidebarClose})}>
+            <ul className='sidebar__content__navigation__workspace'>
+              { workspaceList.map(ws =>
+                <WorkspaceListItem
+                  idWs={ws.id}
+                  label={ws.label}
+                  allowedApp={ws.sidebarEntry}
+                  lang={activeLang}
+                  activeFilterList={ws.id === workspaceIdInUrl ? [qs.parse(this.props.location.search).type] : []}
+                  isOpenInSidebar={ws.isOpenInSidebar}
+                  onClickTitle={() => this.handleClickWorkspace(ws.id, !ws.isOpenInSidebar)}
+                  onClickAllContent={this.handleClickAllContent}
+                  // onClickContentFilter={this.handleClickContentFilter}
+                  key={ws.id}
+                />
+              )}
+            </ul>
+          </nav>
+
+          <div className='sidebar__content__btnnewworkspace'>
+            <button
+              className='sidebar__content__btnnewworkspace__btn btn btn-primary primaryColorBg primaryColorBorder primaryColorBorderDarkenHover mb-5'
+              onClick={this.handleClickNewWorkspace}
+            >
+              {t('Create a workspace')}
+            </button>
           </div>
 
-          <div className='sidebar__content'>
+        </div>
 
-            <nav className='sidebar__navigation'>
-              <ul className='sidebar__navigation__workspace'>
-                { workspaceList.map(ws =>
-                  <WorkspaceListItem
-                    idWs={ws.id}
-                    label={ws.label}
-                    allowedApp={ws.sidebarEntry}
-                    lang={activeLang}
-                    activeFilterList={ws.id === workspaceIdInUrl ? [qs.parse(this.props.location.search).type] : []}
-                    isOpenInSidebar={ws.isOpenInSidebar}
-                    onClickTitle={() => this.handleClickWorkspace(ws.id, !ws.isOpenInSidebar)}
-                    onClickAllContent={this.handleClickAllContent}
-                    // onClickContentFilter={this.handleClickContentFilter}
-                    key={ws.id}
-                  />
-                )}
-              </ul>
-            </nav>
-
-            <div className='sidebar__btnnewworkspace'>
-              <button
-                className='sidebar__btnnewworkspace__btn btn btn-primary primaryColorBg primaryColorBorder primaryColorBorderDarkenHover mb-5'
-                onClick={this.handleClickNewWorkspace}
-              >
-                {t('Create a workspace')}
-              </button>
-            </div>
-
-          </div>
-
-          <div className='sidebar__footer mb-2'>
-            <div className='sidebar__footer__text whiteFontColor d-flex align-items-end justify-content-center'>
-              Copyright - 2013 - 2018
-              <div className='sidebar__footer__text__link'>
-                <a href='http://www.tracim.fr/' target='_blank' className='ml-3'>tracim.fr</a>
-              </div>
+        <div className='sidebar__footer mb-2'>
+          <div className='sidebar__footer__text whiteFontColor d-flex align-items-end justify-content-center'>
+            Copyright - 2013 - 2018
+            <div className='sidebar__footer__text__link'>
+              <a href='http://www.tracim.fr/' target='_blank' className='ml-3'>tracim.fr</a>
             </div>
           </div>
         </div>
