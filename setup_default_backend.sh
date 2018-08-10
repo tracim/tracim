33d3937476dc016b34f4340ed903f84aa7985a4e
Guénaël Muller <guenael.muller@algoo.fr>
2018-08-10 15:57:10 +0200

    update backend setup script

--- setup_default_backend.sh
+++ setup_default_backend.sh
@@ -1,32 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 . bash_library.sh # source bash_library.sh
+. backend_lib.sh # source backend_lib.sh
 
-log "install base debian-packaged-dep for backend..."
-sudo apt update
-sudo apt install python3 python3-venv python3-dev python3-pip
-sudo apt install redis-server
-
-log "install deps for dealing with most preview..."
-sudo apt install zlib1g-dev libjpeg-dev
-sudo apt install imagemagick libmagickwand-dev ghostscript
-sudo apt install libreoffice # most office documents file and text format
-sudo apt install inkscape # for .svg files.
+install_backend_system_deb
 
 log "go to backend subdir.."
 cd backend || exit 1;
 
-log "setup python3 env.."
-python3 -m venv env
-source env/bin/activate
-pip install --upgrade pip setuptools
 
-log "install tracim-backend (sqlite_backend)..."
-pip install -e ".[testing]"
-
-
-log "configure tracim with default conf..."
-cp development.ini.sample development.ini
-cp wsgidav.conf.sample wsgidav.conf
-tracimcli db init
+install_backend_system_dep
+setup_pyenv
+install_backend_python_packages
+setup_config_file
+setup_db
 
 log "backend of tracim was correctly set-up."
