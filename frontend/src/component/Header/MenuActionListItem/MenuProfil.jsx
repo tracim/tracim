6b5207af8804f33a52dec99197e1430ec76942ac
Skylsmoi <come.huguies@gmail.com>
2018-08-10 12:25:02 +0200

    [https://github.com/tracim/tracim/issues/787] added link to admin pages in top right user dropdown

--- frontend/src/component/Header/MenuActionListItem/MenuProfil.jsx
+++ frontend/src/component/Header/MenuActionListItem/MenuProfil.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { PAGE } from '../../../helper.js'
+import { PAGE, PROFILE } from '../../../helper.js'
 import { translate } from 'react-i18next'
 
 const MenuProfil = props => {
@@ -11,15 +11,32 @@ const MenuProfil = props => {
         <div className='profilgroup dropdown'>
           <button className='profilgroup__name btn btn-outline-primary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
             <img className='profilgroup__name__imgprofil' src={props.user.avatar_url} />
+
             <div className='profilgroup__name__text'>
-              {props.user.name}
+              {props.user.public_name}
             </div>
           </button>
+
           <div className='profilgroup__setting dropdown-menu' aria-labelledby='dropdownMenuButton'>
+            {props.user.profile === PROFILE.ADMINISTRATOR &&
+              <Link className='setting__link dropdown-item' to={PAGE.ADMIN.WORKSPACE}>
+                <i className='fa fa-fw fa-space-shuttle mr-2' />
+                {props.t('Admin workspace')}
+              </Link>
+            }
+
+            {props.user.profile === PROFILE.ADMINISTRATOR &&
+              <Link className='setting__link dropdown-item' to={PAGE.ADMIN.USER}>
+                <i className='fa fa-fw fa-users mr-2' />
+                {props.t('Admin user')}
+              </Link>
+            }
+
             <Link className='setting__link dropdown-item' to={PAGE.ACCOUNT}>
               <i className='fa fa-fw fa-user-o mr-2' />
               {props.t('My Account')}
             </Link>
+
             {/* <div className='setting__link dropdown-item'>Mot de passe</div> */}
             <div className='setting__link dropdown-item' onClick={props.onClickLogout}>
               <i className='fa fa-fw fa-sign-out mr-2' />
--- frontend/src/container/AppFullscreenRouter.jsx
+++ frontend/src/container/AppFullscreenRouter.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { Route } from 'react-router-dom'
-import { PAGE } from '../helper.js'
+import { Route, Redirect } from 'react-router-dom'
+import { PAGE, PROFILE } from '../helper.js'
 import appFactory from '../appFactory.js'
 
-class AppFullscreenManager extends React.Component {
+class AppFullscreenRouter extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -23,13 +23,17 @@ class AppFullscreenManager extends React.Component {
         <div id='appFullscreenContainer' />
 
         {this.state.isMounted && (// we must wait for the component to be fully mounted to be sure the div#appFullscreenContainer exists in DOM
-          <div className='emptyDiForRoute'>
+          <div className='emptyDivForRoute'>
             <Route path={PAGE.ADMIN.WORKSPACE} render={() => {
+              if (props.user.profile !== PROFILE.ADMINISTRATOR) return <Redirect to={{pathname: '/'}} />
+
               props.renderAppFullscreen({slug: 'admin_workspace_user', hexcolor: '#7d4e24', type: 'workspace'}, props.user, {})
               return null
             }} />
 
             <Route path={PAGE.ADMIN.USER} render={() => {
+              if (props.user.profile !== PROFILE.ADMINISTRATOR) return <Redirect to={{pathname: '/'}} />
+
               props.renderAppFullscreen({slug: 'admin_workspace_user', hexcolor: '#7d4e24', type: 'user'}, props.user, {})
               return null
             }} />
@@ -41,4 +45,4 @@ class AppFullscreenManager extends React.Component {
 }
 
 const mapStateToProps = ({ user }) => ({ user })
-export default connect(mapStateToProps)(withRouter(appFactory(AppFullscreenManager)))
+export default withRouter(connect(mapStateToProps)(appFactory(AppFullscreenRouter)))
