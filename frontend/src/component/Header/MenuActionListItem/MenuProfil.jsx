ae249693019a382e9db89ea3ae25dbee3ea21e59
Skylsmoi <come.huguies@gmail.com>
2018-08-10 16:03:30 +0200

    [https://github.com/tracim/tracim/issues/792] added remember me feature

--- frontend/src/component/Header/MenuActionListItem/MenuProfil.jsx
+++ frontend/src/component/Header/MenuActionListItem/MenuProfil.jsx
@@ -8,31 +8,31 @@ const MenuProfil = props => {
   if (!props.user.logged) return null
 
   return (
-      <li className='header__menu__rightside__itemprofil'>
-        <div className='profilgroup dropdown'>
-          <button className='profilgroup__name btn btn-outline-primary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-            <img className='profilgroup__name__imgprofil' src={props.user.avatar_url} />
+    <li className='header__menu__rightside__itemprofil'>
+      <div className='profilgroup dropdown'>
+        <button className='profilgroup__name btn btn-outline-primary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+          <img className='profilgroup__name__imgprofil' src={props.user.avatar_url} />
 
-            <div className='profilgroup__name__text'>
-              {props.user.public_name}
-            </div>
-          </button>
+          <div className='profilgroup__name__text'>
+            {props.user.public_name}
+          </div>
+        </button>
 
-          <div className='profilgroup__setting dropdown-menu' aria-labelledby='dropdownMenuButton'>
-            <Link className='setting__link dropdown-item' to={PAGE.ACCOUNT}>
-              <i className='fa fa-fw fa-user-o mr-2' />
-              {props.t('My Account')}
-            </Link>
+        <div className='profilgroup__setting dropdown-menu' aria-labelledby='dropdownMenuButton'>
+          <Link className='setting__link dropdown-item' to={PAGE.ACCOUNT}>
+            <i className='fa fa-fw fa-user-o mr-2' />
+            {props.t('My Account')}
+          </Link>
 
-            {/* <div className='setting__link dropdown-item'>Mot de passe</div> */}
-            <div className='setting__link dropdown-item' onClick={props.onClickLogout}>
-              <i className='fa fa-fw fa-sign-out mr-2' />
-              {props.t('Logout')}
-            </div>
+          {/* <div className='setting__link dropdown-item'>Mot de passe</div> */}
+          <div className='setting__link dropdown-item' onClick={props.onClickLogout}>
+            <i className='fa fa-fw fa-sign-out mr-2' />
+            {props.t('Logout')}
           </div>
         </div>
-      </li>
-    )
+      </div>
+    </li>
+  )
 }
 export default translate()(MenuProfil)
 
