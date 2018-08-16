3da3c3cd65567c26a886a18671e333beec65baf4
philippe <philippe.accorsi@algoo.fr>
2018-08-16 11:06:10 +0200

    add check if nodejs installed, if not, install nodejs

--- install_frontend_dependencies.sh
+++ install_frontend_dependencies.sh
@@ -3,6 +3,23 @@
 # shellcheck disable=SC1091
 . bash_library.sh # source bash_library.sh
 
+# install nodjs if not installed
+
+(
+  log "verify if nodjs is installed"
+  dpkg -l | grep '^ii' | grep 'noidejs\s'
+
+  if [ $? -eq 0 ]; then
+    log "nodjs is installed"
+  else
+    log "install nodejs"
+    sudo apt install curl
+    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    sudo apt install -y nodejs
+  fi
+)
+
+
 # install Tracim Lib
 
 (
