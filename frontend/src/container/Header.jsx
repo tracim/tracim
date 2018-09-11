d5662db1b97923f3a053a9ee370d22b725f4871f
AlexiCauvin <cauvin.alexi@gmail.com>
2018-09-11 12:11:09 +0200

    https://github.com/tracim/tracim/issues/681, enable the breadcrumb in header.jsx file

--- frontend/src/container/Header.jsx
+++ frontend/src/container/Header.jsx
@@ -71,11 +71,9 @@ class Header extends React.Component {
         <nav className='navbar navbar-expand-md navbar-light bg-light'>
           <Logo logoSrc={logoHeader} onClickImg={this.handleClickLogo} />
 
-          { /*
-            <div className='header__breadcrumb d-none d-lg-block ml-4'>
-              Dev Tracim - liste des contenus
-            </div>
-          */ }
+          <div className='header__breadcrumb d-none d-lg-block ml-4'>
+            Dev Tracim - liste des contenus
+          </div>
 
           <NavbarToggler />
 
