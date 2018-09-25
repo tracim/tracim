07921b77dff3c1464f17f8ac32593d918d3cd84e
Guénaël Muller <guenael.muller@algoo.fr>
2018-09-25 15:27:57 +0200

    fix wrong error_code in reset_password + add test for expired token

--- backend/tracim_backend/tests/functional/test_reset_password.py
+++ backend/tracim_backend/tests/functional/test_reset_password.py
@@ -1,5 +1,6 @@
 import requests
 import transaction
+from freezegun import freeze_time
 from tracim_backend import models
 from tracim_backend.error_code import *
 from tracim_backend.models import get_tm_session
@@ -52,7 +53,6 @@ class TestResetPasswordRequestEndpointMailSync(FunctionalTest):
 
 
 class TestResetPasswordCheckTokenEndpoint(FunctionalTest):
-
     def test_api__reset_password_check_token__ok_204__nominal_case(self):
         dbsession = get_tm_session(self.session_factory, transaction.manager)
         admin = dbsession.query(models.User) \
@@ -98,11 +98,10 @@ class TestResetPasswordCheckTokenEndpoint(FunctionalTest):
         )
         assert isinstance(res.json, dict)
         assert 'code' in res.json.keys()
-        assert res.json_body['code'] == ERROR_CODE_EXPIRED_RESET_PASSWORD_TOKEN
+        assert res.json_body['code'] == ERROR_CODE_INVALID_RESET_PASSWORD_TOKEN
 
 
 class TestResetPasswordModifyEndpoint(FunctionalTest):
-
     def test_api__reset_password_reset__ok_204__nominal_case(self):
         dbsession = get_tm_session(self.session_factory, transaction.manager)
         admin = dbsession.query(models.User) \
@@ -151,7 +150,39 @@ class TestResetPasswordModifyEndpoint(FunctionalTest):
         )
         assert isinstance(res.json, dict)
         assert 'code' in res.json.keys()
-        assert res.json_body['code'] == ERROR_CODE_EXPIRED_RESET_PASSWORD_TOKEN
+        assert res.json_body['code'] == ERROR_CODE_INVALID_RESET_PASSWORD_TOKEN
+
+    def test_api__reset_password_reset__err_400__expired_token(self):
+        dbsession = get_tm_session(self.session_factory, transaction.manager)
+        admin = dbsession.query(models.User) \
+            .filter(models.User.email == 'admin@admin.admin') \
+            .one()
+        uapi = UserApi(
+            current_user=admin,
+            session=dbsession,
+            config=self.app_config,
+        )
+        with freeze_time("1999-12-31 23:59:59"):
+            reset_password_token = uapi.reset_password_notification(
+                admin,
+                do_save=True
+            )
+            params = {
+                'email': 'admin@admin.admin',
+                'reset_password_token': reset_password_token,
+                'new_password': 'mynewpassword',
+                'new_password2': 'mynewpassword',
+            }
+            transaction.commit()
+        with freeze_time("2000-01-01 00:00:05"):
+            res = self.testapp.post_json(
+                '/api/v2/reset_password/modify',
+                status=401,
+                params=params,
+            )
+            assert isinstance(res.json, dict)
+            assert 'code' in res.json.keys()
+            assert res.json_body['code'] == ERROR_CODE_EXPIRED_RESET_PASSWORD_TOKEN  # nopep8
 
     def test_api__reset_password_reset__err_400__password_does_not_match(self):
         dbsession = get_tm_session(self.session_factory, transaction.manager)
