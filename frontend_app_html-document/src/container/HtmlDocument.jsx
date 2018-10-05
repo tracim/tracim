bec7f0261fcf09a0c49d34f3433bde0f2c3bc0e3
Skylsmoi <come.huguies@gmail.com>
2018-10-05 11:52:49 +0200

    [https://github.com/tracim/tracim/issues/984] if user is reader, app htmldoc wont open in edit mode if only one version

--- frontend_app_html-document/src/container/HtmlDocument.jsx
+++ frontend_app_html-document/src/container/HtmlDocument.jsx
@@ -168,7 +168,7 @@ class HtmlDocument extends React.Component {
 
         this.setState({
           timeline: revisionWithComment,
-          mode: resRevision.body.length === 1 ? MODE.EDIT : MODE.VIEW // first time editing the doc, open in edit mode
+          mode: resRevision.body.length === 1 && loggedUser.idRoleUserWorkspace >= 2 ? MODE.EDIT : MODE.VIEW // first time editing the doc, open in edit mode
         })
       })
       .catch(e => {
