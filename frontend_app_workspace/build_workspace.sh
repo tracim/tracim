5b090228a3b436b15342fe01bed9bc6aea3fbd65
philippe.accorsi@algoo.fr <philippe.accorsi@algoo.fr>
2018-10-18 16:26:56 +0200

    fix script

--- frontend_app_workspace/build_workspace.sh
+++ frontend_app_workspace/build_workspace.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-. ../bash_library.sh # source bash_library.sh
+# Main in bottom
+
+YELLOW='\033[1;33m'
+BROWN='\033[0;33m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+function log {
+    echo -e "\n${YELLOW}[$(date +'%H:%M:%S')]${BROWN} $ $1${NC}"
+}
+
+function loggood {
+    echo -e "\n${YELLOW}[$(date +'%H:%M:%S')]${GREEN} $ $1${NC}"
+}
+
+function logerror {
+    echo -e "\n${RED}[$(date +'%H:%M:%S')]${RED} $ $1${NC}"
+}
 
 windoz=""
 if [[ $1 = "-w" ]]; then
