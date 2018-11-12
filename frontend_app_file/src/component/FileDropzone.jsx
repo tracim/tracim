904eebc43e2d3139fe84113665021b09f2a17234
Skylsmoi <come.huguies@gmail.com>
2018-11-12 11:01:59 +0100

    app File, code clean

--- frontend_app_file/src/component/FileDropzone.jsx
+++ frontend_app_file/src/component/FileDropzone.jsx
@@ -15,43 +15,40 @@ export const FileDropzone = props => {
       <div className='filecontent__form mb-4' drop='true'>
         {(() => {
           switch (props.preview) {
-            case null:
-              return (
-                <div>
-                  <div
-                    className='filecontent__form__icon d-flex justify-content-center'
-                    style={{color: props.hexcolor}}
-                  >
-                    <i className='fa fa-download' />
-                  </div>
+            case null: return (
+              <div>
+                <div
+                  className='filecontent__form__icon d-flex justify-content-center'
+                  style={{color: props.hexcolor}}
+                >
+                  <i className='fa fa-download' />
+                </div>
 
-                  <div
-                    className='filecontent__form__instruction text-center'
-                    style={{color: props.hexcolor}}
-                  >
-                    {props.t('Drag and drop your file here')}
-                  </div>
+                <div
+                  className='filecontent__form__instruction text-center'
+                  style={{color: props.hexcolor}}
+                >
+                  {props.t('Drag and drop your file here')}
+                </div>
 
-                  <div className='filecontent__form__text text-center'>
-                    {props.t('You can also import your file by clicking here')}
-                  </div>
+                <div className='filecontent__form__text text-center'>
+                  {props.t('You can also import your file by clicking here')}
                 </div>
-              )
-            case false:
-              return (
-                <div className='filecontent__preview' drop='true'>
-                  <i className='filecontent__preview__nopreview-icon fa fa-paperclip' style={{color: props.hexcolor}} />
-                  <div className='filecontent__preview__nopreview-msg'>
-                    {props.filename}
-                  </div>
+              </div>
+            )
+            case false: return (
+              <div className='filecontent__preview' drop='true'>
+                <i className='filecontent__preview__nopreview-icon fa fa-paperclip' style={{color: props.hexcolor}} />
+                <div className='filecontent__preview__nopreview-msg'>
+                  {props.filename}
                 </div>
-              )
-            default:
-              return (
-                <div className='filecontent__preview' drop='true'>
-                  <img src={props.preview} />
-                </div>
-              )
+              </div>
+            )
+            default: return (
+              <div className='filecontent__preview' drop='true'>
+                <img src={props.preview} />
+              </div>
+            )
           }
         })()}
       </div>
