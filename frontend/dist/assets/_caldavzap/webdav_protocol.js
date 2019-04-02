e4fc936b2de9eabfa518729e9127cc32641b7419
Skylsmoi <come.huguies@gmail.com>
2019-04-02 17:05:28 +0200

    [#1539] fixed caldavzap timezone selection and default value

--- frontend/dist/assets/_caldavzap/webdav_protocol.js
+++ frontend/dist/assets/_caldavzap/webdav_protocol.js
@@ -1897,6 +1897,9 @@ function netLoadResource(inputResource, inputHref, hrefMode, inputResourceIndex,
 }// Save the client settings (stored as DAV property on server)
 function netSaveSettings(inputResource, inputSettings, isFormSave, collectionLoad)
 {
+  // @INFO - CH - 2019-04-02 - removed this because we don't understand how it works and it cause some side effects
+  // Side effect example: it saves the timezone and canno't be changed afterwards
+  return true
 	var tmp = algoo_replace_regex(inputResource.href, globalAccountSettings);
 	var baseHref=tmp[1]+tmp[2];
 	var uidBase=tmp[1]+inputResource.userAuth.userName+'@'+tmp[2];
