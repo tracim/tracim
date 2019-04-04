ebf7e2bba3baead8109255be2075fef7a5f8415b
Skylsmoi <come.huguies@gmail.com>
2019-04-04 16:24:15 +0200

    [#1067] added handling of error code 2041 when switching apps status

--- frontend_app_thread/src/container/Thread.jsx
+++ frontend_app_thread/src/container/Thread.jsx
@@ -263,18 +263,18 @@ class Thread extends React.Component {
   handleToggleWysiwyg = () => this.setState(prev => ({timelineWysiwyg: !prev.timelineWysiwyg}))
 
   handleChangeStatus = async newStatus => {
-    const { config, content } = this.state
+    const { state, props } = this
 
-    const fetchResultSaveEditStatus = putThreadStatus(config.apiUrl, content.workspace_id, content.content_id, newStatus)
+    if (newStatus === state.content.status) return
 
-    handleFetchResult(await fetchResultSaveEditStatus)
-      .then(resSave => {
-        if (resSave.status === 204) { // 204 no content so dont take status from resSave.apiResponse.status
-          this.loadContent()
-        } else {
-          console.warn('Error saving thread comment. Result:', resSave, 'content:', content, 'config:', config)
-        }
-      })
+    const fetchResultSaveEditStatus = await handleFetchResult(
+      await putThreadStatus(state.config.apiUrl, state.content.workspace_id, state.content.content_id, newStatus)
+    )
+
+    switch (fetchResultSaveEditStatus.status) {
+      case 204: this.loadContent(); break
+      default: this.sendGlobalFlashMessage(props.t('Error while changing status'))
+    }
   }
 
   handleClickArchive = async () => {
