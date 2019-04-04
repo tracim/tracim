240cf366469f3d2a145b0f6ed941e46c1d0e59fe
Skylsmoi <come.huguies@gmail.com>
2019-04-04 12:10:13 +0200

    removed typo in comment

--- frontend/src/container/Dashboard.jsx
+++ frontend/src/container/Dashboard.jsx
@@ -434,7 +434,7 @@ class Dashboard extends React.Component {
                           hexcolor={app.hexcolor}
                           label={app.label}
                           faIcon={app.faIcon}
-                          // TODO - Côme - 2018/09/12 - translation key bellow is a little hacky:
+                          // TODO - Côme - 2018/09/12 - translation key below is a little hacky:
                           // The creation label comes from api but since there is no translation in backend
                           // every files has a 'externalTradList' array just to generate the translation key in the json files through i18n.scanner
                           creationLabel={props.t(app.creationLabel)}
