790888d86c09ab7378f39c8dbcdda8ac5125ec51
philippe.accorsi@algoo.fr <philippe.accorsi@algoo.fr>
2019-04-10 15:05:54 +0200

    [#1565] modify doc and script about necessary package

--- setup_default_backend.sh
+++ setup_default_backend.sh
@@ -25,52 +25,48 @@ function logerror {
 function install_backend_system_dep {
     log "install base debian-packaged-dep for backend..."
     $SUDO apt update
-    $SUDO apt install -y python3 python3-venv python3-dev python3-pip && loggood "success" || logerror "some error"
-    $SUDO apt install -y redis-server && loggood "success" || logerror "some error"
-
-    log "install deps for dealing with most preview..."
-    $SUDO apt install -y zlib1g-dev libjpeg-dev && loggood "success" || logerror "some error"
-    $SUDO apt install -y imagemagick libmagickwand-dev libpq-dev ghostscript libfile-mimeinfo-perl poppler-utils libimage-exiftool-perl qpdf && loggood "success" || logerror "some error"
-    $SUDO apt install -y libldap2-dev libsasl2-dev && loggood "success" || logerror "some error" # for ldap support
-    $SUDO apt install -y libreoffice && loggood "success" || logerror "some error" # most office documents file and text format
-    $SUDO apt install -y inkscape && loggood "success" || logerror "some error" # for .svg files.
+    PACKAGE_LIST='python3 python3-venv python3-dev python3-pip redis-server zlib1g-dev libjpeg-dev imagemagick libmagickwand-dev libpq-dev ghostscript libfile-mimeinfo-perl poppler-utils libimage-exiftool-perl qpdf libldap2-dev libsasl2-dev libreoffice inkscape'
+    for PACKAGE in $PACKAGE_LIST
+    do
+        $SUDO apt install -y $PACKAGE && loggood "$PACKAGE correctly installed" || logerror "failed to install $PACKAGE"
+    done
 }
 
 function setup_pyenv {
    log "setup python3 env.."
-   python3 -m venv env && loggood "success" || logerror "some error"
-   source env/bin/activate && loggood "success" || logerror "some error"
+   python3 -m venv env && loggood "setup python3 env successfull" || logerror "problem to setup python3 env"
+   source env/bin/activate && loggood "python3 env activated" || logerror "problem to activate python3 env"
 }
 
 function install_backend_python_packages {
     log "install pip and setuptools"
-    pip install --upgrade pip setuptools && loggood "success" || logerror "some error"
+    pip install --upgrade pip setuptools && loggood "install success" || logerror "install error"
     log "install dependencies from requirements.txt"
-    pip install -r "requirements.txt" && loggood "success" || logerror "some error"
+    pip install -r "requirements.txt" && loggood "install success" || logerror "install error"
     log "install tracim-backend (sqlite_backend)..."
-    pip install -e ".[testing]" && loggood "success" || logerror "some error"
+    pip install -e ".[testing]" && loggood "install success" || logerror "install error"
 }
 
 function setup_config_file {
     log "configure tracim with default conf..."
     if [ ! -f development.ini ]; then
        log "generate missing development.ini ..."
-       cp development.ini.sample development.ini && loggood "success" || logerror "some error"
+       cp development.ini.sample development.ini && loggood "copy file success" || logerror "copy file error"
     fi
 
     if [ ! -f ../color.json ]; then
        log "generate missing color.json ..."
-       cp ../color.json.sample ../color.json && loggood "success" || logerror "some error"
+       cp ../color.json.sample ../color.json && loggood "copy file success" || logerror "copy file error"
     fi
 
     if [ -d "$DEFAULTDIR/backend/sessions_data/" ]; then
         log "remove folder \"sessions_data\" in \"$DEFAULTDIR/backend\" if exist"
-        rm -R $DEFAULTDIR/backend/sessions_data/ && loggood "success" || logerror "some error"
+        rm -R $DEFAULTDIR/backend/sessions_data/ && loggood "remove success" || logerror "remove error"
     fi
 
     if [ -d "$DEFAULTDIR/backend/sessions_lock/" ]; then
         log "remove folder \"sessions_lock\" in \"$DEFAULTDIR/backend\" if exist"
-        rm -R $DEFAULTDIR/backend/sessions_lock/ && loggood "success" || logerror "some error"
+        rm -R $DEFAULTDIR/backend/sessions_lock/ && loggood "remove success" || logerror "remove error"
     fi
 }
 
@@ -79,11 +75,11 @@ function setup_db {
     result=$(alembic -c development.ini current)
     if [ $? -eq 0 ] && [ ! "$result" == '' ]; then
        log "check database migration..."
-       alembic -c development.ini upgrade head && loggood "success" || logerror "some error"
+       alembic -c development.ini upgrade head && loggood "alembic upgrade success" || logerror "alembic upgrade error"
     else
        log "database seems missing, init it..."
-       tracimcli db init && loggood "success" || logerror "some error"
-       alembic -c development.ini stamp head && loggood "success" || logerror "some error"
+       tracimcli db init && loggood "db init success" || logerror "db init error"
+       alembic -c development.ini stamp head && loggood "alembic stamp success" || logerror "alembic stamp error"
     fi
 }
 
@@ -95,10 +91,10 @@ function install_npm_and_nodejs {
     else
         logerror "npm not installed"
         log "install npm with nodejs"
-        $SUDO apt install -y curl && loggood "success" || logerror "some error"
+        $SUDO apt install -y curl && loggood "install curl success" || logerror "error to install curl"
         curl -sL https://deb.nodesource.com/setup_8.x | $SUDOCURL bash -
         $SUDO apt update
-        $SUDO apt install -y nodejs && loggood "success" || logerror "some error"
+        $SUDO apt install -y nodejs && loggood "install nodejs success" || logerror "error to install nodejs"
         log "verify if nodejs 8.x is now installed"
         dpkg -l | grep '^ii' | grep 'nodejs\s' | grep '\s8.'
         if [ $? -eq 0 ]; then
@@ -119,7 +115,7 @@ function install_npm_and_nodejs {
 
 function translate_email {
     log "translate email"
-    $SUDO npm install i18next-conv -g && loggood "success" || logerror "some error"
+    $SUDO npm install i18next-conv -g && loggood "install i18next-conv success" || logerror "error to install i18next-conv"
     ./update_i18n_json_file.sh
 }
 
@@ -136,7 +132,7 @@ fi
 
 DEFAULTDIR=$(pwd)
 export DEFAULTDIR
-echo "This is DEFAULTDIR of tracim => \"$DEFAULTDIR\""
+echo "DEFAULTDIR of tracim => \"$DEFAULTDIR\""
 
 install_npm_and_nodejs
 log "go to backend subdir.."
@@ -147,8 +143,6 @@ install_backend_python_packages
 setup_config_file
 setup_db
 translate_email
-loggood "backend of tracim was correctly set-up."
-
 
 # Return to "$DEFAULTDIR/"
 log "cd $DEFAULTDIR"
