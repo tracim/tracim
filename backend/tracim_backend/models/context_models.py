49b9364f91e05efde9af019c3b4f0c51e61b6dc1
Guénaël Muller <guenael.muller@algoo.fr>
2019-04-25 15:36:52 +0200

    add webdav base_url to config endpoint

--- backend/tracim_backend/models/context_models.py
+++ backend/tracim_backend/models/context_models.py
@@ -47,10 +47,12 @@ class ConfigModel(object):
         email_notification_activated: bool,
         new_user_invitation_do_notify: bool,
         webdav_enabled: bool,
+        webdav_url: str
     ) -> None:
         self.email_notification_activated = email_notification_activated
         self.new_user_invitation_do_notify = new_user_invitation_do_notify
         self.webdav_enabled = webdav_enabled
+        self.webdav_url = webdav_url
 
 
 class ErrorCodeModel(object):
