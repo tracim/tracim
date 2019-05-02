64000018e730c77b625196cf9d8aba9105cd25b9
Guénaël Muller <guenael.muller@algoo.fr>
2019-05-02 11:52:07 +0200

    Simplify copy method code

--- backend/tracim_backend/lib/core/content.py
+++ backend/tracim_backend/lib/core/content.py
@@ -1472,35 +1472,6 @@ class ContentApi(object):
         :param do_notify: notify copy or not
         :return: Newly copied item
         """
-        new_content = self._copy_current(
-            item=item,
-            new_label=new_label,
-            new_file_extension=new_file_extension,
-            new_parent=new_parent,
-            new_workspace=new_workspace,
-            do_notify=do_notify,
-            do_save=do_save,
-        )
-
-    def _copy_current(
-        self,
-        item: Content,
-        new_parent: Content = None,
-        new_label: str = None,
-        new_workspace: Workspace = None,
-        new_file_extension: str = None,
-        do_save: bool = True,
-        do_notify: bool = True,
-    ) -> Content:
-        """
-        Copy nearly all content, revision included. Children not included, see
-        "copy_children" for this.
-        :param item: Item to copy
-        :param new_parent: new parent of the new copied item
-        :param new_label: new label of the new copied item
-        :param do_notify: notify copy or not
-        :return: Newly copied item
-        """
         if (not new_parent and not new_label and not new_file_extension and not new_workspace) or (
             new_parent == item.parent
             and new_label == item.label
