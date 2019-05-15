65b82ab26f3f21f97ea2d336fd69079c682b2a35
Skylsmoi <come.huguies@gmail.com>
2019-05-15 10:18:28 +0200

    [wip] fixed app folder + wip cypress test for app folder

--- functionnal_tests/cypress/integration/app_folder/navigation_create_folder.js
+++ functionnal_tests/cypress/integration/app_folder/navigation_create_folder.js
@@ -1,36 +1,16 @@
 // Not connected
-// describe('navigate :: workspace > create_new > folder', function () {
-//    before(function () {
-//    //login
-//        cy.visit('/login')
-//        cy.get('input[type=email]').should('be.visible')
-//        cy.get('input[type=email]').type('admin@admin.admin')
-//        cy.get('input[type=password]').type('admin@admin.admin')
-//        cy.get('.connection__form__btnsubmit').click()
-//    })
-//    after(function() {
-//        cy.get('#dropdownMenuButton').click()
-//        cy.get('div.setting__link').click()
-//        cy.url().should('include', '/login')
-//    })
-//    it ('header button', function () {
-//        cy.get('#dropdownCreateBtn.workspace__header__btnaddcontent__label').click()
-//        cy.get('.show .subdropdown__link__folder__icon').click()
-//        cy.get('.createcontent__contentname').should('be.visible')
-//        cy.get('.createcontent__form__input').should('have.attr', 'placeholder')
-//        cy.get('.createcontent__form__input').type('folder1')
-//        cy.get('.createcontent__form__input').should('have.attr', 'value', 'folder1')
-//        cy.get('.cardPopup__close').click()
-//        cy.get('.createcontent__contentname').should('not.be.visible')
-//    })
-//    it ('content button', function () {
-//        cy.get('.workspace__content__button.dropdownCreateBtn .btn-primary').click()
-//        cy.get('.show .subdropdown__link__folder__icon').click()
-//        cy.get('.createcontent__contentname').should('be.visible')
-//        cy.get('.createcontent__form__input').should('have.attr', 'placeholder')
-//        cy.get('.createcontent__form__input').type('folder2')
-//        cy.get('.createcontent__form__input').should('have.attr', 'value', 'folder2')
-//        cy.get('.cardPopup__close').click()
-//        cy.get('.createcontent__contentname').should('not.be.visible')
-//    })
-// })
+describe('App Folder Advanced', function () {
+  beforeEach(function () {
+    cy.resetDB()
+    cy.setupBaseDB()
+    cy.loginAs('administrators')
+    cy
+      .fixture('baseWorkspace').as('workspace').then(workspace => {
+      cy.visit(`/ui/workspaces/${workspace.workspace_id}/contents`)
+    })
+  })
+
+   it ('should open when editing a folder', function () {
+
+   })
+})
