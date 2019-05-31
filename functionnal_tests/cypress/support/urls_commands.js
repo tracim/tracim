0e9388024532acc8d66b10cd20749b19d00551d3
basile <basilelegal@gmail.com>
2019-05-31 09:44:35 -0400

    front - reworks urls commands

--- functionnal_tests/cypress/support/urls_commands.js
+++ functionnal_tests/cypress/support/urls_commands.js
@@ -3,28 +3,46 @@ const PAGES = {
   DASHBOARD: 'dashboard'
 }
 
-const reverseUrl = (pageName, extra={}) => {
-  switch  (pageName) {
-    case PAGES.CONTENTS:
-      if (extra.type) {
-          return ({workspaceId}) => `/ui/workspaces/${workspaceId}/contents?type=${extra.type}`
-      }
-      return ({workspaceId}) => `/ui/workspaces/${workspaceId}/contents`
-    case PAGES.DASHBOARD:
-      return ({workspaceId}) => `/ui/workspaces/${workspaceId}/dashboard`
-    default:
-      throw `No page found for page name ${pageName} and extra args ${extra}`
-  }
+const URLS = {
+  contents: ({workspaceId}) => `/ui/workspaces/${workspaceId}/contents/`,
+  dashboard: ({workspaceId}) => `/ui/workspaces/${workspaceId}/dashboard/`
 }
 
-const formatUrl = ({pageName, extra, param = {},}) => {
-  return reverseUrl(pageName, extra)(param)
+/** 
+  * Generate a lazy url.
+  * @param {string} pageName: key of the url mapped in URLS.
+ */
+const reverseUrl = (pageName) => {
+  if (! (pageName in URLS)) {
+    throw `No page found for page name ${pageName}`
+  }
+  return URLS[pageName]
 }
+
+/** 
+  * Format a url for a given pageName mapped in URLS and applies getters at the end.
+  * @param {string} pageName: key of the url mapped in URLS.
+  * @param {Object} param: object containing the key/value to use on the lazy url.
+  * @param {Object} getters: object containing the key/value to format the GET paramaters. 
+ */
+const formatUrl = ({pageName, params = {}, getters = null}) => {
+  let url = reverseUrl(pageName)(params)
+  if (getters) {
+    url += '?'
+    Object.entries(getters).forEach(([key, value]) => url += `${key}=${value}`)
+  }
+  return url
+}
+
+Cypress.Commands.add('visitPage', ({pageName, params = {}, getters = null}) => {
+  let url = formatUrl({pageName: pageName, params: params, getters: getters})
+  return cy.visit(url)
+})
 export { PAGES, reverseUrl, formatUrl }
 
 /*
 EXEMPLE
-formatUrl(pageName: PAGES.CONTENTS, extra: {type: FILE}, param: {workspaceId: 1}})
+formatUrl(pageName: PAGES.CONTENTS, getters: {type: 'file'}, param: {workspaceId: 1}})
 > '/ui/workspaces/1/contents?type=file'
 ----
 
