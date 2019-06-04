a922372ba086b4dc37462d1e527fe11ef024115c
basile <basilelegal@gmail.com>
2019-06-04 09:11:25 -0400

    front - standardjs

--- functionnal_tests/cypress/support/generic_selector_commands.js
+++ functionnal_tests/cypress/support/generic_selector_commands.js
@@ -4,31 +4,30 @@ const SELECTORS = {
   WORKSPACE_MENU: 'workspaceMenu'
 }
 
-
 const TAGS = {
   [SELECTORS.CONTENT_FRAME]: () => '[data-cy="popinFixed"]',
   [SELECTORS.CONTENT_IN_LIST]: () => `.workspace__content__fileandfolder > .content`,
-  [SELECTORS.WORKSPACE_MENU]: ({workspaceId}) => `[data-cy=sidebar__content__navigation__workspace__item_${workspaceId}]`
+  [SELECTORS.WORKSPACE_MENU]: ({ workspaceId }) => `[data-cy=sidebar__content__navigation__workspace__item_${workspaceId}]`
 }
 
-/** 
+/**
   * Generate a lazy html tag.
   * @param {selectorName} selectorName: key of the tag mapped in TAGS.
  */
 const reverseTag = (selectorName) => {
-  if (! (selectorName in TAGS)) {
+  if (!(selectorName in TAGS)) {
     throw `No selector found for selector name ${selectorName}`
   }
   return TAGS[selectorName]
 }
 
-/** 
+/**
   * Format a url for a given pageName mapped in URLS and applies getters at the end.
   * @param {string} selectorName: key of the tag mapped in TAGS.
   * @param {Object} param: object containing the key/value to use on the lazy tag.
-  * @param {Object} attrs: object containing the key/value to format html attrs selector. 
+  * @param {Object} attrs: object containing the key/value to format html attrs selector.
  */
-const formatTag = ({selectorName, params = {}, attrs = null}) => {
+const formatTag = ({ selectorName, params = {}, attrs = null }) => {
   let tag = reverseTag(selectorName)(params)
   if (attrs) {
     Object.entries(attrs).forEach(([key, value]) => tag += `[${key}="${value}"]`)
@@ -36,8 +35,8 @@ const formatTag = ({selectorName, params = {}, attrs = null}) => {
   return tag
 }
 
-Cypress.Commands.add('getTag', ({selectorName, params = {}, attrs = null}) => {
-  return cy.get(formatTag({selectorName: selectorName, params: params, attrs: attrs}))
+Cypress.Commands.add('getTag', ({ selectorName, params = {}, attrs = null }) => {
+  return cy.get(formatTag({ selectorName: selectorName, params: params, attrs: attrs }))
 })
 
 export { SELECTORS, formatTag, reverseTag }
