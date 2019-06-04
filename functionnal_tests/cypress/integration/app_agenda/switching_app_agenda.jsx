a7faea477bf91b0b32ac66b0c34cfe15d25e1a8e
Skylsmoi <come.huguies@gmail.com>
2019-06-04 16:52:49 +0200

    [wip] cypress test in progress but wait until full modif of basile in merged in develop

--- functionnal_tests/cypress/integration/app_agenda/switching_app_agenda.jsx
+++ functionnal_tests/cypress/integration/app_agenda/switching_app_agenda.jsx
@@ -1,19 +1,32 @@
 
 describe('App Agenda', () => {
-  let workspaceId
+  let workspace1 = {}
+  let workspace2 = {}
 
   before(function () {
     cy.resetDB()
     cy.setupBaseDB()
     cy.loginAs('administrators')
     cy.fixture('baseWorkspace').as('workspace').then(workspace => {
-      workspaceId = workspace.workspace_id
+      workspace1 = workspace
+    })
+    cy.createRandomWorkspace().then(workspace => {
+      workspace2 = workspace
     })
   })
 
   describe('Switching from app agenda of different workspace', () => {
     it('Should reload the iframe with the proper workspace id', () => {
-      // cy.visit(PAGE.WORKSPACE.AGENDA(workspaceId))
+      cy.visit(`/ui/workspaces/${workspace1.workspace_id}/agenda`)
+
+      cy.get('[data-cy="layoutPageTitle"]').contains(workspace1.label)
+
+      cy.get(`.sidebar__content__navigation__workspace__item__name[title="${workspace2.label}"]`)
+        .click()
+
+      cy.get(`.sidebar__content__navigation__workspace__item__name[title="${workspace2.label}"]`)
+        .parents('[data-cy="sidebar__content__navigation__workspace__item"]')
+
     })
   })
 })
