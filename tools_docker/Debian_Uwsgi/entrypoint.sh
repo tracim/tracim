0f79fd09ce7dd24a4babcbdec287b70595ab4d27
philippe.accorsi@algoo.fr <philippe.accorsi@algoo.fr>
2019-06-05 18:11:28 +0200

    add default config[skip ci]

--- tools_docker/Debian_Uwsgi/entrypoint.sh
+++ tools_docker/Debian_Uwsgi/entrypoint.sh
@@ -72,14 +72,12 @@ a2enmod proxy proxy_http proxy_ajp rewrite deflate headers proxy_html dav_fs dav
 service redis-server start  # async email sending
 supervisord -c /tracim/tools_docker/Debian_Uwsgi/supervisord_tracim.conf
 
+# Start daemon for async email
+supervisorctl start tracim_mail_notifier
+
 # Activate daemon for reply by email
 if [ "$REPLY_BY_EMAIL" = "1" ];then
     supervisorctl start tracim_mail_fetcher
-fi
-
-# Activate daemon for sending email in async
-if [ "$EMAIL_MODE_ASYNC" = "1" ];then
-    supervisorctl start tracim_mail_notifier
 fi
 
 # Activate or deactivate webdav
@@ -87,10 +85,12 @@ if [ "$START_WEBDAV" = "1" ]; then
     if [ ! -L /etc/uwsgi/apps-enabled/tracim_webdav.ini ]; then
         ln -s /etc/uwsgi/apps-available/tracim_webdav.ini /etc/uwsgi/apps-enabled/tracim_webdav.ini
     fi
+    sed -i "s|webdav.ui.enabled = .*|webdav.ui.enabled = True|g" /etc/tracim/development.ini
     sed -i "s|^\s*#ProxyPass /webdav http://127.0.0.1:3030/webdav|    ProxyPass /webdav http://127.0.0.1:3030/webdav|g" /etc/tracim/apache2.conf
     sed -i "s|^\s*#ProxyPassReverse /webdav http://127.0.0.1:3030/webdav|    ProxyPassReverse /webdav http://127.0.0.1:3030/webdav|g" /etc/tracim/apache2.conf
 else
     rm -f /etc/uwsgi/apps-enabled/tracim_webdav.ini
+    sed -i "s|webdav.ui.enabled = .*|webdav.ui.enabled = False|g" /etc/tracim/development.ini
     sed -i "s|^\s*ProxyPass /webdav http://127.0.0.1:3030/webdav|    #ProxyPass /webdav http://127.0.0.1:3030/webdav|g" /etc/tracim/apache2.conf
     sed -i "s|^\s*ProxyPassReverse /webdav http://127.0.0.1:3030/webdav|    #ProxyPassReverse /webdav http://127.0.0.1:3030/webdav|g" /etc/tracim/apache2.conf
 fi
@@ -108,6 +108,19 @@ else
     sed -i "s|caldav.enabled = .*|caldav.enabled = False|g" /etc/tracim/development.ini
     sed -i "s|^\s*ProxyPass /agenda http://127.0.0.1:8080/agenda|    #ProxyPass /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
     sed -i "s|^\s*ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|    #ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
+fi
+
+# Activate elastic search
+if [ "$START_ELASTIC" = "1" ]; then
+    sed -i "s|search.engine = .*|search.engine = elasticsearch|g" /etc/tracim/development.ini
+    sed -i "s|;search.elasticsearch.host = .*|search.elasticsearch.host = localhost|g" /etc/tracim/development.ini
+    sed -i "s|;search.elasticsearch.port = .*|search.elasticsearch.port = 9200|g" /etc/tracim/development.ini
+    sed -i "s|index = .*|index = value|g" /etc/tracim/development.ini
+else
+    sed -i "s|search.engine = .*|search.engine = simple|g" /etc/tracim/development.ini
+    sed -i "s|search.elasticsearch.host = .*|;search.elasticsearch.host = localhost|g" /etc/tracim/development.ini
+    sed -i "s|search.elasticsearch.port = .*|;search.elasticsearch.port = 9200|g" /etc/tracim/development.ini
+    sed -i "s|index = .*|index = value|g" /etc/tracim/development.ini
 fi
 
 # Reload apache config
