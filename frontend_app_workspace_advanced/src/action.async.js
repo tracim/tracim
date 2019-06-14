18cd310364248c348f2c8886fa65bbc8dae9eeca
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2019-06-14 10:41:13 +0200

    [#1910] add debug to Workspace Advanced App and update index.html at all changed apps

--- frontend_app_workspace_advanced/src/action.async.js
+++ frontend_app_workspace_advanced/src/action.async.js
@@ -1,4 +1,4 @@
-import { FETCH_CONFIG } from './helper.js'
+import { FETCH_CONFIG } from 'tracim_frontend_lib'
 
 export const getWorkspaceDetail = (apiUrl, idWorkspace) =>
   fetch(`${apiUrl}/workspaces/${idWorkspace}`, {
