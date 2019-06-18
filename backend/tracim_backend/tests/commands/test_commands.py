099cafd3deb12bccc7509c783d7e2406caceacbc
Guénaël Muller <guenael.muller@algoo.fr>
2019-06-18 09:32:00 +0200

    rename search commands

--- backend/tracim_backend/tests/commands/test_commands.py
+++ backend/tracim_backend/tests/commands/test_commands.py
@@ -45,10 +45,10 @@ class TestCommands(CommandFunctionalTest):
         assert output.find("webdav start") > 0
         assert output.find("caldav start") > 0
         assert output.find("caldav sync") > 0
-        assert output.find("search init") > 0
-        assert output.find("search index") > 0
-        assert output.find("search upgrade") > 0
-        assert output.find("search delete") > 0
+        assert output.find("search index-create") > 0
+        assert output.find("search index-populate") > 0
+        assert output.find("search index-upgrade-experimental") > 0
+        assert output.find("search index-drop") > 0
 
     def test_func__user_create_command__ok__nominal_case(self) -> None:
         """
