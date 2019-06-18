f1585c5d40bac0c6e919e126148a2c6e35159fc3
Skylsmoi <come.huguies@gmail.com>
2019-06-18 18:40:17 +0200

    [#1845] appInterface now try/catch when trying to load an app + file is now ES5 complient

--- frontend/dist/assets/tracim/appInterface.js
+++ frontend/dist/assets/tracim/appInterface.js
@@ -1,9 +1,15 @@
-(function () {
-  let prevSelectedAppFeature = ''
-  let prevSelectedAppFullScreen = ''
+var prevSelectedAppFeature = ''
+var prevSelectedAppFullScreen = ''
+var APP_NOT_LOADED = 'appNotLoaded'
+var TIME_TO_RETRY = 500
+var RETRY_TIMEOUT = 5000
 
-  getSelectedApp = name => {
-    switch (name) {
+function getSelectedApp (appName) {
+  // FIXME - CH - 2019-06-18 - The try/catch is a temporary solution to solve the frontend, apps and appInterface.js
+  // loading and execution order. If getSelectedApp return APP_NOT_LOADED, GLOBAL_renderAppFeature and GLOBAL_renderAppFullscreen
+  // will retry every TIME_TO_RETRY ms for RETRY_TIMEOUT ms
+  try {
+    switch (appName) {
       case 'html-document':
         return (appHtmlDocument || {default: {}}).default
       case 'thread':
@@ -23,90 +29,130 @@
       default:
         return null
     }
+  } catch (e) {
+    return APP_NOT_LOADED
   }
+}
 
-  //@TODO make a file action.tracimCustomEvent.js that will contains all customEvent that tracim_frontend call
-  // => pb with that is that appInterface cant use import since it is not part of the build webpack
-  // use module.export and require
-  // doesn't work, cant resolve a file outside of the build dir
+//@TODO make a file action.tracimCustomEvent.js that will contains all customEvent that tracim_frontend call
+// => pb with that is that appInterface cant use import since it is not part of the build webpack
+// use module.export and require
+// doesn't work, cant resolve a file outside of the build dir
 
-  GLOBAL_renderAppFeature = app => {
-    console.log('%cGLOBAL_renderAppFeature', 'color: #5cebeb', app)
+function GLOBAL_renderAppFeature (app, retryCount) {
+  console.log('%cGLOBAL_renderAppFeature', 'color: #5cebeb', app)
 
-    const selectedApp = getSelectedApp(app.config.slug)
+  var selectedApp = getSelectedApp(app.config.slug)
 
-    if (selectedApp.isRendered) {
-      GLOBAL_dispatchEvent({type: `${app.config.slug}_showApp`, data: app})
-    } else {
-      selectedApp.renderAppFeature(app)
-      selectedApp.isRendered = true
-      ;(getSelectedApp(prevSelectedAppFeature) || {isRendered: null}).isRendered = false
-      prevSelectedAppFeature = selectedApp.name
-    }
-  }
+  if (selectedApp === APP_NOT_LOADED) {
+    retryCount = retryCount || 0 // INFO - CH - 2019-06-18 - old school way for default param
 
-  GLOBAL_renderAppFullscreen = app => {
-    console.log('%cGLOBAL_renderAppFullscreen', 'color: #5cebeb', app)
+    console.log(app.config.slug + ' does not exists. Maybe it hasn\'t finished loading yet ? Retrying in ' + TIME_TO_RETRY + 'ms')
+    var retryTime = retryCount + TIME_TO_RETRY
 
-    const selectedApp = getSelectedApp(app.config.slug)
-
-    if (selectedApp.isRendered) {
-      GLOBAL_dispatchEvent({type: `${app.config.slug}_showApp`, data: app})
-    } else {
-      selectedApp.renderAppFullscreen(app)
-      selectedApp.isRendered = true
-      ;(getSelectedApp(prevSelectedAppFullScreen) || {isRendered: null}).isRendered = false
-      prevSelectedAppFullScreen = selectedApp.name
-    }
-  }
-
-  GLOBAL_renderAppPopupCreation = app => {
-    console.log('%cGLOBAL_renderAppPopupCreation', 'color: #5cebeb', app)
-
-    const selectedApp = getSelectedApp(app.config.slug)
-
-    if (!selectedApp) {
-      console.log('Error in GLOBAL_renderAppPopupCreation, selectedApp is undefined', app)
+    if (retryTime < RETRY_TIMEOUT) {
+      setTimeout(function () {GLOBAL_renderAppFeature(app, retryTime)}, TIME_TO_RETRY)
       return
     }
 
-    getSelectedApp(app.config.slug).renderAppPopupCreation(app)
+    console.error('Timed out waiting for app ' + app.config.slug + ' to exists')
+    return
   }
 
-  GLOBAL_dispatchEvent = ({type, data}) => {
-    console.log('%cGLOBAL_dispatchEvent', 'color: #fff', type, data)
-
-    const event = new CustomEvent('appCustomEvent', {detail: {type, data}})
-    document.dispatchEvent(event)
+  if (selectedApp.isRendered) {
+    GLOBAL_dispatchEvent({type: app.config.slug + '_showApp', data: app})
+  } else {
+    selectedApp.renderAppFeature(app)
+    selectedApp.isRendered = true
+    ;(getSelectedApp(prevSelectedAppFeature) || {isRendered: null}).isRendered = false
+    prevSelectedAppFeature = selectedApp.name
   }
+}
 
-  GLOBAL_eventReducer = ({detail: {type, data}}) => {
-    switch (type) {
-      case 'hide_popupCreateContent':
-      case 'hide_popupCreateWorkspace':
-        console.log('%cGLOBAL_eventReducer Custom Event', 'color: #28a745', type, data)
-        getSelectedApp(data.name).unmountApp('popupCreateContentContainer')
-        break
-      case 'unmount_app':
-        console.log('%cGLOBAL_eventReducer Custom Event', 'color: #28a745', type, data)
+function GLOBAL_renderAppFullscreen (app, retryCount) {
+  console.log('%cGLOBAL_renderAppFullscreen', 'color: #5cebeb', app)
 
-        if (prevSelectedAppFeature !== '') {
-          const selectedApp = getSelectedApp(prevSelectedAppFeature)
-          selectedApp.unmountApp('appFeatureContainer')
-          selectedApp.unmountApp('popupCreateContentContainer')
-          selectedApp.isRendered = false
-          prevSelectedAppFeature = ''
-        }
+  var selectedApp = getSelectedApp(app.config.slug)
 
-        if (prevSelectedAppFullScreen !== '') {
-          const selectedApp = getSelectedApp(prevSelectedAppFullScreen)
-          selectedApp.unmountApp('appFullscreenContainer')
-          selectedApp.isRendered = false
-          prevSelectedAppFullScreen = ''
-        }
-        break
+  if (selectedApp === APP_NOT_LOADED) {
+    retryCount = retryCount || 0 // INFO - CH - 2019-06-18 - old school way for default param
+
+    console.log(app.config.slug + ' does not exists. Maybe it hasn\'t finished loading yet ? Retrying in ' + TIME_TO_RETRY + 'ms')
+
+    var retryTime = retryCount + TIME_TO_RETRY
+
+    if (retryTime < RETRY_TIMEOUT) {
+      setTimeout(function () {GLOBAL_renderAppFullscreen(app, retryTime)}, TIME_TO_RETRY)
+      return
     }
+
+    console.error('Timed out waiting for app ' + app.config.slug + ' to exists')
+    return
   }
 
-  document.addEventListener('appCustomEvent', GLOBAL_eventReducer)
-})()
+  if (selectedApp.isRendered) {
+    GLOBAL_dispatchEvent({type: app.config.slug + '_showApp', data: app})
+  } else {
+    selectedApp.renderAppFullscreen(app)
+    selectedApp.isRendered = true
+    ;(getSelectedApp(prevSelectedAppFullScreen) || {isRendered: null}).isRendered = false
+    prevSelectedAppFullScreen = selectedApp.name
+  }
+}
+
+function GLOBAL_renderAppPopupCreation (app) {
+  console.log('%cGLOBAL_renderAppPopupCreation', 'color: #5cebeb', app)
+
+  var selectedApp = getSelectedApp(app.config.slug)
+
+  if (!selectedApp) {
+    console.log('Error in GLOBAL_renderAppPopupCreation, selectedApp is undefined', app)
+    return
+  }
+
+  getSelectedApp(app.config.slug).renderAppPopupCreation(app)
+}
+
+function GLOBAL_dispatchEvent (event) {
+  var type = event.type
+  var data = event.data
+  console.log('%cGLOBAL_dispatchEvent', 'color: #fff', type, data)
+
+  var customEvent = new CustomEvent('appCustomEvent', {detail: {type, data}})
+  document.dispatchEvent(customEvent)
+}
+
+function GLOBAL_eventReducer (event) {
+  var type = event.detail.type
+  var data = event.detail.data
+
+  switch (type) {
+    case 'hide_popupCreateContent':
+    case 'hide_popupCreateWorkspace':
+      console.log('%cGLOBAL_eventReducer Custom Event', 'color: #28a745', type, data)
+      getSelectedApp(data.name).unmountApp('popupCreateContentContainer')
+      break
+    case 'unmount_app':
+      console.log('%cGLOBAL_eventReducer Custom Event', 'color: #28a745', type, data)
+
+      var selectedApp
+
+      if (prevSelectedAppFeature !== '') {
+        selectedApp = getSelectedApp(prevSelectedAppFeature)
+        selectedApp.unmountApp('appFeatureContainer')
+        selectedApp.unmountApp('popupCreateContentContainer')
+        selectedApp.isRendered = false
+        prevSelectedAppFeature = ''
+      }
+
+      if (prevSelectedAppFullScreen !== '') {
+        selectedApp = getSelectedApp(prevSelectedAppFullScreen)
+        selectedApp.unmountApp('appFullscreenContainer')
+        selectedApp.isRendered = false
+        prevSelectedAppFullScreen = ''
+      }
+      break
+  }
+}
+
+document.addEventListener('appCustomEvent', GLOBAL_eventReducer)
