2b1680aa33a6924b7d493e291001ddff135694d1
Skylsmoi <come.huguies@gmail.com>
2019-06-26 09:56:39 +0200

    add script to run all unit test + add missing babel property in package.json

---
+++ run_frontend_unit_test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Main in bottom
+
+YELLOW='\033[1;33m'
+BROWN='\033[0;33m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+function log {
+    echo -e "\n${YELLOW}[$(date +'%H:%M:%S')]${BROWN} $ $1${NC}"
+}
+
+function loggood {
+    echo -e "\n${YELLOW}[$(date +'%H:%M:%S')]${GREEN} $ $1${NC}"
+}
+
+function logerror {
+    echo -e "\n${RED}[$(date +'%H:%M:%S')]${RED} $ $1${NC}"
+}
+
+DEFAULTDIR=$(pwd)
+export DEFAULTDIR
+echo "This is DEFAULTDIR \"$DEFAULTDIR\""
+
+cd $DEFAULTDIR/frontend_lib || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_html-document || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_file || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_thread || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_folder_advanced || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_workspace || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_workspace_advanced || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_agenda || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+cd $DEFAULTDIR/frontend_app_admin_workspace_user || exit 1
+npm run test && loggood "success" || logerror "some error"
+
+log "cd $DEFAULTDIR"
+cd $DEFAULTDIR || exit 1
