67b2cca4e3684c8b1b048e1d84e88caecacc97d9
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2019-07-04 11:11:06 +0200

    [#2030] update guest download page

--- frontend/src/container/GuestDownload.jsx
+++ frontend/src/container/GuestDownload.jsx
@@ -1,65 +1,40 @@
 import React from 'react'
 import { translate } from 'react-i18next'
-import { connect } from 'react-redux'
+import { Popover, PopoverBody } from 'reactstrap'
 import Card from '../component/common/Card/Card.jsx'
 import CardHeader from '../component/common/Card/CardHeader.jsx'
 import CardBody from '../component/common/Card/CardBody.jsx'
+import Button from '../component/common/Input/Button.jsx'
 import InputGroupText from '../component/common/Input/InputGroupText.jsx'
 import FooterLogin from '../component/Login/FooterLogin.jsx'
 
 class GuestDownload extends React.Component {
   constructor (props) {
     super(props)
+    this.toggle = this.toggle.bind(this)
     this.state = {
-      guestName: '',
-      guestComment: '',
+      popoverOpen: false,
+      userName: 'USER',
+      file: {
+        fileName: 'FILE',
+        fileSize: 'SIZE'
+      },
       guestPassword: {
         value: '',
         isInvalid: false
-      },
-      appName: 'file',
-      uploadFile: null,
-      uploadFilePreview: null,
-      progressUpload: {
-        display: false,
-        percent: 0
       }
     }
   }
 
-  handleChangeFullName = e => this.setState({guestName: e.target.value})
-  handleChangeComment = e => this.setState({guestComment: e.target.value})
+  toggle () {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    })
+  }
+
   handleChangePassword = e => this.setState({guestPassword: {...this.state.guestPassword, value: e.target.value}})
 
-  handleClickSend = () => {}
-
-  handleChangeFile = newFile => {
-    if (!newFile || !newFile[0]) return
-
-    const fileToSave = newFile[0]
-
-    if (
-      !fileToSave.type.includes('image') ||
-      fileToSave.size > 2000000
-    ) {
-      this.setState({
-        uploadFile: fileToSave,
-        uploadFilePreview: false
-      })
-      return
-    }
-
-    this.setState({uploadFile: fileToSave})
-
-    var reader = new FileReader()
-    reader.onload = e => {
-      this.setState({uploadFilePreview: e.total > 0 ? e.target.result : false})
-      const img = new Image()
-      img.src = e.target.result
-      img.onerror = () => this.setState({uploadFilePreview: false})
-    }
-    reader.readAsDataURL(fileToSave)
-  }
+  handleClickDowload = () => {}
 
   render () {
     const { props, state } = this
@@ -68,19 +43,47 @@ class GuestDownload extends React.Component {
       <section className='guestdownload primaryColorBg'>
         <Card customClass='guestdownload__card'>
           <CardHeader customClass='guestdownload__card__header primaryColorBgLighten'>
-            {props.t('Download files')}
+            {props.t('Download file')}
           </CardHeader>
 
           <CardBody formClass='guestdownload__card__form'>
             <form>
-              <InputGroupText
-                parentClassName='guestdownload__card__form__fullname'
-                customClass='mb-3'
-                type='text'
-                placeHolder={props.t('Full name')}
-                value={state.guestName}
-                onChange={this.handleChangeFullName}
-              />
+              <div className='guestdownload__card__form__text'>
+                {state.userName} {props.t('shared with you the file')} {state.file.fileName} ({state.file.fileSize})
+              </div>
+              <div className='d-flex'>
+                <InputGroupText
+                  parentClassName='guestdownload__card__form__groupepw'
+                  customClass=''
+                  icon='fa-lock'
+                  type='password'
+                  placeHolder={props.t('Password')}
+                  invalidMsg={props.t('Invalid password')}
+                  isInvalid={state.guestPassword.isInvalid}
+                  value={state.guestPassword.value}
+                  onChange={this.handleChangePassword}
+                  onKeyDown={() => {}}
+                />
+                <button
+                  type='button'
+                  className='guestupload__card__form__groupepw__question mb-3'
+                  id='popoverQuestion'
+                >
+                  <i className='fa fa-fw fa-question-circle' />
+                </button>
+                <Popover placement='bottom' isOpen={this.state.popoverOpen} target='popoverQuestion' toggle={this.toggle}>
+                  <PopoverBody>{props.t('The person who sent you this file protected it with a password. If you do not know the password, please contact her.')}</PopoverBody>
+                </Popover>
+              </div>
+              <div className='d-flex'>
+                <Button
+                  htmlType='button'
+                  bootstrapType='primary'
+                  customClass='guestupload__card__form__btndownload btn btnSubmit ml-auto'
+                  label={props.t('Download')}
+                  onClick={this.handleClickDowload}
+                />
+              </div>
             </form>
           </CardBody>
         </Card>
@@ -90,6 +93,4 @@ class GuestDownload extends React.Component {
     )
   }
 }
-
-const mapStateToProps = ({ contentType }) => ({ contentType })
-export default connect(mapStateToProps)(translate()(GuestDownload))
+export default translate()(GuestDownload)
