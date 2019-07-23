63d6c32ff95962bc3a768277704a7f5c570ac831
metzgegu <Guillaume.metzger5@gmail.com>
2019-07-23 14:50:09 +0200

    Refactor workspace advanced cypress test, (add cy.wait before clear the description input)

--- functionnal_tests/cypress/integration/app_workspace_advanced/operation_workspace_advanced.js
+++ functionnal_tests/cypress/integration/app_workspace_advanced/operation_workspace_advanced.js
@@ -1,7 +1,7 @@
 import { SELECTORS as s } from '../../support/generic_selector_commands'
 import { PAGES as p } from '../../support/urls_commands'
 
-describe('app workspace advanced', function () {
+describe('App Workspace Advanced', function () {
     const newDescription = 'myNewDescription'
     const workspaceId = 1
 
@@ -14,26 +14,28 @@ describe('app workspace advanced', function () {
         cy.loginAs('administrators')
         cy.visitPage({pageName: p.DASHBOARD, params: { workspaceId }})
     })
+    describe("Changing the workspace's description", () => {
+        it('Should update the description in the dashboard', function () {
+            cy.getTag({selectorName: s.WORKSPACE_DASHBOARD})
+                .find('.dashboard__header__advancedmode__button.btn')
+                .click()
 
-    it('Change workspace description', function () {
-        cy.getTag({selectorName: s.WORKSPACE_DASHBOARD})
-            .find('.dashboard__header__advancedmode__button.btn')
-            .click()
+            cy.wait(1000)
+            cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
+                .find('.workspace_advanced__description__text__textarea')
+                .clear()
 
-        cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
-            .find('.workspace_advanced__description__text__textarea')
-            .clear()
+            cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
+                .find('.workspace_advanced__description__text__textarea')
+                .type(newDescription)
 
-        cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
-            .find('.workspace_advanced__description__text__textarea')
-            .type(newDescription)
+            cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
+                .find('.workspace_advanced__description__bottom__btn')
+                .click()
 
-        cy.getTag({selectorName: s.APP_FEATURE_CONTAINER})
-            .find('.workspace_advanced__description__bottom__btn')
-            .click()
-
-        cy.getTag({selectorName: s.WORKSPACE_DASHBOARD})
-            .find('.dashboard__workspace__detail__description')
-            .contains(newDescription)
+            cy.getTag({selectorName: s.WORKSPACE_DASHBOARD})
+                .find('.dashboard__workspace__detail__description')
+                .contains(newDescription)
+        })
     })
 })
