537603a84cd32938deab315fa736a859af27ea83
Guénaël Muller <guenael.muller@algoo.fr>
2019-08-02 11:28:58 +0200

    remove collabora specific code collabora api

--- backend/tracim_backend/lib/collaborative_document_edition/collaboration_document_edition.py
+++ backend/tracim_backend/lib/collaborative_document_edition/collaboration_document_edition.py
@@ -4,8 +4,6 @@ from os.path import isfile
 from os.path import join
 import typing
 
-from defusedxml import ElementTree
-import requests
 from sqlalchemy.orm import Session
 
 from tracim_backend import CFG
@@ -41,31 +39,7 @@ class CollaborativeDocumentEditionApi(object):
         """
         Get list of supported file type for collaborative editions
         """
-        response = requests.get(
-            self._config.COLLABORATIVE_DOCUMENT_EDITION__COLLABORA__BASE_URL + "/hosting/discovery",
-            timeout=2,
-        )
-        root = ElementTree.fromstring(response.text)
-        supported_collabora_file = []  # type: typing.List[CollaborativeDocumentEditionFileType]
-        for xml_app in root.findall("net-zone/app"):
-            mimetype = xml_app.get("name")
-            # INFO - G.M - 2019-07-17 - this list return also a non mimetype type,
-            # we should not add him.
-            if mimetype == "Capabilities":
-                continue
-            for xml_action in xml_app:
-                extension = xml_action.get("ext")
-                url_source = xml_action.get("urlsrc")
-                associated_action = xml_action.get("name")
-                supported_collabora_file.append(
-                    CollaborativeDocumentEditionFileType(
-                        mimetype=mimetype,
-                        extension=extension,
-                        associated_action=associated_action,
-                        url_source=url_source,
-                    )
-                )
-        return supported_collabora_file
+        raise NotImplementedError()
 
     def get_config(self) -> CollaborativeDocumentEditionConfig:
         return CollaborativeDocumentEditionConfig(
