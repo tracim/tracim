b6ecbcfa455d4cba5cc0754722729fe163e07310
philippe.accorsi@algoo.fr <philippe.accorsi@algoo.fr>
2019-08-09 16:37:29 +0200

    new tag build_029 [skip ci]

--- backend/tracim_backend/lib/core/system.py
+++ backend/tracim_backend/lib/core/system.py
@@ -18,7 +18,7 @@ class SystemApi(object):
         # TODO - G.M - 2018-09-26 - Set version correctly
         return AboutModel(
             name="Tracim",
-            version="2.3.2_build_027",
+            version="2.3.2_build_029",
             datetime=datetime.datetime.now(),
             website="https://www.tracim.fr",
         )
