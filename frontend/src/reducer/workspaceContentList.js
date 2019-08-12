df1f0bdf7a8685d36f081bdc075487bb724ecdce
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2019-08-12 11:50:19 +0200

    [#2030] share folder at content list

--- frontend/src/reducer/workspaceContentList.js
+++ frontend/src/reducer/workspaceContentList.js
@@ -24,7 +24,8 @@ const serializeContent = c => ({
   statusSlug: c.status,
   subContentTypeList: c.sub_content_types,
   isOpen: c.isOpen ? c.isOpen : false, // only useful for folder
-  activedShares: c.active_shares
+  activedShares: c.active_shares,
+  created: c.created
 })
 
 export default function workspaceContentList (state = [], action) {
