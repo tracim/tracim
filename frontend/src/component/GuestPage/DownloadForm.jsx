aebbdb36eba211a0a4fe334d0c51a29bca0be297
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2019-08-22 15:04:08 +0200

    [#2246] allowing download with a password

--- frontend/src/component/GuestPage/DownloadForm.jsx
+++ frontend/src/component/GuestPage/DownloadForm.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { translate } from 'react-i18next'
 import { Popover, PopoverBody } from 'reactstrap'
 import InputGroupText from '../common/Input/InputGroupText.jsx'
+import {
+  postDownloadFile
+} from '../../action-creator.async.js'
+import {
+  handleFetchResult,
+  CUSTOM_EVENT
+} from 'tracim_frontend_lib'
 
 class DownloadForm extends React.Component {
   constructor (props) {
@@ -18,62 +25,110 @@ class DownloadForm extends React.Component {
     }))
   }
 
+  sendGlobalFlashMessage = msg => GLOBAL_dispatchEvent({
+    type: CUSTOM_EVENT.ADD_FLASH_MSG,
+    data: {
+      msg: this.props.t(msg),
+      type: 'warning',
+      delay: undefined
+    }
+  })
+
+  handleSubmit = async (e) => {
+    const { props } = this
+
+    const guestPassword = props.guestPassword.value !== '' ? props.guestPassword.value : null
+    e.preventDefault()
+    const request = await postDownloadFile(props.token, guestPassword)
+    const response = await handleFetchResult(request)
+
+    // FIXME - G.B. - 2019-08-22 - handleFetch doesn't return the same response everytime, in case of success there is no "apiResponse"
+    if (response.apiResponse) {
+      switch (response.apiResponse.status) {
+        case 204:
+          this.refs['test'].submit()
+          return true
+        case 400:
+          this.sendGlobalFlashMessage(props.t('Error in the URL'))
+          break
+        case 403:
+          switch (response.body.code) {
+            case 2053:
+              this.sendGlobalFlashMessage(props.t('Invalid password'))
+              break
+            default:
+              this.sendGlobalFlashMessage(props.t('Error while downloading file'))
+          }
+          break
+        default:
+          this.sendGlobalFlashMessage(props.t('Error while downloading file'))
+      }
+    } else {
+      this.refs['test'].submit()
+    }
+    return false
+  }
+
+  handleKeyDown = e => { if (e.key === 'Enter') this.handleSubmit(e) }
+
   render () {
     const { props } = this
 
     return (
-      <form>
-        <div className='guestdownload__card__form__text'>
-          {props.t('{{userName}} shared with you the file {{fileName}}',
-            { userName: props.userName, fileName: props.file.fileName, interpolation: { escapeValue: false } }
-          )} ({props.file.fileSize})
-        </div>
-
-        {props.file.isProtected &&
-          <div className='d-flex'>
-            <InputGroupText
-              parentClassName='guestdownload__card__form__groupepw'
-              customClass=''
-              icon='fa-lock'
-              type='password'
-              placeHolder={props.t('Password')}
-              invalidMsg={props.t('Invalid password')}
-              isInvalid={props.guestPassword.isInvalid}
-              value={props.guestPassword.value}
-              onChange={props.onChangePassword}
-            />
-
-            <button
-              type='button'
-              className='guestdownload__card__form__groupepw__question'
-              id='popoverPasswordInfo'
-            >
-              <i className='fa fa-fw fa-question-circle' />
-            </button>
-
-            <Popover
-              placement='bottom'
-              isOpen={this.state.popoverPasswordInfoOpen}
-              target='popoverPasswordInfo'
-              toggle={this.handleTogglePopoverPasswordInfo}
-            >
-              <PopoverBody>
-                {props.t('The person who sent you this file protected it with a password. If you do not know the password, please contact her.')}
-              </PopoverBody>
-            </Popover>
+      <div>
+        <form method='post' action={props.downloadUrl} ref='test'>
+          <div className='guestdownload__card__form__text'>
+            {props.t('{{userName}} shared with you the file {{fileName}}',
+              { userName: props.userName, fileName: props.file.fileName, interpolation: { escapeValue: false } }
+            )} ({props.file.fileSize})
           </div>
-        }
+
+          {props.file.isProtected &&
+            <div className='d-flex'>
+              <InputGroupText
+                parentClassName='guestdownload__card__form__groupepw'
+                customClass=''
+                icon='fa-lock'
+                type='password'
+                placeHolder={props.t('Password')}
+                invalidMsg={props.t('Invalid password')}
+                isInvalid={props.guestPassword.isInvalid}
+                value={props.guestPassword.value}
+                onChange={props.onChangePassword}
+                name='password'
+                onKeyDown={this.handleKeyDown}
+              />
+
+              <button
+                type='button'
+                className='guestdownload__card__form__groupepw__question'
+                id='popoverPasswordInfo'
+              >
+                <i className='fa fa-fw fa-question-circle' />
+              </button>
+
+              <Popover
+                placement='bottom'
+                isOpen={this.state.popoverPasswordInfoOpen}
+                target='popoverPasswordInfo'
+                toggle={this.handleTogglePopoverPasswordInfo}
+              >
+                <PopoverBody>
+                  {props.t('The person who sent you this file protected it with a password. If you do not know the password, please contact her.')}
+                </PopoverBody>
+              </Popover>
+            </div>
+          }
+        </form>
         <div className='d-flex'>
-          <a
+          <button
             className='btn highlightBtn primaryColorBg primaryColorBgDarkenHover guestdownload__card__form__right__btn'
-            href={props.onDownloadFile()}
-            target='_blank'
-            download
+            onClick={this.handleSubmit}
           >
             {props.t('Download')}
-          </a>
+          </button>
         </div>
-      </form>
+      </div>
     )
   }
 }
