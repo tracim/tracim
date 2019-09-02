0b1204935d6d7752fed5d534ec778516a1f81dc7
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2019-09-02 11:01:48 +0200

    [#2194] adding functionnal tests

--- functionnal_tests/cypress/integration/dashboard/button_list__spec.js
+++ functionnal_tests/cypress/integration/dashboard/button_list__spec.js
@@ -1,12 +1,21 @@
 import { SELECTORS as s } from '../../support/generic_selector_commands'
 import { PAGES as p } from '../../support/urls_commands'
 
-describe('Dashboard button list', () => {
+let workspaceId
 
-  beforeEach(() => {
+describe('Dashboard button list', () => {
+  before(function () {
     cy.resetDB()
     cy.setupBaseDB()
     cy.loginAs('administrators')
+    cy.fixture('baseWorkspace').as('workspace').then(workspace => {
+      workspaceId = workspace.workspace_id
+    })
+  })
+
+  beforeEach(() => {
+    cy.loginAs('administrators')
+    cy.visitPage({pageName: p.DASHBOARD, params: {workspaceId: workspaceId}})
   })
 
   afterEach(() => {
@@ -16,14 +25,10 @@ describe('Dashboard button list', () => {
   describe('as a reader', () => {
     describe('if agenda is enabled', () => {
       beforeEach(() => {
-        cy.fixture('baseWorkspace').then(workspace => {
-          cy.createRandomUser().then( user => {
-            cy.log(workspace)
-            cy.addUserToWorkspace(user.user_id, workspace.workspace_id, 'reader')
-            cy.logout()
-            cy.login(user)
-            cy.visitPage({pageName: p.DASHBOARD, params: {workspaceId: workspace.workspace_id}})
-          })
+        cy.createRandomUser().then( user => {
+          cy.addUserToWorkspace(user.user_id, workspaceId, 'reader')
+          cy.login(user)
+          cy.visitPage({pageName: p.DASHBOARD, params: {workspaceId: workspaceId}})
         })
       })
 
@@ -52,5 +57,15 @@ describe('Dashboard button list', () => {
         cy.get('[data-cy="contentTypeBtn_contents/all"]')
       })
     })
+  })
+
+  it('should not have a button for share content', () => {
+    cy.get('[data-cy=contentTypeBtn_share_content]').should('not.exist')
+    cy.get('[data-cy="contentTypeBtn_contents/all"]')
+  })
+
+  it('should not have a button for upload permissions', () => {
+    cy.get('[data-cy=contentTypeBtn_upload_permission]').should('not.exist')
+    cy.get('[data-cy="contentTypeBtn_contents/all"]')
   })
 })
