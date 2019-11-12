2b2ba730dadf92632f95b6b0c92a536dca7a433b
metzgegu <31687635+metzgegu@users.noreply.github.com>
2019-11-12 17:25:48 +0100

    Fix/2500  fix broken cypress test (#2507)

--- functionnal_tests/cypress/integration/app_html-document/operation_dashboard_dashboardbutton_create_html-document.js
+++ functionnal_tests/cypress/integration/app_html-document/operation_dashboard_dashboardbutton_create_html-document.js
@@ -1,4 +1,8 @@
+import { PAGES as p } from '../../support/urls_commands.js'
+
 describe('operation :: workspace > create_new > html-document', function () {
+  const titre1 = 'dashboard button html'
+
   before(() => {
     cy.resetDB()
     cy.setupBaseDB()
@@ -8,11 +12,13 @@ describe('operation :: workspace > create_new > html-document', function () {
     cy.loginAs('administrators')
   })
   it('dashborad > button', function () {
-    cy.visit('/ui/workspaces/1/dashboard')
+    cy.visitPage({
+      pageName: p.DASHBOARD,
+      params: { workspaceId: 1 }
+    })
     cy.get('.dashboard__workspace__detail').should('be.visible')
     cy.get('.dashboard__calltoaction .fa-file-text-o').should('be.visible')
     cy.get('.dashboard__calltoaction .fa-file-text-o').click()
-    var titre1 = 'dashboard button html'
     cy.get('.cardPopup__container .createcontent .createcontent__contentname').should('be.visible')
     cy.get('.cardPopup__container .createcontent .createcontent__form__input').should('have.attr', 'placeholder')
     cy.get('.cardPopup__container .createcontent .createcontent__form__input').type(titre1)
@@ -22,29 +28,14 @@ describe('operation :: workspace > create_new > html-document', function () {
     cy.get('.html-document.visible').should('be.visible')
     cy.get('.html-document.visible .html-document__contentpage__messagelist__version.revision').should('be.visible')
     cy.get('.html-document.visible .wsContentGeneric__header__title').contains(titre1)
-    cy.wait(2000)
-    cy.get('#wysiwygNewVersion_ifr').click()
-    cy.get('#wysiwygNewVersion_ifr').then(($iframe) => {
-      const iframe = $iframe.contents()
 
-      const myInput = iframe.find('#tinymce')
-      cy.wrap(myInput).type('example', { force: true })
+    cy.waitForTinyMCELoaded()
+      .then(() => cy.typeInTinyMCE('example'))
 
-      // you don't need to trigger events like keyup or change
-    })
-
-    cy.get('button.html-document__editionmode__submit.editionmode__button__submit').click({ force: true })
-    cy.wait(2000)
+    cy.get('button.html-document__editionmode__submit.editionmode__button__submit').click()
     cy.get('.html-document.visible .wsContentGeneric__header__close.html-document__header__close').should('be.visible')
     cy.get('.html-document.visible .wsContentGeneric__header__close.html-document__header__close').click()
     cy.get('.html-document.visible').should('not.be.visible')
-    cy.wait(2000)
     cy.get('.content__name').contains(titre1).should('be.visible')
-
-    //        Problem to write text in iframe
-    //        cy.get('#wysiwygNewVersion_ifr').click()
-    //        cy.get('body').type('Ceci est le début du document')
-    //        cy.get('.html-document__editionmode__submit.editionmode__button__submit').click()
-    //        cy.get('.html-document__contentpage__textnote__text span').contains('Ceci est le début du document')
   })
 })
--- functionnal_tests/cypress/integration/workspace/content_dashboard_activity.js
+++ functionnal_tests/cypress/integration/workspace/content_dashboard_activity.js
@@ -1,22 +1,40 @@
-describe('content :: workspace > dashbord', function () {
+import { PAGES as p } from '../../support/urls_commands.js'
+
+describe('recentactivity in dashboard page', function () {
   const threadTitle = 'Thread title'
+  var workspaceId = 0
 
   beforeEach(function () {
     cy.resetDB()
     cy.setupBaseDB()
     cy.loginAs('administrators')
     cy.fixture('baseWorkspace').as('workspace').then(workspace => {
-      cy.createThread(threadTitle, workspace.workspace_id)
-      cy.visit(`/ui/workspaces/${workspace.workspace_id}/dashboard`)
+      workspaceId = workspace.workspace_id
+      cy.visitPage({
+        pageName: p.DASHBOARD,
+        params: { workspaceId }
+      })
     })
   })
 
-  it('dashboard__workspaceInfo > recentactivity', function () {
+  it('recentactivity should be visible', function () {
     cy.get('.recentactivity').scrollIntoView()
     cy.get('.recentactivity .recentactivity__header__title').should('be.visible')
     cy.get('.recentactivity .recentactivity__header__allread').should('be.visible')
-    cy.get('.recentactivity .recentactivity__list').should('be.visible')
-    cy.get('.recentactivity .recentactivity__list .recentactivity__empty').should('be.visible')
-    cy.get('.recentactivity .recentactivity__list button.recentactivity__more__btn').should('be.visible')
   })
+
+  describe('With no recent activity', function () {
+    it('should show the label .recentactivity__empty', function () {
+      cy.get('.recentactivity .recentactivity__list').should('be.visible')
+    })
+  })
+
+  describe('With one recent activity', function () {
+    it('should show the activity and a button to see more', function () {
+      cy.createThread(threadTitle, workspaceId)
+      cy.get('.recentactivity .recentactivity__list .recentactivity__empty').should('be.visible')
+      cy.get('.recentactivity .recentactivity__list button.recentactivity__more__btn').should('be.visible')
+    })
+  })
+
 })
