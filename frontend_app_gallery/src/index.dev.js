1d650bd23bbafa2ae96a7897efb5e530f66fedf5
Skylsmoi <come.huguies@gmail.com>
2019-11-27 11:34:34 +0100

    fix servdev

--- frontend_app_gallery/src/index.dev.js
+++ frontend_app_gallery/src/index.dev.js
@@ -3,10 +3,16 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import ReactDOM from 'react-dom'
 import Gallery from './container/Gallery.jsx'
+import { debug } from './debug.js'
+import { Router } from 'react-router'
+
+export const history = require('history').createBrowserHistory()
 
 require('./css/index.styl')
 
 ReactDOM.render(
-  <Gallery data={undefined} />
+  <Router history={history}>
+    <Gallery data={debug} />
+  </Router>
   , document.getElementById('content')
 )
