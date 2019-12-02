7646b10c4adee050a211de995220fb03d2c9f92c
Guénaël Muller <guenael.muller@algoo.fr>
2019-12-02 15:04:42 +0100

    update doc for plugin

--- backend/doc/hello_world_plugin.py
+++ backend/doc/hello_world_plugin.py
@@ -1,5 +1,5 @@
 """
-This is just a sample of Tracim backend plugin using
+This is just a sample of Tracim backend plugin using pluggy hook
 """
 from tracim_backend.lib.core.plugins import hookimpl
 from tracim_backend.lib.core.plugins import TracimPluginManager
