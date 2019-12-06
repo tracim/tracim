8d73712a53723549d7f7401f5057d078947af735
Skylsmoi <come.huguies@gmail.com>
2019-12-06 11:16:51 +0100

    [#2345] add error handling 6004 on file upload (exceed storage capacity)

--- frontend_app_file/src/container/PopupCreateFile.jsx
+++ frontend_app_file/src/container/PopupCreateFile.jsx
@@ -159,6 +159,7 @@ class PopupCreateFile extends React.Component {
               case 3002: this.sendGlobalFlashMessage(props.t('A content with the same name already exists')); break
               case 6002: this.sendGlobalFlashMessage(props.t('The file is larger than the maximum file size allowed')); break
               case 6003: this.sendGlobalFlashMessage(props.t('Error, the shared space exceed its maximum size')); break
+              case 6004: this.sendGlobalFlashMessage(props.t('You have reach your storage limit, you cannot add new files')); break
             }
             break
           default: this.sendGlobalFlashMessage(props.t('Error while creating file')); break
