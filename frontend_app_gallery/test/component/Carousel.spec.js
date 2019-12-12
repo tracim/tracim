f012077d79aafa1b7027238561455a6b4b42d5fe
metzgegu <guillaume.metzger5@gmail.com>
2019-12-12 10:21:36 +0100

    [#2574] add lazyloading behavior test to carousel.js

--- frontend_app_gallery/test/component/Carousel.spec.js
+++ frontend_app_gallery/test/component/Carousel.spec.js
@@ -10,14 +10,31 @@ describe('<Carousel />', () => {
   const props = {
     onCarouselPositionChange: onCarouselPositionChange,
     isWorkspaceRoot: false,
-    slides: []
+    slides: [
+      {
+        src: 'testSrc1',
+        rotationAngle: 0
+      }
+    ]
   }
 
 
   const wrapper = shallow(<CarouselWithoutHOC {...props} t={tradKey => tradKey} />)
 
   describe('static design', () => {
+    describe('lazyloading behavior', () => {
+      it('lazyLoading should change behavior according to autoplay', () => {
+        expect(wrapper.find('Slider').first().dive().props().lazyLoad).to.equal('ondemand')
+        wrapper.setProps({ autoPlay: true })
+        expect(wrapper.find('Slider').first().dive().props().lazyLoad).to.equal('progressive')
+        wrapper.setProps({ autoPlay: false })
+      })
+    })
     describe('empty slide list message', () => {
+      before(() => {
+        wrapper.setProps({ slides: [] })
+      })
+
       it('should display the right message when it is a folder', () => {
         expect(wrapper.find('.gallery__noContent').text()).to.contain('There isn\'t any previewable content at that folder\'s root.')
       })
