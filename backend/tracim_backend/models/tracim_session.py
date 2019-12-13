11a1e28b4239ad357207b5dcf736b5f330aa60e8
inkhey <guenael.muller@algoo.fr>
2019-12-13 12:31:26 +0100

    feat(BACKEND): allow bypass of revision protection
    Add context_manager  `unprotected_content_revision` allowing to bypass revision deletion protection.
    
    Ref: #2589

---
+++ backend/tracim_backend/models/tracim_session.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+import typing
+
+from sqlalchemy.orm import Session
+
+
+class TracimSession(Session):
+    """
+    Subclass of Sqlalchemy session to add tracim specific stuff
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self._allow_revision_deletion = False  # type: bool
+
+    def set_allowed_revision_deletion(self, value: bool) -> None:
+        self._allow_revision_deletion = value
+
+    def get_allowed_revision_deletion(self) -> bool:
+        return self._allow_revision_deletion
+
+
+@contextmanager
+def unprotected_content_revision(
+    session: TracimSession,
+) -> typing.Generator[TracimSession, None, None]:
+    """
+    allow to bypass protection on tracim revisions
+    """
+    original_allow_revision_deletion_status = session.get_allowed_revision_deletion()
+    try:
+        session.set_allowed_revision_deletion(True)
+        yield session
+    finally:
+        session.set_allowed_revision_deletion(original_allow_revision_deletion_status)
