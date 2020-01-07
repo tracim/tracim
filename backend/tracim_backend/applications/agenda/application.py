c36b6fb174a9c2a69dabd3376d0fd6d0906cec0f
Guénaël Muller <guenael.muller@algoo.fr>
2020-01-07 13:54:11 +0100

    Add agenda as app

--- backend/tracim_backend/applications/agenda/application.py
+++ backend/tracim_backend/applications/agenda/application.py
@@ -1,14 +1,16 @@
 from tracim_backend.app_models.applications import Application
 from tracim_backend.config import CFG
+from tracim_backend.lib.utils.app import TracimAppFactory
 
 
-def get_app(app_config: CFG) -> Application:
-    return Application(
-        label="Agenda",
-        slug="agenda",
-        fa_icon="calendar",
-        is_active=app_config.CALDAV__ENABLED,
-        config={},
-        main_route="/ui/workspaces/{workspace_id}/agenda",
-        app_config=app_config,
-    )
+class AgendaAppFactory(TracimAppFactory):
+    def create_app(self, app_config: CFG) -> Application:
+        return Application(
+            label="Agenda",
+            slug="agenda",
+            fa_icon="calendar",
+            is_active=app_config.CALDAV__ENABLED,
+            config={},
+            main_route="/ui/workspaces/{workspace_id}/agenda",
+            app_config=app_config,
+        )
