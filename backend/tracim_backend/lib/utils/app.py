33d49fb1f59e01a4b716efad9aba09579a21c4ff
Guénaël Muller <guenael.muller@algoo.fr>
2020-01-07 13:56:08 +0100

    add doc about app factory and config object

--- backend/tracim_backend/lib/utils/app.py
+++ backend/tracim_backend/lib/utils/app.py
@@ -4,20 +4,31 @@ import typing
 from hapic.ext.pyramid import PyramidContext
 from pyramid.config import Configurator
 
+from tracim_backend.app_models.applications import Application
+
 if typing.TYPE_CHECKING:
     from tracim_backend.config import CFG
 
 
 class TracimAppFactory(ABC):
-    def create_app(self, app_config: "CFG"):
+    def create_app(self, app_config: "CFG") -> Application:
+        """ Create Tracim application"""
         raise NotImplementedError()
 
 
 class TracimAppConfig(ABC):
-    def load_config(self, app_config: "CFG"):
+    def load_config(self, app_config: "CFG") -> "CFG":
+        """
+        Allow to load specific config parameter, example:
+        >>> app_config.TEST__EXAMPLE_CONFIG_PARAMETER = app_config.get_raw_config("test.example_config_parameter")
+        """
         raise NotImplementedError()
 
-    def check_config(self, app_config: "CFG"):
+    def check_config(self, app_config: "CFG") -> "CFG":
+        """
+        Check app specific config consistency, example:
+        >>> app_config.check_mandatory_param("TEST__EXAMPLE_CONFIG_PARAMETER", app_config.TEST__EXAMPLE_CONFIG_PARAMETER)
+        """
         raise NotImplementedError()
 
 
@@ -29,4 +40,7 @@ class TracimControllerImporter(ABC):
         route_prefix: str,
         context: PyramidContext,
     ) -> Configurator:
+        """
+        Allow to import Controller and other stuff in Tracim web context
+        """
         raise NotImplementedError()
