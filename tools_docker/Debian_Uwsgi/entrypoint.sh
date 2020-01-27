0b7c801cd7ffff4dfa584dba7610426dbdf24988
Guénaël Muller <guenael.muller@algoo.fr>
2020-01-27 12:34:14 +0100

    update docker to handle properly new app_list mecanism

--- tools_docker/Debian_Uwsgi/entrypoint.sh
+++ tools_docker/Debian_Uwsgi/entrypoint.sh
@@ -106,16 +106,16 @@ if [ "$START_CALDAV" = "1" ]; then
     if [ ! -L /etc/uwsgi/apps-enabled/tracim_caldav.ini ]; then
         ln -s /etc/uwsgi/apps-available/tracim_caldav.ini /etc/uwsgi/apps-enabled/tracim_caldav.ini
     fi
-    sed -i "s|caldav.enabled = .*|caldav.enabled = True|g" /etc/tracim/development.ini
+    DEFAULT_APP_LIST="$DEFAULT_APP_LIST,agenda"
     sed -i "s|^\s*#ProxyPass /agenda http://127.0.0.1:8080/agenda|    ProxyPass /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
     sed -i "s|^\s*#ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|    ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
 else
     rm -f /etc/uwsgi/apps-enabled/tracim_caldav.ini
-    sed -i "s|caldav.enabled = .*|caldav.enabled = False|g" /etc/tracim/development.ini
     sed -i "s|^\s*ProxyPass /agenda http://127.0.0.1:8080/agenda|    #ProxyPass /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
     sed -i "s|^\s*ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|    #ProxyPassReverse /agenda http://127.0.0.1:8080/agenda|g" /etc/tracim/apache2.conf
 fi
 
+sed -i "s|;\s*app.enabled = .*|app.enabled = $DEFAULT_APP_LIST|g" /etc/tracim/development.ini
 # TODO PA 2019-06-19 Rework the index-create part according to https://github.com/tracim/tracim/issues/1961
 # Make sure index is created in case of Elastic Search based search. (the command does nothing in case of simple search)
 cd /tracim/backend/
