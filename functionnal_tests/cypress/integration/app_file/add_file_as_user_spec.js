e5d8af6f29df384f24b29537a8cece78cbace430
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-02-18 14:25:57 +0100

    Add a little assertion to test url after a new file is created in the app file cypress test

--- functionnal_tests/cypress/integration/app_file/add_file_as_user_spec.js
+++ functionnal_tests/cypress/integration/app_file/add_file_as_user_spec.js
@@ -37,6 +37,7 @@ context('Add file(s) with PopupCreateFile', function () {
         .get('.previewcomponent__dloption__icon').should('have.length', 1)
       cy.getTag({ selectorName: s.CONTENT_FRAME })
         .get('#dropdownMenu2').contains('Opened')
+      cy.url().should('include', `/ui/workspaces/${workspaceId}/contents/file/`)
     })
   })
 
