f178acb626cbf25adb2cc0902cfe164696975305
Raphaël Jakse <raphael.jakse@algoo.fr>
2020-03-09 13:57:46 +0100

    fix(frontend): fix the size of rotated images in the gallery app
    The size of the main image in the gallery is now computed using
    javascript when rotated. This is necessary because when rotating
    an image by an algo multiple of 90, its height becomes its width
    and vice-versa, so max-width: 100% and max-height:100% cannot be
    used anymore.
    
    Signed-off-by: Raphaël Jakse <raphael.jakse@algoo.fr>

--- frontend_app_gallery/src/component/MainPreview.jsx
+++ frontend_app_gallery/src/component/MainPreview.jsx
@@ -2,32 +2,88 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
-class MainPreview extends React.Component {
+export class MainPreview extends React.Component {
   constructor (props) {
     super(props)
-
+    this.imgContainer = React.createRef()
+    this.img = React.createRef()
     this.state = {
-      height: 0,
-      width: 0,
-      imageLoaded: false
+      imageLoaded: false,
+      style: {
+        maxWidth: '100%',
+        maxHeight: '100%',
+        width: '',
+        height: ''
+      }
     }
   }
 
   onLoad ({ target: img }) {
     this.setState({
-      height: img.height,
-      width: img.width,
       imageLoaded: true
     })
   }
 
+  fixImageSize () {
+    if (this.props.rotationAngle % 180) {
+      // if the image is rotated such as the height "becomes" the width,
+      // let us fix its maximum dimensions when the DOM is ready
+
+      window.requestAnimationFrame(() => {
+        const imgContainer = this.imgContainer.current
+        const img = this.img.current
+
+        const imgContainerStyle = window.getComputedStyle(imgContainer)
+
+        const imgContainerHeight = imgContainer.clientHeight - (
+          parseFloat(imgContainerStyle.paddingTop) +
+          parseFloat(imgContainerStyle.paddingBottom)
+        )
+
+        const imgContainerWidth = imgContainer.clientHeight - (
+          parseFloat(imgContainerStyle.paddingLeft) +
+          parseFloat(imgContainerStyle.paddingRight)
+        )
+
+        let width = Math.min(img.naturalWidth, imgContainerHeight)
+        let height = width * img.naturalHeight / img.naturalWidth
+
+        if (height > imgContainer.clientWidth) {
+          height = Math.min(img.naturalHeight, imgContainerWidth)
+          width = height * img.naturalWidth / img.naturalHeight
+        }
+
+        this.setState({
+          style: {
+            maxWidth: '',
+            maxHeight: '',
+            width: width,
+            height: height
+          }
+        })
+      })
+    } else if (this.state.style.maxWidth !== '100%') {
+      this.setState({
+        style: {
+          maxWidth: '100%',
+          maxHeight: '100%',
+          width: '',
+          height: ''
+        }
+      })
+    }
+  }
+
+  componentDidMount () {
+    this.fixImageSize()
+  }
+
+  componentDidUpdate () {
+    this.fixImageSize()
+  }
+
   render () {
     const { props, state } = this
-
-    let width = state.width
-    if (props.rotationAngle === 90 || props.rotationAngle === 270) {
-      width = state.height
-    }
 
     return (
       <div className='carousel__item__preview'>
@@ -37,14 +93,15 @@ class MainPreview extends React.Component {
               <i className='fa fa-spinner fa-spin gallery__loader__icon' />
             </div>
           )}
-          <div className='carousel__item__preview__content__image'>
+          <div className='carousel__item__preview__content__image' ref={this.imgContainer}>
             <img
               src={props.previewSrc}
               className={classnames(`rotate${props.rotationAngle}`, state.imageLoaded ? 'img-thumbnail' : null)}
               onClick={props.handleClickShowImageRaw}
               onLoad={this.onLoad.bind(this)}
-              width={width && state.width > width ? width : null}
               alt={props.fileName}
+              style={this.state.style}
+              ref={this.img}
             />
           </div>
         </span>
