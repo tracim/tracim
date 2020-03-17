6ea9548e74e51efa71261127620f7dd8c8268e9d
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-03-17 10:46:43 +0100

    [#2679] add a comment

--- frontend/dist/assets/tracim/tinymceInit.js
+++ frontend/dist/assets/tracim/tinymceInit.js
@@ -55,6 +55,7 @@
       content_style: 'div {height: 100%;}',
       setup: function ($editor) {
         $editor.on('init', function (e) {
+          // INFO - GM - 2020/03/17 - theses 3 lines enable autofocus at the end of the document
           $editor.focus()
           $editor.selection.select($editor.getBody(), true)
           $editor.selection.collapse(false)
