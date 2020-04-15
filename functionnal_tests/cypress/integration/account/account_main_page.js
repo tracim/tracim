aeb336062317aa9b69489d208dd4dcf257b34183
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-04-15 16:29:09 +0200

    Change Cypress Test

--- functionnal_tests/cypress/integration/account/account_main_page.js
+++ functionnal_tests/cypress/integration/account/account_main_page.js
@@ -1,3 +1,6 @@
+import { PAGES } from '../../support/urls_commands.js'
+import { SELECTORS as s } from '../../support/generic_selector_commands.js'
+
 describe('Account page', function () {
   before(() => {
     cy.resetDB()
@@ -6,19 +9,39 @@ describe('Account page', function () {
 
   beforeEach(function () {
     cy.loginAs('users')
-    cy.visit('/')
+    cy.visitPage({ pageName: PAGES.HOME })
   })
-  it("should be able it access it through header's button", () => {
-    cy.get('[data-cy=menuprofil__dropdown__button]').click()
-    cy.get('[data-cy=menuprofil__dropdown__account__link]').click()
-    cy.get('.userinfo')
+  describe("header's buttons", () => {
+    it("should be able it access it through header's buttons", () => {
+      cy.getTag({ selectorName: s.HEADER })
+        .find('[data-cy=menuprofil__dropdown__button]')
+        .click()
+
+      cy.get('[data-cy=menuprofil__dropdown__account__link]')
+        .click()
+
+      cy.get('.userinfo')
+    })
   })
 
-  it("Avatar should not be empty", () => {
-    cy.get('[data-cy=menuprofil__dropdown__button]').click()
-    cy.get('[data-cy=menuprofil__dropdown__account__link]').click()
-    cy.get('[data-cy=personaldata__form__txtinput__fullname]').type("      User FromOuterSpace    ")
-    cy.get('.personaldata__form__button').click()
-    cy.get('.avatar').contains("UF")
+  describe('Avatar', () => {
+    it('Avatar letter should be displayed correctly when the profile name has a name with spaces', () => {
+      cy.getTag({ selectorName: s.HEADER })
+        .find('[data-cy=menuprofil__dropdown__button]')
+        .click()
+
+      cy.get('[data-cy=menuprofil__dropdown__account__link]')
+        .click()
+
+      cy.get('[data-cy=personaldata__form__txtinput__fullname]')
+        .type("      User FromOuterSpace    ")
+
+      cy.get('.personaldata__form__button')
+        .click()
+
+      cy.getTag({ selectorName: s.HEADER })
+        .find('.avatar')
+        .contains("UF")
+    })
   })
 })
