b39cf12f99a491c2270ce4044a388362f01097fa
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-04-15 16:54:54 +0200

    Remove functionnal test because it is less revelant than unit test

--- functionnal_tests/cypress/integration/account/account_main_page.js
+++ functionnal_tests/cypress/integration/account/account_main_page.js
@@ -11,37 +11,15 @@ describe('Account page', function () {
     cy.loginAs('users')
     cy.visitPage({ pageName: PAGES.HOME })
   })
-  describe("header's buttons", () => {
-    it("should be able it access it through header's buttons", () => {
-      cy.getTag({ selectorName: s.HEADER })
-        .find('[data-cy=menuprofil__dropdown__button]')
-        .click()
 
-      cy.get('[data-cy=menuprofil__dropdown__account__link]')
-        .click()
+  it("should be able it access it through header's buttons", () => {
+    cy.getTag({ selectorName: s.HEADER })
+      .find('[data-cy=menuprofil__dropdown__button]')
+      .click()
 
-      cy.get('.userinfo')
-    })
-  })
+    cy.get('[data-cy=menuprofil__dropdown__account__link]')
+      .click()
 
-  describe('Avatar', () => {
-    it('Avatar letter should be displayed correctly when the profile name has a name with spaces', () => {
-      cy.getTag({ selectorName: s.HEADER })
-        .find('[data-cy=menuprofil__dropdown__button]')
-        .click()
-
-      cy.get('[data-cy=menuprofil__dropdown__account__link]')
-        .click()
-
-      cy.get('[data-cy=personaldata__form__txtinput__fullname]')
-        .type("      User FromOuterSpace    ")
-
-      cy.get('.personaldata__form__button')
-        .click()
-
-      cy.getTag({ selectorName: s.HEADER })
-        .find('.avatar')
-        .contains("UF")
-    })
+    cy.get('.userinfo')
   })
 })
