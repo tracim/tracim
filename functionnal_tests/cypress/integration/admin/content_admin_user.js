be9e13a653b7f021a79242a72f8e5d5516eaa1bb
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2020-04-22 14:47:55 +0200

    [#1855] add integration tests

--- functionnal_tests/cypress/integration/admin/content_admin_user.js
+++ functionnal_tests/cypress/integration/admin/content_admin_user.js
@@ -1,21 +1,25 @@
-// describe('navigation :: admin > user', function () {
-//    before(function () {
-//        //login
-//        cy.visit('/login')
-//        cy.get('input[type=email]').should('be.visible')
-//        cy.get('input[type=email]').type('admin@admin.admin')
-//        cy.get('input[type=password]').type('admin@admin.admin')
-//        cy.get('.connection__form__btnsubmit').click()
-//        cy.get('.adminlink__btn.dropdown-toggle').click()
-//        cy.get('a.setting__link[href="/admin/user"]').click()
-//        cy.url().should('include', '/admin/user')
-//    })
-//    after(function() {
-//        cy.get('#dropdownMenuButton').click()
-//        cy.get('div.setting__link').click()
-//        cy.url().should('include', '/login')
-//    })
-//    it ('', function(){
-//        cy.get('').should('')
-//    })
-// })
+import { PAGES } from '../../support/urls_commands'
+
+
+describe('An admin seeing a user profile', () => {
+  before(function () {
+    cy.resetDB()
+    cy.setupBaseDB()
+    cy.loginAs('administrators')
+    cy.fixture('baseWorkspace').as('workspace')
+    cy.createRandomUser()
+    cy.visitPage({pageName: PAGES.HOME})
+  })
+
+  afterEach(function () {
+    cy.cancelXHR()
+  })
+
+  it('should show the users agenda', () => {
+    cy.get('.adminlink').click()
+    cy.get('[data-cy=adminlink__user__link]').click()
+    cy.get('.adminUser__table__tr__td-link').first().click()
+    cy.get('[data-cy=menusubcomponent__list__agenda]').click()
+    cy.get('.agendaInfo__content__link').should('be.visible')
+  })
+})
