20c337095de85cf74c672c5124535f6877e1737b
Raphaël Jakse <raphael.jakse@univ-grenoble-alpes.fr>
2020-05-10 12:59:23 +0200

    parallel frontend build: spawn the right number of processes (configurable)

--- build_full_frontend.sh
+++ build_full_frontend.sh
@@ -6,10 +6,6 @@ GREEN='\033[1;32m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-if [ -z "${PARALLEL_BUILD+x}" ]; then
-    PARALLEL_BUILD=true
-fi
-
 if [ -z "${QUIET_BUILD+x}" ]; then
     export QUIET_BUILD=true
 fi
@@ -37,6 +33,76 @@ function build_app {
     ./build_*.sh $dev || logerror "Failed building $app."
 }
 
+function build_app_with_success {
+    build_app "$1" && loggood "Built $1 successfully"
+}
+
+##### Functions to manage parallel builds. #####
+
+# See https://stackoverflow.com/questions/6481005/how-to-obtain-the-number-of-cpus-cores-in-linux-from-the-command-line
+
+# initialize a semaphore with a given number of tokens
+function open_sem {
+    tmppipe="$(mktemp -u)"
+    mkfifo -m 600 "$tmppipe"
+    exec 3<>"$tmppipe"
+    rm "$tmppipe"
+    local i=$1
+    for((;i>0;i--)); do
+        printf %s 000 >&3
+    done
+}
+
+function parallel_build_failure {
+    kill $(jobs -p)
+    wait
+    exit 1
+}
+
+# run the given command asynchronously and pop/push tokens
+function run_with_lock {
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || parallel_build_failure
+    (
+        ( "$@"; )
+
+        # push the return code of the command to the semaphore
+        printf '%.3d' $? >&3
+    )&
+}
+
+##### #####
+
+function build_apps {
+    log "Building apps..."
+
+    # Initialize parallel build
+    if ! [ "$PARALLEL_BUILD" = 1 ]; then
+        open_sem "$PARALLEL_BUILD"
+    fi
+
+    # Loop over the apps
+    for app in "$DEFAULTDIR"/frontend_app_*; do
+        if [ "$PARALLEL_BUILD" = 1 ]; then
+            log "Building $app"
+            build_app $app
+        else
+            run_with_lock build_app_with_success "$app"
+        fi
+    done
+
+    # Loop over the apps
+    if ! [ "$PARALLEL_BUILD" = 1 ]; then
+        for app in "$DEFAULTDIR"/frontend_app_*; do
+            if ! wait -n 1; then
+                kill $(jobs -p)
+                exit 1
+            fi
+        done
+    fi
+}
+
 windoz=""
 if [[ $1 = "-w" || $2 = "-w" ]]; then
     windoz="windoz"
@@ -48,6 +114,14 @@ if [[ $1 = "-d" || $2 = "-d" ]]; then
 fi
 
 echo -e "\n${BROWN}/!\ ${NC}this script does not run 'yarn install'\n${BROWN}/!\ ${NC}"
+
+if [ -z "${PARALLEL_BUILD+x}" ]; then
+    PARALLEL_BUILD="$(which nproc > /dev/null && nproc --all || echo 1)"
+elif [ "${PARALLEL_BUILD}" = "false" ] || [ "${PARALLEL_BUILD}" = "0" ] ; then
+    PARALLEL_BUILD=1
+fi
+
+log "Number of parallel jobs for building apps: $PARALLEL_BUILD"
 
 DEFAULTDIR=$(pwd)
 export DEFAULTDIR
@@ -64,24 +138,7 @@ cd "$DEFAULTDIR/frontend_vendors"
 log "Building tracim_frontend_lib"
 yarn workspace tracim_frontend_lib run buildtracimlib$windoz && loggood "Built tracim_frontend_vendors successfully" || logerror "Failed to build tracim_frontend_lib"
 
-log "Building apps..."
-for app in "$DEFAULTDIR"/frontend_app_*; do
-    if [ "$PARALLEL_BUILD" = "true" ]; then
-        build_app $app && loggood "Built $app successfully" &
-    else
-        log "Building $app"
-        build_app $app
-    fi
-done
-
-if [ "$PARALLEL_BUILD" = "true" ]; then
-    for app in "$DEFAULTDIR"/frontend_app_*; do
-        if ! wait -n 1; then
-            kill $(jobs -p)
-            exit 1
-        fi
-    done
-fi
+build_apps
 
 # build the Frontend
 log "Building the Tracim frontend"
