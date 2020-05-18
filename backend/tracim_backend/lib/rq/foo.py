29e5770d7a627f9f3f520af05006b422a96e70e6
Sébastien Grignard <seb@amakaze.org>
2020-05-18 14:06:12 +0200

    chore(backend): remove useless file

--- backend/tracim_backend/lib/rq/foo.py
+++
@@ -1,9 +0,0 @@
-from .worker import worker_session
-
-
-def do_database_job() -> None:
-    from tracim_backend.models.event import Message
-
-    with worker_session() as session:
-        messages = session.query(Message).all()
-        print("We have {} messages in the database".format(len(messages)))
