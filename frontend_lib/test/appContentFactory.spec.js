2df9815f43362de137a379ce807aa2d36a24d057
Guénaël Muller <guenael.muller@algoo.fr>
2020-05-27 17:33:51 +0200

    remove /v2 from frontend files

--- frontend_lib/test/appContentFactory.spec.js
+++ frontend_lib/test/appContentFactory.spec.js
@@ -46,7 +46,7 @@ describe('appContentFactory.js', () => {
   }
   const appContentSlug = 'appContentSlugExample'
 
-  const fakeApiUrl = 'http://fake.url/api/v2'
+  const fakeApiUrl = 'http://fake.url/api'
 
   describe('The wrapped component', () => {
     it('should have all the new props', () => {
