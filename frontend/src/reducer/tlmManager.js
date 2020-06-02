85286f88628bb311fd666e963b18b6b542576d6b
Skylsmoi <come.huguies@gmail.com>
2020-06-02 12:07:49 +0200

    linting fix

--- frontend/src/reducer/tlmManager.js
+++ frontend/src/reducer/tlmManager.js
@@ -1,7 +1,7 @@
 import { SET, TLM_MANAGER } from '../action-creator.sync.js'
 
 export default function tlmManager (state = null, action) {
-  switch(action.type) {
+  switch (action.type) {
     case `${SET}/${TLM_MANAGER}`:
       return action.TLMManager
     default:
