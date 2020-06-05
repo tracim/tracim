75d462ddbbd0b177017fae121361927d0415b709
Raphaël Jakse <raphael.jakse@algoo.fr>
2020-06-05 09:14:22 +0200

    fix failing cypress tests

--- functionnal_tests/cypress/integration/home_page/translation.js
+++ functionnal_tests/cypress/integration/home_page/translation.js
@@ -1,20 +1,32 @@
 import { PAGES } from '../../support/urls_commands'
 
 describe('content :: home_page', function () {
-  before(() => {
+  it('should have translations when no workspace is present', () => {
+    cy.resetDB()
+    cy.loginAs('administrators')
+    cy.visitPage({ pageName: PAGES.HOME })
+
+    cy.get('#content').contains('Create a shared space')
+
+    cy.changeLanguage('fr')
+    cy.get('#content').contains('Créer un espace partagé')
+
+    cy.changeLanguage('pt')
+    cy.get('#content').contains('Criar um espaço partilhado')
+  })
+
+  it('should have translations', () => {
     cy.resetDB()
     cy.setupBaseDB()
     cy.loginAs('users')
     cy.visitPage({ pageName: PAGES.HOME })
-  })
 
-  it('should have translations', () => {
-    cy.get('.homepagecard__title').contains('Welcome to Tracim')
+    cy.get('#content').contains('Welcome to Tracim')
 
     cy.changeLanguage('fr')
-    cy.get('.homepagecard__title').contains('Bienvenue sur Tracim')
+    cy.get('#content').contains('Bienvenue sur Tracim')
 
     cy.changeLanguage('pt')
-    cy.get('.homepagecard__title').contains('Bem-vindo ao Tracim')
+    cy.get('#content').contains('Bem-vindo ao Tracim')
   })
 })
