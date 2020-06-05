1f21295bbb25bbf6ff13ba7c1f1946ddb71bd6a9
metzgegu <31687635+metzgegu@users.noreply.github.com>
2020-06-05 16:52:08 +0200

    feature(front): refactor TLM and custom Events at frontend_app_file
    ref: #3065

---
+++ frontend_lib/test/fixture/tracimLiveMessageData/commentTlm.js
@@ -0,0 +1,10 @@
+import { author } from '../author.js'
+
+export const commentTlm = {
+  is_editable: true,
+  content_id: 1,
+  parent_id: 2,
+  raw_content: 'Hello',
+  author,
+  created: '2012-02-23T10:28:43.511Z'
+}
