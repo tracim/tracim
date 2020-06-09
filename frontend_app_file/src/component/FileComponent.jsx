9639ac7e43c98e8c852c6817597413131ca176a9
Skylsmoi <11177014+Skylsmoi@users.noreply.github.com>
2020-06-09 11:57:24 +0200

    fix(front): video player of app file properly change video source (#3152)

--- frontend_app_file/src/component/FileComponent.jsx
+++ frontend_app_file/src/component/FileComponent.jsx
@@ -13,120 +13,147 @@ import { ALLOWED_VIDEO_MIME_TYPE_LIST } from '../helper.js'
 
 const color = require('color')
 
-export const FileComponent = props => (
-  <div className={classnames(
-    'file__contentpage__statewrapper',
-    { 'displayState': props.isArchived || props.isDeleted || props.isDeprecated }
-  )}>
-    <div style={{ visibility: 'hidden' }} ref={props.myForwardedRef} />
+export class FileComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.videoPlayerRef = React.createRef()
+  }
 
-    {props.isArchived && (
-      <DisplayState
-        msg={props.t('This content is archived')}
-        btnType='button'
-        icon='archive'
-        btnLabel={props.t('Restore')}
-        onClickBtn={props.onClickRestoreArchived}
-      />
-    )}
+  componentDidUpdate (prevProps) {
+    const { props } = this
 
-    {props.isDeleted && (
-      <DisplayState
-        msg={props.t('This content is deleted')}
-        btnType='button'
-        icon='trash'
-        btnLabel={props.t('Restore')}
-        onClickBtn={props.onClickRestoreDeleted}
-      />
-    )}
+    if (prevProps.previewVideo && !props.previewVideo) this.unLoadVideoPlayer()
+    else if (!prevProps.previewVideo && props.previewVideo && ALLOWED_VIDEO_MIME_TYPE_LIST.includes(props.mimeType)){
+      this.loadVideoPlayer(props.downloadRawUrl, props.mimeType)
+    }
+  }
 
-    {props.isDeprecated && (
-      <DisplayState
-        msg={props.t('This content is deprecated')}
-        icon={props.deprecatedStatus.faIcon}
-      />
-    )}
+  loadVideoPlayer (videoUrl, videoMimeType) {
+    const video = document.createElement('video')
+    video.controls = true
+    video.preload = 'metadata'
 
-    {(props.mode === APP_FEATURE_MODE.VIEW || props.mode === APP_FEATURE_MODE.REVISION) && (
-      <PreviewComponent
-        color={props.customColor}
-        downloadRawUrl={props.downloadRawUrl}
-        isPdfAvailable={props.isPdfAvailable}
-        isJpegAvailable={props.isJpegAvailable}
-        downloadPdfPageUrl={props.downloadPdfPageUrl}
-        downloadPdfFullUrl={props.downloadPdfFullUrl}
-        previewUrl={props.previewUrl}
-        filePageNb={props.filePageNb}
-        fileCurrentPage={props.fileCurrentPage}
-        lightboxUrlList={props.lightboxUrlList}
-        onClickPreviousPage={props.onClickPreviousPage}
-        onClickNextPage={props.onClickNextPage}
-      />
-    )}
+    const source = document.createElement('source')
+    source.src = videoUrl
+    source.type = videoMimeType
 
-    {ALLOWED_VIDEO_MIME_TYPE_LIST.includes(props.mimeType) && (
-      <div
-        className={classnames('file__previewVideo', props.previewVideo ? 'open' : '')}
-        style={{ visibility: props.previewVideo ? 'visible' : 'hidden' }}
-        onClick={props.onClickClosePreviewVideo}
-      >
-        <video controls>
-          <source
-            src={props.downloadRawUrl}
-            type={props.mimeType}
+    video.appendChild(source)
+    this.videoPlayerRef.current.appendChild(video)
+  }
+
+  unLoadVideoPlayer () {
+    this.videoPlayerRef.current.innerHTML = ''
+  }
+
+  render () {
+    const { props } = this
+    return (
+      <div className={classnames(
+        'file__contentpage__statewrapper',
+        { 'displayState': props.isArchived || props.isDeleted || props.isDeprecated }
+      )}>
+        <div style={{ visibility: 'hidden' }} ref={props.myForwardedRef} />
+
+        {props.isArchived && (
+          <DisplayState
+            msg={props.t('This content is archived')}
+            btnType='button'
+            icon='archive'
+            btnLabel={props.t('Restore')}
+            onClickBtn={props.onClickRestoreArchived}
           />
-          {props.t("Unfortunately, your browser can't play the video.")}
-        </video>
-      </div>
-    )}
+        )}
 
-    {props.mode === APP_FEATURE_MODE.EDIT &&
-      <div className='file__contentpage__dropzone'>
-        {props.progressUpload.display &&
-          <PopupProgressUpload
+        {props.isDeleted && (
+          <DisplayState
+            msg={props.t('This content is deleted')}
+            btnType='button'
+            icon='trash'
+            btnLabel={props.t('Restore')}
+            onClickBtn={props.onClickRestoreDeleted}
+          />
+        )}
+
+        {props.isDeprecated && (
+          <DisplayState
+            msg={props.t('This content is deprecated')}
+            icon={props.deprecatedStatus.faIcon}
+          />
+        )}
+
+        {(props.mode === APP_FEATURE_MODE.VIEW || props.mode === APP_FEATURE_MODE.REVISION) && (
+          <PreviewComponent
             color={props.customColor}
-            percent={props.progressUpload.percent}
-            filename={props.newFile ? props.newFile.name : ''}
+            downloadRawUrl={props.downloadRawUrl}
+            isPdfAvailable={props.isPdfAvailable}
+            isJpegAvailable={props.isJpegAvailable}
+            downloadPdfPageUrl={props.downloadPdfPageUrl}
+            downloadPdfFullUrl={props.downloadPdfFullUrl}
+            previewUrl={props.previewUrl}
+            filePageNb={props.filePageNb}
+            fileCurrentPage={props.fileCurrentPage}
+            lightboxUrlList={props.lightboxUrlList}
+            onClickPreviousPage={props.onClickPreviousPage}
+            onClickNextPage={props.onClickNextPage}
           />
-        }
+        )}
 
-        <FileDropzone
-          onDrop={props.onChangeFile}
-          onClick={props.onChangeFile}
-          hexcolor={props.customColor}
-          preview={props.newFilePreview}
-          filename={props.newFile ? props.newFile.name : ''}
+        <div
+          className={classnames('file__previewVideo', props.previewVideo ? 'open' : '')}
+          style={{ display: props.previewVideo ? 'flex' : 'none' }}
+          onClick={props.onClickClosePreviewVideo}
+          ref={this.videoPlayerRef}
         />
 
-        <div className='file__contentpage__dropzone__btn'>
-          <button
-            type='button'
-            className='file__contentpage__dropzone__btn__cancel btn outlineTextBtn nohover'
-            style={{ borderColor: props.customColor }}
-            onClick={props.onClickDropzoneCancel}
-          >
-            {props.t('Cancel')}
-          </button>
+        {props.mode === APP_FEATURE_MODE.EDIT && (
+          <div className='file__contentpage__dropzone'>
+            {props.progressUpload.display && (
+              <PopupProgressUpload
+                color={props.customColor}
+                percent={props.progressUpload.percent}
+                filename={props.newFile ? props.newFile.name : ''}
+              />
+            )}
 
-          <button
-            type='button'
-            className='file__contentpage__dropzone__btn__validate btn highlightBtn'
-            style={{
-              backgroundColor: props.customColor,
-              ':hover': {
-                backgroundColor: color(props.customColor).darken(0.15).hex()
-              }
-            }}
-            onClick={props.onClickDropzoneValidate}
-            disabled={props.newFile === ''}
-          >
-            {props.t('Validate')}
-          </button>
-        </div>
+            <FileDropzone
+              onDrop={props.onChangeFile}
+              onClick={props.onChangeFile}
+              hexcolor={props.customColor}
+              preview={props.newFilePreview}
+              filename={props.newFile ? props.newFile.name : ''}
+            />
+
+            <div className='file__contentpage__dropzone__btn'>
+              <button
+                type='button'
+                className='file__contentpage__dropzone__btn__cancel btn outlineTextBtn nohover'
+                style={{ borderColor: props.customColor }}
+                onClick={props.onClickDropzoneCancel}
+              >
+                {props.t('Cancel')}
+              </button>
+
+              <button
+                type='button'
+                className='file__contentpage__dropzone__btn__validate btn highlightBtn'
+                style={{
+                  backgroundColor: props.customColor,
+                  ':hover': {
+                    backgroundColor: color(props.customColor).darken(0.15).hex()
+                  }
+                }}
+                onClick={props.onClickDropzoneValidate}
+                disabled={props.newFile === ''}
+              >
+                {props.t('Validate')}
+              </button>
+            </div>
+          </div>
+        )}
       </div>
-    }
-  </div>
-)
+    )
+  }
+}
 
 // INFO - CH - 2019-09-13 - FileComponentWithHOC const is used to be able to forward the ref though HOC
 const FileComponentWithHOC = translate()(Radium(FileComponent))
