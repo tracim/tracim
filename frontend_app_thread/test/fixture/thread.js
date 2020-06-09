f2baf961e7c6a5467908d7b3d2aab3d5609d7598
Skylsmoi <11177014+Skylsmoi@users.noreply.github.com>
2020-06-09 10:11:25 +0200

    feature(front): implement TLM for app Thread

---
+++ frontend_app_thread/test/fixture/thread.js
@@ -0,0 +1,37 @@
+export const thread = {
+  current_revision_type: 'undeletion',
+  slug: 'bonjour-deleted-2020-06-04t16-47-03-091795',
+  content_namespace: 'content',
+  modified: '2020-06-04T14:47:06Z',
+  parent_id: null,
+  author: {
+    user_id: 1,
+    username: 'TheAdmin',
+    avatar_url: null,
+    public_name: 'Global manager'
+  },
+  raw_content: '',
+  is_deleted: false,
+  filename: 'bonjour.thread.html',
+  workspace_id: 1,
+  actives_shares: 0,
+  created: '2020-06-04T14:20:14Z',
+  content_type: 'thread',
+  last_modifier: {
+    user_id: 2,
+    username: 'mrlapin',
+    avatar_url: null,
+    public_name: 'mrlapin@mrlapin.mrlapin'
+  },
+  is_editable: true,
+  label: 'bonjour',
+  status: 'open',
+  is_archived: false,
+  current_revision_id: 12,
+  content_id: 1,
+  show_in_ui: true,
+  sub_content_types: [
+    'comment'
+  ],
+  file_extension: '.thread.html'
+}
