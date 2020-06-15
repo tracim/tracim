81c40b1a0b8a3d37aac2dd27a53c37a06ded82e5
METZGER Guillaume <guillaume.metzger5@gmail.com>
2020-06-15 12:34:13 +0200

    [#3116] Update timeline when a file is modified, deleted or undeleted. Add unit test cases for this and remove `is_deleted: true/false` when deleting or restoring a content because TLM datas has already this field

--- frontend_app_file/src/container/File.jsx
+++ frontend_app_file/src/container/File.jsx
@@ -36,7 +36,8 @@ import {
   APP_FEATURE_MODE,
   computeProgressionPercentage,
   FILE_PREVIEW_STATE,
-  sortTimelineByDate
+  sortTimelineByDate,
+  addRevisionFromTLM
 } from 'tracim_frontend_lib'
 import { PAGE } from '../helper.js'
 import { debug } from '../debug.js'
@@ -157,7 +158,8 @@ export class File extends React.Component {
         lightboxUrlList: (new Array(data.content.page_nb)).fill(null).map((n, i) => i + 1).map(pageNb => // create an array [1..revision.page_nb]
           buildFilePreviewUrl(state.config.apiUrl, state.content.workspace_id, data.content.content_id, data.content.current_revision_id, filenameNoExtension, pageNb, 1920, 1080)
         )
-      }
+      },
+      timeline: addRevisionFromTLM(data, prev.timeline, prev.loggedUser.lang)
     }))
   }
 
@@ -185,10 +187,10 @@ export class File extends React.Component {
       ({
         content: {
           ...prev.content,
-          ...data.content,
-          is_deleted: true
+          ...data.content
         },
-        mode: APP_FEATURE_MODE.VIEW
+        mode: APP_FEATURE_MODE.VIEW,
+        timeline: addRevisionFromTLM(data, prev.timeline, prev.loggedUser.lang)
       })
     )
   }
@@ -199,16 +201,13 @@ export class File extends React.Component {
 
     this.sendGlobalFlashMessage(props.t('File has been restored'), 'info')
 
-    this.setState(prev =>
-      ({
-        content:
-          {
-            ...prev.content,
-            ...data.content,
-            is_deleted: false
-          }
-      })
-    )
+    this.setState(prev => ({
+      content: {
+        ...prev.content,
+        ...data.content
+      },
+      timeline: addRevisionFromTLM(data, prev.timeline, prev.loggedUser.lang)
+    }))
   }
 
   async componentDidMount () {
