1a452e92d70b4172acf6f239d015aa83deb76c99
Sébastien Grignard <seb@amakaze.org>
2020-06-15 16:11:19 +0200

    test(frontend): complete tests for admin workspace

--- functionnal_tests/cypress/integration/admin/content_admin_workspace.js
+++ functionnal_tests/cypress/integration/admin/content_admin_workspace.js
@@ -1,12 +1,11 @@
+import { PAGES } from '../../support/urls_commands'
+
 describe('content :: admin > workspace', function () {
-  before(() => {
+  beforeEach(function () {
     cy.resetDB()
     cy.setupBaseDB()
-  })
-
-  beforeEach(function () {
     cy.loginAs('administrators')
-    cy.visit('/ui/admin/workspace')
+    cy.visitPage({ pageName: PAGES.ADMIN_WORKSPACE })
   })
   it('checks the visibility of crucial elements', function () {
     cy.get('.adminWorkspace__description').should('be.visible')
@@ -16,11 +15,43 @@ describe('content :: admin > workspace', function () {
     // homepage if we remove wait.
     cy.wait(2000)
   })
-  it('content of workspaceTable', function () {
+  it('checks the columns of the workspace table', function () {
     cy.get('.adminWorkspace__workspaceTable th:nth-child(1)[scope="col"]').should('be.visible')
     cy.get('.adminWorkspace__workspaceTable th:nth-child(2)[scope="col"]').should('be.visible')
     cy.get('.adminWorkspace__workspaceTable th:nth-child(3)[scope="col"]').should('be.visible')
     cy.get('.adminWorkspace__workspaceTable th:nth-child(4)[scope="col"]').should('be.visible')
     cy.get('.adminWorkspace__workspaceTable th:nth-child(5)[scope="col"]').should('be.visible')
+  })
+  it('checks the delete workspace button', function() {
+    cy.get('.adminWorkspace__workspaceTable tbody tr:first .adminWorkspace__table__delete__icon').click()
+
+    // Cancel
+    cy.get('.adminworkspaceuser__popup').should('be.visible')
+    cy.get('.adminworkspaceuser__popup .outlineTextBtn').click()
+    cy.get('.adminWorkspace__workspaceTable tbody tr:first td:nth-child(2)').contains('My Workspace')
+
+    // Delete
+    cy.get('.adminWorkspace__workspaceTable tbody tr:first .adminWorkspace__table__delete__icon').click()
+    cy.get('.adminworkspaceuser__popup .highlightBtn').click()
+    cy.get('.adminWorkspace__workspaceTable tbody tr:first td:nth-child(2)').contains('There is no shared space yet')
+  })
+  it('checks the create workspace button', function() {
+    cy.get('.adminWorkspace__btnnewworkspace__btn').click()
+    cy.get('.createcontent__form__input').click().type('A test workspace')
+    cy.get('button.createcontent__form__button').click()
+    cy.location('pathname').should('be.equal', '/ui/workspaces/2/dashboard')
+  })
+  it('checks workspace modified TLM', function() {
+    cy.request('PUT', 'api/workspaces/1', { label: 'Modified workspace', description: ''})
+    cy.wait(500)
+    cy.get('.adminWorkspace__workspaceTable tbody tr:first td:nth-child(2)').contains('Modified workspace')
+  })
+  it('checks workspace added and deleted TLM', function() {
+    cy.request('POST', 'api/workspaces', { label: 'A new workspace', description: ''})
+    cy.wait(500)
+    cy.get('.adminWorkspace__workspaceTable tbody').find('tr').its('length').should('eq', 2)
+    cy.request('PUT', 'api/workspaces/2/trashed')
+    cy.wait(500)
+    cy.get('.adminWorkspace__workspaceTable tbody').find('tr').its('length').should('eq', 1)
   })
 })
