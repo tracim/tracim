540ee5b5d17e3450ad23c70e66941ed53dfd654e
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2020-06-18 15:02:20 +0200

    update fixture and code details

--- frontend_app_workspace_advanced/src/container/WorkspaceAdvanced.jsx
+++ frontend_app_workspace_advanced/src/container/WorkspaceAdvanced.jsx
@@ -172,7 +172,7 @@ export class WorkspaceAdvanced extends React.Component {
 
   componentDidUpdate (prevProps, prevState) {
     const { state } = this
-    console.log('%c<WorkspaceAdvanced> did update', `color: ${state.config.hexcolor}`, prevState, state)
+    // console.log('%c<WorkspaceAdvanced> did update', `color: ${state.config.hexcolor}`, prevState, state)
     if (prevState.content && state.content && prevState.content.workspace_id !== state.content.workspace_id) {
       this.loadContent()
     }
@@ -227,6 +227,7 @@ export class WorkspaceAdvanced extends React.Component {
 
   handleClickBtnCloseApp = () => {
     this.setState({ isVisible: false })
+    GLOBAL_dispatchEvent({ type: CUSTOM_EVENT.APP_CLOSED, data: {} })
   }
 
   handleSaveEditLabel = async newLabel => {
--- frontend_lib/test/fixture/workspace.js
+++ frontend_lib/test/fixture/workspace.js
@@ -1,4 +1,5 @@
 import { author } from './author.js'
+import { sidebarEntryList } from './sidebarEntryList.js'
 
 export const workspace = {
   agenda_enabled: true,
@@ -9,51 +10,7 @@ export const workspace = {
   owner: author,
   public_download_enabled: true,
   public_upload_enabled: true,
-  sidebar_entries: [
-    {
-      slug: 'dashboard',
-      route: '/ui/workspaces/6/dashboard',
-      hexcolor: '#fdfdfd',
-      fa_icon: 'home',
-      label: 'Dashboard'
-    }, {
-      slug: 'contents/all',
-      route: '/ui/workspaces/6/contents',
-      hexcolor: '#bbbbbb',
-      fa_icon: 'th',
-      label: 'All Contents'
-    }, {
-      slug: 'contents/thread',
-      route: '/ui/workspaces/6/contents?type=thread',
-      hexcolor: '#428BCA',
-      fa_icon: 'comments-o',
-      label: 'Threads'
-    }, {
-      slug: 'contents/file',
-      route: '/ui/workspaces/6/contents?type=file',
-      hexcolor: '#ffa500',
-      fa_icon: 'paperclip',
-      label: 'Files'
-    }, {
-      slug: 'contents/html-document',
-      route: '/ui/workspaces/6/contents?type=html-document',
-      hexcolor: '#00CC00',
-      fa_icon: 'file-text-o',
-      label: 'Text Documents'
-    }, {
-      slug: 'agenda',
-      route: '/ui/workspaces/6/agenda',
-      hexcolor: '#ff4b3a',
-      fa_icon: 'calendar',
-      label: 'Agenda'
-    }, {
-      slug: 'gallery',
-      route: '/ui/workspaces/6/gallery',
-      hexcolor: '#7d4e24',
-      fa_icon: 'picture-o',
-      label: 'Gallery'
-    }
-  ],
+  sidebar_entries: sidebarEntryList,
   slug: 'slug',
   workspace_id: 1
 }
