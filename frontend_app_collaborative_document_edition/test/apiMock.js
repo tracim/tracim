6daf2cdc7f44bb1faa201ba21c282e8a0e265b77
Skylsmoi <11177014+Skylsmoi@users.noreply.github.com>
2020-06-19 16:18:46 +0200

    Update frontend_app_collaborative_document_edition/test/apiMock.js
    Co-authored-by: gbonaspetti <35422692+gbonaspetti@users.noreply.github.com>

--- frontend_app_collaborative_document_edition/test/apiMock.js
+++ frontend_app_collaborative_document_edition/test/apiMock.js
@@ -2,7 +2,7 @@ const nock = require('nock')
 
 const mockGetCollaborativeDocumentTemplates200 = (apiUrl) => {
   return nock(apiUrl)
-    .get(`/collaborative-document-edition/templates`)
+    .get('/collaborative-document-edition/templates')
     .reply(200, {
       file_templates: ["default.odp", "default.ods", "default.odt"]
     })
