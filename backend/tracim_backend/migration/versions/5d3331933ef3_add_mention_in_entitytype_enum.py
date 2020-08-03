9f08b7c9b85b8d1079d901dc91f1305bc1827f96
Sébastien Grignard <sebastien.grignard@algoo.fr>
2020-08-03 08:57:06 +0200

    tests(backend): correct migration script for PostgreSQL

--- backend/tracim_backend/migration/versions/5d3331933ef3_add_mention_in_entitytype_enum.py
+++ backend/tracim_backend/migration/versions/5d3331933ef3_add_mention_in_entitytype_enum.py
@@ -5,6 +5,8 @@ Revises: fb2ae8c604ac
 Create Date: 2020-07-30 09:03:37.687747
 
 """
+import typing
+
 from alembic import op
 import sqlalchemy as sa
 
@@ -18,7 +20,13 @@ old_entity_type_enum = sa.Enum(*old_entity_types, name="entitytype")
 new_entity_type_enum = sa.Enum(*(old_entity_types + ("MENTION",)), name="entitytype")
 
 
-def replace_enum(table_name: str, column_name: str, from_enum: sa.Enum, to_enum: sa.Enum) -> None:
+def replace_enum(
+    table_name: str,
+    column_name: str,
+    from_enum: sa.Enum,
+    to_enum: sa.Enum,
+    default_value: typing.Any,
+) -> None:
     if op.get_context().dialect.name == "postgresql":
         op.execute("ALTER TYPE {} RENAME TO {}_old".format(from_enum.name, from_enum.name))
         op.execute("ALTER TABLE {} alter {} drop default".format(table_name, column_name))
@@ -28,7 +36,7 @@ def replace_enum(table_name: str, column_name: str, from_enum: sa.Enum, to_enum:
                 column_name,
                 type_=to_enum,
                 postgresql_using="{}::text::{}".format(column_name, to_enum.name),
-                server_default="INTERNAL",
+                server_default=default_value,
             )
         op.execute("DROP TYPE {}_old".format(from_enum.name))
     else:
@@ -38,8 +46,10 @@ def replace_enum(table_name: str, column_name: str, from_enum: sa.Enum, to_enum:
 
 
 def upgrade() -> None:
-    replace_enum("events", "entity_type", old_entity_type_enum, new_entity_type_enum)
+    replace_enum("events", "entity_type", old_entity_type_enum, new_entity_type_enum, None)
 
 
 def downgrade() -> None:
-    replace_enum("events", "entity_type", new_entity_type_enum, old_entity_type_enum)
+    replace_enum(
+        "events", "entity_type", new_entity_type_enum, old_entity_type_enum, None,
+    )
