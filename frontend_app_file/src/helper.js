567203a9928a1a7907f6de6a204bad5926d686c7
Sébastien Grignard <seb@amakaze.org>
2020-08-07 21:20:32 +0200

    chore(frontend): cleaning duplicated code during merge with master [skip ci pytest]

--- frontend_app_file/src/helper.js
+++ frontend_app_file/src/helper.js
@@ -17,13 +17,3 @@ export const isVideoMimeTypeAndIsAllowed = (mimeType, disallowedMimeTypeList) =>
   if (!mimeType || !mimeType.startsWith('video/')) return false
   return !disallowedMimeTypeList.includes(mimeType)
 }
-
-export const DISALLOWED_VIDEO_MIME_TYPE_LIST = [
-  // INFO - CH - 2020-06-11 - put mime types that you don't handle here
-  // example 'video/webm',
-]
-
-export const isVideoMimeTypeAndIsAllowed = (mimeType, disallowedMimeTypeList) => {
-  if (!mimeType || !mimeType.startsWith('video/')) return false
-  return !disallowedMimeTypeList.includes(mimeType)
-}
