ec6dec36bd57b5fa12aa61977e6fb1dcb566217b
gbonaspetti <35422692+gbonaspetti@users.noreply.github.com>
2020-08-24 10:19:13 +0200

    Update frontend_app_thread/src/container/Thread.jsx [skip ci]
    Co-authored-by: raphj <3817365+raphj@users.noreply.github.com>

--- frontend_app_thread/src/container/Thread.jsx
+++ frontend_app_thread/src/container/Thread.jsx
@@ -288,7 +288,7 @@ export class Thread extends React.Component {
     try {
       props.appContentSaveNewComment(state.content, state.timelineWysiwyg, state.newComment, this.setState.bind(this), state.config.slug)
     } catch (e) {
-      this.sendGlobalFlashMessage(e.message ? e.message : props.t('Error while saving the comment'))
+      this.sendGlobalFlashMessage(e.message || props.t('Error while saving the comment'))
     }
   }
 
