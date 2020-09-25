3cf86892cbd70686a3e2c6c8d7eac9b7018e5297
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2020-09-25 16:22:22 +0200

    update tests [skip ci pytest]

--- frontend/test/src/component/DropdownLang.spec.js
+++ frontend/test/src/component/DropdownLang.spec.js
@@ -28,21 +28,13 @@ describe('<DropdownLang />', () => {
   const wrapper = shallow(<DropdownLang {...props} />)
 
   describe('static design', () => {
-    it(`should display the icon of the current active lang: ${props.langActiveId}`, () =>
-      expect(wrapper.find('img.dropdownlang__dropdown__btnlanguage__imgselected').prop('src')).to.equal(props.langList[0].icon)
-    )
-
-    it(`should display the label of the current active lang: ${props.langActiveId}`, () =>
-      expect(wrapper.find('button.dropdownlang__dropdown__btnlanguage')).to.text().contains(props.langList[0].label)
-    )
-
     it(`should display ${props.langList.length - 1} dropdown link for the others languages`, () => {
-      expect(wrapper.find('div.dropdownlang__dropdown__subdropdown__link').length).to.equal(props.langList.length - 1)
+      expect(wrapper.find('button.transparentButton').length).to.equal(props.langList.length - 1)
     })
 
     it('should display the label of each other language', () => {
       for (let i = 1; i < props.langList.length; i++) {
-        expect(wrapper.find('div.dropdownlang__dropdown__subdropdown__link').at(i - 1)).to.text().equal(props.langList[i].label)
+        expect(wrapper.find('button.transparentButton').at(i - 1)).to.text().equal(props.langList[i].label)
       }
     })
 
@@ -55,7 +47,7 @@ describe('<DropdownLang />', () => {
 
   describe('handler', () => {
     it('onChangeLangCallBack should be called when a language link is clicked', () => {
-      wrapper.find('div.dropdownlang__dropdown__subdropdown__link').at(0).simulate('click')
+      wrapper.find('button.transparentButton').at(0).simulate('click')
       expect(onChangeLangCallBack.called).to.equal(true)
     })
   })
