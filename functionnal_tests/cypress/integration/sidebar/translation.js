d0092a470f99c679ed51444b9e005b08e41f6c8e
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2020-10-05 11:26:02 +0200

    update cypress tests

--- functionnal_tests/cypress/integration/sidebar/translation.js
+++ functionnal_tests/cypress/integration/sidebar/translation.js
@@ -9,14 +9,17 @@ describe('content :: home_page', function () {
   })
 
   it('should have translations', () => {
+    cy.get('.sidebar__content__navigation__workspace__item').should('be.visible').click()
     cy.get('.sidebar__content__navigation__workspace__item__menu').should('be.visible').click()
     cy.get('li').contains('All Contents').should('be.visible')
 
     cy.changeLanguage('fr')
+    cy.get('.sidebar__content__navigation__workspace__item').should('be.visible').click()
     cy.get('.sidebar__content__navigation__workspace__item__menu').should('be.visible').click()
     cy.get('li').contains('Tous les contenus').should('be.visible')
 
     cy.changeLanguage('pt')
+    cy.get('.sidebar__content__navigation__workspace__item').should('be.visible').click()
     cy.get('.sidebar__content__navigation__workspace__item__menu').should('be.visible').click()
     cy.get('li').contains('Todos os conteúdos').should('be.visible')
   })
