611660eda920e46444c188e3e9625377ccb81136
Raphaël Jakse <raphael.jakse@algoo.fr>
2020-10-15 00:32:28 +0200

    more readable optimized vendors externals lines

--- frontend_app_thread/webpack.optimized.config.js
+++ frontend_app_thread/webpack.optimized.config.js
@@ -1,2 +1,12 @@
 module.exports = require('./webpack.config.js')
-module.exports.externals = { tracim_frontend_lib: 'tracim_frontend_lib.lib', ...Object.fromEntries(Object.keys(require('tracim_frontend_vendors')).map(dep => [dep, "tracim_frontend_vendors['" + dep + "']"])) }
+
+const optimizedVendors = Object.fromEntries(
+  Object.keys(require('tracim_frontend_vendors')).map(
+    dep => [dep, `tracim_frontend_vendors['${dep}']`]
+  )
+)
+
+module.exports.externals = {
+    tracim_frontend_lib: 'tracim_frontend_lib.lib',
+    ...optimizedVendors
+}
--- frontend_lib/webpack.optimized.config.js
+++ frontend_lib/webpack.optimized.config.js
@@ -1,5 +1,12 @@
 module.exports = require('./webpack.config.js')
-module.exports.externals = Object.fromEntries(Object.keys(require('tracim_frontend_vendors')).map(dep => [dep, "tracim_frontend_vendors['" + dep + "']"]))
+
+const optimizedVendors = Object.fromEntries(
+  Object.keys(require('tracim_frontend_vendors')).map(
+    dep => [dep, `tracim_frontend_vendors['${dep}']`]
+  )
+)
+
+module.exports.externals = optimizedVendors
 
 Object.assign(module.exports.output, {
   libraryTarget: 'var',
