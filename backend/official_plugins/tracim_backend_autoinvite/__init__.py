8bf94d3c28db341b2a0c6130c61e77c3ee7273de
Guénaël Muller <guenael.muller@algoo.fr>
2020-10-21 16:17:56 +0200

    pre-commit on plugins too

--- backend/official_plugins/tracim_backend_autoinvite/__init__.py
+++ backend/official_plugins/tracim_backend_autoinvite/__init__.py
@@ -1,7 +1,5 @@
 from pluggy import PluginManager
-from pyramid.config import Configurator
-from tracim_backend.config import CFG
-from tracim_backend import logger
+
 from tracim_backend.lib.core.plugins import hookimpl
 from tracim_backend.lib.core.user import UserApi
 from tracim_backend.lib.core.userworkspace import RoleApi
@@ -24,16 +22,17 @@ class HookImpl:
             session=context.dbsession,
             config=context.app_config,
             current_user=None,
-            access_types_filter=[WorkspaceAccessType.OPEN]
+            access_types_filter=[WorkspaceAccessType.OPEN],
         ).get_all()
-        rapi = RoleApi(
-            session=context.dbsession,
-            config=context.app_config,
-            current_user=None
-        )
+        rapi = RoleApi(session=context.dbsession, config=context.app_config, current_user=None)
         for workspace in open_workspaces:
-            rapi.create_one(user=user, workspace=workspace,
-                            role_level=workspace.default_user_role.level, with_notif=True, flush=False)
+            rapi.create_one(
+                user=user,
+                workspace=workspace,
+                role_level=workspace.default_user_role.level,
+                with_notif=True,
+                flush=False,
+            )
 
     @hookimpl
     def on_workspace_created(self, workspace: Workspace, context: TracimContext) -> None:
@@ -42,18 +41,18 @@ class HookImpl:
         """
         if workspace.access_type == WorkspaceAccessType.OPEN:
             all_users = UserApi(
-                session=context.dbsession,
-                config=context.app_config,
-                current_user=None
+                session=context.dbsession, config=context.app_config, current_user=None
             ).get_all()
-            rapi = RoleApi(
-                session=context.dbsession,
-                config=context.app_config,
-                current_user=None
-            )
+            rapi = RoleApi(session=context.dbsession, config=context.app_config, current_user=None)
             for user in all_users:
                 if user != workspace.owner:
-                    rapi.create_one(user=user, workspace=workspace, role_level=workspace.default_user_role.level, with_notif=True, flush=False)
+                    rapi.create_one(
+                        user=user,
+                        workspace=workspace,
+                        role_level=workspace.default_user_role.level,
+                        with_notif=True,
+                        flush=False,
+                    )
 
 
 def register_tracim_plugin(plugin_manager: PluginManager):
