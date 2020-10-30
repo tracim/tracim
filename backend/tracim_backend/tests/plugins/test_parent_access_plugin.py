1a12a77e5c60f28d4764e4f84e29f802aabc8e89
asf-algoo <71391182+asf-algoo@users.noreply.github.com>
2020-10-30 12:04:56 +0100

    fix(backend): always add user to all ancestors (#3762)
    Refs #3752 
    
    Co-authored-by: Sébastien Grignard <seb@amakaze.org>

--- backend/tracim_backend/tests/plugins/test_parent_access_plugin.py
+++ backend/tracim_backend/tests/plugins/test_parent_access_plugin.py
@@ -43,3 +43,42 @@ class TestParentAccessPlugin(object):
             assert grandson_workspace.get_user_role(user_1) == WorkspaceRoles.CONTENT_MANAGER.level
             assert child_workspace.get_user_role(user_1) == WorkspaceRoles.CONTRIBUTOR.level
             assert parent_workspace.get_user_role(user_1) == WorkspaceRoles.READER.level
+
+    def test__add_new_user_to_parent_workspaces__ok__user_in_child_space(
+        self,
+        admin_user,
+        session,
+        app_config,
+        workspace_api_factory,
+        role_api_factory,
+        user_api_factory,
+        load_parent_access_plugin,
+    ):
+        """
+            Test if new users are correctly added to open workspace with auto_invite_plugin enabled
+            Case when the user is already present in the child workspace
+            """
+        wapi = workspace_api_factory.get()
+        parent_workspace = wapi.create_workspace(
+            label="parent", default_user_role=WorkspaceRoles.READER
+        )
+        child_workspace = wapi.create_workspace(
+            label="child", parent=parent_workspace, default_user_role=WorkspaceRoles.CONTRIBUTOR
+        )
+        grandson_workspace = wapi.create_workspace(
+            label="grandson", parent=child_workspace, default_user_role=WorkspaceRoles.READER
+        )
+        uapi = user_api_factory.get()
+        user_1 = uapi.create_user(
+            email="u.1@u.u", auth_type=AuthType.INTERNAL, do_save=True, do_notify=False
+        )
+        role_api = role_api_factory.get()
+        role_api.create_one(user_1, child_workspace, WorkspaceRoles.CONTRIBUTOR.level, False)
+        with load_parent_access_plugin:
+            role_api.create_one(
+                user_1, grandson_workspace, WorkspaceRoles.CONTENT_MANAGER.level, False
+            )
+
+            assert grandson_workspace.get_user_role(user_1) == WorkspaceRoles.CONTENT_MANAGER.level
+            assert child_workspace.get_user_role(user_1) == WorkspaceRoles.CONTRIBUTOR.level
+            assert parent_workspace.get_user_role(user_1) == WorkspaceRoles.READER.level
