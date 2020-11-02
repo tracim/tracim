0a46945c723ec4b34cb1af4d05cf5dcb79eaca27
Raphaël Jakse <raphael.jakse@algoo.fr>
2020-11-02 12:20:58 +0100

    explicit vendors externals and documentation [skip ci pytest]

--- frontend_vendors/webpack.list.config.js
+++ frontend_vendors/webpack.list.config.js
@@ -11,7 +11,24 @@ module.exports = {
   },
   externals: [
     (context, request, callback) => {
-      if (request.startsWith('./')) return callback()
+      // RJ - 2020-11-02 - NOTE
+      //
+      // This function intercepts all the calls to require.
+      // It replaces require calls to libraries by the variable this.
+      // This is needed because these libraries assume a browser and break
+      // when run in webpack.
+
+      //  Parameters:
+      //   - request  - the string that was passed to require
+      //   - callback - the function used to tell how to resolve this dependency
+      //     - callback() says: resolve this dependency as usual
+      //     - callback(null, 'var this') says: use variable 'this' for this
+      //       dependency.
+
+      if (request === './index.js' || request === './src/list.js') {
+        return callback()
+      }
+
       return callback(null, 'var this')
     }
   ]
