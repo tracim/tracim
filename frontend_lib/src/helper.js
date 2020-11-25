089e3d5623f58b67cbec026576c6b5fa5be4b5cd
Sébastien Grignard <seb@amakaze.org>
2020-11-25 12:07:55 +0100

    style(frontend): fix indentation

--- frontend_lib/src/helper.js
+++ frontend_lib/src/helper.js
@@ -303,21 +303,21 @@ export const updateTLMAuthor = author => {
   return author
     ? { ...author, is_from_system_admin: false }
     : {
-        allowed_space: 0,
-        auth_type: 'internal',
-        avatar_url: null,
-        created: '',
-        email: '',
-        is_active: true,
-        is_deleted: false,
-        is_from_system_admin: true,
-        lang: 'en',
-        profile: 'administrators',
-        public_name: i18n.t('System Administrator'),
-        timezone: '',
-        user_id: 0,
-        username: ''
-      }
+      allowed_space: 0,
+      auth_type: 'internal',
+      avatar_url: null,
+      created: '',
+      email: '',
+      is_active: true,
+      is_deleted: false,
+      is_from_system_admin: true,
+      lang: 'en',
+      profile: 'administrators',
+      public_name: i18n.t('System Administrator'),
+      timezone: '',
+      user_id: 0,
+      username: ''
+    }
 }
 
 // INFO - GB - 2019-07-05 - This password generator function was based on
