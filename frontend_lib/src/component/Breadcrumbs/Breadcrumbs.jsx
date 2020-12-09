9659e525825d13ca41220a5ef8f75a2eb503ff2f
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2020-12-09 17:33:23 +0100

    ellipsis in big path [skip ci]

--- frontend_lib/src/component/Breadcrumbs/Breadcrumbs.jsx
+++ frontend_lib/src/component/Breadcrumbs/Breadcrumbs.jsx
@@ -3,9 +3,28 @@ import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
 
 export const Breadcrumbs = props => {
+  const MAX_NUMBER_OF_LAYERS = 6
+  const LAST_VISIBLE_LAYERS = 3
+  let breadcrumbsList = props.breadcrumbsList
+
+  if (props.breadcrumbsList.length >= MAX_NUMBER_OF_LAYERS) {
+    const beginingList = props.breadcrumbsList[0].label === props.t('Home')
+      ? [props.breadcrumbsList[0], props.breadcrumbsList[1]]
+      : [props.breadcrumbsList[0]]
+    breadcrumbsList = [
+      ...beginingList,
+      {
+        label: props.breadcrumbsList.map(crumb => crumb.label).join(' > '),
+        link: <span>...</span>,
+        notALink: true
+      },
+      ...props.breadcrumbsList.slice(Math.max(props.breadcrumbsList.length - LAST_VISIBLE_LAYERS, 0))
+    ]
+  }
+
   return (
     <ul className='breadcrumbs'>
-      {props.breadcrumbsList.map((crumb, i) =>
+      {breadcrumbsList.map((crumb, i) =>
         <li
           className={`breadcrumbs__item ${crumb.notALink ? '' : 'primaryColorFont primaryColorFontDarkenHover'}`}
           key={`breacrumbs_${i}`}
@@ -21,5 +40,5 @@ export const Breadcrumbs = props => {
 export default translate()(Breadcrumbs)
 
 Breadcrumbs.propTypes = {
-  breadcrumbsList: PropTypes.array.isRequired
+  breadcrumbsList: PropTypes.array.isRequired,
 }
