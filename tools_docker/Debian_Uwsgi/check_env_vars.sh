b7d75772aae51f5c150ac091d42a9451c9c0305a
Guénaël Muller <guenael.muller@algoo.fr>
2020-12-15 17:04:00 +0100

    fix issue in docker

--- tools_docker/Debian_Uwsgi/check_env_vars.sh
+++ tools_docker/Debian_Uwsgi/check_env_vars.sh
@@ -40,23 +40,23 @@ if ! [ "$DATABASE_TYPE" = sqlite ]; then
 fi
 
 if [ "$ENABLE_GOCRYPTFS_ENCRYPTION" = "1" ]; then
-    if ! [ -n "GOCRYPTFS_PASSWORD_PATH"]:
+    if ! [ -n "GOCRYPTFS_PASSWORD_PATH"]; then
         echo "You must set GOCRYPTFS_PASSWORD_PATH environment variable"
         exit 1
     fi
-    if ! [ -n "$GOCRYPTFS_PREVIEW_STORAGE_DIR"]:
+    if ! [ -n "$GOCRYPTFS_PREVIEW_STORAGE_DIR"]; then
         echo "You must set GOCRYPTFS_PREVIEW_STORAGE_DIR environment variable"
         exit 1
     fi
-    if ! [ -n "$GOCRYPTFS_UPLOADED_FILES_STORAGE_DIR"]:
+    if ! [ -n "$GOCRYPTFS_UPLOADED_FILES_STORAGE_DIR"]; then
         echo "You must set GOCRYPTFS_UPLOADED_FILES_STORAGE_DIR environment variable"
         exit 1
     fi
-    if ! [ -n "$TRACIM_PREVIEW_CACHE_DIR"]:
+    if ! [ -n "$TRACIM_PREVIEW_CACHE_DIR"]; then
         echo "You must set TRACIM_PREVIEW_CACHE_DIR environment variable"
         exit 1
     fi
-    if ! [ -n "$TRACIM_DEPOT_STORAGE_DIR"]:
+    if ! [ -n "$TRACIM_DEPOT_STORAGE_DIR"]; then
         echo "You must set TRACIM_DEPOT_STORAGE_DIR environment variable"
         exit 1
     fi
