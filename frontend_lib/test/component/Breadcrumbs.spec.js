85922c0cea7cab0850b622b9c86130adbb1c1e6a
Skylsmoi <come.huguies@gmail.com>
2021-01-11 18:33:40 +0100

    fix(front): fix breadcrumbs unit tests

--- frontend_lib/test/component/Breadcrumbs.spec.js
+++ frontend_lib/test/component/Breadcrumbs.spec.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { BrowserRouter } from 'react-router-dom'
 import { Breadcrumbs } from '../../src/component/Breadcrumbs/Breadcrumbs.jsx'
 require('../../src/component/Breadcrumbs/Breadcrumbs.styl')
 
 describe('<Breadcrumbs />', () => {
   const breadcrumbsList = [{
-    link: <a href='/ui'><i className='fa fa-home' />Home</a>,
+    link: '/ui',
     label: 'Home',
-    type: 'CORE'
+    type: 'CORE',
+    isALink: true
   }, {
-    link: <span className='nolink'>First level</span>,
+    link: '',
     label: 'First level',
     type: 'CORE',
     isALink: false
   }, {
-    link: <a className='secondlvl' href='/ui/second'>Second level</a>,
+    link: '/ui/second',
     label: 'Second level',
-    type: 'CORE'
+    type: 'CORE',
+    isALink: true
   }]
 
   const props = {
@@ -25,23 +28,29 @@ describe('<Breadcrumbs />', () => {
     keepLastBreadcrumbAsLink: true,
     t: key => key
   }
-  const wrapper = shallow(
-    <Breadcrumbs {...props} />
+  const history = require('history').createBrowserHistory()
+
+  // INFO - CH - 2020-01-11 - <Breadcrumbs /> contains <Link> components from react router.
+  // So we need to wrap it in a Router so we need to use mount() instead of shallow()
+  const wrapper = mount(
+    <BrowserRouter history={history}>
+      <Breadcrumbs {...props} />
+    </BrowserRouter>
   )
 
   describe('The first level', () => {
-    it('should display "First level"', () => expect(wrapper.find('.nolink')).to.have.text().to.be.equal('First level'))
+    it('should display "First level"', () => expect(wrapper.find('.breadcrumbs__item').at(1)).to.have.text().to.be.equal('First level'))
     it('should NOT have the class primaryColorFontDarkenHover and primaryColorFont', () => {
-      expect(wrapper.find('.nolink').parent()).to.have.attr('class').to.not.contains('primaryColorFontDarkenHover')
-      expect(wrapper.find('.nolink').parent()).to.have.attr('class').to.not.contains('primaryColorFont')
+      expect(wrapper.find('.breadcrumbs__item').at(1)).to.have.attr('class').to.not.contains('primaryColorFontDarkenHover')
+      expect(wrapper.find('.breadcrumbs__item').at(1)).to.have.attr('class').to.not.contains('primaryColorFont')
     })
   })
 
   describe('The second level', () => {
-    it('should display "Second level"', () => expect(wrapper.find('.secondlvl')).to.have.text().to.be.equal('Second level'))
+    it('should display "Second level"', () => expect(wrapper.find('.breadcrumbs__item').at(2)).to.have.text().to.be.equal('Second level'))
     it('should have the class primaryColorFontDarkenHover and primaryColorFont', () => {
-      expect(wrapper.find('.secondlvl').parent()).to.have.attr('class').to.contains('primaryColorFontDarkenHover')
-      expect(wrapper.find('.secondlvl').parent()).to.have.attr('class').to.contains('primaryColorFont')
+      expect(wrapper.find('.breadcrumbs__item').at(2)).to.have.attr('class').to.contains('primaryColorFontDarkenHover')
+      expect(wrapper.find('.breadcrumbs__item').at(2)).to.have.attr('class').to.contains('primaryColorFont')
     })
   })
 })
