e0e44f9ba5b65479707bce71dfc449dbc28cb92b
Giulia Bonaspetti Martins <gbonaspetti@hotmail.com>
2021-01-29 10:50:18 +0100

    divise isEditable function in two [skip ci pytest]

--- frontend/src/container/PublicProfile.jsx
+++ frontend/src/container/PublicProfile.jsx
@@ -371,14 +371,18 @@ export class PublicProfile extends React.Component {
     }
   }
 
-  isPublicProfileEditable = (connectedUser, publicProfileId, profileObject, schemaObject) => {
-    if ((schemaObject && Object.keys(schemaObject).length === 0) ||
-      (schemaObject && schemaObject.properties && Object.keys(schemaObject.properties).length === 0)
-    ) return false
+  isPublicProfileEditable = (connectedUser, publicProfileId, profileObject) => {
     const isConnectedUserOnHisOwnProfile = connectedUser.userId === publicProfileId
     const isUserAdmin = connectedUser.profile === profileObject.administrator.slug
 
     return isConnectedUserOnHisOwnProfile || isUserAdmin
+  }
+
+  isSchemaObjectEmpty = (schemaObject) => {
+    return (
+      (schemaObject && Object.keys(schemaObject).length === 0) ||
+      (schemaObject && schemaObject.properties && Object.keys(schemaObject.properties).length === 0)
+    )
   }
 
   handleCloseUploadPopup = () => this.setState({ displayUploadPopup: undefined })
@@ -387,7 +391,8 @@ export class PublicProfile extends React.Component {
     const { props, state } = this
 
     const userId = state.displayedUser ? state.displayedUser.userId : props.match.params.userid
-    const isPublicProfileEditable = schemaObject => this.isPublicProfileEditable(props.user, userId, PROFILE, schemaObject)
+    const isPublicProfileEditable = this.isPublicProfileEditable(props.user, userId, PROFILE)
+    const isFieldEditable = schemaObject => isPublicProfileEditable && !this.isSchemaObjectEmpty(schemaObject)
     const avatarBaseUrl = getAvatarBaseUrl(FETCH_CONFIG.apiUrl, userId)
 
     const coverBaseUrl = getCoverBaseUrl(FETCH_CONFIG.apiUrl, userId)
@@ -426,7 +431,7 @@ export class PublicProfile extends React.Component {
 
           <CoverImage
             displayedUser={state.displayedUser}
-            changeEnabled={isPublicProfileEditable()}
+            changeEnabled={isPublicProfileEditable}
             onChangeCoverClick={this.handleChangeCoverClick}
             coverBaseUrl={coverBaseUrl}
             coverImageName={coverImageName}
@@ -437,7 +442,7 @@ export class PublicProfile extends React.Component {
             displayedUser={state.displayedUser}
             breadcrumbsList={props.breadcrumbs}
             onChangeAvatarClick={this.handleChangeAvatarClick}
-            changeAvatarEnabled={isPublicProfileEditable()}
+            changeAvatarEnabled={isPublicProfileEditable}
           />
 
           <div className='profile__content'>
@@ -448,7 +453,7 @@ export class PublicProfile extends React.Component {
                     schemaObject={state.informationSchemaObject}
                     uiSchemaObject={state.uiSchemaObject}
                     dataSchemaObject={state.informationDataSchema}
-                    displayEditButton={isPublicProfileEditable(state.informationSchemaObject)}
+                    displayEditButton={isFieldEditable(state.informationSchemaObject)}
                     registrationDate={(new Date(state.displayedUser.created).toLocaleDateString())}
                     authoredContentRevisionsCount={state.displayedUser.authoredContentRevisionsCount}
                     authoredContentRevisionsSpaceCount={state.displayedUser.authoredContentRevisionsSpaceCount}
@@ -472,7 +477,7 @@ export class PublicProfile extends React.Component {
                     schemaObject={state.personalPageSchemaObject}
                     uiSchemaObject={state.uiSchemaObject}
                     dataSchemaObject={state.personalPageDataSchema}
-                    displayEditButton={isPublicProfileEditable(state.personalPageSchemaObject)}
+                    displayEditButton={isFieldEditable(state.personalPageSchemaObject)}
                     onSubmitDataSchema={this.handleSubmitDataSchema}
                   />
                 )
