d3270e6a9301919525a6141fc0684e156ddd4e71
Guénaël Muller <guenael.muller@algoo.fr>
2021-02-04 14:27:27 +0100

    workaround to support mysql case in migration script

--- backend/tracim_backend/migration/versions/5d54d8602f5a_add_raw_content_column_to_content_.py
+++ backend/tracim_backend/migration/versions/5d54d8602f5a_add_raw_content_column_to_content_.py
@@ -12,6 +12,10 @@ import sqlalchemy as sa
 revision = "5d54d8602f5a"
 down_revision = "7c6641d4f934"
 
+default_raw_content_value = """
+update content_revisions
+set raw_content = ''
+"""
 set_comment_html_document_raw_content = """
 update content_revisions
 set raw_content = description, description = ''
@@ -26,12 +30,16 @@ where type in ('html-document', 'comment', 'thread')
 
 
 def upgrade():
+    # INFO - G.M - 2021-02-04 - Workaround to set default value in migration script
+    # as Mysql 8+ doesn't support server_default for Text:
+    # nullable true -> set all as default value -> nullable false.
     with op.batch_alter_table("content_revisions") as batch_op:
-        batch_op.add_column(
-            sa.Column("raw_content", sa.Text(), nullable=False, default="", server_default="")
-        )
+        batch_op.add_column(sa.Column("raw_content", sa.Text(), nullable=True))
     connection = op.get_bind()
+    connection.execute(default_raw_content_value)
     connection.execute(set_comment_html_document_raw_content)
+    with op.batch_alter_table("content_revisions") as batch_op:
+        batch_op.alter_column("raw_content", existing_type=sa.Text(), nullable=False)
 
 
 def downgrade():
