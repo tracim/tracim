ef276a3d350c4d199357a8482dbe3daa29988eb0
Sébastien Grignard <sebastien.grignard@algoo.fr>
2021-03-10 17:28:21 +0100

    wip

---
+++ backend/tracim_backend/migration/versions/78a01733957f_add_publication_namespace.py
@@ -0,0 +1,21 @@
+"""add publication namespace
+
+Revision ID: 78a01733957f
+Revises: 5d54d8602f5a
+Create Date: 2021-03-10 17:24:12.238148
+
+"""
+# from alembic import op
+# import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "78a01733957f"
+down_revision = "5d54d8602f5a"
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
