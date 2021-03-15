743d6cdcf27ed7ce4a7ccafa4d2869a39313783a
Sébastien Grignard <sebastien.grignard@algoo.fr>
2021-03-15 14:17:17 +0100

    doc(backend): add docstring
    [skip ci cypress]

--- backend/tracim_backend/models/utils.py
+++ backend/tracim_backend/models/utils.py
@@ -6,10 +6,16 @@ from tracim_backend.models.meta import DeclarativeBase
 
 
 def get_sort_expression(
-    sort_string: str, model: DeclarativeBase, column_names_mapping: typing.Dict[str, str] = {}
+    sort_string: str, model: DeclarativeBase, attribute_names_mapping: typing.Dict[str, str] = {}
 ) -> Column:
+    """Return the SQL sort expression represented by the given string.
+    Currently only one column is supported, in ascending or descending order.
+    The string syntax is "attribute_alias_name:(asc|desc)".
+    attribute_alias_name can either be the model's attribute name or a key of
+    attribute_names_mapping which has the corresponding model'attribute as value.
+    """
     name, order = sort_string.split(":")
-    column = getattr(model, column_names_mapping.get(name, name))
+    column = getattr(model, attribute_names_mapping.get(name, name))
     if order == "asc":
         return column.asc()
     elif order == "desc":
