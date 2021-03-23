77149f55bb4b8db86ef35d35a89c9efaaf67991a
Skylsmoi <come.huguies@gmail.com>
2021-03-23 16:38:46 +0100

    fix(front): fix typo in comment (next) [skip ci pytest]

--- frontend_app_thread/src/container/Thread.jsx
+++ frontend_app_thread/src/container/Thread.jsx
@@ -84,7 +84,7 @@ export class Thread extends React.Component {
     props.registerLiveMessageHandlerList([
       { entityType: TLM_ET.CONTENT, coreEntityType: TLM_CET.MODIFIED, optionalSubType: TLM_ST.THREAD, handler: this.handleContentChanged },
       { entityType: TLM_ET.CONTENT, coreEntityType: TLM_CET.CREATED, optionalSubType: TLM_ST.COMMENT, handler: this.handleCommentCreated },
-      // INFO - CH - 20210322 - handler bellow is to handle the addition of comment as file
+      // INFO - CH - 20210322 - handler below is to handle the addition of comment as file
       { entityType: TLM_ET.CONTENT, coreEntityType: TLM_CET.CREATED, optionalSubType: TLM_ST.FILE, handler: this.handleCommentCreated },
       { entityType: TLM_ET.CONTENT, coreEntityType: TLM_CET.DELETED, optionalSubType: TLM_ST.THREAD, handler: this.handleContentChanged },
       { entityType: TLM_ET.CONTENT, coreEntityType: TLM_CET.UNDELETED, optionalSubType: TLM_ST.THREAD, handler: this.handleContentChanged },
