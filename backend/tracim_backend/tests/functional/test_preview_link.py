689b9e6b352119f7079512292c7f2d601a786269
Guénaël Muller <guenael.muller@algoo.fr>
2021-03-31 14:39:10 +0200

    better test for preview link

--- backend/tracim_backend/tests/functional/test_preview_link.py
+++ backend/tracim_backend/tests/functional/test_preview_link.py
@@ -12,11 +12,11 @@ simple_opengraph_html = """
 <html>
 <header>
 <meta property="og:title" content="Example title of article">
-<meta property="og:site_name" content="example.com website">
+<meta property="og:site_name" content="example.invalid website">
 <meta property="og:type" content="article">
-<meta property="og:url" content="http://example.com/example-title-of-article">
-<meta property="og:image" content="http://example.com/article_thumbnail.jpg">
-<meta property="og:image" content="http://example.com/website_logo.png">
+<meta property="og:url" content="http://example.invalid/example-title-of-article">
+<meta property="og:image" content="http://example.invalid/article_thumbnail.jpg">
+<meta property="og:image" content="http://example.invalid/website_logo.png">
 <meta property="og:description" content="This example article is an example of OpenGraph protocol.">
 </header>
 <body>
@@ -27,7 +27,7 @@ simple_opengraph_html = """
 simple_opengraph_html_result_endpoint = {
     "title": "Example title of article",
     "description": "This example article is an example of OpenGraph protocol.",
-    "image": "http://example.com/article_thumbnail.jpg",
+    "image": "http://example.invalid/article_thumbnail.jpg",
 }
 
 
@@ -41,7 +41,7 @@ class TestUrlPreview(object):
         "link_name, link_content, endpoint_result",
         (
             pytest.param(
-                "http://example.com/example-title-of-article",
+                "http://example.invalid/example-title-of-article",
                 simple_opengraph_html,
                 simple_opengraph_html_result_endpoint,
                 id="simple link example",
@@ -65,3 +65,43 @@ class TestUrlPreview(object):
             "/api/url-preview?{params}".format(params=urlencode(params)), status=200,
         )
         assert res.json_body == endpoint_result
+
+    @responses.activate
+    @pytest.mark.parametrize(
+        "link_name, link_content, endpoint_result",
+        (
+            pytest.param(
+                "http://empty.invalid",
+                "",
+                {"title": None, "description": None, "image": None},
+                id="empty page",
+            ),
+        ),
+    )
+    def test_api__url_preview__ok_200__no_preview(
+        self, link_name, link_content, endpoint_result, web_testapp,
+    ):
+        responses.add(
+            responses.GET,
+            link_name,
+            body=link_content,
+            status=200,
+            content_type="text/html",
+            stream=True,
+        )
+        params = {"url": link_name}
+        web_testapp.authorization = ("Basic", ("admin@admin.admin", "admin@admin.admin"))
+        res = web_testapp.get(
+            "/api/url-preview?{params}".format(params=urlencode(params)), status=200,
+        )
+        assert res.json_body == endpoint_result
+
+    @responses.activate
+    def test_api__url_preview__err_502__do_not_exist(
+        self, web_testapp,
+    ):
+        params = {"url": "http://thisurldoesnotexist.invalid"}
+        web_testapp.authorization = ("Basic", ("admin@admin.admin", "admin@admin.admin"))
+        web_testapp.get(
+            "/api/url-preview?{params}".format(params=urlencode(params)), status=502,
+        )
