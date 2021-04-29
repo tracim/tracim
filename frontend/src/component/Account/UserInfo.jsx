cde47a8766598ad69b995a98fc6525a21540d1c3
charline-rageade <charline.rageade@algoo.fr>
2021-04-29 14:29:03 +0200

    feat/4551 improve the readibility of user data page [skip ci pythest] Tooltip to Popover

--- frontend/src/component/Account/UserInfo.jsx
+++ frontend/src/component/Account/UserInfo.jsx
@@ -1,76 +1,105 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { translate } from 'react-i18next'
 import { getUserProfile, FETCH_CONFIG } from '../../util/helper.js'
 import { Avatar, AVATAR_SIZE } from 'tracim_frontend_lib'
-import { Tooltip } from 'reactstrap'
+import { Popover, PopoverBody } from 'reactstrap'
+import { isMobile } from 'react-device-detect'
 
 require('./UserInfo.styl')
 
-export const UserInfo = props => {
-  const [tooltipOpenUsername, setTooltipOpenUsername] = useState(false)
-  const [tooltipOpenPublicName, setTooltipOpenPublicName] = useState(false)
-  const toggleTooltipPublicName = () => setTooltipOpenPublicName(!tooltipOpenPublicName)
-  const toggleTooltipUserName = () => setTooltipOpenUsername(!tooltipOpenUsername)
+export class UserInfo extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      popoverPublicNameOpen: false,
+      popoverUsernameOpen: false
+    }
+  }
 
-  return (
-    <div className='userinfo mr-5 ml-5 mb-5' data-cy='userinfo'>
-      <div className='userinfo__avatar' data-cy='userinfo__avatar'>
-        <Avatar
-          size={AVATAR_SIZE.BIG}
-          user={props.user}
-          apiUrl={FETCH_CONFIG.apiUrl}
-        />
-      </div>
+  handleTogglePopoverPublicName = () => {
+    this.setState(prevState => ({
+      popoverPublicNameOpen: !prevState.popoverPublicNameOpen
+    }))
+  }
 
-      <div className='userinfo__wrapper'>
-        <div div className='userinfo__name primaryColorFont' data-cy='userinfo__name'>
-          <span href='#' id='TooltipPublicName'>{`${props.user.publicName}`}
-          </span>
-          <Tooltip
-            placement='right'
-            isOpen={tooltipOpenPublicName}
-            target='TooltipPublicName'
-            toggle={toggleTooltipPublicName}
-          >
-            {props.t('Public Name')}
-          </Tooltip>
-        </div>
+  handleTogglePopoverUsername = () => {
+    this.setState(prevState => ({
+      popoverUsernameOpen: !prevState.popoverUsernameOpen
+    }))
+  }
 
-        {props.user.username && (
-          <div className='userinfo__username' data-cy='userinfo__username'>
-            <span href='#' id='TooltipUsername'>{`@${props.user.username}`}
-            </span>
-            <Tooltip
-              placement='right'
-              isOpen={tooltipOpenUsername}
-              target='TooltipUsername'
-              toggle={toggleTooltipUserName}
-            >
-              {props.t('Username')}
-            </Tooltip>
-          </div>
-        )}
+  render () {
+    const { props } = this
 
-        {props.user.email && (
-          <div className='userinfo__email d-block mt-3' data-cy='userinfo__email'>
-            <i
-              className='fas fa-envelope mr-2'
-            />
-            <a href={`mailto:${props.user.email}`}>
-              {props.user.email}
-            </a>
-          </div>
-        )}
-
-        <div className='userinfo__profile mt-3 mb-3' data-cy='userinfo__profile'>
-          <i
-            className={`${getUserProfile(props.user.profile).faIcon} mr-2`}
-            style={{ color: getUserProfile(props.user.profile).hexcolor }}
+    return (
+      <div className='userinfo mr-5 ml-5 mb-5' data-cy='userinfo'>
+        <div className='userinfo__avatar' data-cy='userinfo__avatar'>
+          <Avatar
+            size={AVATAR_SIZE.BIG}
+            user={props.user}
+            apiUrl={FETCH_CONFIG.apiUrl}
           />
-          {props.t(getUserProfile(props.user.profile).label)}
+        </div>
+
+        <div className='userinfo__wrapper'>
+          <div div className='userinfo__name primaryColorFont' data-cy='userinfo__name'>
+            <span href='#' id='popoverPublicName'>{`${props.user.publicName}`}
+            </span>
+            <Popover
+              placement='right'
+              isOpen={this.state.popoverPublicNameOpen}
+              target='popoverPublicName'
+              // INFO - CR - 2021-04-29 - ignoring rule react/jsx-handler-names for prop bellow because it comes from external lib
+              toggle={this.handleTogglePopoverPublicName}// eslint-disable-line react/jsx-handler-names
+              trigger={isMobile ? 'focus' : 'hover'}
+            >
+              <PopoverBody>
+                {props.t('Public Name')}
+              </PopoverBody>
+            </Popover>
+          </div>
+
+          {props.user.username && (
+            <div className='userinfo__username' data-cy='userinfo__username'>
+              <span href='#' id='popoverUsername'>{`@${props.user.username}`}
+              </span>
+              <Popover
+                placement='right'
+                isOpen={this.state.popoverUsernameOpen}
+                target='popoverUsername'
+                // INFO - CR - 2021-04-29 - ignoring rule react/jsx-handler-names for prop bellow because it comes from external lib
+                toggle={this.handleTogglePopoverUsername}// eslint-disable-line react/jsx-handler-names
+                trigger={isMobile ? 'focus' : 'hover'}
+              >
+                <PopoverBody>
+                  {props.t('Username')}
+                </PopoverBody>
+              </Popover>
+            </div>
+          )}
+
+          {props.user.email && (
+            <div className='userinfo__email d-block mt-3' data-cy='userinfo__email'>
+              <i
+                className='fas fa-envelope mr-2'
+              />
+              <a href={`mailto:${props.user.email}`}>
+                {props.user.email}
+              </a>
+            </div>
+          )}
+
+          <div className='userinfo__profile mt-3 mb-3' data-cy='userinfo__profile'>
+            <i
+              className={`${getUserProfile(props.user.profile).faIcon} mr-2`}
+              style={{ color: getUserProfile(props.user.profile).hexcolor }}
+            />
+            {props.t(getUserProfile(props.user.profile).label)}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
 export default translate()(UserInfo)
