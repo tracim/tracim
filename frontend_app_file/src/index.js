27e403920bb694781c29ee03e644f84762e00c42
Sébastien Grignard <sebastien.grignard@algoo.fr>
2021-05-25 17:45:53 +0200

    fix(frontend): ensure react state has been updated
    During rendered -> unmounted & unmounted -> rendered transitions.
    
    [skip ci pytest]

--- frontend_app_file/src/index.js
+++ frontend_app_file/src/index.js
@@ -11,16 +11,34 @@ require('./css/index.styl')
 const appInterface = {
   name: 'file',
   isRendered: false,
-  renderAppFeature: (data) => {
-    return ReactDOM.render(
+  component: null,
+
+  renderAppFeature: data => {
+    const element = document.getElementById(data.config.domContainer)
+    if (element._reactRootContainer) {
+      console.log('Component is already mounted, waiting for unmount!!', appInterface)
+      setTimeout(() => { appInterface.renderAppFeature(data) }, 50)
+      return
+    }
+
+    const component = ReactDOM.render(
       <Router history={data.config.history}>
         <File data={data} />
       </Router>
-      , document.getElementById(data.config.domContainer)
+      , element
     )
+    appInterface.component = component
+    return appInterface.component
   },
   unmountApp: domId => {
-    return ReactDOM.unmountComponentAtNode(document.getElementById(domId)) // returns bool
+    const element = document.getElementById(domId)
+    if (domId === 'appFeatureContainer' && !element._reactRootContainer) {
+      console.log('Component is not mounted, waiting for mount!!', appInterface)
+      setTimeout(() => { appInterface.unmountApp(domId) }, 50)
+      return
+    }
+    appInterface.component = null
+    return ReactDOM.unmountComponentAtNode(element) // returns bool
   },
   renderAppPopupCreation: data => {
     return ReactDOM.render(
--- frontend_app_html-document/src/index.js
+++ frontend_app_html-document/src/index.js
@@ -11,16 +11,34 @@ require('./css/index.styl')
 const appInterface = {
   name: 'html-document',
   isRendered: false,
+  component: null,
+
   renderAppFeature: data => {
-    return ReactDOM.render(
+    const element = document.getElementById(data.config.domContainer)
+    if (element._reactRootContainer) {
+      console.log('Component is already mounted, waiting for unmount!!', appInterface)
+      setTimeout(() => { appInterface.renderAppFeature(data) }, 50)
+      return
+    }
+
+    const component = ReactDOM.render(
       <Router history={data.config.history}>
         <HtmlDocument data={data} />
       </Router>
-      , document.getElementById(data.config.domContainer)
+      , element
     )
+    appInterface.component = component
+    return appInterface.component
   },
   unmountApp: domId => {
-    return ReactDOM.unmountComponentAtNode(document.getElementById(domId)) // returns bool
+    const element = document.getElementById(domId)
+    if (domId === 'appFeatureContainer' && !element._reactRootContainer) {
+      console.log('Component is not mounted, waiting for mount!!', appInterface)
+      setTimeout(() => { appInterface.unmountApp(domId) }, 50)
+      return
+    }
+    appInterface.component = null
+    return ReactDOM.unmountComponentAtNode(element) // returns bool
   },
   renderAppPopupCreation: data => {
     return ReactDOM.render(
