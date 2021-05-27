c074446d24792d4c83682f24298c7dc19059db84
MariePrandstetter <50776142+MariePrandstetter@users.noreply.github.com>
2021-05-27 08:54:11 +0200

    Fix(frontend) Add filter to ActivityFeed
    Ref: #4654 #4675

--- frontend/src/component/Activity/ActivityList.jsx
+++ frontend/src/component/Activity/ActivityList.jsx
@@ -115,6 +115,9 @@ const ActivityList = (props) => {
     return <div className='activityList__item' data-cy='activityList__item' key={activity.id}>{component}</div>
   }
 
+  // FIXME - MB - 2021-05-26 - this code is duplicated for activityDisplayFilter, in withActivity
+  // See this ticket https://github.com/tracim/tracim/issues/4677
+
   const isSubscriptionRequestOrRejection = (activity) => {
     return (activity.entityType === TLM_ET.SHAREDSPACE_SUBSCRIPTION &&
       DISPLAYED_SUBSCRIPTION_STATE_LIST.includes(activity.newestMessage.fields.subscription.state))
