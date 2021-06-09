f07b678b2258791d794e94131ed732e22dc76fe4
Raphaël Jakse <raphael.jakse@algoo.fr>
2021-06-09 11:35:12 +0200

    cypress tests [skip ci pytest]

--- functionnal_tests/cypress/integration/tags/tags.spec.js
+++ functionnal_tests/cypress/integration/tags/tags.spec.js
@@ -1,8 +1,10 @@
-// TODO 
+import { PAGES } from '../../support/urls_commands'
+
+// TODO
 // * Check create new tag
 //  ** Create a new tag
 //  ** click on "validate"
-//  ** check if new tag has been created 
+//  ** check if new tag has been created
 
 // * Unchecking a tag
 //  ** click on a checked item on the list
@@ -11,94 +13,96 @@
 // * Re-checking a tag
 //  ** go back to the unchecked tag
 //  ** check it
-//  **  see if it is marked as a content tag 
+//  **  see if it is marked as a content tag
 
+describe('Create tags', () => {
+  // const onClickCloseAddTagBtnCallBack = sinon.spy()
+  // const onClickBtnValidateCallBack = sinon.spy()
 
-// describe('Create tags', () => {
-//   // const onClickCloseAddTagBtnCallBack = sinon.spy()
-//   // const onClickBtnValidateCallBack = sinon.spy()
-  
-//   // const props = {
-//   //   onClickCloseAddTagBtn: onClickCloseAddTagBtnCallBack,
-//   //   apiUrl: '/',
-//   //   searchedKnownTagList: [
-//   //     { tagName: 'Tag 1', tag_id: 1 },
-//   //     { tagName: 'Tag 2', tag_id: 2 },
-//   //     { tagName: 'Tag 6', tag_id: 3 },
-//   //     { tagName: 'Tag 89', tag_id: 4 },
-//   //     { tagName: 'Tag 11', tag_id: 5 },
-//   //     { tagName: 'Tag 98', tag_id: 6 }
-//   //   ],
-//   //   onClickAutoComplete: onClickAutoCompleteCallBack,
-//   //   autoCompleteClicked: true,
-//   //   onClickBtnValidate: onClickBtnValidateCallBack,
-//   //   autoCompleteActive: false
-//   // }
-  
-//   // const wrapper = mount(<NewTagForm {...props} />)
+  // const props = {
+  //   onClickCloseAddTagBtn: onClickCloseAddTagBtnCallBack,
+  //   apiUrl: '/',
+  //   searchedKnownTagList: [
+  //     { tagName: 'Tag 1', tag_id: 1 },
+  //     { tagName: 'Tag 2', tag_id: 2 },
+  //     { tagName: 'Tag 6', tag_id: 3 },
+  //     { tagName: 'Tag 89', tag_id: 4 },
+  //     { tagName: 'Tag 11', tag_id: 5 },
+  //     { tagName: 'Tag 98', tag_id: 6 }
+  //   ],
+  //   onClickAutoComplete: onClickAutoCompleteCallBack,
+  //   autoCompleteClicked: true,
+  //   onClickBtnValidate: onClickBtnValidateCallBack,
+  //   autoCompleteActive: false
+  // }
 
-//   let htmlContentId
-//   let workspaceId
-//   let fileContentId
+  // const wrapper = mount(<NewTagForm {...props} />)
 
-//     before(() => {
-//       cy.resetDB()
-//       cy.setupBaseDB()
-//       cy.loginAs('administrators')
-//       cy.fixture('baseWorkspace').as('workspace').then(workspace => {
-//         workspaceId = workspace.workspace_id
-  
-//         cy.createHtmlDocument(htmlDocTitle, workspaceId)
-//           .then(({ content_id: contentId }) => {
-//             htmlContentId = contentId
-//         })
-//         cy.createFile(fullFilename, mimeType, fileTitle, workspaceId)
-//           .then(({ content_id: contentId }) => {
-//             fileContentId = contentId
-//         })
-//       })
-//     })
+  let htmlContentId
+  let workspaceId
 
-//   describe('Tag list', () => {
-//     beforeEach(() => {
-//       cy.loginAs('administrators')
-//       createTag('file', fileContentId, 'TagOne')
-//     })
+  const fileTitle = 'FileForSwitch'
+  const fullFilename = 'Linux-Free-PNG.png'
+  const contentType = 'image/png'
 
-//     function createTag (contentType, contentId, tagName) {
-//       cy.visitPage({
-//         pageName: PAGES.CONTENT_OPEN,
-//         params: { workspaceId, contentType, contentId }
-//       })
-//     }
+  before(() => {
+    cy.resetDB()
+    cy.setupBaseDB()
+  })
 
-//     it('should create a tag', () => {
-//       cy.get('[data-cy=popin_right_part_tag]').click()
-//       cy.get('[data-cy=taglist__btnadd]').click()
-//       cy.get('[data-cy=addtag]').type('TagTwo')
-//       cy.get('[data-cy=ValidateTag]').click()
-//       // switch tab and come back
-//       cy.get('[data-cy=popin_right_part_share]').click()
-//       cy.get('[data-cy=popin_right_part_tag]').click()
-//     })
+  describe('Tag list', () => {
+    for (const testedContent of ['file', 'note']) {
+      describe(`in a ${testedContent}`, () => {
+        before(() => {
+          cy.loginAs('administrators')
+          cy.fixture('baseWorkspace').as('workspace').then(({ workspace_id: workspaceId }) => {
+            if (testedContent === 'file') {
+              cy.createFile(fullFilename, contentType, fileTitle, workspaceId).then(({ content_id: contentId }) => {
+                cy.visitPage({
+                  pageName: PAGES.CONTENT_OPEN,
+                  params: { workspaceId, contentType: 'file', contentId }
+                })
+              })
+            } else {
+              cy.createHtmlDocument('A note', workspaceId).then(({ content_id: contentId }) => {
+                cy.visitPage({
+                  pageName: PAGES.CONTENT_OPEN,
+                  params: { workspaceId, contentType: 'html-document', contentId }
+                })
+              })
+            }
+          })
+        })
 
-//     it('should list the tags', () => {
-//       cy.get('[data-cy=tag_list]').its('length').should('be.equal', 2)
-//       cy.get('[data-cy=tag_list]').first().should('contain', 'TagTwo')
-//     })
+        it('should create two tags', () => {
+          cy.get('[data-cy=popin_right_part_tag]').click()
+          cy.get('[data-cy=taglist__btnadd]').click()
+          cy.get('[data-cy=addtag]').type('TagOne')
+          cy.get('[data-cy=ValidateTag]').click()
+          cy.get('[data-cy=addtag]').type('TagTwo')
+          cy.get('[data-cy=ValidateTag]').click()
+          // switch tab and come back
+          cy.get('[data-cy=popin_right_part_timeline]').click()
+          cy.get('[data-cy=popin_right_part_tag]').click()
+        })
 
-//     it('clicking on a tag should uncheck it', () => {
-//       cy.get('[data-cy=tag_list]').its('length').should('be.equal', 2) // number of checked tags 
-//       cy.get('[data-cy=tag]').first().click()
-//       cy.get('[data-cy=tag_list]').its('length').should('be.equal', 1)
-//       cy.get('[data-cy=tag_list]').first().should('contain', 'Another tag')
-//     })
+        it('should list the tags', () => {
+          cy.get('[data-cy=taglist] li').its('length').should('be.equal', 2)
+          cy.get('[data-cy=taglist]').first().should('contain', 'TagTwo')
+        })
 
-//     it('clicking on an unchecked tag should check it', () => {
-//       cy.get('[data-cy=tag_list]').its('length').should('be.equal', 1)
-//       cy.get('[data-cy=tag_list]').first().click()
-//       cy.get('[data-cy=tag_list]').its('length').should('be.equal', 2)
-//       cy.get('[data-cy=tag_list]').first().should('contain', 'Another tag')
-//     })
-//   })
-// })
+        it('clicking on a tag should uncheck it', () => {
+          cy.get('[data-cy=taglist] .checkboxCustom__checked').its('length').should('be.equal', 2)
+          cy.get('[data-cy=taglist] li label').first().click()
+          cy.get('[data-cy=taglist] .checkboxCustom__checked').its('length').should('be.equal', 1)
+        })
+
+        it('clicking on an unchecked tag should check it', () => {
+          cy.get('[data-cy=taglist] .checkboxCustom__checked').its('length').should('be.equal', 1)
+          cy.get('[data-cy=taglist] li label').first().click()
+          cy.get('[data-cy=taglist] .checkboxCustom__checked').its('length').should('be.equal', 2)
+        })
+      })
+    }
+  })
+})
