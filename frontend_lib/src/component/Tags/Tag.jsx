3384bd8c4a1c17ad64509d8dc71d077af932ed57
Marie Bégué <marie.begue@algoo.fr>
2021-07-06 14:38:21 +0200

    add circle icon [skip ci pytest]

--- frontend_lib/src/component/Tags/Tag.jsx
+++ frontend_lib/src/component/Tags/Tag.jsx
@@ -2,27 +2,35 @@ import React from 'react'
 import { translate } from 'react-i18next'
 import PropTypes from 'prop-types'
 import IconButton from '../Button/IconButton.jsx'
+import Icon from '../Icon/Icon.jsx'
 
 require('./TagList.styl')
 
 export const Tag = props => {
   return (
-    <div className='tagList__list__item'>
-      <label
-        className='tagList__list__item__info'
-        title={props.name}
-      >
-        {props.name}
-      </label>
-      {!props.isReadOnlyMode && (
-        <IconButton
-          intent='link'
-          onClick={props.onClickDeleteTag}
-          icon={props.isContent ? 'fas fa-times' : 'far fa-trash-alt'}
-          title={props.isContent ? props.t('Remove tag from content') : props.t('Delete tag from space')}
-          dataCy='IconButton_DeleteTagFromSpace'
+    <div className='tagList__list__item__wrapper'>
+      <div className='tagList__list__item'>
+        <Icon
+          icon='fas fa-circle'
+          color='#f0f0f0'
+          className='tagList__list__item__circle'
         />
-      )}
+        <label
+          className='tagList__list__item__info'
+          title={props.name}
+        >
+          {props.name}
+        </label>
+        {!props.isReadOnlyMode && (
+          <IconButton
+            intent='link'
+            onClick={props.onClickDeleteTag}
+            icon={props.isContent ? 'fas fa-times' : 'far fa-trash-alt'}
+            title={props.isContent ? props.t('Remove tag from content') : props.t('Delete tag from space')}
+            dataCy='IconButton_DeleteTagFromSpace'
+          />
+        )}
+      </div>
     </div>
   )
 }
