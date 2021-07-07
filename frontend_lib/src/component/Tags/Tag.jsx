a263db912a6cb308dd9f39dbdf233f923408a547
Marie Bégué <marie.begue@algoo.fr>
2021-07-07 12:00:50 +0200

    fix reviews

--- frontend_lib/src/component/Tags/Tag.jsx
+++ frontend_lib/src/component/Tags/Tag.jsx
@@ -8,15 +8,15 @@ require('./TagList.styl')
 export const Tag = props => {
   return (
     <div className='tagList__list__item__wrapper'>
-      <i className='fas fa-play tagList__list__item__triangle'></i>
+      <i className='fas fa-play tagList__list__item__triangle' />
       <div className='tagList__list__item'>
         <div className='tagList__list__item__circle' />
-        <label
+        <span
           className='tagList__list__item__info'
           title={props.name}
         >
           {props.name}
-        </label>
+        </span>
         {!props.isReadOnlyMode && (
           <IconButton
             intent='link'
@@ -36,7 +36,7 @@ export default translate()(Tag)
 Tag.propTypes = {
   name: PropTypes.string.isRequired,
   isContent: PropTypes.bool,
-  onClickDeleteTag: PropTypes.func,
+  onClickDeleteTag: PropTypes.func.isRequired,
   isReadOnlyMode: PropTypes.bool
 }
 
