7222f0d9a34df081112febf7e78f47dccba74cc3
Raphaël Jakse <raphael.jakse@algoo.fr>
2021-07-23 10:19:18 +0200

    fix(frontend): check whether the sidebar is closed when opening an app

---
+++ functionnal_tests/cypress/integration/sidebar/feature_app_size_spec.js
@@ -0,0 +1,27 @@
+import { PAGES as p } from '../../support/urls_commands.js'
+
+describe('Sidebar and feature apps', function () {
+  before(() => {
+    cy.resetDB()
+    cy.setupBaseDB()
+    cy.loginAs('administrators')
+    cy.createWorkspace('baseChildWorkspace')
+  })
+
+  beforeEach(function () {
+    cy.visitPage({
+      pageName: p.HOME
+    })
+  })
+
+  it('should open the app maximized when the sidebar is closed', function () {
+    cy.get('[title="Hide sidebar"]').click()
+    cy.get('li[title="My space"] a[href]').first().click()
+    cy.get('button[title="Space settings"]').click()
+    cy.get('[data-cy="popinFixed"]').should('not.have.class', 'sidebarVisible')
+    cy.get('[title="See sidebar"]').click()
+    cy.get('[data-cy="popinFixed"]').should('have.class', 'sidebarVisible')
+    cy.get('[title="Hide sidebar"]').click()
+    cy.get('[data-cy="popinFixed"]').should('not.have.class', 'sidebarVisible')
+  })
+})
