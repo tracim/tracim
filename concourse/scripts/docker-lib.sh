56dd408ce802e3dc0ac090c25760456d4dc8672f
Sébastien Grignard <sebastien.grignard@algoo.fr>
2021-08-03 17:11:07 +0200

    wip

---
+++ concourse/scripts/docker-lib.sh
@@ -0,0 +1,17 @@
+--2021-08-03 14:49:20--  http://curl/
+Résolution de curl (curl)… échec : Échec temporaire dans la résolution du nom.
+wget : impossible de résoudre l’adresse de l’hôte «curl»
+--2021-08-03 14:49:20--  https://raw.githubusercontent.com/taylorsilva/dcind/main/docker-lib.sh
+Résolution de raw.githubusercontent.com (raw.githubusercontent.com)… 185.199.108.133, 185.199.109.133, 185.199.110.133, ...
+Connexion à raw.githubusercontent.com (raw.githubusercontent.com)|185.199.108.133|:443… connecté.
+requête HTTP transmise, en attente de la réponse… 200 OK
+Taille : 3096 (3,0K) [text/plain]
+Enregistre : «docker-lib.sh.1»
+
+     0K ...                                                   100% 84,1M=0s
+
+2021-08-03 14:49:21 (84,1 MB/s) - «docker-lib.sh.1» enregistré [3096/3096]
+
+Terminé — 2021-08-03 14:49:21 —
+Temps total effectif : 0,05s
+Téléchargés : 1 fichiers, 3,0K en 0s (84,1 MB/s)
