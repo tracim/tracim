f8243e28909edc617804b3600e8edff5396c25b1
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2021-08-23 17:41:16 +0200

    wip grouping notifications [skip ci]

--- frontend/src/reducer/notificationPage.js
+++ frontend/src/reducer/notificationPage.js
@@ -49,12 +49,139 @@ export const serializeNotification = notification => {
   }
 }
 
+const groupNotificationList = (notificationList) => {
+  let newNotificationList = []
+  let newIndex = 0
+  notificationList.forEach((notification, index) => {
+    let content = false
+    let workspace = false
+    let author = false
+    if (index < 2) return
+    // debugger
+    if (newNotificationList[newIndex - 1] &&
+      newNotificationList[newIndex - 1].type.startsWith('group')
+    ) {
+      if (
+        newNotificationList[newIndex - 1].type.match('group.content.workspace.author') &&
+        newNotificationList[newIndex - 1].content.id === notification.content.id &&
+        newNotificationList[newIndex - 1].workspace.id === notification.workspace.id &&
+        newNotificationList[newIndex - 1].author.userId === notification.author.userId
+      ) {
+        newNotificationList[newIndex - 1].group.push(notification)
+        return
+      }
+
+      if (
+        newNotificationList[newIndex - 1].type.match('group.content.workspace') &&
+        newNotificationList[newIndex - 1].content.id === notification.content.id &&
+        newNotificationList[newIndex - 1].workspace.id === notification.workspace.id
+      ) {
+        newNotificationList[newIndex - 1].group.push(notification)
+        return
+      }
+
+      if (
+        newNotificationList[newIndex - 1].type.match('group.content.author') &&
+        newNotificationList[newIndex - 1].content.id === notification.content.id &&
+        newNotificationList[newIndex - 1].author.userId === notification.author.userId
+      ) {
+        newNotificationList[newIndex - 1].group.push(notification)
+        return
+      }
+
+      if (
+        newNotificationList[newIndex - 1].type.match('group.author.workspace') &&
+        newNotificationList[newIndex - 1].author.userId === notification.author.userId &&
+        newNotificationList[newIndex - 1].workspace.id === notification.workspace.id
+      ) {
+        newNotificationList[newIndex - 1].group.push(notification)
+        return
+      }
+    }
+
+    if (
+      notificationList[index - 2].content &&
+      notificationList[index - 1].content &&
+      notificationList[index].content
+    ) {
+      const contentId0 = notificationList[index].type.includes('comment') ? notificationList[index].content.parentId : notificationList[index].content.id
+      const contentId1 = notificationList[index - 1].type.includes('comment') ? notificationList[index - 1].content.parentId : notificationList[index - 1].content.id
+      const contentId2 = notificationList[index - 2].type.includes('comment') ? notificationList[index - 2].content.parentId : notificationList[index - 2].content.id
+      if (contentId2 === contentId1 && contentId1 === contentId0) content = true
+    }
+    if (
+      notificationList[index - 2].workspace &&
+      notificationList[index - 1].workspace &&
+      notificationList[index].workspace &&
+      notificationList[index - 2].workspace.id === notificationList[index - 1].workspace.id &&
+      notificationList[index - 1].workspace.id === notificationList[index].workspace.id
+    ) {
+      workspace = true
+    }
+    if (
+      notificationList[index - 2].author &&
+      notificationList[index - 1].author &&
+      notificationList[index].author &&
+      notificationList[index - 2].author.userId === notificationList[index - 1].author.userId &&
+      notificationList[index - 1].author.userId === notificationList[index].author.userId
+    ) {
+      author = true
+    }
+
+    newIndex++
+
+    if (content && workspace && author) {
+      newNotificationList.push({
+        ...notification,
+        read: notificationList[index - 2].read && notificationList[index - 1].read && notificationList[index].read,
+        type: 'group.content.workspace.author',
+        group: [notificationList[index - 2], notificationList[index - 1], notification]
+      })
+      return
+    }
+
+    if (content && workspace) {
+      newNotificationList.push({
+        ...notification,
+        read: notificationList[index - 2].read && notificationList[index - 1].read && notificationList[index].read,
+        type: 'group.content.workspace',
+        group: [notificationList[index - 2], notificationList[index - 1], notification]
+      })
+      return
+    }
+
+    if (content && author) {
+      newNotificationList.push({
+        ...notification,
+        read: notificationList[index - 2].read && notificationList[index - 1].read && notificationList[index].read,
+        type: 'group.content.author',
+        group: [notificationList[index - 2], notificationList[index - 1], notification]
+      })
+      return
+    }
+
+    if (author && workspace) {
+      newNotificationList.push({
+        ...notification,
+        read: notificationList[index - 2].read && notificationList[index - 1].read && notificationList[index].read,
+        type: 'group.author.workspace',
+        group: [notificationList[index - 2], notificationList[index - 1], notification]
+      })
+      return
+    }
+
+    newNotificationList.push(notificationList[index - 2]) // FIXME - Adding the wrong one
+  })
+  console.log('newNotificationList', newNotificationList)
+}
+
 export default function notificationPage (state = defaultNotificationsObject, action) {
   const mentionCreated = 'mention.created'
   switch (action.type) {
     case `${SET}/${NOTIFICATION_LIST}`: {
       const notificationList = action.notificationList
         .map(no => (serializeNotification(no)))
+      groupNotificationList(notificationList)
       return { ...state, list: uniqBy(notificationList, 'id') }
     }
 
