42ecdc44f25d070eb8fa1445b9cc8f622062de10
Marie Bégué <marie.begue@algoo.fr>
2021-09-14 11:30:01 +0200

    add test [skip ci pytest]

---
+++ functionnal_tests/cypress/integration/publication/toggle_comments.js
@@ -0,0 +1,30 @@
+import { PAGES } from '../../support/urls_commands'
+
+const publishButton = '.publications__publishArea__buttons__submit'
+const text = 'Hello, world'
+
+describe('Publications page', () => {
+  beforeEach(function () {
+    cy.resetDB()
+    cy.setupBaseDB()
+    cy.loginAs('administrators')
+    cy.fixture('baseWorkspace').as('workspace').then((workspace) => {
+      cy.visitPage({
+        pageName: PAGES.PUBLICATION,
+        params: { workspaceId: workspace.workspace_id },
+        waitForTlm: true
+      })
+    })
+  })
+
+  it('should have translations', () => {
+    cy.get('#wysiwygTimelineCommentPublication').type(text)
+    cy.contains(publishButton, 'Publish').click()
+
+    cy.contains('.buttonComments', 'Commenter').should('be.visible')
+    cy.contains('.buttonComments', 'Commenter').click()
+
+    cy.contains('.buttonComments', 'Masquer').should('be.visible')
+    cy.contains('.buttonComments', 'Masquer').click()
+  })
+})
