950f499927a778595a97be9e49e62594c90ce2e4
Guénaël Muller <guenael.muller@algoo.fr>
2021-09-15 10:27:14 +0200

    simple email validation

--- backend/tracim_backend/app_models/email_validators.py
+++ backend/tracim_backend/app_models/email_validators.py
@@ -1,26 +1,40 @@
 import re
 
 from marshmallow import ValidationError
-from marshmallow.validate import Email
+from marshmallow.validate import Validator
 
 
-class TracimEmailValidator(Email):
-    """ Special Email Validator who accept email from subdomain"""
+class TracimEmailValidator(Validator):
+    """
+    Special Email Validator who accept email from subdomain
+    - check existing "@".
+    - check if both part are not empty.
+    """
 
-    USER_REGEX = re.compile(
-        r"(^[-!#$%&'*+/=?^`{}|~\w]+(\.[-!#$%&'*+/=?^`{}|~\w]+)*\Z"  # dot-atom
-        # quoted-string
-        r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]' r'|\\[\001-\011\013\014\016-\177])*"\Z)',
-        re.IGNORECASE | re.UNICODE,
-    )
+    USER_REGEX = re.compile(r".+")
+    DOMAIN_REGEX = re.compile(r".+")
 
-    DOMAIN_REGEX = re.compile(
-        # domain
-        r"(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.?)+" r"(?:[A-Z]{2,6}|[A-Z0-9-]{2,})\Z"
-        # literal form, ipv4 address (SMTP 4.1.3)
-        r"|^\[(25[0-5]|2[0-4]\d|[0-1]?\d?\d)" r"(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}\]\Z",
-        re.IGNORECASE | re.UNICODE,
-    )
+    default_message = "Not a valid email address."
+
+    def __init__(self, error=None):
+        self.error = error or self.default_message
+
+    def _format_error(self, value):
+        return self.error.format(input=value)
+
+    def __call__(self, value):
+        message = self._format_error(value)
+
+        if not value or "@" not in value:
+            raise ValidationError(message)
+
+        user_part, domain_part = value.rsplit("@", 1)
+
+        if not self.USER_REGEX.match(user_part):
+            raise ValidationError(message)
+        if self.DOMAIN_REGEX.match(domain_part):
+            return value
+        raise ValidationError(message)
 
 
 class RFCEmailValidator(TracimEmailValidator):
