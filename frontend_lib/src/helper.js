81f832b6931b5535095c364f53894bd28b2f46ab
inkhey <guenael.muller@algoo.fr>
2021-09-15 19:17:18 +0200

    fix(backend, frontend): allow subdomain emails
    issue ref: #4912 #4056 
    pr ref: #4913

--- frontend_lib/src/helper.js
+++ frontend_lib/src/helper.js
@@ -449,13 +449,12 @@ export const parserStringToList = (string, separatorList = [',', ';', '\n']) => 
   return parsedString.split(',').filter(notEmptyString => notEmptyString !== '')
 }
 
-// INFO - GB - 2019-07-31 - This function check if the email has three parts arranged like somethig@something.somethig
+// INFO - GB - 2021-09-15 - This function checks if the string looks like an email (username@domain)
+// with a non empty username and a non-empty domain and without <, >, new line, comma or semicolon
 export const checkEmailValidity = email => {
-  const parts = email.split('@')
-  if (parts.length !== 2) return false
-
-  const domainParts = parts[1].split('.')
-  return domainParts.length === 2
+  const firstPart = email.substr(0, email.lastIndexOf('@'))
+  const secondPart = email.substr(email.lastIndexOf('@') + 1)
+  return firstPart !== '' && secondPart !== '' && !/[,;<>\n]/.test(email)
 }
 
 export const buildFilePreviewUrl = (apiUrl, workspaceId, contentId, revisionId, filenameNoExtension, page, width, height) => {
