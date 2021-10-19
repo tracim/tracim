f1fe070189376853282b9607744633979774022f
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2021-10-19 14:18:12 +0200

    fix and add tests

---
+++ functionnal_tests/cypress/integration/timeline/local_storage_spec.js
@@ -0,0 +1,36 @@
+import { PAGES } from '../../support/urls_commands'
+
+let workspaceId
+const noteTitle = 'Note'
+const commentField = '#wysiwygTimelineComment'
+const text = 'Hello world'
+
+describe('Timeline', () => {
+  before(function () {
+    cy.resetDB()
+    cy.setupBaseDB()
+    cy.loginAs('administrators')
+    cy.fixture('baseWorkspace').as('workspace').then(workspace => {
+      workspaceId = workspace.workspace_id
+      cy.createHtmlDocument(noteTitle, workspaceId)
+      cy.visitPage({ pageName: PAGES.CONTENTS, params: { workspaceId: workspaceId } })
+    })
+
+  })
+
+  afterEach(() => {
+    cy.cancelXHR()
+  })
+
+  it('should save the cooment draft', () => {
+    cy.contains('[data-cy=FilenameWithExtension__label]', noteTitle).click()
+    cy.get('.editionmode__button__cancel').click()
+    cy.get(commentField).type(text)
+    cy.visitPage({ pageName: PAGES.ACCOUNT })
+    cy.contains('.account__userpreference__setting', 'Change my account settings')
+    cy.visitPage({ pageName: PAGES.CONTENTS, params: { workspaceId: workspaceId } })
+    cy.contains('[data-cy=FilenameWithExtension__label]', noteTitle).click()
+    cy.get('.editionmode__button__cancel').click()
+    cy.contains(commentField, text)
+  })
+})
