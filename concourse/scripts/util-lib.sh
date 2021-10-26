02545069bd9447df653c72f4c478be76c17e4d97
Sébastien Grignard <sebastien.grignard@algoo.fr>
2021-10-26 16:50:17 +0200

    wip

--- concourse/scripts/util-lib.sh
+++ concourse/scripts/util-lib.sh
@@ -3,8 +3,9 @@
 # Arguments are given to grep so a regex/complex expression is possible.
 # Changed files are read in concourse's resource change_files.
 skip_if_no_changed_file_match() {
-    if [ -f .git/resource/changed_files ] && $(egrep -q $@ .git/resource/changed_files); then
-        echo "Changed files do not match $1, skipping the task."
+    if [ -f .git/resource/changed_files ] && ! $(egrep -q "$@" .git/resource/changed_files); then
+        echo "Changed files do not match '$@', skipping the task."
         exit 0
     fi
+    echo "Found files matching '$@', running the task"
 }
