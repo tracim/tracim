c7b48851976766e6935bffbf7ac2bb7d948ec6f1
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2021-10-27 10:59:41 +0200

    use style variables and delete forgotten comments

--- frontend/src/component/PublicProfile/ProfileMainBar.jsx
+++ frontend/src/component/PublicProfile/ProfileMainBar.jsx
@@ -68,22 +68,11 @@ export const ProfileMainBar = props => {
             </div>
             {props.system.config.call__enabled && props.user.userId !== props.displayedUser.userId && (
               <IconButton
-                title={props.t('Call')}
                 text={props.t('Call')}
                 icon='fas fa-phone'
-                onClick={props.onClickDisplayCallPopup} // ici mettre le state createNewCall
+                onClick={props.onClickDisplayCallPopup}
                 customClass='profile__mainBar__callBtn'
-                // intent='secondary'
-                // dataCy='profile_avatar_changeBtn'
               />
-              // <IconButton
-              //   text={props.t('Change cover')}
-              //   icon='fas fa-upload'
-              //   onClick={props.onChangeCoverClick}
-              //   customClass='profile__cover__changeBtn'
-              //   intent='secondary'
-              //   dataCy='profile_cover_changeBtn'
-              // />
             )}
           </div>
         )
@@ -102,7 +91,7 @@ ProfileMainBar.propTypes = {
   displayedUser: PropTypes.object,
   onChangeAvatarClick: PropTypes.func.isRequired,
   changeAvatarEnabled: PropTypes.bool,
-  onClickDisplayCallPopup: PropTypes.func // voir avec boolean
+  onClickDisplayCallPopup: PropTypes.func
 }
 
 ProfileMainBar.defaultProps = {
