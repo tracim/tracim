288f16ac9d6e127962bd837ca3442d6e631b1557
Marie Bégué <marie.begue@algoo.fr>
2021-11-08 11:26:56 +0100

    keep cypress timeout fix and check if it works

--- functionnal_tests/cypress/integration/app_workspace_advanced/operation_workspace_advanced.js
+++ functionnal_tests/cypress/integration/app_workspace_advanced/operation_workspace_advanced.js
@@ -8,7 +8,6 @@ describe('App Workspace Advanced', function () {
   before(() => {
     cy.resetDB()
     cy.setupBaseDB()
-    cy.loginAs('administrators')
     cy.fixture('baseWorkspace').as('workspace').then(workspace => {
       workspaceId = workspace.workspace_id
     })
@@ -17,7 +16,12 @@ describe('App Workspace Advanced', function () {
   beforeEach(function () {
     cy.loginAs('administrators')
     cy.visitPage({ pageName: p.DASHBOARD, params: { workspaceId } })
-    cy.contains('.userstatus__role__text', 'Space manager')
+
+    // NOTE - MP - 05-11-2021 - If the page isn't loaded after 30s
+    // there is a problem somewhere
+    cy.get('.userstatus__role__text', { timeout: 30000 })
+      .contains('Space manager')
+
     cy.getTag({ selectorName: s.WORKSPACE_DASHBOARD })
       .find('.dashboard__workspace__detail__buttons .iconbutton')
       .click()
