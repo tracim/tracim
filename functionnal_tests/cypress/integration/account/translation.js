b4b28d1bd38a89cececa46b4357ffbc66a6c69db
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2021-12-07 15:02:08 +0100

    fix and comment functionnal tests

--- functionnal_tests/cypress/integration/account/translation.js
+++ functionnal_tests/cypress/integration/account/translation.js
@@ -4,22 +4,49 @@ describe('Account page', function () {
   before(() => {
     cy.resetDB()
     cy.setupBaseDB()
+  })
+
+  beforeEach(() => {
     cy.loginAs('administrators')
     cy.visitPage({ pageName: PAGES.ACCOUNT })
   })
 
-  it('should have translations', () => {
-    cy.get('.account__userpreference__setting').contains('Change my account settings')
+  it('should have english translations', () => {
+    cy.changeLanguage('en')
+    cy.contains('.account__userpreference__setting', 'Change my account settings')
+    cy.get('[data-cy=menusubcomponent__list__spacesConfig]')
+      .should('be.visible')
+      .click()
+    cy.contains('.iconbutton__text_with_icon', 'Manage user spaces')
+      .click()
+    cy.contains('.cardPopup__header__title', 'Space management of the user Global manager')
+  })
 
+  it('should have french translations', () => {
     cy.changeLanguage('fr')
-    cy.get('.account__userpreference__setting').contains('Changer les paramètres de mon compte')
+    cy.contains('.account__userpreference__setting', 'Changer les paramètres de mon compte')
+    cy.get('[data-cy=menusubcomponent__list__spacesConfig]')
+      .should('be.visible')
+      .click()
+    cy.contains('.iconbutton__text_with_icon', "Gérer les espaces de l'utilisateur")
+      .click()
+    cy.contains('.cardPopup__header__title', "Gestion des espaces de l'utilisateur Global manager")
+  })
 
+  it('should have portuguese translations', () => {
     cy.changeLanguage('pt')
-    cy.get('.account__userpreference__setting').contains('Mudar minhas definições de conta')
+    cy.contains('.account__userpreference__setting', 'Mudar minhas definições de conta')
+    cy.get('[data-cy=menusubcomponent__list__spacesConfig]')
+      .should('be.visible')
+      .click()
+    cy.contains('.iconbutton__text_with_icon', 'Gerir os espaços do utilizador')
+      .click()
+    cy.contains('.cardPopup__header__title', 'Gestão dos espaços do utilizador Global manager')
+  })
 
+  it('should have german translations', () => {
     cy.changeLanguage('de')
     cy.contains('.account__userpreference__setting', 'Meine Kontoeinstellungen ändern')
-
     cy.get('[data-cy=menusubcomponent__list__spacesConfig]')
       .should('be.visible')
       .click()
