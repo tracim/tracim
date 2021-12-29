cfe3462f1d5418b65d5d7aa47eee2d727810d416
Skylsmoi <come.huguies@gmail.com>
2021-12-29 10:03:48 +0100

    feat(front): code cleaning

--- frontend_lib/webpack.servdev.config.js
+++ frontend_lib/webpack.servdev.config.js
@@ -6,13 +6,13 @@ module.exports.output.library = undefined
 
 module.exports.devServer = {
   contentBase: path.join(__dirname, 'dist/'),
-    proxy: { '/api': 'http://127.0.0.1:7999' },
+  proxy: { '/api': 'http://127.0.0.1:7999' },
   port: 8070,
-    hot: true,
-    noInfo: true,
-    overlay: {
-    warnings: false,
-      errors: true
+  hot: true,
+  noInfo: true,
+  overlay: {
+  warnings: false,
+    errors: true
   },
   historyApiFallback: true
   // headers: {
