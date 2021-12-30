0b0a0f41ac9dcd32ad0d973235f88629acc8ac3c
Skylsmoi <come.huguies@gmail.com>
2021-12-30 15:53:22 +0100

    feat(front): refactor app space

--- frontend_app_workspace/build_app.sh
+++ frontend_app_workspace/build_app.sh
@@ -24,15 +24,15 @@ function logerror {
 
 dev=""
 if [ "$1" = "-d" ]; then
-    dev="-dev"
+    dev=":dev"
 fi
 
-log "creating debug file"
-cp src/debug.js.sample src/debug.js
+log "creating debug file if not already exists"
+cp -u src/debug.js.sample src/debug.js
 log "building frontend_app_workspace"
-yarn run buildoptimized$dev && loggood "success" || logerror "some error"
+yarn run build:optimized$dev && loggood "success" || logerror "some error"
 log "copying built file to frontend/"
-cp dist/workspace.app.js ../frontend/dist/app/workspace.app.js && loggood "success" || logerror "some error"
+cp dist/workspace.app.optimized.js ../frontend/dist/app/workspace.app.optimized.js && loggood "success" || logerror "some error"
 log "copying en translation.json"
 cp i18next.scanner/en/translation.json ../frontend/dist/app/workspace_en_translation.json && loggood "success" || logerror "some error"
 log "copying fr translation.json"
