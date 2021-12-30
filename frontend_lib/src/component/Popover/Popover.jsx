c5484fb187e15b37da3c8f034b7a114927381cf5
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2021-12-30 16:17:27 +0100

    fix unit tests

--- frontend_lib/src/component/Popover/Popover.jsx
+++ frontend_lib/src/component/Popover/Popover.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { isMobile } from 'react-device-detect'
 import {
@@ -6,23 +6,33 @@ import {
   PopoverBody
 } from 'reactstrap'
 
-function Popover (props) {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+class Popover extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isPopoverOpen: false
+    }
+  }
 
-  return (
-    <ReactStrapPopover
-      placement={props.placement}
-      isOpen={isPopoverOpen}
-      target={props.targetId}
-      // INFO - GB - 2021-12-29 - ignoring rule react/jsx-handler-names for prop bellow because it comes from external lib
-      toggle={() => setIsPopoverOpen(!isPopoverOpen)} // eslint-disable-line react/jsx-handler-names
-      trigger={isMobile ? 'click' : 'hover'}
-    >
-      <PopoverBody>
-        {props.popoverBody}
-      </PopoverBody>
-    </ReactStrapPopover>
-  )
+  handleTogglePopover = () => this.setState(prev => ({ isPopoverOpen: !prev.isPopoverOpen }))
+
+  render () {
+    const { props, state } = this
+    return (
+      <ReactStrapPopover
+        placement={props.placement}
+        isOpen={state.isPopoverOpen}
+        target={props.targetId}
+        // INFO - GB - 2021-12-29 - ignoring rule react/jsx-handler-names for prop bellow because it comes from external lib
+        toggle={this.handleTogglePopover} // eslint-disable-line react/jsx-handler-names
+        trigger={isMobile ? 'click' : 'hover'}
+      >
+        <PopoverBody>
+          {props.popoverBody}
+        </PopoverBody>
+      </ReactStrapPopover>
+    )
+  }
 }
 export default Popover
 
