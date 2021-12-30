65c377a737229023aad4313e1c6e6b4f5b72dd1b
Skylsmoi <come.huguies@gmail.com>
2021-12-30 14:55:24 +0100

    feat(front): refactor app gallery

--- frontend_app_gallery/webpack.standalone.config.js
+++ frontend_app_gallery/webpack.standalone.config.js
@@ -13,19 +13,6 @@ module.exports = {
     library: 'appGallery',
     libraryTarget: isProduction ? 'var' : undefined
   },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist/'),
-    proxy: { '/api': 'http://127.0.0.1:7999', '/assets': 'http://127.0.0.1:7999' },
-    host: '0.0.0.0',
-    port: 8081,
-    hot: true,
-    noInfo: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    historyApiFallback: true
-  },
   devtool: isProduction ? false : 'eval-cheap-module-source-map',
   performance: {
     hints: false
