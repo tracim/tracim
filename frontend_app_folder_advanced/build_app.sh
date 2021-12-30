5124f94c9c53f8e017de405301267c91e400e7ed
Skylsmoi <come.huguies@gmail.com>
2021-12-30 14:51:35 +0100

    feat(front): refactor app folder advanced

--- frontend_app_folder_advanced/build_app.sh
+++ frontend_app_folder_advanced/build_app.sh
@@ -24,15 +24,15 @@ function logerror {
 
 dev=""
 if [ "$1" = "-d" ]; then
-    dev="-dev"
+    dev=":dev"
 fi
 
-log "creating debug file"
-cp src/debug.js.sample src/debug.js
+log "creating debug file if not already exists"
+cp -u src/debug.js.sample src/debug.js
 log "building frontend_app_folder"
-yarn run buildoptimized$dev && loggood "success" || logerror "some error"
+yarn run build:optimized$dev && loggood "success" || logerror "some error"
 log "copying built file to frontend/"
-cp dist/folder.app.js ../frontend/dist/app/folder.app.js && loggood "success" || logerror "some error"
+cp dist/folder.app.optimized.js ../frontend/dist/app/folder.app.optimized.js && loggood "success" || logerror "some error"
 log "copying en translation.json"
 cp i18next.scanner/en/translation.json ../frontend/dist/app/folder_en_translation.json && loggood "success" || logerror "some error"
 log "copying fr translation.json"
