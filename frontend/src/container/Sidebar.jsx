86018d9c4d804346a39f973c1ff9aff92004859d
Mathis Perrier <mathis.perrier@algoo.fr>
2022-01-27 16:51:50 +0100

    Fix linting test
    Had unused import

--- frontend/src/container/Sidebar.jsx
+++ frontend/src/container/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import i18next from 'i18next'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import classnames from 'classnames'
