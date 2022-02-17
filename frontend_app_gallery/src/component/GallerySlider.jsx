ff06ecb2f90469c06cd1370e2b6811b07bf669fe
Sébastien Grignard <51887114+grignards@users.noreply.github.com>
2022-02-17 09:26:20 +0100

    Update frontend_app_gallery/src/component/GallerySlider.jsx
    Co-authored-by: raphj <3817365+raphj@users.noreply.github.com>

--- frontend_app_gallery/src/component/GallerySlider.jsx
+++ frontend_app_gallery/src/component/GallerySlider.jsx
@@ -3,6 +3,7 @@ import Slider from 'react-slick'
 // NOTE 2022-02-16 - SG - Extending Slider
 // to enforce the right initial slide selection at start as 'initialIndex'
 // of react-slick does not work in every case (when several slides are displayed)
+// use the displayedPictureIndex prop provided by this class instead
 export default class GallerySlider extends Slider {
   constructor (props) {
     super(props)
