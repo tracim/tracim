f16615941c4a55fb61449f50fe125f98d7c006b6
Guénaël Muller <guenael.muller@algoo.fr>
2022-02-21 15:40:43 +0100

    Revert "add minimal support for image preview of image"
    This reverts commit 1e649c905f8418b03247ab68e0242d9ad91ae3cd.

--- backend/tracim_backend/lib/core/url_preview.py
+++ backend/tracim_backend/lib/core/url_preview.py
@@ -1,17 +1,12 @@
 from typing import Optional
 from urllib.parse import urljoin
-from urllib.parse import urlparse
 
-import requests
-from requests import Response
 from requests.exceptions import InvalidURL
 from webpreview import WebpreviewException
 from webpreview import web_preview
 
 from tracim_backend.config import CFG
 from tracim_backend.exceptions import UnavailableURLPreview
-
-MAX_CONTENT_LENGTH_FOR_URL_PREVIEW = 1048576  # 1Mo
 
 # NOTE - SG - 2021-04-16: uncomment those lines to debug the request headers/response
 # import http.client
@@ -44,62 +39,25 @@ class URLPreview:
         self.image = image
 
 
-def get_filename_from_response(response: Response):
-    # TODO - G.M - 2022-02-16 - use content-disposition header if exist isntead
-    # of url path.
-    # this is tricky as we need to decode it if utf-8 encoded.
-    return urlparse(response.url).path.rsplit("/", 1)[-1]
-
-
 class URLPreviewLib(object):
     def __init__(self, config: CFG,) -> None:
         self.app_config = config
 
     def get_preview(self, url: str) -> URLPreview:
-        with requests.get(
-            url,
-            timeout=self.app_config.URL_PREVIEW__FETCH_TIMEOUT,
-            stream=True,
-            headers={
-                "User-Agent": "Mozilla/5.0 (compatible; Tracim bot; +https://www.tracim.fr)",
-                "Accept": "image/*,text/html",
-                # INFO - SG - 2021-04-16 - Needed to bypass google and youtube cookie consent banner
-                # See https://github.com/tracim/tracim/issues/4470
-                "Cookie": "CONSENT=PENDING+999",
-            },
-        ) as r:
-            content_type = r.headers.get("content-type")
-            content_length = r.headers.get("content-length")
-            title = get_filename_from_response(r)
-            description = content_type
-            image_url = None
-            if not content_type:
-                pass
-            elif content_type.startswith("text/html"):
-                try:
-                    title, description, image_url = web_preview(
-                        url,
-                        timeout=self.app_config.URL_PREVIEW__FETCH_TIMEOUT,
-                        headers={
-                            "User-Agent": "Mozilla/5.0 (compatible; Tracim bot; +https://www.tracim.fr)",
-                            "Accept": "text/html",
-                            # INFO - SG - 2021-04-16 - Needed to bypass google and youtube cookie consent banner
-                            # See https://github.com/tracim/tracim/issues/4470
-                            "Cookie": "CONSENT=PENDING+999",
-                        },
-                    )
-                except (WebpreviewException, InvalidURL) as exc:
-                    raise UnavailableURLPreview(
-                        'Can\'t generate URL preview for "{}"'.format(url)
-                    ) from exc
-                image_url = urljoin(url, image_url) if image_url else None
-            if not content_length:
-                pass
-            elif int(content_length) > MAX_CONTENT_LENGTH_FOR_URL_PREVIEW:
-                pass
-            elif content_type and content_type.startswith("image/"):
-                description = content_type
-                image_url = url
-            else:
-                pass
+
+        try:
+            title, description, image_url = web_preview(
+                url,
+                timeout=self.app_config.URL_PREVIEW__FETCH_TIMEOUT,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (compatible; Tracim bot; +https://www.tracim.fr)",
+                    "Accept": "image/*,text/html;q=0.8",
+                    # INFO - SG - 2021-04-16 - Needed to bypass google and youtube cookie consent banner
+                    # See https://github.com/tracim/tracim/issues/4470
+                    "Cookie": "CONSENT=PENDING+999",
+                },
+            )
+        except (WebpreviewException, InvalidURL) as exc:
+            raise UnavailableURLPreview('Can\'t generate URL preview for "{}"'.format(url)) from exc
+        image_url = urljoin(url, image_url) if image_url else None
         return URLPreview(title=title, description=description, image=image_url)
