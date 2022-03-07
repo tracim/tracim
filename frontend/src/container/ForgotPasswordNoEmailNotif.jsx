dcc491b96bc38548fb5cc75880de74e7692e541f
Skylsmoi <come.huguies@gmail.com>
2022-03-07 15:01:32 +0100

    fix(front): refactor ForgotPasswordNoEmailNotif.jsx to use hooks

--- frontend/src/container/ForgotPasswordNoEmailNotif.jsx
+++ frontend/src/container/ForgotPasswordNoEmailNotif.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { translate } from 'react-i18next'
 import Card from '../component/common/Card/Card.jsx'
@@ -9,94 +9,79 @@ import { resetBreadcrumbs, setConfig, setHeadTitle } from '../action-creator.syn
 import { CUSTOM_EVENT, IconButton, PAGE } from 'tracim_frontend_lib'
 import { getConfig } from '../action-creator.async'
 
-export class ForgotPasswordNoEmailNotif extends React.Component {
-  constructor (props) {
-    super(props)
+export const ForgotPasswordNoEmailNotif = props => {
+  useEffect(() => {
+    document.addEventListener(CUSTOM_EVENT.APP_CUSTOM_EVENT_LISTENER, customEventReducer)
+    return () => {
+      document.removeEventListener(CUSTOM_EVENT.APP_CUSTOM_EVENT_LISTENER, customEventReducer)
+    }
+  }, [])
 
-    document.addEventListener(CUSTOM_EVENT.APP_CUSTOM_EVENT_LISTENER, this.customEventReducer)
-  }
+  useEffect(() => {
+    props.dispatch(resetBreadcrumbs())
+    if (!props.system.config.instance_name) loadConfig()
+  }, [])
 
-  customEventReducer = ({ detail: { type } }) => {
+  useEffect(() => {
+    updateHeadTitle()
+  }, [props.system.config.instance_name])
+
+  const customEventReducer = ({ detail: { type } }) => {
     switch (type) {
       case CUSTOM_EVENT.ALL_APP_CHANGE_LANGUAGE:
-        this.setHeadTitle()
+        updateHeadTitle()
         break
     }
   }
 
-  componentDidUpdate (prevProps, prevState) {
-    const { props } = this
-
-    if (prevProps.system.config.instance_name !== props.system.config.instance_name) {
-      this.setHeadTitle()
-    }
-  }
-
-  componentDidMount () {
-    this.setHeadTitle()
-    const { props } = this
-
-    props.dispatch(resetBreadcrumbs())
-    if (!props.system.config.instance_name) this.loadConfig()
-  }
-
-  loadConfig = async () => {
-    const { props } = this
-
+  const loadConfig = async () => {
     const fetchGetConfig = await props.dispatch(getConfig())
     if (fetchGetConfig.status === 200) {
       props.dispatch(setConfig(fetchGetConfig.json))
     }
   }
 
-  setHeadTitle = () => {
-    const { props } = this
-
+  const updateHeadTitle = () => {
     props.dispatch(setHeadTitle(props.t('Forgotten password')))
   }
 
-  handleClickBack = () => this.props.history.push(PAGE.LOGIN)
+  const handleClickBack = () => props.history.push(PAGE.LOGIN)
 
-  render () {
-    const { props } = this
+  return (
+    <section className='forgotpassword'>
+      <Card customClass='forgotpassword__card'>
+        <CardHeader customClass='forgotpassword__card__header primaryColorBgLighten text-center'>
+          {props.t('Did you forget your password?')}
+        </CardHeader>
 
-    return (
-      <section className='forgotpassword'>
-        <Card customClass='forgotpassword__card'>
-          <CardHeader customClass='forgotpassword__card__header primaryColorBgLighten text-center'>
-            {props.t('Did you forget your password?')}
-          </CardHeader>
-
-          <CardBody formClass='forgotpassword__card__body'>
-            <div>
-              <div className='forgotpassword__card__body__submsg'>
-                {props.t("Unfortunately, email notifications aren't activated, we can't send you a reset password email")}.
-              </div>
-
-              <div className='forgotpassword__card__body__submsg'>
-                {props.t('To change your password, please refer to an administrator')}.
-              </div>
-
-              <div className='forgotpassword__card__body__btn-no-email'>
-                <IconButton
-                  customClass='forgotpassword__card__body__btnsubmit'
-                  intent='primary'
-                  mode='light'
-                  onClick={this.handleClickBack}
-                  icon='fas fa-arrow-left'
-                  text={props.t('Go back to previous page')}
-                />
-              </div>
+        <CardBody formClass='forgotpassword__card__body'>
+          <div>
+            <div className='forgotpassword__card__body__submsg'>
+              {props.t("Unfortunately, email notifications aren't activated, we can't send you a reset password email")}.
             </div>
-          </CardBody>
-        </Card>
 
-        <FooterLogin />
-      </section>
-    )
-  }
+            <div className='forgotpassword__card__body__submsg'>
+              {props.t('To change your password, please refer to an administrator')}.
+            </div>
+
+            <div className='forgotpassword__card__body__btn-no-email'>
+              <IconButton
+                customClass='forgotpassword__card__body__btnsubmit'
+                intent='primary'
+                mode='light'
+                onClick={handleClickBack}
+                icon='fas fa-arrow-left'
+                text={props.t('Go back to previous page')}
+              />
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+
+      <FooterLogin />
+    </section>
+  )
 }
 
 const mapStateToProps = ({ system }) => ({ system })
-
 export default connect(mapStateToProps)(translate()(ForgotPasswordNoEmailNotif))
