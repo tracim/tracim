5befb07c1fe0b34c032142724fbcb00207c5eb8a
Mathis Perrier <mathis.perrier73@gmail.com>
2022-03-16 17:12:54 +0100

    feat(frontend): notification refactoring
    issue ref: #5497 
    pr ref: #5521
    
    Co-authored-by: Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>

--- frontend/src/container/GroupedNotificationItem.jsx
+++ frontend/src/container/GroupedNotificationItem.jsx
@@ -1,213 +1,175 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import classnames from 'classnames'
 import { translate } from 'react-i18next'
 import PropTypes from 'prop-types'
 import {
-  FETCH_CONFIG,
-  GROUP_NOTIFICATION_CRITERIA
-} from '../util/helper.js'
-import {
-  AVATAR_SIZE,
   PAGE,
   TLM_SUB_TYPE as TLM_SUB,
-  Avatar,
-  Icon,
-  TracimComponent,
-  formatAbsoluteDate
+  TracimComponent
 } from 'tracim_frontend_lib'
-import { updateNotification } from '../action-creator.sync'
+import NotificationItem from '../component/NotificationItem.jsx'
+import GroupRender from '../../GroupedNotificationItem/GroupRender.jsx'
+// import putNotificationAsRead from '../action-creator.async.js'
 import { escape as escapeHtml, uniqBy } from 'lodash'
 
-function getGroupedNotificationDetails (props, notification) {
-  const { getNotificationDetails, t } = props
+export const GroupedNotificationItem = props => {
+  const [notificationList, setNotificationList] = useState([])
+  const [isGrouped, setIsGrouped] = useState(true)
 
-  const numberOfContributionTypes = uniqBy(notification.group, 'type').length
-  const numberOfWorkspaces = uniqBy(notification.group.map(notification => notification.workspace), 'id').length
-  const numberOfContents = uniqBy(notification.group.filter(notification => notification.content)
-    .map(notification => {
-      return notification.content.type === TLM_SUB.COMMENT
-        ? notification.content.parentId
-        : notification.content.id
-    })).length
-
-  let escapedAuthorList = ''
-  if (notification.author) {
-    if (notification.author.length === 1) {
-      escapedAuthorList = escapeHtml(notification.author[0].publicName)
-    } else if (notification.author.length === 2) {
-      escapedAuthorList = `${escapeHtml(notification.author[0].publicName)} ${t('and')} ${escapeHtml(notification.author[1].publicName)}`
-    } else {
-      escapedAuthorList = `${escapeHtml(notification.author[0].publicName)} ${t('and {{numberOfAuthors}} other people', { numberOfAuthors: notification.author.length - 1 })}`
-    }
-  }
-
-  let escapedContentLabel = ''
-  if (notification.group.some(notification => notification.content)) {
-    if (numberOfContents > 1) {
-      escapedContentLabel = t('{{numberOfContents}} contents', { numberOfContents: numberOfContents })
-    } else {
-      const content = notification.group.find(notification => notification.content).content
-      escapedContentLabel = escapeHtml(content.type === TLM_SUB.COMMENT
-        ? content.parentLabel
-        : content.label
-      )
-    }
-  }
-
-  const userList = uniqBy(notification.group.map(notification => notification.user), 'userId')
-  const escapedUser = userList.length === 1 && userList[0]
-    ? escapeHtml(userList[0].publicName)
-    : t('{{numberOfUsers}} users', { numberOfUsers: userList.length })
-
-  const i18nOpts = {
-    user: `<span title='${escapedUser}'>${escapedUser}</span>`,
-    author: `<span title='${escapedAuthorList}'>${escapedAuthorList}</span>`,
-    content: `<span title='${escapedContentLabel}' class='contentTitle__highlight'>${escapedContentLabel}</span>`,
-    numberOfContribution: notification.group.length,
-    numberOfWorkspaces: numberOfWorkspaces,
-    interpolation: { escapeValue: false }
-  }
-
-  if (numberOfContributionTypes > 1) {
-    let contentUrl
-    if (numberOfContents === 1) {
-      const content = notification.group.find(notification => notification.content).content
-      contentUrl = PAGE.CONTENT(content.type === TLM_SUB.COMMENT
-        ? content.parentId
-        : content.id
-      )
-    }
-
-    return {
-      text: numberOfWorkspaces > 1
-        ? t('{{author}} made {{numberOfContribution}} contributions in {{numberOfWorkspaces}} spaces', i18nOpts)
-        : numberOfContents === 1
-          ? t('{{author}} made {{numberOfContribution}} contributions on {{content}}', i18nOpts)
-          : t('{{author}} made {{numberOfContribution}} contributions', i18nOpts),
-      url: contentUrl
-    }
-  } else {
-    const notificationDetails = getNotificationDetails({
-      ...notification.group[0],
-      author: { publicName: escapedAuthorList },
-      content: { ...notification.group[0].content, label: escapedContentLabel, parentLabel: escapedContentLabel },
-      numberOfWorkspaces: numberOfWorkspaces,
-      numberOfContents: numberOfContents,
-      type: notification.group[0].type,
-      user: { ...notification.group[0].user, publicName: escapedUser }
+  useEffect(() => {
+    const tmpNotificationList = []
+    props.groupedNotifications.group.forEach(notification => {
+      tmpNotificationList.push(notification)
     })
+    setNotificationList(tmpNotificationList)
+  }, [props.groupedNotifications])
 
-    if (numberOfContents !== 1) delete notificationDetails.url
-    return notificationDetails
+  const getGroupedNotificationDetails = (notification) => {
+    const numberOfContributionTypes = uniqBy(notification.group, 'type').length
+    const numberOfWorkspaces = uniqBy(notification.group.map(notification => notification.workspace), 'id').length
+    const numberOfContents = uniqBy(notification.group.filter(notification => notification.content)
+      .map(notification => {
+        return notification.content.type === TLM_SUB.COMMENT
+          ? notification.content.parentId
+          : notification.content.id
+      })).length
+
+    let escapedAuthorList = ''
+    if (notification.author) {
+      if (notification.author.length === 1) {
+        escapedAuthorList = escapeHtml(notification.author[0].publicName)
+      } else if (notification.author.length === 2) {
+        escapedAuthorList = `${escapeHtml(notification.author[0].publicName)} ${props.t('and')} ${escapeHtml(notification.author[1].publicName)}`
+      } else {
+        escapedAuthorList = `${escapeHtml(notification.author[0].publicName)} ${props.t('and {{numberOfAuthors}} other people', { numberOfAuthors: notification.author.length - 1 })}`
+      }
+    }
+
+    let escapedContentLabel = ''
+    if (notification.group.some(notification => notification.content)) {
+      if (numberOfContents > 1) {
+        escapedContentLabel = props.t('{{numberOfContents}} contents', { numberOfContents: numberOfContents })
+      } else {
+        const content = notification.group.find(notification => notification.content).content
+        escapedContentLabel = escapeHtml(content.type === TLM_SUB.COMMENT
+          ? content.parentLabel
+          : content.label
+        )
+      }
+    }
+
+    const userList = uniqBy(notification.group.map(notification => notification.user), 'userId')
+    const escapedUser = userList.length === 1 && userList[0]
+      ? escapeHtml(userList[0].publicName)
+      : props.t('{{numberOfUsers}} users', { numberOfUsers: userList.length })
+
+    const i18nOpts = {
+      user: `<span title='${escapedUser}'>${escapedUser}</span>`,
+      author: `<span title='${escapedAuthorList}'>${escapedAuthorList}</span>`,
+      content: `<span title='${escapedContentLabel}' class='contentTitle__highlight'>${escapedContentLabel}</span>`,
+      numberOfContribution: notification.group.length,
+      numberOfWorkspaces: numberOfWorkspaces,
+      interpolation: { escapeValue: false }
+    }
+
+    if (numberOfContributionTypes > 1) {
+      let contentUrl
+      if (numberOfContents === 1) {
+        const content = notification.group.find(notification => notification.content).content
+        contentUrl = PAGE.CONTENT(content.type === TLM_SUB.COMMENT
+          ? content.parentId
+          : content.id
+        )
+      }
+
+      return {
+        text: numberOfWorkspaces > 1
+          ? props.t('{{author}} made {{numberOfContribution}} contributions in {{numberOfWorkspaces}} spaces', i18nOpts)
+          : numberOfContents === 1
+            ? props.t('{{author}} made {{numberOfContribution}} contributions on {{content}}', i18nOpts)
+            : props.t('{{author}} made {{numberOfContribution}} contributions', i18nOpts),
+        url: contentUrl
+      }
+    } else {
+      const notificationDetails = props.getNotificationDetails({
+        ...notification.group[0],
+        author: { publicName: escapedAuthorList },
+        content: { ...notification.group[0].content, label: escapedContentLabel, parentLabel: escapedContentLabel },
+        numberOfWorkspaces: numberOfWorkspaces,
+        numberOfContents: numberOfContents,
+        type: notification.group[0].type,
+        user: { ...notification.group[0].user, publicName: escapedUser }
+      })
+
+      if (numberOfContents !== 1) delete notificationDetails.url
+      return notificationDetails
+    }
   }
-}
 
-function handleClickGroupedNotification (props, e, notification) {
-  // INFO - GB - 2021-09-01 - A grouped notification is only updated on click if it's not grouped by content, in this case it just redirects to the content
-  if (notification.type.includes(GROUP_NOTIFICATION_CRITERIA.CONTENT)) {
-    notification.group.forEach(notification => props.onClickNotification(e, notification.id, {
-      url: PAGE.CONTENT(notification.content.type === TLM_SUB.COMMENT
-        ? notification.content.parentId
-        : notification.content.id)
-    }))
-  } else {
-    props.dispatch(updateNotification(notification.id, notification.group))
+  // FIXME - MP - 14-03-2022 - Function removed so I can safely push
+  // However this is a regression, we can't put a group notification as read
+  // Issue : https://github.com/tracim/tracim/issues/5526
+  const handleReadGroupNotification = async (groupNotification) => {
+  //   // TODO - MP - 14-03-2022
+  //   // Create a props.dispatch(putGroupNotificationsAsRead(props.user.userId, groupNotification.group))
+  //   // Where it will read every notification in groupNotification.group
+  //   // related to https://github.com/tracim/tracim/issues/5526
+  //
+  //   const fetchPutNotificationAsRead = { status = 404 }
+  //   switch (fetchPutNotificationAsRead.status) {
+  //     case 204: {
+  //       props.dispatch(readGroupNotification(notificationId))
+  //       break
+  //     }
+  //     default:
+  //       props.dispatch(newFlashMessage(props.t('Error while marking the notification as read'), 'warning'))
+  //   }
   }
-}
 
-const GroupedNotificationItem = props => {
-  const { notification, user } = props
-  const notificationDetails = getGroupedNotificationDetails(props, notification)
+  const handleClickGroupedNotification = () => {
+    // NOTE - MP - 14-03-2022 - Since we redirect to the content if this is a group about a content
+    // we close the wall, then we ungroup the group
+    if (notificationDetails.url) {
+      props.onCloseNotificationWall()
+    }
+    setIsGrouped(false)
+  }
+
+  const notificationDetails = getGroupedNotificationDetails(props.groupedNotifications)
   if (Object.keys(notificationDetails).length === 0) return null
 
-  const numberOfWorkspaces = uniqBy(notification.group.map(notification => notification.workspace), 'id').length
-  const numberOfAuthors = notification.author.length
-  const readStatus = notification.group.map(notification => notification.read)
-    .reduce((acc, current) => acc && current)
-
-  return (
-    <Link
-      to={notificationDetails.url || '#'}
-      onClick={(e) => handleClickGroupedNotification(props, e, notification)}
-      className={classnames('notification__list__item',
-        { itemRead: readStatus, isMention: notificationDetails.isMention }
-      )}
-      key={notification.id}
-    >
-      <div className='notification__list__item__text'>
-        {numberOfAuthors <= 2
-          ? (
-            <div className='notification__list__item__text__avatars'>
-              <Avatar
-                size={numberOfAuthors === 2 ? AVATAR_SIZE.MINI : AVATAR_SIZE.SMALL}
-                apiUrl={FETCH_CONFIG.apiUrl}
-                user={notification.author[0]}
-              />
-              {numberOfAuthors === 2 && (
-                <Avatar
-                  size={AVATAR_SIZE.MINI}
-                  apiUrl={FETCH_CONFIG.apiUrl}
-                  user={notification.author[1]}
-                  customClass='notification__list__item__text__avatars__second'
-                />
-              )}
-            </div>
-          ) : (
-            <Icon
-              customClass='notification__list__item__text__usersAvatar'
-              icon='fas fa-fw fa-users'
-              color='#fdfdfd' // INFO - GB - 2021-08-26 - offWhite color
-              title={notification.author.map(author => author.publicName)}
-            />
-          )}
-        <span
-          className='notification__list__item__text__content'
-          dangerouslySetInnerHTML={{
-            __html: (
-              notificationDetails.text + ' ' +
-              `<span title='${escapeHtml(formatAbsoluteDate(notification.created, user.lang))}'></span>`
-            )
-          }}
+  const listRender =
+    notificationList.map((notification) => {
+      return (
+        <NotificationItem
+          onCloseNotificationWall={props.onCloseNotificationWall}
+          getNotificationDetails={props.getNotificationDetails}
+          key={notification.id}
+          notification={notification}
         />
-      </div>
-      <div className='notification__list__item__meta'>
-        <div
-          className='notification__list__item__meta__date'
-          title={formatAbsoluteDate(notification.created, user.lang)}
-        >
-          {props.shortDate}
-        </div>
-        <div className='notification__list__item__meta__space'>
-          {(numberOfWorkspaces === 1 && notification.group[0].workspace &&
-            notification.group[0].workspace.label
-          )}
-        </div>
-      </div>
-      <div className='notification__list__item__circle__wrapper'>
-        {!readStatus &&
-          <i
-            className='notification__list__item__circle fas fa-circle'
-            onClick={(e) => {
-              notification.group.forEach(n => {
-                props.onClickCircle(n.id)
-              })
-            }}
-          />}
-      </div>
-    </Link>
+      )
+    })
 
-  )
+  return isGrouped
+    ? (
+      <GroupRender
+        groupedNotifications={props.groupedNotifications}
+        notificationDetails={notificationDetails}
+        handleClickGroupedNotification={handleClickGroupedNotification}
+        readStatus={props.groupedNotifications.group.map(notification => notification.read).reduce((acc, current) => acc && current)}
+        numberOfAuthors={props.groupedNotifications.author.length}
+        numberOfWorkspaces={uniqBy(props.groupedNotifications.group.map(notification => notification.workspace), 'id').length}
+        handleReadGroupNotification={handleReadGroupNotification}
+      />
+    )
+    : listRender
 }
 
 const mapStateToProps = ({ user }) => ({ user })
 export default connect(mapStateToProps)(translate()(TracimComponent(GroupedNotificationItem)))
 
 GroupedNotificationItem.propTypes = {
+  onCloseNotificationWall: PropTypes.func.isRequired,
   getNotificationDetails: PropTypes.func.isRequired,
-  notification: PropTypes.object.isRequired,
-  onClickCircle: PropTypes.func.isRequired,
-  onClickNotification: PropTypes.func.isRequired,
-  shortDate: PropTypes.string.isRequired
+  groupedNotifications: PropTypes.object.isRequired
 }
--- frontend/test/src/reducer/notificationPage.spec.js
+++ frontend/test/src/reducer/notificationPage.spec.js
@@ -19,12 +19,7 @@ import {
   updateNotification
 } from '../../../src/action-creator.sync.js'
 import notificationPage, {
-  belongsToGroup,
-  hasSameAuthor,
-  hasSameContent,
-  hasSameWorkspace,
-  serializeNotification,
-  sortByCreatedDate
+  serializeNotification
 } from '../../../src/reducer/notificationPage.js'
 import { globalManagerFromApi } from '../../fixture/user/globalManagerFromApi.js'
 import { firstWorkspaceFromApi } from '../../fixture/workspace/firstWorkspace.js'
@@ -94,7 +89,6 @@ describe('reducer notificationPage.js', () => {
   describe('actions', () => {
     const initialState = {
       list: [],
-      flattenList: [],
       hasNextPage: false,
       nextPageToken: '',
       unreadNotificationCount: 0,
@@ -107,8 +101,7 @@ describe('reducer notificationPage.js', () => {
       it('should return the list of notification from the objects passed as parameter', () => {
         expect(listOfNotification).to.deep.equal({
           ...initialState,
-          list: [notification],
-          flattenList: [notification]
+          list: [notification]
         })
       })
     })
@@ -121,7 +114,6 @@ describe('reducer notificationPage.js', () => {
         expect(listOfNotification).to.deep.equal({
           ...initialState,
           list: [notification],
-          flattenList: [notification],
           unreadMentionCount: 0,
           unreadNotificationCount: 1
         })
@@ -131,7 +123,6 @@ describe('reducer notificationPage.js', () => {
         expect(listOfMention).to.deep.equal({
           ...initialState,
           list: [mention],
-          flattenList: [mention],
           unreadMentionCount: 1,
           unreadNotificationCount: 1
         })
@@ -144,56 +135,18 @@ describe('reducer notificationPage.js', () => {
       it('should return the list of notification with the notification replaced by the list', () => {
         expect(listOfNotification).to.deep.equal({
           ...initialState,
-          list: [notification, notification],
-          flattenList: [notification, notification]
+          list: [notification, notification]
         })
       })
     })
 
     describe(`${READ}/${NOTIFICATION}`, () => {
       it('should read a notification in a flat list', () => {
-        const initState = { ...initialState, list: [notification], flattenList: [notification], unreadNotificationCount: 1 }
+        const initState = { ...initialState, list: [notification], unreadNotificationCount: 1 }
         const listOfNotification = notificationPage(initState, readNotification(notification.id))
         expect(listOfNotification).to.deep.equal({
           ...initialState,
           list: [{ ...notification, read: true }],
-          flattenList: [{ ...notification, read: true }],
-          unreadNotificationCount: 0
-        })
-      })
-
-      it('should read a notification in a group', () => {
-        const initGroup = {
-          author: [
-            {
-              publicName: globalManagerFromApi.public_name,
-              userId: globalManagerFromApi.user_id,
-              hasAvatar: false,
-              hasCover: false
-            }
-          ],
-          created: '2020-07-23T12:44:50Z',
-          id: 583, // NOTE - MP - 10-03-2022 - Group id must match notification id
-          type: '2.author.workspace',
-          group: [notification]
-        }
-
-        const initState = { ...initialState, list: [initGroup], flattenList: [notification], unreadNotificationCount: 1 }
-        const listOfNotification = notificationPage(initState, readNotification(notification.id))
-
-        expect(listOfNotification).to.deep.equal({
-          ...initState,
-          list: [{
-            ...initGroup,
-            group: [{
-              ...notification,
-              read: true
-            }]
-          }],
-          flattenList: [{
-            ...notification,
-            read: true
-          }],
           unreadNotificationCount: 0
         })
       })
@@ -205,29 +158,29 @@ describe('reducer notificationPage.js', () => {
         content: { label: 'test', id: 5 }
       }
       const listOfNotification = notificationPage(
-        { ...initialState, list: [notificationWithContent], flattenList: [notificationWithContent], unreadNotificationCount: 1 },
+        { ...initialState, list: [notificationWithContent], unreadNotificationCount: 1 },
         readContentNotification(5)
       )
 
       it('should return the list of objects with read set as true and counts as 0', () => {
         expect(listOfNotification).to.deep.equal(
-          { ...initialState, list: [{ ...notificationWithContent, read: true }], flattenList: [{ ...notificationWithContent, read: true }], unreadNotificationCount: 0 })
+          { ...initialState, list: [{ ...notificationWithContent, read: true }], unreadNotificationCount: 0 })
       })
     })
 
     describe(`${READ}/${NOTIFICATION_LIST}`, () => {
-      const listOfNotification = notificationPage({ ...initialState, list: [notification], flattenList: [notification], unreadNotificationCount: 1 }, readNotificationList())
+      const listOfNotification = notificationPage({ ...initialState, list: [notification], unreadNotificationCount: 1 }, readNotificationList())
 
       it('should return the list of objects passed as parameter', () => {
-        expect(listOfNotification).to.deep.equal({ ...initialState, list: [{ ...notification, read: true }], flattenList: [{ ...notification, read: true }], unreadNotificationCount: 0 })
+        expect(listOfNotification).to.deep.equal({ ...initialState, list: [{ ...notification, read: true }], unreadNotificationCount: 0 })
       })
     })
 
     describe(`${APPEND}/${NOTIFICATION_LIST}`, () => {
-      const listOfNotification = notificationPage({ ...initialState, list: [notification], flattenList: [notification] }, appendNotificationList([{ ...TLM, event_id: 999 }]))
+      const listOfNotification = notificationPage({ ...initialState, list: [notification] }, appendNotificationList([{ ...TLM, event_id: 999 }]))
 
       it('should return the list of notifications appended with the list passed as parameter', () => {
-        expect(listOfNotification).to.deep.equal({ ...initialState, list: [notification, { ...notification, id: 999 }], flattenList: [notification, { ...notification, id: 999 }] })
+        expect(listOfNotification).to.deep.equal({ ...initialState, list: [notification, { ...notification, id: 999 }] })
       })
     })
 
@@ -248,114 +201,5 @@ describe('serializeNotification', () => {
 
   it('should return an object (in camelCase)', () => {
     expect(serializeNotification(TLMMention)).to.deep.equal(mention)
-  })
-})
-
-describe('hasSameAuthor', () => {
-  it('should return false if list has a null or undefined element', () => {
-    expect(hasSameAuthor([null, { userId: 1 }, { userId: 1 }])).to.be.equal(false)
-  })
-
-  it('should return false if a element has a different author id', () => {
-    expect(hasSameAuthor([{ userId: 2 }, { userId: 1 }])).to.be.equal(false)
-  })
-
-  it('should return true if all elements has same authorid', () => {
-    expect(hasSameAuthor([{ userId: 1 }, { userId: 1 }])).to.be.equal(true)
-  })
-})
-
-describe('hasSameWorkspace', () => {
-  it('should return false if list has a null or undefined element', () => {
-    expect(hasSameWorkspace([null, { id: 1 }, { id: 1 }])).to.be.equal(false)
-  })
-
-  it('should return false if a element has a different workspace id', () => {
-    expect(hasSameWorkspace([{ id: 2 }, { id: 1 }])).to.be.equal(false)
-  })
-
-  it('should return true if all elements has same workspace id', () => {
-    expect(hasSameWorkspace([{ id: 1 }, { id: 1 }])).to.be.equal(true)
-  })
-})
-
-describe('hasSameContent', () => {
-  it('should return false if list has a element with null or undefined content', () => {
-    expect(hasSameContent([
-      { content: null },
-      { content: { id: 1, parentId: 2 }, type: 'content' },
-      { content: { id: 1, parentId: 2 }, type: 'content' }
-    ])).to.be.equal(false)
-  })
-
-  describe('only with contents', () => {
-    it('should return false if a element has a different content id', () => {
-      expect(hasSameContent([
-        { content: { id: 2, parentId: 2 }, type: 'content' },
-        { content: { id: 1, parentId: 2 }, type: 'content' }
-      ])).to.be.equal(false)
-    })
-
-    it('should return true if all elements has same content id', () => {
-      expect(hasSameContent([
-        { content: { id: 1, parentId: 2 }, type: 'content' },
-        { content: { id: 1, parentId: 3 }, type: 'content' }
-      ])).to.be.equal(true)
-    })
-  })
-
-  describe('with contents and comments', () => {
-    it('should return false if a element has a different content id or parentId', () => {
-      expect(hasSameContent([
-        { content: { id: 2, parentId: 3 }, type: 'content' },
-        { content: { id: 1, parentId: 3 }, type: 'comment' }
-      ])).to.be.equal(false)
-    })
-
-    it('should return true if all elements has same content', () => {
-      expect(hasSameContent([
-        { content: { id: 2, parentId: 3 }, type: 'content' },
-        { content: { id: 1, parentId: 2 }, type: 'comment' }
-      ])).to.be.equal(true)
-    })
-  })
-
-  describe('only with comments', () => {
-    it('should return false if a element has a different content id or parentId', () => {
-      expect(hasSameContent([
-        { content: { id: 2, parentId: 1 }, type: 'comment' },
-        { content: { id: 1, parentId: 2 }, type: 'comment' }
-      ])).to.be.equal(false)
-    })
-
-    it('should return true if all elements has same content', () => {
-      expect(hasSameContent([
-        { content: { id: 2, parentId: 3 }, type: 'comment' },
-        { content: { id: 1, parentId: 3 }, type: 'comment' }
-      ])).to.be.equal(true)
-    })
-  })
-})
-
-describe('belongsToGroup', () => {
-  const defaultElement = {
-    author: { userId: 1 },
-    content: { id: 2 },
-    type: 'content',
-    workspace: { id: 3 }
-  }
-
-  it('should return false if groupedNotification is null', () => {
-    expect(belongsToGroup(defaultElement, null, 2)).to.be.equal(false)
-  })
-
-  it('should return false if groupedNotification has no group', () => {
-    expect(belongsToGroup(defaultElement, {}, 2)).to.be.equal(false)
-  })
-})
-
-describe('sortByCreatedDate', () => {
-  it('should return the array sorted by created', () => {
-    expect(sortByCreatedDate([{ created: 5 }, { created: 2 }, { created: 9 }])).to.deep.equal([{ created: 9 }, { created: 5 }, { created: 2 }])
   })
 })
