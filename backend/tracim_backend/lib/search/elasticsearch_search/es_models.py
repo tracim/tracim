6aa42162cf6b7b36d2331d026762148716669f59
Guénaël Muller <guenael.muller@algoo.fr>
2022-04-07 13:49:34 +0200

    replace underscore by minus in elasticsearch in order to split token on underscore

--- backend/tracim_backend/lib/search/elasticsearch_search/es_models.py
+++ backend/tracim_backend/lib/search/elasticsearch_search/es_models.py
@@ -28,8 +28,20 @@ EXACT_FIELD = "exact"
 # that's why we set different analyzer for search (we search word given) and indexing (we index ngram
 # of label of content to allow autocompletion)
 
+underscore_as_minus = analysis.char_filter("underscore_as_space", type="mapping", mappings=["_=>-"])
+
+# INFO - G.M - 2022-04-07 - Locally  Testing analyser withotu running tracim is possible with elyzer
+# (need same name python package)  with command like :
+#  elyzer --es "http://localhost:9200" --index my-tracim-content --analyzer folding "Mary had_a little lamb"
+# This permit to better understand each step of the analyzer process.
+
 # INFO - G.M - 2019-05-23 - search_analyser: do search for content given an some similar word
-folding = analyzer("folding", tokenizer="standard", filter=["lowercase", "asciifolding"])
+folding = analyzer(
+    "folding",
+    tokenizer="standard",
+    filter=["lowercase", "asciifolding"],
+    char_filter=underscore_as_minus,
+)
 # INFO - G.M - 2019-05-23 -  index_analysers, index edge ngram for autocompletion and strip html for indexing
 edge_ngram_token_filter = analysis.token_filter(
     "edge_ngram_filter", type="edge_ngram", min_ngram=2, max_gram=20
@@ -53,18 +65,19 @@ edge_ngram_folding = analyzer(
     "edge_ngram_folding",
     tokenizer="standard",
     filter=[max_token_length_filter, "lowercase", "asciifolding", edge_ngram_token_filter],
+    char_filter=underscore_as_minus,
 )
 html_folding = analyzer(
     "html_folding",
     tokenizer="standard",
     filter=[max_token_length_filter, "lowercase", "asciifolding", edge_ngram_token_filter],
-    char_filter="html_strip",
+    char_filter=["html_strip", underscore_as_minus],
 )
 html_exact_folding = analyzer(
     "html_exact_folding",
     tokenizer="standard",
     filter=[max_token_length_filter],
-    char_filter="html_strip",
+    char_filter=["html_strip", underscore_as_minus],
 )
 
 
