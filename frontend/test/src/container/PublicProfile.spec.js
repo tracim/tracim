b04ecb53384edddb9807feb1322227e3adf391d5
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2022-04-13 16:53:14 +0200

    fix unit tests and add link on space requests

--- frontend/test/src/container/PublicProfile.spec.js
+++ frontend/test/src/container/PublicProfile.spec.js
@@ -2,20 +2,40 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import { PublicProfile } from '../../../src/container/PublicProfile.jsx'
-import { user } from '../../hocMock/redux/user/user'
+import { user, userFromApi } from '../../hocMock/redux/user/user'
+import { mockGetCustomPropertiesSchema200, mockGetAboutUser200 } from '../../apiMock.js'
+import { FETCH_CONFIG } from '../../../src/util/helper.js'
+import { isFunction } from '../../hocMock/helper'
 
 describe('<PublicProfile />', () => {
+  const dispatchMock = params => {
+    if (isFunction(params)) return params(dispatchMock)
+    return params
+  }
+
   const props = {
+    breadcrumbs: [],
     user: user,
+    system: {
+      config: {
+        call__unanswered_timeout: 42
+      }
+    },
     match: {
       params: {
-        userid: 1
+        userid: user.userId
       }
     },
     registerLiveMessageHandlerList: () => {},
     registerCustomEventHandlerList: () => {},
-    t: tradKey => tradKey
+    t: tradKey => tradKey,
+    dispatch: dispatchMock
   }
+
+  mockGetAboutUser200(FETCH_CONFIG.apiUrl, user.userId, userFromApi)
+  mockGetCustomPropertiesSchema200(FETCH_CONFIG.apiUrl, {
+    json: { json_schema: { }, ui_schema: { }, code: 2 }
+  })
 
   const wrapper = shallow(<PublicProfile {...props} />)
 
