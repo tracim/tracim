815e9cfa6de8f373755465a96c14d99adf61b57e
Mathis Perrier <mathis.perrier@algoo.fr>
2022-05-02 11:23:12 +0200

    UserSpacesConfig now in function; Loader added

--- frontend/src/component/Account/UserSpacesConfig.jsx
+++ frontend/src/component/Account/UserSpacesConfig.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
 import {
-  IconButton,
-  sortWorkspaceList,
-  ConfirmPopup,
   PAGE,
   PROFILE,
-  TracimComponent,
   ROLE,
+  ConfirmPopup,
+  IconButton,
+  Loading,
+  sortWorkspaceList,
+  TracimComponent,
   TLM_ENTITY_TYPE as TLM_ET,
   TLM_CORE_EVENT_TYPE as TLM_CET
 } from 'tracim_frontend_lib'
@@ -19,141 +20,26 @@ import { deleteWorkspaceMember, getUserWorkspaceList, getWorkspaceMemberList } f
 import AdminUserSpacesConfig from '../../container/AdminUserSpacesConfig.jsx'
 import UserSpacesConfigLine from './UserSpacesConfigLine.jsx'
 
-export class UserSpacesConfig extends React.Component {
-  constructor (props) {
-    super(props)
+export const UserSpacesConfig = (props) => {
+  const [spaceList, setSpaceList] = useState([])
+  const [spaceBeingDeleted, setSpaceBeingDeleted] = useState(null)
+  const [entries, setEntries] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-    this.state = {
-      workspaceList: [],
-      spaceBeingDeleted: null
-    }
-
+  useEffect(() => {
     props.registerLiveMessageHandlerList([
-      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.CREATED, handler: this.handleMemberCreated },
-      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.MODIFIED, handler: this.handleMemberModified },
-      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.DELETED, handler: this.handleMemberDeleted }
+      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.CREATED, handler: handleMemberCreated },
+      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.MODIFIED, handler: handleMemberModified },
+      { entityType: TLM_ET.SHAREDSPACE_MEMBER, coreEntityType: TLM_CET.DELETED, handler: handleMemberDeleted }
     ])
-  }
 
-  handleMemberModified = data => {
-    this.setState(prev => ({
-      workspaceList: prev.workspaceList.map(space =>
-        space.workspace_id === data.fields.workspace.workspace_id
-          ? {
-            ...space,
-            memberList: space.memberList.map(member => member.user_id === data.fields.user.user_id
-              ? { ...member, ...data.fields.member }
-              : member
-            )
-          }
-          : space
-      )
-    }))
-  }
-
-  handleMemberDeleted = async data => {
-    const { props } = this
-
-    if (Number(props.userToEditId) === data.fields.user.user_id) {
-      this.setState(prev => ({
-        workspaceList: prev.workspaceList.filter(space =>
-          space.workspace_id !== data.fields.workspace.workspace_id
-        )
-      }))
-    } else {
-      this.updateMemberList(data)
+    return () => {
+      // unregisterLiveMessageHandlerList
     }
-  }
+  }, [])
 
-  handleMemberCreated = async data => {
-    this.updateMemberList(data)
-  }
-
-  updateMemberList = async (data) => {
-    // RJ - 2020-10-28 - FIXME - https://github.com/tracim/tracim/issues/3740
-    // We should update the member list with using information in data instead of re-fetching it
-
-    const { props } = this
-    const spaceIndex = this.state.workspaceList.findIndex(s => s.workspace_id === data.fields.workspace.workspace_id)
-
-    const space = await this.fillMemberList(data.fields.workspace)
-
-    if (spaceIndex === -1 && Number(props.userToEditId) !== data.fields.user.user_id) return
-
-    this.setState(prev => ({
-      workspaceList: (
-        spaceIndex === -1
-          ? sortWorkspaceList([...prev.workspaceList, space])
-          : [...prev.workspaceList.slice(0, spaceIndex), space, ...prev.workspaceList.slice(spaceIndex + 1)]
-      )
-    }))
-  }
-
-  getWorkspaceList = async () => {
-    const { props } = this
-
-    const fetchGetUserWorkspaceList = await props.dispatch(getUserWorkspaceList(props.userToEditId, false))
-
-    switch (fetchGetUserWorkspaceList.status) {
-      case 200: this.getUserWorkspaceListMemberList(fetchGetUserWorkspaceList.json); break
-      default: props.dispatch(newFlashMessage(props.t('Error while loading user')))
-    }
-  }
-
-  fillMemberList = async space => {
-    const fetchMemberList = await this.props.dispatch(getWorkspaceMemberList(space.workspace_id))
-
-    return {
-      ...space,
-      memberList: fetchMemberList.json || [] // handle error?
-    }
-  }
-
-  getUserWorkspaceListMemberList = async (spaceList) => {
-    const workspaceList = await Promise.all(spaceList.map(this.fillMemberList))
-    this.setState({ workspaceList })
-  }
-
-  handleConfirmDeleteSpace = async () => {
-    const { props } = this
-    const spaceId = this.state.spaceBeingDeleted
-    if (!spaceId) return
-    this.setState({ spaceBeingDeleted: null })
-
-    const fetchResult = await props.dispatch(deleteWorkspaceMember(spaceId, props.userToEditId))
-    if (fetchResult.status !== 204) {
-      props.dispatch(newFlashMessage(props.t('Error while leaving the space'), 'warning'))
-    }
-  }
-
-  handleLeaveSpace = (spaceBeingDeleted) => {
-    this.setState({ spaceBeingDeleted })
-  }
-
-  componentDidMount () {
-    this.getWorkspaceList()
-  }
-
-  componentDidUpdate (prevProps) {
-    if (prevProps.userToEditId !== this.props.userToEditId) {
-      this.getWorkspaceList()
-    }
-  }
-
-  onlyManager (member, memberList) {
-    const manager = ROLE.workspaceManager.slug
-
-    if (member.role !== manager) {
-      return false
-    }
-
-    return !memberList.some(u => u.user_id !== this.props.userToEditId && u.role === manager)
-  }
-
-  render () {
-    const { props, state } = this
-
-    const entries = state.workspaceList.reduce((res, space) => {
+  useEffect(() => {
+    const entries = spaceList.reduce((res, space) => {
       if (space.memberList.length > 0) {
         const member = space.memberList.find(u => u.user_id === props.userToEditId)
         if (member) {
@@ -163,10 +49,10 @@ export class UserSpacesConfig extends React.Component {
               member={member}
               key={space.workspace_id}
               onChangeSubscriptionNotif={props.onChangeSubscriptionNotif}
-              onLeaveSpace={this.handleLeaveSpace}
+              onLeaveSpace={handleLeaveSpace}
               admin={props.admin}
               system={props.system}
-              onlyManager={this.onlyManager(member, space.memberList)}
+              onlyManager={onlyManager(member, space.memberList)}
             />
           )
         }
@@ -174,75 +60,185 @@ export class UserSpacesConfig extends React.Component {
       }
     }, [])
 
-    return (
-      <div className='account__userpreference__setting__spacename'>
-        <div className='spaceconfig__sectiontitle subTitle'>
-          {props.t('Spaces')}
-          {(props.user && props.user.profile === PROFILE.administrator.slug) && (
-            <IconButton
-              mode='dark'
-              intent='secondary'
-              onClick={(() => props.history.push(PAGE.ADMIN.USER_SPACE_LIST(props.userToEditId)))}
-              icon='fas fa-user-cog'
-              text={props.t('Manage user spaces')}
-              dataCy='account__userpreference__setting__spacename'
-            />
-          )}
-        </div>
+    setEntries(entries)
+  }, [spaceList])
 
-        {props.openSpacesManagement && (
-          <AdminUserSpacesConfig
-            userToEditId={props.userToEditId}
-            userEmail={props.userEmail}
-            userPublicName={props.userPublicName}
-            userUsername={props.userUsername}
-            onChangeSubscriptionNotif={props.onChangeSubscriptionNotif}
-            onClose={(() => props.history.push(PAGE.ADMIN.USER_EDIT(props.userToEditId), 'spacesConfig'))}
-          />
-        )}
+  useEffect(() => {
+    getSpaceList()
+  }, [props.userToEditId])
 
-        {(entries.length
-          ? (
-            <div className='spaceconfig__table'>
-              <table className='table'>
-                <thead>
-                  <tr>
-                    <th>{props.t('Space')}</th>
-                    <th>{props.t('Role')}</th>
-                    {props.system.config.email_notification_activated && <th>{props.t('Email notifications')}</th>}
-                    <th />
-                  </tr>
-                </thead>
+  const handleMemberModified = (data) => {
+    setSpaceList(spaceList.map(space => {
+      if (space.workspace_id === data.workspace_id) {
+        return {
+          ...space,
+          memberList: space.memberList.map(member => {
+            if (member.user_id === data.fields.user.user_id) {
+              return { ...member, ...data.fields.user }
+            } else {
+              return member
+            }
+          })
+        }
+      } else {
+        return space
+      }
+    }))
+  }
 
-                <tbody>{entries}</tbody>
-              </table>
-              {(this.state.spaceBeingDeleted && (
-                <ConfirmPopup
-                  onConfirm={this.handleConfirmDeleteSpace}
-                  onCancel={() => this.setState({ spaceBeingDeleted: null })}
-                  msg={
-                    props.admin
-                      ? props.t('Are you sure you want to remove this member from the space?')
-                      : props.t('Are you sure you want to leave the space?')
-                  }
-                  confirmLabel={
-                    props.admin
-                      ? props.t('Remove from space')
-                      : props.t('Leave space')
-                  }
-                  confirmIcon='fa-fw fas fa-sign-out-alt'
-                />
-              ))}
-            </div>
-          ) : (
-            props.admin
-              ? props.t('This user is not a member of any space yet')
-              : props.t('You are not a member of any space yet')
-          )
-        )}
-      </div>
+  const handleMemberDeleted = async (data) => {
+    if (Number(props.userToEditId) === data.fields.user.user_id) {
+      setSpaceList(spaceList.filter(space => space.workspace_id !== data.workspace_id))
+    } else {
+      updateMemberList(data)
+    }
+  }
+
+  const handleMemberCreated = async (data) => {
+    updateMemberList(data)
+  }
+
+  const updateMemberList = async (data) => {
+    // RJ - 2020-10-28 - FIXME - https://github.com/tracim/tracim/issues/3740
+    // We should update the member list with using information in data instead of re-fetching it
+
+    const spaceIndex = spaceList.findIndex(s => s.workspace_id === data.fields.workspace.workspace_id)
+    const space = await fillMemberList(data.fields.workspace)
+
+    if (spaceIndex === -1 && Number(props.userToEditId) !== data.fields.user.user_id) return
+
+    setSpaceList(
+      spaceIndex === -1
+        ? sortWorkspaceList([...spaceList, space])
+        : [...spaceList.slice(0, spaceIndex), space, ...spaceList.slice(spaceIndex + 1)]
     )
   }
+
+  const getSpaceList = async () => {
+    const fetchGetUserWorkspaceList = await props.dispatch(getUserWorkspaceList(props.userToEditId, false))
+
+    switch (fetchGetUserWorkspaceList.status) {
+      case 200: {
+        getUserSpaceListMemberList(fetchGetUserWorkspaceList.json)
+        break
+      }
+      default: props.dispatch(newFlashMessage(props.t('Error while loading user')))
+    }
+  }
+
+  const fillMemberList = async (space) => {
+    const fetchMemberList = await props.dispatch(getWorkspaceMemberList(space.workspace_id))
+    return {
+      ...space,
+      memberList: fetchMemberList.json || [] // handle error?
+    }
+  }
+
+  const getUserSpaceListMemberList = async (fetchedSpaceList) => {
+    const spaceListResult = await Promise.all(fetchedSpaceList.map(fillMemberList))
+    setSpaceList(spaceListResult)
+
+    setIsLoading(false)
+  }
+
+  const handleConfirmDeleteSpace = async () => {
+    const spaceId = spaceBeingDeleted
+    if (!spaceId) return
+
+    setSpaceBeingDeleted(null)
+
+    const fetchResult = await props.dispatch(deleteWorkspaceMember(spaceId, props.userToEditId))
+    if (fetchResult.status !== 204) {
+      props.dispatch(newFlashMessage(props.t('Error while leaving the space'), 'warning'))
+    }
+  }
+
+  const handleLeaveSpace = (spaceBeingDeleted) => {
+    setSpaceBeingDeleted(spaceBeingDeleted)
+  }
+
+  const onlyManager = (member, memberList) => {
+    const manager = ROLE.workspaceManager.slug
+
+    if (member.role !== manager) {
+      return false
+    }
+
+    return !memberList.some(u => u.user_id !== props.userToEditId && u.role === manager)
+  }
+
+  return (
+    isLoading
+      ? <Loading />
+      : (
+        <div className='account__userpreference__setting__spacename'>
+          <div className='spaceconfig__sectiontitle subTitle'>
+            {props.t('Spaces')}
+            {(props.user && props.user.profile === PROFILE.administrator.slug) && (
+              <IconButton
+                mode='dark'
+                intent='secondary'
+                onClick={(() => props.history.push(PAGE.ADMIN.USER_SPACE_LIST(props.userToEditId)))}
+                icon='fas fa-user-cog'
+                text={props.t('Manage user spaces')}
+                dataCy='account__userpreference__setting__spacename'
+              />
+            )}
+          </div>
+
+          {props.openSpacesManagement && (
+            <AdminUserSpacesConfig
+              userToEditId={props.userToEditId}
+              userEmail={props.userEmail}
+              userPublicName={props.userPublicName}
+              userUsername={props.userUsername}
+              onChangeSubscriptionNotif={props.onChangeSubscriptionNotif}
+              onClose={(() => props.history.push(PAGE.ADMIN.USER_EDIT(props.userToEditId), 'spacesConfig'))}
+            />
+          )}
+
+          {(entries.length
+            ? (
+              <div className='spaceconfig__table'>
+                <table className='table'>
+                  <thead>
+                    <tr>
+                      <th>{props.t('Space')}</th>
+                      <th>{props.t('Role')}</th>
+                      {props.system.config.email_notification_activated && <th>{props.t('Email notifications')}</th>}
+                      <th />
+                    </tr>
+                  </thead>
+
+                  <tbody>{entries}</tbody>
+                </table>
+                {(spaceBeingDeleted && (
+                  <ConfirmPopup
+                    onConfirm={handleConfirmDeleteSpace}
+                    onCancel={() => setSpaceBeingDeleted(null)}
+                    msg={
+                      props.admin
+                        ? props.t('Are you sure you want to remove this member from the space?')
+                        : props.t('Are you sure you want to leave the space?')
+                    }
+                    confirmLabel={
+                      props.admin
+                        ? props.t('Remove from space')
+                        : props.t('Leave space')
+                    }
+                    confirmIcon='fa-fw fas fa-sign-out-alt'
+                  />
+                ))}
+              </div>
+            ) : (
+              props.admin
+                ? props.t('This user is not a member of any space yet')
+                : props.t('You are not a member of any space yet')
+            )
+          )}
+        </div>
+      )
+  )
 }
 
 const mapStateToProps = ({ system, user }) => ({ system, user })
