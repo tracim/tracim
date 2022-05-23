1b743982d5bcd93dfa6942a6dab9db1cd514c954
Guénaël Muller <guenael.muller@algoo.fr>
2022-05-23 10:45:07 +0200

    allow to get deleted/archived file in content redirection endpoint

--- backend/tracim_backend/tests/functional/test_contents.py
+++ backend/tracim_backend/tests/functional/test_contents.py
@@ -1636,6 +1636,36 @@ class TestFiles(object):
         content = res.json_body
         assert content["content_type"] == KANBAN_TYPE
 
+    def test_api__get_kanban__ok_200__deleted_kanban(
+        self, workspace_api_factory, content_api_factory, session, web_testapp, content_type_list
+    ) -> None:
+        """
+        Get a kanban from content endpoint
+        """
+
+        workspace_api = workspace_api_factory.get()
+        content_api = content_api_factory.get()
+        business_workspace = workspace_api.get_one(1)
+        tool_folder = content_api.get_one(1, content_type=content_type_list.Any_SLUG)
+        test_file = content_api.create(
+            content_type_slug=KANBAN_TYPE,
+            workspace=business_workspace,
+            parent=tool_folder,
+            label="Test file",
+            do_save=False,
+            do_notify=False,
+        )
+        with new_revision(session=session, tm=transaction.manager, content=test_file):
+            content_api.delete(test_file)
+        transaction.commit()
+
+        web_testapp.authorization = ("Basic", ("admin@admin.admin", "admin@admin.admin"))
+        res = web_testapp.get("/api/contents/{}".format(test_file.content_id), status=302).follow(
+            status=200
+        )
+        content = res.json_body
+        assert content["content_type"] == KANBAN_TYPE
+
     def test_api__get_file__ok_200__no_file_add(
         self, workspace_api_factory, content_api_factory, session, web_testapp, content_type_list
     ) -> None:
