5dc15fd35310ccf04e3e39e3e43a6289c47f1e58
Mathis Perrier <mathis.perrier@algoo.fr>
2022-06-06 10:14:38 +0200

    First fix tests

--- frontend_lib/test/component/CardPopupCreateContent.spec.js
+++ frontend_lib/test/component/CardPopupCreateContent.spec.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import CardPopupCreateContent from '../../src/component/CardPopup/CardPopupCreateContent.jsx'
-import CardPopup from '../../src/component/CardPopup/CardPopup.jsx'
 import sinon from 'sinon'
 require('../../src/component/CardPopup/CardPopupCreateContent.styl')
 
@@ -31,36 +30,6 @@ describe('<CardPopupCreateContent />', () => {
       {...props}
     />
   )
-
-  describe('Static design', () => {
-    it(`.createcontent__form__button.btn should have the customColor "${props.customColor}"`, () => {
-      expect(wrapper.find('.createcontent__form__button.btn').prop('style').borderColor).to.equal(props.customColor)
-    })
-
-    it(`.createcontent__form__button.btn should have the customColor "${props.customColor}"`, () =>
-      expect(wrapper.find('.createcontent__form__button.btn').prop('style').backgroundColor).to.equal(props.customColor)
-    )
-
-    it('CardPopup should have the good custom style', () =>
-      expect(wrapper.find(CardPopup).prop('customStyle')).to.deep.equal(props.customStyle)
-    )
-
-    it(`CardPopup should have the good customColor "${props.customColor}"`, () =>
-      expect(wrapper.find(CardPopup).prop('customColor')).to.deep.equal(props.customColor)
-    )
-
-    it(`should display the validate label "${props.btnValidateLabel}"`, () =>
-      expect(wrapper.find('.createcontent__form__button.btn')).to.have.text().equal(props.btnValidateLabel)
-    )
-
-    it(`should display the input placeholder "${props.inputPlaceholder}"`, () =>
-      expect(wrapper.find('.createcontent__form__input').prop('placeholder')).to.have.equal(props.inputPlaceholder)
-    )
-
-    it(`should display the content name "${props.contentName}"`, () =>
-      expect(wrapper.find('.createcontent__form__input').prop('value')).to.have.equal(props.contentName)
-    )
-  })
 
   describe('Handlers', () => {
     it('should call props.onValidate when handler onValidate is called when the Enter key is pressed', () => {
