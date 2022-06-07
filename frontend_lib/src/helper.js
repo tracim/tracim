541a49ad94bb9dfa676d70a42df8f489d9bab255
gbonaspetti <giulia.bonaspetti@algoo.fr>
2022-06-07 16:03:49 +0200

    fix(frontend): redirect to content list folder
    Issue ref: #5395 
    PR ref: #5695

--- frontend_lib/src/helper.js
+++ frontend_lib/src/helper.js
@@ -34,7 +34,8 @@ export const PAGE = {
     GALLERY: (idws = ':idws') => `/ui/workspaces/${idws}/gallery`,
     RECENT_ACTIVITIES: (idws = ':idws') => `/ui/workspaces/${idws}/recent-activities`,
     PUBLICATION: (idws = ':idws', idcts = ':idcts') => `/ui/workspaces/${idws}/publications/${idcts}`,
-    PUBLICATIONS: (idws = ':idws') => `/ui/workspaces/${idws}/publications`
+    PUBLICATIONS: (idws = ':idws') => `/ui/workspaces/${idws}/publications`,
+    FOLDER_OPEN: (idws = ':idws', folderList) => `/ui/workspaces/${idws}/contents?folder_open=${folderList.toString()}`
   },
   LOGIN: '/ui/login',
   FORGOT_PASSWORD: '/ui/forgot-password',
@@ -769,13 +770,17 @@ export const buildContentPathBreadcrumbs = async (apiUrl, content) => {
   const fetchGetContentPath = await handleFetchResult(await getContentPath(apiUrl, contentId))
 
   switch (fetchGetContentPath.apiResponse.status) {
-    case 200:
+    case 200: {
+      const contentPathList = fetchGetContentPath.body.items.map(content => content.content_id)
       return fetchGetContentPath.body.items.map(crumb => ({
-        link: PAGE.WORKSPACE.CONTENT(workspaceId, crumb.content_type, crumb.content_id),
+        link: crumb.content_type === CONTENT_TYPE.FOLDER
+          ? PAGE.WORKSPACE.FOLDER_OPEN(workspaceId, contentPathList)
+          : PAGE.WORKSPACE.CONTENT(workspaceId, crumb.content_type, crumb.content_id),
         label: crumb.label,
         type: BREADCRUMBS_TYPE.APP_FEATURE,
         isALink: true
       }))
+    }
     default:
       console.error('Error getting breadcrumbs data', fetchGetContentPath)
       throw new Error('Error getting breadcrumbs data')
