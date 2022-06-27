587b69affc6745d72f724c3c4fd7f99f8c8f9007
Giulia Bonaspetti Martins <giulia.bonaspetti@algoo.fr>
2022-06-27 14:08:21 +0200

    start of to do page [skip ci]

--- frontend/src/container/ToDo.jsx
+++ frontend/src/container/ToDo.jsx
@@ -4,6 +4,7 @@ import { translate } from 'react-i18next'
 import {
   BREADCRUMBS_TYPE,
   buildHeadTitle,
+  IconButton,
   PAGE,
   PageContent,
   PageTitle,
@@ -43,34 +44,55 @@ const ToDo = props => {
     <div className='tracim__content-scrollview'>
       <PageWrapper customClass='toDo__wrapper'>
         <PageTitle
-          title={props.t('To Do')}
+          title={props.t('Tasks to do')}
           icon='fas fa-check-square'
           breadcrumbsList={props.breadcrumbs}
         />
         <PageContent>
-          <div className='content__header'>
-            <div />
-            <div>
+          <div className='toDo__header'>
+            <div className='toDo__header__check' />
+            <div className='toDo__header__text'>
               {props.t('Task')}
             </div>
-            <div>
+            <div className='toDo__header__author'>
               {props.t('Author')}
             </div>
-            <div>
+            <div className='toDo__header__path'>
               {props.t('Contenu/Espace')}
             </div>
-            <div>
+            <div className='toDo__header__created'>
               {props.t('Last Modification')}
             </div>
           </div>
+
+          {toDoList.map(toDo =>
+            <div className='toDo__item'>
+              <div className='toDo__header__check'>
+                <IconButton
+                  text={props.t('To do')}
+                  icon='far fa-square'
+                  onClick={() => { }}
+                />
+              </div>
+              <div className='toDo__item__text'>
+                text
+              </div>
+              <div className='toDo__item__author'>
+                author
+              </div>
+              <div className='toDo__item__path'>
+                path
+              </div>
+              <div className='toDo__item__created'>
+                date
+              </div>
+            </div>
+          )}
         </PageContent>
       </PageWrapper>
     </div>
   )
 }
 
-const mapStateToProps = ({ breadcrumbs, user }) => ({
-  breadcrumbs,
-  user
-})
+const mapStateToProps = ({ breadcrumbs, user }) => ({ breadcrumbs, user })
 export default connect(mapStateToProps)(translate()(TracimComponent(ToDo)))
