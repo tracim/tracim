e81db895dde05063520ab4491006ff1b4325a666
Arnaud Jallut <arnaud.jallut@algoo.fr>
2022-07-01 10:51:55 +0200

    feat(frontend):minor syntax update

--- frontend_lib/src/component/PopinFixed/PopinFixedRightPartContent.jsx
+++ frontend_lib/src/component/PopinFixed/PopinFixedRightPartContent.jsx
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types'
 const PopinFixedRightPartContent = props => {
   const [progressBarWidth, setProgessBarWidth] = useState('0%')
   const [progessStatus, setProgessStatus] = useState('')
-	useEffect(() => {
+  useEffect(() => {
     const toDoArray = props.toDoList
     let sum = 0
     const isClosed = (el) => {
-      if(el.status === 'closed-validated') {
+      if (el.status === 'closed-validated') {
         sum += 1
       }
     }
     toDoArray.forEach(isClosed)
-    const progressBarWidth = Math.round(sum/props.toDoList.length * 100) + '%'
+    const progressBarWidth = Math.round(sum / props.toDoList.length * 100) + '%'
     setProgessBarWidth(progressBarWidth)
     setProgessStatus('(' + (props.toDoList.length - sum) + '/' + props.toDoList.length + ')')
   }, [props.toDoList])
@@ -22,9 +22,8 @@ const PopinFixedRightPartContent = props => {
     <div className='wsContentGeneric__content__right__content'>
       {props.id === 'todo' && props.showProgress &&
         <div className='toDo__progressBar_container'>
-          <div className='toDo__progressBar' style={{width: `${progressBarWidth}`}}></div>
-        </div>
-      }
+          <div className='toDo__progressBar' style={{ width: `${progressBarWidth}` }} />
+        </div>}
       {props.showTitle && props.id !== 'todo' && (
         <div className='wsContentGeneric__content__right__content__title'>
           {props.label}
