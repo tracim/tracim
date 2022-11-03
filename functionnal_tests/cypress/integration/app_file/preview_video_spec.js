c14aa69670194df7778ffcff3835039535557990
Millefeuille42 <mathieu.labourier@algoo.fr>
2022-11-03 14:44:20 +0100

    added button visibility to increase the delay

--- functionnal_tests/cypress/integration/app_file/preview_video_spec.js
+++ functionnal_tests/cypress/integration/app_file/preview_video_spec.js
@@ -39,7 +39,7 @@ describe('A video file', () => {
     cy.contains('[data-cy=FilenameWithBadges__label]', fileTitle).then(() => {
       cy.get('.previewcomponent__fileimg__text').should('be.not.visible')
       cy.get('.previewcomponent__fileimg').should('be.visible')
-      cy.get('.previewcomponent__fileimg__play').click()
+      cy.get('.previewcomponent__fileimg__play').should('be.visible').click()
       cy.get('#videoWrapperDiv > video').should('be.visible')
     })
   })
