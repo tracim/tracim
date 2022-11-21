0a1002abfafa162090b916680694c0e2afdcd399
Millefeuille42 <mathieu.labourier@algoo.fr>
2022-11-21 16:48:56 +0100

    feat(adminWorkspace): Added type and id filter on adminWorkspace

--- frontend_app_admin_workspace_user/src/component/AdminWorkspace.jsx
+++ frontend_app_admin_workspace_user/src/component/AdminWorkspace.jsx
@@ -21,9 +21,14 @@ const AdminWorkspace = props => {
   const filterWorkspaceList = () => {
     return userFilter === ''
       ? props.workspaceList
-      : props.workspaceList.filter(wksp =>
-        wksp.label.toUpperCase().includes(userFilter.toUpperCase()) ||
-        wksp.description.toUpperCase().includes(userFilter.toUpperCase())
+      : props.workspaceList.filter(space => {
+        const spaceType = SPACE_TYPE_LIST.find(type => type.slug === space.access_type) || { label: ''}
+
+        return space.label.toUpperCase().includes(userFilter.toUpperCase()) ||
+          space.description.toUpperCase().includes(userFilter.toUpperCase()) ||
+          props.t(spaceType.label).toUpperCase().includes(userFilter.toUpperCase()) ||
+          space.workspace_id.toString().includes(userFilter)
+        }
       )
   }
 
