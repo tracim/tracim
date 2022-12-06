37a1367887f17518ed144c671018886c2dffb7f2
Millefeuille42 <mathieu.labourier@algoo.fr>
2022-12-06 16:52:31 +0100

    fix(doc): Rewrote documentation

--- frontend_lib/src/columns/timedEventColumn.jsx
+++ frontend_lib/src/columns/timedEventColumn.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { createColumnHelper } from '@tanstack/react-table'
 
-import TitleListHeader from '../component/Lists/ListHeader/TitleListHeader.jsx'
-import { SORT_BY } from '../sortListHelper.js'
-import TimedEvent from '../component/TimedEvent/TimedEvent.jsx'
 import {
   stringIncludes,
   getRevisionTypeLabel
 } from '../helper.js'
+
+import { SORT_BY } from '../sortListHelper.js'
+import TimedEvent from '../component/TimedEvent/TimedEvent.jsx'
+import TitleListHeader from '../component/Lists/ListHeader/TitleListHeader.jsx'
 
 const timedEventColumn = (settings) => {
   const columnHelper = createColumnHelper()
