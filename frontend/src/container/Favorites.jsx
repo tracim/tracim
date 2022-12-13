9bd6db93cb0595b52cbdbf1a79e3c85f2d2bd035
Millefeuille42 <mathieu.labourier@algoo.fr>
2022-12-13 10:30:00 +0100

    refactor(favorites): Changed Favorites to use the new table

--- frontend/src/container/Favorites.jsx
+++ frontend/src/container/Favorites.jsx
@@ -5,26 +5,15 @@ import {
   BREADCRUMBS_TYPE,
   buildHeadTitle,
   CUSTOM_EVENT,
-  EmptyListMessage,
   PAGE,
   PageContent,
   PageTitle,
   PageWrapper,
   TracimComponent,
-  FavoriteButton,
-  FAVORITE_STATE,
   getContentComment,
   getContentPath,
-  Icon,
-  ListItemWrapper,
   Loading,
-  SORT_BY,
-  SORT_ORDER,
-  sortListBy,
-  TitleListHeader,
-  FilterBar,
-  handleFetchResult,
-  stringIncludes
+  handleFetchResult
 } from 'tracim_frontend_lib'
 
 import {
@@ -39,93 +28,12 @@ import {
   getFavoriteContentList,
   deleteContentFromFavoriteList
 } from '../action-creator.async.js'
+
 import {
   FETCH_CONFIG
 } from '../util/helper.js'
 
-import ContentListItem from '../component/ContentListItem.jsx'
-import ContentType from '../component/ContentType.jsx'
-
-const FavoritesHeader = translate()(props => {
-  return (
-    <div className='favoritesHeader content__header'>
-      <TitleListHeader
-        title={props.t('Type')}
-        onClickTitle={() => props.onClickTitle(SORT_BY.CONTENT_TYPE)}
-        customClass='favoritesHeader__type'
-        isOrderAscending={props.isOrderAscending}
-        isSelected={props.selectedSortCriterion === SORT_BY.CONTENT_TYPE}
-        tootltip={props.t('Sort by type')}
-      />
-      <TitleListHeader
-        title={props.t('Title and path')}
-        onClickTitle={() => props.onClickTitle(SORT_BY.LABEL)}
-        customClass='favoritesHeader__title'
-        isOrderAscending={props.isOrderAscending}
-        isSelected={props.selectedSortCriterion === SORT_BY.LABEL}
-        tootltip={props.t('Sort by title')}
-      />
-      {/* Header for windows smaller than max-sm */}
-      <TitleListHeader
-        title={props.t('Title')}
-        onClickTitle={() => props.onClickTitle(SORT_BY.LABEL)}
-        customClass='favoritesHeader__title-max-sm'
-        isOrderAscending={props.isOrderAscending}
-        isSelected={props.selectedSortCriterion === SORT_BY.LABEL}
-        tootltip={props.t('Sort by title')}
-      />
-      <TitleListHeader
-        title={props.t('Last Modification')}
-        onClickTitle={() => props.onClickTitle(SORT_BY.MODIFICATION_DATE)}
-        customClass='favoritesHeader__modification'
-        isOrderAscending={props.isOrderAscending}
-        isSelected={props.selectedSortCriterion === SORT_BY.MODIFICATION_DATE}
-        tootltip={props.t('Sort by last modification')}
-      />
-      <TitleListHeader
-        title={props.t('Information')}
-        onClickTitle={() => props.onClickTitle(SORT_BY.STATUS)}
-        customClass='favoritesHeader__information'
-        isOrderAscending={props.isOrderAscending}
-        isSelected={props.selectedSortCriterion === SORT_BY.STATUS}
-        tootltip={props.t('Sort by information')}
-      />
-      <div className='favoritesHeader__favoriteButton'>
-        {props.t('Favorite')}
-      </div>
-    </div>
-  )
-})
-
-const UnavailableContent = translate()(props => {
-  return (
-    <ListItemWrapper
-      label={props.label}
-      read
-      contentType={props.contentTypeInfo}
-      isLast={props.isLast}
-      isFirst={props.isFirst}
-      customClass='unavailableContent contentListItem'
-    >
-      <ContentType
-        contentTypeInfo={props.contentTypeInfo}
-        customClass='contentListItem__type'
-      />
-      <div className='contentListItem__name_path unavailableContent__name_warning'>
-        {props.label}
-        <span className='unavailableContent__warning'>
-          <Icon
-            icon='fas fa-exclamation-triangle'
-            title={props.t('Warning')}
-          />
-          &nbsp;
-          {props.t('content is not available')}
-        </span>
-      </div>
-      {props.children}
-    </ListItemWrapper>
-  )
-})
+import FavoritesTable from './tables/FavoriteTable/FavoritesTable.jsx'
 
 export class Favorites extends React.Component {
   constructor (props) {
@@ -133,11 +41,7 @@ export class Favorites extends React.Component {
 
     this.state = {
       contentCommentsCountList: [],
-      displayedFavoritesList: [],
-      isLoading: true,
-      selectedSortCriterion: SORT_BY.LABEL,
-      sortOrder: SORT_ORDER.ASCENDING,
-      userFilter: ''
+      isLoading: true
     }
 
     props.registerCustomEventHandlerList([
@@ -171,11 +75,6 @@ export class Favorites extends React.Component {
     this.setHeadTitle()
     this.buildBreadcrumbs()
     this.loadFavoriteList()
-    this.setDisplayedFavoritesList()
-  }
-
-  componentDidUpdate (prevProps) {
-    if (this.props.favoriteList !== prevProps.favoriteList) this.setDisplayedFavoritesList()
   }
 
   loadFavoriteList = async () => {
@@ -242,117 +141,8 @@ export class Favorites extends React.Component {
     return favorite.content ? favorite.content.label : favorite.originalLabel
   }
 
-  getAvailableContentType = (favorite) => {
-    return favorite.content ? favorite.content.type : favorite.originalType
-  }
-
-  getFavoriteComponent = (favorite, index) => {
-    // A favorite can point to an unavailable content (changed space access, deleted…)
-    // In this case a special component is displayed for the favorite
-    const { props, state } = this
-    const favoriteButton = (
-      <FavoriteButton
-        favoriteState={FAVORITE_STATE.FAVORITE}
-        onClickRemoveFromFavoriteList={() => this.handleClickRemoveFromFavoriteList(favorite)}
-        onClickAddToFavoriteList={() => { }}
-        customClass='favorites__item__favoriteButton'
-      />
-    )
-    const isLast = index === props.favoriteList.length - 1
-    const isFirst = index === 0
-    if (!favorite.content) {
-      const contentTypeInfo = props.contentType.find(info => info.slug === favorite.originalType)
-      return (
-        <UnavailableContent
-          contentTypeInfo={contentTypeInfo}
-          label={favorite.originalLabel}
-          key={favorite.contentId}
-          isLast={isLast}
-          isFirst={isFirst}
-        >
-          {favoriteButton}
-        </UnavailableContent>
-      )
-    }
-    const contentTypeInfo = props.contentType.find(info => info.slug === favorite.content.type)
-    return (
-      <ContentListItem
-        content={favorite.content}
-        contentTypeInfo={contentTypeInfo}
-        userLang={props.user.lang}
-        key={favorite.contentId}
-        isLast={isLast}
-        isFirst={isFirst}
-        breadcrumbsList={favorite.breadcrumbs}
-        commentsCount={state.contentCommentsCountList[index]}
-        customClass='favorites__item'
-        dataCy='favorites__item'
-      >
-        {favoriteButton}
-      </ContentListItem>
-    )
-  }
-
-  setDisplayedFavoritesList = () => {
-    const { props, state } = this
-
-    const sortedList = sortListBy(
-      props.favoriteList,
-      state.selectedSortCriterion,
-      state.sortOrder,
-      props.user.lang
-    )
-
-    this.setState({ displayedFavoritesList: sortedList })
-  }
-
-  handleClickTitleToSort = (criterion) => {
-    this.setState(prev => {
-      const sortOrder = prev.selectedSortCriterion === criterion && prev.sortOrder === SORT_ORDER.ASCENDING
-        ? SORT_ORDER.DESCENDING
-        : SORT_ORDER.ASCENDING
-      return {
-        displayedFavoritesList: sortListBy(prev.displayedFavoritesList, criterion, sortOrder, this.props.user.lang),
-        selectedSortCriterion: criterion,
-        sortOrder: sortOrder
-      }
-    })
-  }
-
-  filterFavoriteList = () => {
-    const { props, state } = this
-
-    if (state.userFilter === '') return state.displayedFavoritesList
-
-    return state.displayedFavoritesList.filter((favorite) => {
-      if (!favorite.content || !favorite.breadcrumbs) return false
-
-      const contentTypeInfo = props.contentType.find(info => info.slug === favorite.content.type)
-      const statusInfo = contentTypeInfo.availableStatuses.find(
-        s => s.slug === favorite.content.statusSlug
-      )
-
-      const includesFilter = stringIncludes(state.userFilter)
-
-      const hasFilterMatchOnContentLabel = includesFilter(favorite.content.label)
-      const hasFilterMatchOnLastModifier = includesFilter(favorite.content.lastModifier.publicName)
-      const hasFilterMatchOnBreadcrumbs = favorite.breadcrumbs.some(item => includesFilter(item.label))
-      const hasFilterMatchOnContentType = contentTypeInfo && includesFilter(props.t(contentTypeInfo.label))
-      const hasFilterMatchOnContentStatus = statusInfo && includesFilter(props.t(statusInfo.label))
-
-      return (
-        hasFilterMatchOnContentLabel ||
-        hasFilterMatchOnLastModifier ||
-        hasFilterMatchOnBreadcrumbs ||
-        hasFilterMatchOnContentType ||
-        hasFilterMatchOnContentStatus
-      )
-    })
-  }
-
   render () {
     const { props, state } = this
-    const filteredFavoriteList = this.filterFavoriteList()
 
     return (
       <div className='tracim__content-scrollview'>
@@ -368,33 +158,10 @@ export class Favorites extends React.Component {
             ? <Loading />
             : (
               <PageContent>
-                <FilterBar
-                  onChange={e => {
-                    const newFilter = e.target.value
-                    this.setState({ userFilter: newFilter })
-                  }}
-                  value={state.userFilter}
-                  placeholder={props.t('Filter my favorites')}
+                <FavoritesTable
+                  favoriteList={props.favoriteList}
+                  onFavoriteButtonClick={this.handleClickRemoveFromFavoriteList}
                 />
-
-                {filteredFavoriteList.length > 0
-                  ? (
-                    <>
-                      <FavoritesHeader
-                        onClickTitle={this.handleClickTitleToSort}
-                        isOrderAscending={state.sortOrder === SORT_ORDER.ASCENDING}
-                        selectedSortCriterion={state.selectedSortCriterion}
-                      />
-                      {filteredFavoriteList.map((favorite, index) => this.getFavoriteComponent(favorite, index))}
-                    </>
-                  )
-                  : (
-                    <EmptyListMessage>
-                      {props.favoriteList.length <= 0
-                        ? props.t('You did not add any content as favorite yet.')
-                        : props.t('There are no favorites that matches you filter')}
-                    </EmptyListMessage>
-                  )}
               </PageContent>
             )}
         </PageWrapper>
@@ -403,11 +170,10 @@ export class Favorites extends React.Component {
   }
 }
 
-const mapStateToProps = ({ breadcrumbs, user, favoriteList, contentType, system, workspaceList }) => ({
+const mapStateToProps = ({ breadcrumbs, user, favoriteList, system, workspaceList }) => ({
   breadcrumbs,
   user,
   favoriteList,
-  contentType,
   system,
   workspaceList
 })
