88d67b1ac4ae45c2c68e7db56edd6ab48610a820
Sébastien Grignard <sebastien.grignard@algoo.fr>
2022-12-29 09:42:58 +0100

    feat(frontend): performance timing & matomo tracking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,21 +11,82 @@ import i18n from './util/i18n.js'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
 import { history } from './util/helper.js'
+import { MatomoProvider, createInstance } from '@jonkoops/matomo-tracker-react'
+import { useLocation } from 'react-router'
 
 require('./css/index.styl')
 
 require('./util/appInterface.js')
 require('./util/tinymceInit.js')
 
+/**
+ * MatomoRouterProvider provides Matomo hooks for children components
+ * and it tracks page views on location change.
+ *
+ * It should be embedded within <Router>
+ */
+export const MatomoRouterProvider = ({children}) => {
+  const instance = createInstance({
+    urlBase: 'http://localhost:8080',
+    siteId: 1,
+    disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    heartBeat: { // optional, enabled by default
+      active: true, // optional, default value: true
+      seconds: 10 // optional, default value: `15
+    },
+    linkTracking: false, // optional, default value: true
+    configurations: { // optional, default value: {}
+      // any valid matomo configuration, all below are optional
+      disableCookies: true,
+      setSecureCookie: false,
+      setRequestMethod: 'POST'
+    }
+  })
+
+  const location = useLocation()
+  React.useEffect(() => { instance.trackPageView() }, [location])
+
+  React.useEffect(() => {
+    let startTime = performance.now()
+    let lastEntry = null
+    const observer = new PerformanceObserver(list => {
+      const perfEntries = list.getEntries().filter(entry => entry.startTime < startTime + 1000)
+      const last = perfEntries[perfEntries.length - 1]
+      lastEntry = last || lastEntry
+    })
+    observer.observe({entryTypes: ['resource']});
+
+    return () => {
+      if (lastEntry) {
+        instance.trackEvent({
+          category: 'Performance',
+          action: 'IRRE',
+          name: location.pathname,
+          value: lastEntry.responseEnd - startTime
+        })
+      }
+      observer.disconnect()
+    }
+  }, [location])
+
+  return (
+    <MatomoProvider value={instance}>
+      {children}
+    </MatomoProvider>
+  )
+}
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <I18nextProvider i18n={i18n}>
-        <DragDropContextProvider backend={HTML5Backend}>
-          <Tracim />
-        </DragDropContextProvider>
-      </I18nextProvider>
-    </Router>
-  </Provider>
+    <Provider store={store}>
+      <Router history={history}>
+        <MatomoRouterProvider>
+          <I18nextProvider i18n={i18n}>
+            <DragDropContextProvider backend={HTML5Backend}>
+              <Tracim />
+            </DragDropContextProvider>
+          </I18nextProvider>
+        </MatomoRouterProvider>
+      </Router>
+    </Provider>
   , document.getElementById('content')
 )
