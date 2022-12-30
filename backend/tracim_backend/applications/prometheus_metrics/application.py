dea52dc78ec1ece4c9a8a89453e478a58b9c5e82
Sébastien Grignard <sebastien.grignard@algoo.fr>
2022-12-30 16:28:00 +0100

    feat(backend): allow frontend to posting metrics (only IRRE for now)

--- backend/tracim_backend/applications/prometheus_metrics/application.py
+++ backend/tracim_backend/applications/prometheus_metrics/application.py
@@ -1,3 +1,5 @@
+import enum
+from http import HTTPStatus
 import re
 import threading
 import time
@@ -5,6 +7,7 @@ import typing
 
 from easy_profile import SessionProfiler
 from hapic.ext.pyramid import PyramidContext
+import marshmallow
 from prometheus_client import Summary
 from prometheus_client import make_wsgi_app
 import psutil
@@ -12,8 +15,29 @@ from pyramid.config import Configurator
 from pyramid.wsgi import wsgiapp2
 
 from tracim_backend.config import CFG
+from tracim_backend.extensions import hapic
 from tracim_backend.lib.utils.app import TracimApplication
+from tracim_backend.lib.utils.authorization import check_right
+from tracim_backend.lib.utils.authorization import is_user
 from tracim_backend.lib.utils.request import TracimRequest
+from tracim_backend.lib.utils.utils import generate_documentation_swagger_tag
+from tracim_backend.views.core_api.schemas import EnumField
+from tracim_backend.views.core_api.schemas import NoContentSchema
+
+SWAGGER_TAG__METRICS_ENDPOINTS = "Metrics"
+
+PATH_CATEGORY_RE = re.compile("\\/\d+(\\/|$)")
+PATH_CATEGORY_SUB = "/*\g<1>"
+
+
+class FrontendMetricNames(str, enum.Enum):
+    IRRE = "irre"
+
+
+class FrontendMetricSchema(marshmallow.Schema):
+    name = EnumField(FrontendMetricNames)
+    labels = marshmallow.fields.List(marshmallow.fields.String())
+    value = marshmallow.fields.Float()
 
 
 class MonitoringTracimRequest(TracimRequest):
@@ -47,7 +71,7 @@ class MonitoringTracimRequest(TracimRequest):
     def _report_duration(self, _) -> None:
         self._session_profiler.commit()
         duration = max(time.monotonic() - self._start_time, 0)
-        path_category = re.sub(r"\\/\d+(\\/|$)", "/*$1", self.path)
+        path_category = re.sub(PATH_CATEGORY_RE, PATH_CATEGORY_SUB, self.path)
         end_thread_stats = self._get_thread_stats()
         total_cpu_time = (
             end_thread_stats.user_time
@@ -62,6 +86,13 @@ class MonitoringTracimRequest(TracimRequest):
 
 
 class PrometheusMetricsApp(TracimApplication):
+
+    FRONTEND_METRICS = {
+        FrontendMetricNames.IRRE: Summary(
+            "frontend_irre_seconds", "IRRE of each frontend page", ("page", )
+        )
+    }
+
     def load_content_types(self) -> None:
         pass
 
@@ -70,6 +101,20 @@ class PrometheusMetricsApp(TracimApplication):
 
     def check_config(self, app_config: CFG):
         pass
+
+    @hapic.with_api_doc(tags=[SWAGGER_TAG__METRICS_ENDPOINTS])
+    @check_right(is_user)
+    @hapic.input_body(FrontendMetricSchema())
+    @hapic.output_body(NoContentSchema(), default_http_code=HTTPStatus.NO_CONTENT)
+    def add_frontend_metric(self, context, request: TracimRequest, hapic_data=None):
+        metric = self.FRONTEND_METRICS[hapic_data.body["name"]]
+        # TODO - SGD - 2022-12-30 - configure labels to perform replacement on paths only
+        labels = [
+            re.sub(PATH_CATEGORY_RE, PATH_CATEGORY_SUB, label)
+            for label in hapic_data.body["labels"]
+        ]
+        # TODO - SGD - 2022-12-30 - support other metric types (Gauge, Counter, Histogram)
+        metric.labels(*labels).observe(hapic_data.body["value"])
 
     def load_controllers(
         self,
@@ -81,9 +126,15 @@ class PrometheusMetricsApp(TracimApplication):
         # Use our request class which enables monitoring of total, CPU and db time per request
         configurator.set_request_factory(MonitoringTracimRequest)
         # Expose prometheus metrics
-        metrics_view = wsgiapp2(make_wsgi_app())
-        configurator.add_route("metrics", f"{route_prefix}metrics", request_method="GET")
-        configurator.add_view(metrics_view, route_name="metrics")
+        metrics_view = hapic.with_api_doc(tags=[SWAGGER_TAG__METRICS_ENDPOINTS])(
+            wsgiapp2(make_wsgi_app())
+        )
+        configurator.add_route("get_metrics", f"{route_prefix}metrics", request_method="GET")
+        configurator.add_view(metrics_view, route_name="get_metrics")
+
+        # Route for frontend metrics collection
+        configurator.add_route("post_metrics", f"{route_prefix}metrics", request_method="POST")
+        configurator.add_view(self.add_frontend_metric, route_name="post_metrics")
 
 
 def create_app() -> TracimApplication:
@@ -92,5 +143,5 @@ def create_app() -> TracimApplication:
         slug="prometheus-metrics",
         fa_icon="",
         config={},
-        main_route="/metrics",
+        main_route="/api/metrics",
     )
