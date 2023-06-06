d93d4cd0607f95409ab9227cf7df4bdee63a84c7
floreal15 <floreal.sangenis@algoo.fr>
2023-06-06 09:47:02 +0200

    fix(backend): new version of pre-commit

--- backend/tracim_backend/tests/commands/test_commands.py
+++ backend/tracim_backend/tests/commands/test_commands.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
+from depot.manager import DepotManager
 import os
 from os.path import dirname
-import subprocess
-
-from depot.manager import DepotManager
 import pytest
 from sqlalchemy.orm.exc import NoResultFound
+import subprocess
 import transaction
 
 import tracim_backend
@@ -2007,3 +2006,6 @@ class TestCommands(object):
             session.query(ContentRevisionRO).filter(
                 ContentRevisionRO.revision_id == revision_id
             ).one()
+
+
+n
