987b6eb0a5bd71bc56cbcdd5752114d1946d80f3
Skylsmoi <come.huguies@algoo.fr>
2023-11-23 12:24:58 +0100

    fix(front): fix linting and add unit test for knownMemberList

--- frontend/src/reducer/knownMemberList.js
+++ frontend/src/reducer/knownMemberList.js
@@ -7,7 +7,7 @@ import {
 import { serialize } from 'tracim_frontend_lib'
 const initialState = []
 
-const serializeKnownMemberProps = {
+export const serializeKnownMemberProps = {
   username: 'username',
   has_cover: 'hasCover',
   user_id: 'userId',
@@ -17,6 +17,8 @@ const serializeKnownMemberProps = {
 }
 
 export function knownMemberList (state = initialState, action) {
+  let knownMemberToRemove = []
+
   switch (action.type) {
     case SET_KNOWN_MEMBER_LIST:
       return action.knownMemberList.map(apiKnownMember =>
@@ -28,7 +30,7 @@ export function knownMemberList (state = initialState, action) {
       return [...state, ...serialize(action.newMember.user, serializeKnownMemberProps)]
 
     case REMOVE_WORKSPACE_MEMBER:
-      const knownMemberToRemove = state.find(km => km.userId === action.memberId)
+      knownMemberToRemove = state.find(km => km.userId === action.memberId)
       if (knownMemberToRemove.spaceList.length > 1) {
         return state.map(km => km.userId === action.action.memberId
           ? { ...km, spaceList: km.spaceList.filter(spaceId => spaceId !== action.workspaceId) }
