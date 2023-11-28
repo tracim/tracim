554b25401aaa808671bb15237bd7ff74e80000fd
Millefeuille42 <mathieu.labourier@algoo.fr>
2023-11-28 17:22:14 +0100

    fix(backend): removed filter on "normal" path

--- frontend/src/container/Favorites.jsx
+++ frontend/src/container/Favorites.jsx
@@ -10,7 +10,6 @@ import {
   PageTitle,
   PageWrapper,
   TracimComponent,
-  getContentComment,
   getContentPath,
   Loading,
   handleFetchResult
@@ -40,8 +39,8 @@ export class Favorites extends React.Component {
     super(props)
 
     this.state = {
-      contentCommentsCountList: [],
-      isLoading: true
+      isLoading: true,
+      favoriteList: []
     }
 
     props.registerCustomEventHandlerList([
@@ -77,6 +76,25 @@ export class Favorites extends React.Component {
     this.loadFavoriteList()
   }
 
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.workspaceList.length === 0) this.addWorkspaceToBreadcrumbsPath()
+  }
+
+  addWorkspaceToBreadcrumbsPath () {
+    const { props } = this
+    if (props.workspaceList.length === 0) return
+    const { favoriteList } = this.state
+
+    favoriteList.map(async favorite => {
+      if (!favorite.content) return null
+      const workspace = props.workspaceList.find(ws => ws.id === favorite.content.workspace_id)
+      favorite.breadcrumbs = [{ label: workspace.label }].concat(favorite.breadcrumbs)
+    })
+
+    this.setState({ isLoading: false })
+    props.dispatch(setFavoriteList([...favoriteList]))
+  }
+
   loadFavoriteList = async () => {
     const { props } = this
 
@@ -88,22 +106,8 @@ export class Favorites extends React.Component {
       props.dispatch(newFlashMessage(props.t('An error has happened while fetching favorites'), 'warning'))
       return
     }
-    const favoriteList = fetchFavoriteList.json.items
-    // Get comments (for their count in info)
-    const commentsFetchList = favoriteList.map(async favorite => {
-      if (!favorite.content) return null
-      // NOTE - S.G. - 2021-04-01 - here we have the favorite as returned by the backend
-      // hence the snake-case properties
-      const response = await getContentComment(
-        FETCH_CONFIG.apiUrl,
-        favorite.content.workspace_id,
-        favorite.content_id
-      )
-      if (!response.ok) return null
-      return (await response.json()).length
-    })
-    const contentCommentsCountList = await Promise.all(commentsFetchList)
 
+    const favoriteList = fetchFavoriteList.json.items
     // Get the contents' paths (for breadcrumbs)
     await Promise.all(favoriteList.map(async favorite => {
       if (!favorite.content) return null
@@ -112,13 +116,11 @@ export class Favorites extends React.Component {
       const response = await handleFetchResult(
         await getContentPath(FETCH_CONFIG.apiUrl, favorite.content_id)
       )
-      const workspace = props.workspaceList.find(ws => ws.id === favorite.content.workspace_id)
-
-      favorite.breadcrumbs = [{ label: workspace.label }].concat(response.body.items)
+      favorite.breadcrumbs = response.body.items
     }))
 
-    this.setState({ contentCommentsCountList, isLoading: false })
-    props.dispatch(setFavoriteList([...favoriteList]))
+    this.setState({ favoriteList })
+    this.addWorkspaceToBreadcrumbsPath()
   }
 
   handleClickRemoveFromFavoriteList = async (favorite) => {
