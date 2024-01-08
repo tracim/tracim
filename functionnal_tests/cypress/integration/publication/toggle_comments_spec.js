2e8e171ef3823427588d53a9cb3184b701e7235d
Millefeuille <mathieu.labourier@algoo.fr>
2024-01-08 10:12:11 +0100

    feat: add a popup for publication subject when posting a publication
    PR #6331
    
    ---------
    
    Co-authored-by: floreal15 <floreal.sangenis@algoo.fr>

--- functionnal_tests/cypress/integration/publication/toggle_comments_spec.js
+++ functionnal_tests/cypress/integration/publication/toggle_comments_spec.js
@@ -1,4 +1,5 @@
-import { PAGES } from '../../support/urls_commands'
+import { PAGES } from '../../support/urls_commands.js'
+import { SELECTORS } from '../../support/generic_selector_commands.js'
 
 const publishButton = '.commentArea__submit__btn'
 const text = 'Hello, world'
@@ -20,6 +21,9 @@ describe('Publications page', () => {
   it("should change button's label according to its state", () => {
     cy.inputInTinyMCE(text)
     cy.contains(publishButton, 'Publish').click()
+    cy.getTag({ selectorName: SELECTORS.CARD_POPUP_BODY })
+          .get('[data-cy=popup__createcontent__form__button]')
+          .click()
     cy.contains('[data-cy=timeline__comment__body__content__text]', text)
     cy.contains('.buttonComments', 'Comment').should('be.visible').click()
     cy.inputInTinyMCE(text)
