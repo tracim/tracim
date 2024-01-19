b0de89a129c75d2d5b1b6ac9268860b6b4731003
Millefeuille42 <mathieu.labourier@algoo.fr>
2024-01-19 15:15:32 +0100

    fix(frontend): added missing title on button to mark all notifications as read

--- frontend/src/container/NotificationWall.jsx
+++ frontend/src/container/NotificationWall.jsx
@@ -708,6 +708,7 @@ export const NotificationWall = props => {
               onClick={handleClickMarkAllAsRead}
               icon='far fa-envelope-open'
               dataCy='markAllAsReadButton'
+              text={props.t('Mark all as read')}
             />
           </PopinFixedHeader>
 
