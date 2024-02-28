31a5b54bb4824c48c6805cc40add6e1ecfbae994
Millefeuille <mlabouri@student.42.fr>
2024-02-28 14:20:59 +0100

    fix(frontend): adjusted getCurrentDateTime function

--- frontend_app_logbook/src/component/LogbookEntryEditor.jsx
+++ frontend_app_logbook/src/component/LogbookEntryEditor.jsx
@@ -17,14 +17,10 @@ function LogbookEntryEditor (props) {
   const [deadline, setDeadline] = useState(entry.deadline || getCurrentDateTime())
   const [freeInput, setFreeInput] = useState(entry.freeInput || '')
 
+  // NOTE - M.L. - 2024-02-28 - This function is required due to the very specific format requested
+  //  by the 'datetime-local' input type
   function getCurrentDateTime () {
-    const now = new Date()
-    const year = now.getFullYear()
-    const month = `${(now.getMonth() + 1)}`.padStart(2, '0')
-    const day = `${now.getDate()}`.padStart(2, '0')
-    const hours = `${now.getHours()}`.padStart(2, '0')
-    const minutes = `${now.getMinutes()}`.padStart(2, '0')
-    return `${year}-${month}-${day}T${hours}:${minutes}`
+    return new Date().toISOString().replaceAll('Z', '')
   }
 
   function handleValidate (e) {
