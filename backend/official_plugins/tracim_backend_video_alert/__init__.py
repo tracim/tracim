b6fc8927d52fe5925e9f3b52f171d7acddfbddc6
Millefeuille42 <mathieu.labourier@algoo.fr>
2024-03-20 15:01:06 +0100

    fix: converted to parsed config + new mention system

--- backend/official_plugins/tracim_backend_video_alert/__init__.py
+++ backend/official_plugins/tracim_backend_video_alert/__init__.py
@@ -1,20 +1,17 @@
-import os
-import uuid
-
 from pluggy import PluginManager
+from pyramid.config import Configurator
 
-from tracim_backend.lib.core.plugins import hookimpl
+from tracim_backend import sliced_dict
 from tracim_backend.lib.core.content import ContentApi
+from tracim_backend.lib.core.plugins import hookimpl
 from tracim_backend.lib.core.user import UserApi
 from tracim_backend.lib.core.workspace import WorkspaceApi
-from tracim_backend.lib.core.event import EventApi
 from tracim_backend.lib.utils.logger import logger
 from tracim_backend.lib.utils.request import TracimContext
+from tracim_backend.lib.utils.utils import string_to_unique_item_list
 from tracim_backend.models.data import Content
-from tracim_backend.models.event import EntityType, OperationType, Event
 
 # Envoi de la mention
-# Rédaction d'un micro-tuto pour expliquer comment faire un plugin (markdown, hors de Tracim)
 
 # --- CONFIG ---
 
@@ -24,7 +21,7 @@ config = {
     # Message of the warning. Note that a mention to the file's author will be prepended.
     "message": "Oops, I sent an unsupported video format. I should've sent a mp4 or webm file.",
     # Username of the user that will comment the warning. Leave blank to use the author of the file.
-    "username": "beep",
+    "username": "",
     # List of mimetypes that should bypass the blacklist.
     "whitelist": ["video/mp4", "video/webm"],
     # List of mimetypes that should trigger a warning
@@ -33,45 +30,12 @@ config = {
 
 # --- UTILS---
 
-MENTION_NODE_NAME = 'span'
-MENTION_ID_PREFIX = 'mention-'
-MENTION_CLASS = 'mention'
-
-
-def wrap_in_mention_node(recipient: str, id_: str):
-    if recipient == "":
-        return ""
-    return f'<{MENTION_NODE_NAME} id="{MENTION_ID_PREFIX}{id_}" class="{MENTION_CLASS}">' + \
-        f'@{recipient}</{MENTION_NODE_NAME}>'
-
-
-def is_content_supported(content: Content):
-    return content.type == "file"
-
-
-def is_content_whitelisted(content: Content):
-    content_mimetype = content.file_mimetype.lower()
-
-    for ok_mimetype in config["whitelist"]:
-        if ok_mimetype.endswith("/") and content_mimetype.startswith(ok_mimetype):
-            return True
-        elif content_mimetype == ok_mimetype:
-            return True
-    return False
-
-
-def is_content_blacklisted(content: Content):
-    content_mimetype = content.file_mimetype.lower()
-
-    for nok_mimetype in config["blacklist"]:
-        if nok_mimetype.endswith("/") and content_mimetype.startswith(nok_mimetype):
-            return True
-        elif content_mimetype == nok_mimetype:
-            return True
-    return False
+MENTION_NODE_NAME = "html-mention"
+SETTINGS_KEY_PREFIX = "video_alert_plugin."
 
 
 # --- MAIN ---
+
 
 class VideoAlertPlugin:
     """
@@ -80,27 +44,82 @@ class VideoAlertPlugin:
     Needs a registration using 'register_tracim_plugin' function.
     """
 
+    def __init__(self):
+        self.config = None
+
+    @staticmethod
+    def wrap_in_mention_node(recipient: str, id_: str):
+        return (
+            ""
+            if recipient == ""
+            else (f'<{MENTION_NODE_NAME} userid="{id_}">' + f"@{recipient}</{MENTION_NODE_NAME}>")
+        )
+
+    @staticmethod
+    def is_content_supported(content: Content):
+        return content.type == "file"
+
+    def is_content_whitelisted(self, content: Content):
+        content_mimetype = content.file_mimetype.lower()
+
+        for ok_mimetype in self.config["whitelist"]:
+            if ok_mimetype.endswith("/") and content_mimetype.startswith(ok_mimetype):
+                return True
+            elif content_mimetype == ok_mimetype:
+                return True
+        return False
+
+    def is_content_blacklisted(self, content: Content):
+        content_mimetype = content.file_mimetype.lower()
+
+        for nok_mimetype in self.config["blacklist"]:
+            if nok_mimetype.endswith("/") and content_mimetype.startswith(nok_mimetype):
+                return True
+            elif content_mimetype == nok_mimetype:
+                return True
+        return False
+
     @hookimpl
     def on_plugins_loaded(self) -> None:
         """
         This method is called when the plugin is loaded.
         """
-        config["message"] = os.environ.get('TRACIM_VIDEO_ALERT_MESSAGE', config["message"])
-        config["username"] = os.environ.get('TRACIM_VIDEO_ALERT_USERNAME', config["username"])
 
-        whitelist = os.environ.get('TRACIM_VIDEO_ALERT_WHITELIST')
+        from pyramid.threadlocal import get_current_registry
+
+        registry = get_current_registry()
+        configurator = Configurator(registry=registry)
+
+        settings = configurator.get_settings()
+        plugin_settings = sliced_dict(settings, beginning_key_string=SETTINGS_KEY_PREFIX)
+
+        self.config = config
+        self.config["message"] = plugin_settings.get(
+            f"{SETTINGS_KEY_PREFIX}message", config["message"]
+        )
+        self.config["username"] = plugin_settings.get(
+            f"{SETTINGS_KEY_PREFIX}username", config["username"]
+        )
+
+        whitelist = plugin_settings.get(f"{SETTINGS_KEY_PREFIX}whitelist")
         if whitelist:
-            config["whitelist"] = []
-            for mimetype in whitelist.split(','):
-                config["whitelist"].append(mimetype)
+            self.config["whitelist"] = string_to_unique_item_list(
+                whitelist,
+                separator=",",
+                cast_func=str,
+                do_strip=True,
+            )
 
-        blacklist = os.environ.get('TRACIM_VIDEO_ALERT_BLACKLIST')
+        blacklist = plugin_settings.get(f"{SETTINGS_KEY_PREFIX}blacklist")
         if blacklist:
-            config["blacklist"] = []
-            for mimetype in blacklist.split(','):
-                config["blacklist"].append(mimetype)
+            self.config["blacklist"] = string_to_unique_item_list(
+                blacklist,
+                separator=",",
+                cast_func=str,
+                do_strip=True,
+            )
 
-        for key, value in config.items():
+        for key, value in self.config.items():
             logger.info(self, f"{key}: {value}")
 
     @hookimpl
@@ -109,13 +128,13 @@ class VideoAlertPlugin:
         Add a comment to the content if it is a video and not a mp4 file.
         """
 
-        if not is_content_supported(content):
+        if not self.is_content_supported(content):
             return
 
-        if is_content_whitelisted(content) or not is_content_blacklisted(content):
+        if self.is_content_whitelisted(content) or not self.is_content_blacklisted(content):
             return
 
-        username = config["username"]
+        username = self.config["username"]
         if username == "":
             username = content.author.username
 
@@ -136,17 +155,19 @@ class VideoAlertPlugin:
             logger.error(self, f"VIDEO_ALERT_PLUGIN w/ WorkspaceApi {e}")
             return
 
-        mention_id = str(uuid.uuid4())
+        author_id = content.author.user_id
         mention_recipient = content.author.username
         try:
             content_api = ContentApi(
-                session=context.dbsession, current_user=current_user, config=context.app_config,
+                session=context.dbsession,
+                current_user=current_user,
+                config=context.app_config,
             )
             content_api.create_comment(
                 workspace=workspace,
                 parent=content,
-                content=f"<p>{wrap_in_mention_node(mention_recipient, mention_id)} " +
-                        f"- {config['message']}</p>",
+                content=f"<p>{self.wrap_in_mention_node(mention_recipient, str(author_id))} "
+                + f"- {self.config['message']}</p>",
                 do_save=False,
                 do_notify=False,
             )
