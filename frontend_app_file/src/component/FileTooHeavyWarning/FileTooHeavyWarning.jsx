fe00ddc49aec1efae94d9a475b666b3bc7be0dd3
Skylsmoi <come.huguies@algoo.fr>
2025-04-08 12:42:19 +0200

    feat(front): rename class of component FileTooHeavyWarning.jsx + move specific css

--- frontend_app_file/src/component/FileTooHeavyWarning/FileTooHeavyWarning.jsx
+++ frontend_app_file/src/component/FileTooHeavyWarning/FileTooHeavyWarning.jsx
@@ -6,21 +6,23 @@ import {
 } from 'tracim_frontend_lib'
 import { translate } from 'react-i18next'
 
+require('./FileTooHeavyWarning.styl')
+
 export const FileTooHeavyWarning = props => {
   return (
-    <pre className='TextViewerSyntaxHighlight__blocked language-none'>
-      <div className='TextViewerSyntaxHighlight__blocked__msg'>
+    <pre className='FileTooHeavyWarning language-none'>
+      <div className='FileTooHeavyWarning__msg'>
         {props.t('The file weight {{ fileSize }}.', { fileSize: displayFileSize(props.contentSize, 2) })}
         <br />
         {props.t('Viewing it online might slow down the page.')}
       </div>
 
-      <div className='TextViewerSyntaxHighlight__blocked__btn'>
+      <div className='FileTooHeavyWarning__btn'>
         <IconButton
           onClick={props.onRunAnyway}
           text={props.t('View anyway')}
           icon='far fa-eye'
-          customClass='TextViewerSyntaxHighlight__blocked__btn__run'
+          customClass='FileTooHeavyWarning__btn__run'
         />
       </div>
     </pre>
