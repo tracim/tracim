5fcf4ad98d4744dc86987b6cd727a30b8b66ba33
Skylsmoi <come.huguies@algoo.fr>
2025-06-06 14:45:01 +0200

    fix(front): fix card design when there is only a due date

--- frontend_app_kanban/src/component/KanbanCard.jsx
+++ frontend_app_kanban/src/component/KanbanCard.jsx
@@ -138,31 +138,35 @@ function KanbanCard (props) {
 
       <div className='kanban__contentpage__wrapper__board__card__options'>
         <div className='kanban__contentpage__wrapper__board__card__options__date'>
-          {props.card.kickoff !== '' && (
-            <div
-              className='kanban__contentpage__wrapper__board__card__options__date__kickoff'
-              title={props.t('Start date')}
-            >
-              <Icon
-                icon='far fa-calendar'
-                title={props.card.kickoff}
-              />
-              {props.card.kickoff}
-            </div>
-          )}
+          <div
+            className='kanban__contentpage__wrapper__board__card__options__date__kickoff'
+            title={props.t('Start date')}
+          >
+            {props.card.kickoff !== '' && (
+              <>
+                <Icon
+                  icon='far fa-calendar'
+                  title={props.card.kickoff}
+                />
+                {props.card.kickoff}
+              </>
+            )}
+          </div>
 
-          {props.card.deadline !== '' && (
-            <div
-              className='kanban__contentpage__wrapper__board__card__options__date__deadline'
-              title={props.t('Due date')}
-            >
-              <Icon
-                icon='far fa-calendar'
-                title={props.card.deadline}
-              />
-              {props.card.deadline}
-            </div>
-          )}
+          <div
+            className='kanban__contentpage__wrapper__board__card__options__date__deadline'
+            title={props.t('Due date')}
+          >
+            {props.card.deadline !== '' && (
+              <>
+                <Icon
+                  icon='far fa-calendar'
+                  title={props.card.deadline}
+                />
+                {props.card.deadline}
+              </>
+            )}
+          </div>
         </div>
 
         <div className='kanban__contentpage__wrapper__board__card__options__freeInput'>
