678c1539cc3da4a1589d3dd76427d72d7c76bd64
Skylsmoi <come.huguies@algoo.fr>
2025-07-09 15:29:45 +0200

    fix(back): add comment to explain why import isn't at the top of the file

--- backend/tracim_backend/lib/rich_text_preview/html_parser.py
+++ backend/tracim_backend/lib/rich_text_preview/html_parser.py
@@ -24,6 +24,8 @@ class HtmlParser:
     def add_public_name_to_mention(self):
         mention_list = self.soup.find_all("html-mention")
 
+        # HACK - 2025-07-09 - Importing UserApi at the top of the file generate
+        # circular import
         from tracim_backend.lib.core.user import UserApi
 
         user_api = UserApi(None, session=self.session, config=self.app_config)
