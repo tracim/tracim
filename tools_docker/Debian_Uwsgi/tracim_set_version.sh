2438f8f63c6669d9bc0cf03c3a60694a88128d46
Pierre Goiffon <pierre.goiffon@algoo.fr>
2025-10-24 16:40:48 +0200

    Docker uWSGI : set Tracim version from VERSION build arg

---
+++ tools_docker/Debian_Uwsgi/tracim_set_version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+tracim_version=$1
+
+echo "Setting TRACIM version to ${tracim_version}"
+sed -i "s/version\s*=\s*0000.00.00/version = v${tracim_version}/" /tracim/backend/setup.cfg
+sed -i "s/\"tracim_app_version\": \"0000.00.00\"/\"tracim_app_version\": \"v${tracim_version}\"/" /tracim/frontend/src/version.json
