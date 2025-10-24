813f22188eef1adc971e2fce4c40f5d4f2209171
Pierre Goiffon <pierre.goiffon@algoo.fr>
2025-10-24 16:44:35 +0200

    version.json : fix version format

--- tools_docker/Debian_Uwsgi/tracim_set_version.sh
+++ tools_docker/Debian_Uwsgi/tracim_set_version.sh
@@ -4,4 +4,4 @@ tracim_version=$1
 
 echo "Setting TRACIM version to ${tracim_version}"
 sed -i "s/version\s*=\s*0000.00.00/version = v${tracim_version}/" /tracim/backend/setup.cfg
-sed -i "s/\"tracim_app_version\": \"0000.00.00\"/\"tracim_app_version\": \"v${tracim_version}\"/" /tracim/frontend/src/version.json
+sed -i "s/\"tracim_app_version\": \"v0000.00.00\"/\"tracim_app_version\": \"v${tracim_version}\"/" /tracim/frontend/src/version.json
