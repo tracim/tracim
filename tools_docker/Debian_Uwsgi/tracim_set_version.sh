1a184f00db710c3c99168e48cdbc6fbf78b0a031
Pierre Goiffon <pierre.goiffon@algoo.fr>
2025-10-27 11:05:00 +0100

    Docker uWSGI : version rewrite sed commands for better maintainability Use format similar to what is done in common.sh

--- tools_docker/Debian_Uwsgi/tracim_set_version.sh
+++ tools_docker/Debian_Uwsgi/tracim_set_version.sh
@@ -4,5 +4,5 @@ tracim_version=$1
 container_dir=$2
 
 echo "Setting TRACIM version to ${tracim_version}"
-sed -i "s/version\s*=\s*0000.00.00/version = v${tracim_version}/" ${container_dir}/backend/setup.cfg
-sed -i "s/\"tracim_app_version\": \"v0000.00.00\"/\"tracim_app_version\": \"v${tracim_version}\"/" ${container_dir}/frontend/src/version.json
+sed -i "s|^version = 0000.00.00|version = v${tracim_version}|g" ${container_dir}/backend/setup.cfg
+sed -i "s|\"tracim_app_version\": \"v0000.00.00\"|\"tracim_app_version\": \"v${tracim_version}\"|g" ${container_dir}/frontend/src/version.json
