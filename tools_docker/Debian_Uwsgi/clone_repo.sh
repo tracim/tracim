43e649c8e866710e5dc214ebb02d0b57a5e5a678
Pierre Goiffon <pierre.goiffon@algoo.fr>
2025-10-27 10:56:11 +0100

    Docker uWSGI : factorize container Tracim dir

--- tools_docker/Debian_Uwsgi/clone_repo.sh
+++ tools_docker/Debian_Uwsgi/clone_repo.sh
@@ -3,16 +3,17 @@ set -e
 repo=$1
 branch=$2
 tag=$3
+container_dir=$4
 if ([ "$tag" != "" ] && [ "$branch" != "" ]); then
     echo "Only one of tag and branch argument should have a value"
     exit 1
 elif ([ "$tag" != "" ] && [ "$branch" = "" ]); then
     echo "using the tag $tag"
-    git clone -b "$tag" --depth 1 "$repo" /tracim
+    git clone -b "$tag" --depth 1 "$repo" ${container_dir}
 elif ([ "$tag" = "" ] && [ "$branch" != "" ]); then
     echo "using the branch $branch"
-    git clone -b "$branch" --depth 1 "$repo" /tracim;
+    git clone -b "$branch" --depth 1 "$repo" ${container_dir};
 else
     echo "using the default branch (develop)"
-    git clone "$repo" /tracim
+    git clone "$repo" ${container_dir}
 fi
