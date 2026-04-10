tracim/tracim

commit 3bb9d0a43a68856613344eb0e1c201c085438d17

Fix whole address string converted to lowercase in share file link and folder upload link (#6832)

backend/tracim_backend/applications/share/lib.py
@@ -19,7 +19,7 @@ from tracim_backend.exceptions import ContentShareNotFound
 from tracim_backend.exceptions import NotificationSendingFailed
 from tracim_backend.exceptions import WrongSharePassword
 from tracim_backend.lib.core.content import ContentApi
-from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration
+from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration, EmailAddress
 from tracim_backend.lib.utils.logger import logger
 from tracim_backend.lib.utils.utils import core_convert_file_name_to_display
 from tracim_backend.lib.utils.utils import get_frontend_ui_base_url
@@ -63,10 +63,11 @@ class ShareLib(object):
         created = datetime.now()
         share_group_uuid = str(uuid.uuid4())
         for email in emails:
+            email_object = EmailAddress.from_rfc_email_address(email, True)
             content_share = ContentShare(
                 author=self._user,
                 content_id=content.content_id,
-                email=email.lower(),
+                email=email_object.address,
                 share_token=str(uuid.uuid4()),
                 password=password,
                 type=ContentShareType.EMAIL,

backend/tracim_backend/applications/upload_permissions/lib.py
@@ -28,7 +28,7 @@ from tracim_backend.exceptions import WrongSharePassword
 from tracim_backend.lib.core.content import ContentApi
 from tracim_backend.lib.core.user import UserApi
 from tracim_backend.lib.core.workspace import WorkspaceApi
-from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration
+from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration, EmailAddress
 from tracim_backend.lib.utils.logger import logger
 from tracim_backend.lib.utils.translation import Translator
 from tracim_backend.lib.utils.utils import get_frontend_ui_base_url
@@ -75,10 +75,11 @@ class UploadPermissionLib(object):
         created = datetime.utcnow()
         upload_permission_group_uuid = str(uuid.uuid4().hex)
         for email in emails:
+            email_object = EmailAddress.from_rfc_email_address(email, True)
             upload_permission = UploadPermission(
                 author=self._user,
                 workspace_id=workspace.workspace_id,
-                email=email.lower(),
+                email=email_object.address,
                 token=str(uuid.uuid4()),
                 password=password,
                 type=UploadPermissionType.EMAIL,

backend/tracim_backend/tests/library/test_utils.py
@@ -194,6 +194,14 @@ class TestEmailAddress(object):
         assert john_address.force_angle_bracket is False
         assert john_address.address == "john.doe@domainame.ndl"
 
+        john_address_angle_brackets = EmailAddress.from_rfc_email_address(
+            "<john.doe@domainame.ndl>", True)
+        assert john_address_angle_brackets.domain == "domainame.ndl"
+        assert john_address_angle_brackets.label == ""
+        assert john_address_angle_brackets.email == "john.doe@domainame.ndl"
+        assert john_address_angle_brackets.force_angle_bracket is True
+        assert john_address_angle_brackets.address == "<john.doe@domainame.ndl>"
+
     def test_unit__email_address_address__ok__from_rfc_email_address__nominal_case(
         self,
     ):
@@ -208,6 +216,17 @@ class TestEmailAddress(object):
         self,
     ):
         john_address = EmailAddress.from_rfc_email_address('"John Doe" <john.doe@domainame.ndl>')
+        assert john_address.domain == "domainame.ndl"
+        assert john_address.label == "John Doe"
+        assert john_address.email == "john.doe@domainame.ndl"
+        assert john_address.force_angle_bracket is False
+        assert john_address.address == "John Doe <john.doe@domainame.ndl>"
+
+    def test_unit__email_address_address__ok__from_rfc_email_address__with_non_lowercase_email(
+        self,
+    ):
+        john_address = EmailAddress.from_rfc_email_address(
+            '"John Doe" <John.Doe@domainame.ndl>')
         assert john_address.domain == "domainame.ndl"
         assert john_address.label == "John Doe"
         assert john_address.email == "john.doe@domainame.ndl"

backend/tracim_backend/lib/mail_notifier/utils.py
@@ -66,15 +66,27 @@ class EST(object):
 
 class EmailAddress(object):
     def __init__(self, label: str, email: str, force_angle_bracket=False):
+        """
+        See :meth:`from_rfc_email_address` to construct the object from a raw string
+
+        Warning : email will be converted to lowercase, see `🐛 Bug: In a share link, whole field is converted to lowercase even if it consists of a name + email address · Issue #6830 <https://github.com/tracim/tracim/issues/6830>`
+        """
         self.label = label
+
+        # we want email to be always lowercase, see GH#2339 and GH#6830
+        email = email.lower()
         self.email = email
         self.idna_email = self._encode_idna(email)
+
         self.force_angle_bracket = force_angle_bracket
 
     def _encode_idna_part(self, part: str):
         return ".".join([p.encode("idna").decode("ascii") for p in part.split(".")])
 
     def _encode_idna(self, email: str):
+        """
+        Handles international domain names, see `encodings.idna — Internationalized Domain Names in Applications <https://docs.python.org/3/library/codecs.html#module-encodings.idna>`
+        """
         username, domain = [self._encode_idna_part(part) for part in email.rsplit("@", 1)]
 
         username = self._encode_idna_part(username)
@@ -83,12 +95,22 @@ class EmailAddress(object):
         return username + "@" + domain
 
     @classmethod
-    def from_rfc_email_address(cls, rfc_email: str) -> "EmailAddress":
+    def from_rfc_email_address(cls, rfc_email: str, force_angle_bracket=False) -> "EmailAddress":
+        """
+        RFC 5322 "angle-addr" format like:
+           "John Doe" <john.doe@domainame.ndl>
+        Uses :func:`parseaddr`
+        See `RFC 5322 - Internet Message Format <https://datatracker.ietf.org/doc/html/rfc5322#section-3.4>`
+        See :class:`TestEmailAddress` for use cases
+        """
         label, email = parseaddr(rfc_email)
-        return EmailAddress(label, email)
+        return EmailAddress(label, email, force_angle_bracket)
 
     @property
     def address(self):
+        """
+        Uses :meth:email.utils.formataddr
+        """
         if self.label:
             return formataddr((self.label, self.idna_email))
         if self.force_angle_bracket and self.email and self.email[0] != "<":

commit 8ff939cee9dd38cfeba159aae0ab7e77b286e340

Fix linter issues
I introduced the issues with 3bb9d0a4 :/
Now I have the pre commit hook correctly configured

backend/tracim_backend/applications/share/lib.py
@@ -19,7 +19,8 @@ from tracim_backend.exceptions import ContentShareNotFound
 from tracim_backend.exceptions import NotificationSendingFailed
 from tracim_backend.exceptions import WrongSharePassword
 from tracim_backend.lib.core.content import ContentApi
-from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration, EmailAddress
+from tracim_backend.lib.mail_notifier.utils import EmailAddress
+from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration
 from tracim_backend.lib.utils.logger import logger
 from tracim_backend.lib.utils.utils import core_convert_file_name_to_display
 from tracim_backend.lib.utils.utils import get_frontend_ui_base_url

backend/tracim_backend/applications/upload_permissions/lib.py
@@ -28,7 +28,8 @@ from tracim_backend.exceptions import WrongSharePassword
 from tracim_backend.lib.core.content import ContentApi
 from tracim_backend.lib.core.user import UserApi
 from tracim_backend.lib.core.workspace import WorkspaceApi
-from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration, EmailAddress
+from tracim_backend.lib.mail_notifier.utils import EmailAddress
+from tracim_backend.lib.mail_notifier.utils import SmtpConfiguration
 from tracim_backend.lib.utils.logger import logger
 from tracim_backend.lib.utils.translation import Translator
 from tracim_backend.lib.utils.utils import get_frontend_ui_base_url

backend/tracim_backend/tests/library/test_utils.py
@@ -195,7 +195,8 @@ class TestEmailAddress(object):
         assert john_address.address == "john.doe@domainame.ndl"
 
         john_address_angle_brackets = EmailAddress.from_rfc_email_address(
-            "<john.doe@domainame.ndl>", True)
+            "<john.doe@domainame.ndl>", True
+        )
         assert john_address_angle_brackets.domain == "domainame.ndl"
         assert john_address_angle_brackets.label == ""
         assert john_address_angle_brackets.email == "john.doe@domainame.ndl"
@@ -225,8 +226,7 @@ class TestEmailAddress(object):
     def test_unit__email_address_address__ok__from_rfc_email_address__with_non_lowercase_email(
         self,
     ):
-        john_address = EmailAddress.from_rfc_email_address(
-            '"John Doe" <John.Doe@domainame.ndl>')
+        john_address = EmailAddress.from_rfc_email_address('"John Doe" <John.Doe@domainame.ndl>')
         assert john_address.domain == "domainame.ndl"
         assert john_address.label == "John Doe"
         assert john_address.email == "john.doe@domainame.ndl"