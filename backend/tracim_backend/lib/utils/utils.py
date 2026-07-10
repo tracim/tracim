tracim/tracim

commit b77d628f2d1cfe1e8cd16194f4f83a0cb870e86e

Fix "fatal : aucune étiquette ne correspond parfaitement à '...'" errors in test logs

backend/tracim_backend/lib/utils/utils.py
@@ -493,7 +493,6 @@ def get_build_version() -> str:
     Get either tag or commit hash linked to current commit
     :return: tag or commit hash
     """
-    last_commit = os.popen("git rev-parse HEAD 2> /dev/null").read()
     return_value = os.system(f"git describe --exact-match {last_commit} 2> /dev/null")
 
     if return_value:
@@ -501,6 +500,10 @@ def get_build_version() -> str:
         if hash == "":
             return UNKNOWN_BUILD_VERSION
         return hash
+
+    last_commit = (
+        os.popen("git rev-parse HEAD 2> /dev/null").read().strip()
+    )  # strip needed to remove newlines
     return os.popen(f"git describe --exact-match {last_commit} 2> /dev/null").read()
 
 

commit 3fc2d0b290e54912cc726ffee916104adb2f8d9a

Fix last_commit regression Introduced by b77d628f

backend/tracim_backend/lib/utils/utils.py
@@ -493,6 +493,7 @@ def get_build_version() -> str:
     Get either tag or commit hash linked to current commit
     :return: tag or commit hash
     """
+    last_commit = os.popen("git rev-parse HEAD 2> /dev/null").read().strip()
     return_value = os.system(f"git describe --exact-match {last_commit} 2> /dev/null")
 
     if return_value:
@@ -500,10 +501,6 @@ def get_build_version() -> str:
         if hash == "":
             return UNKNOWN_BUILD_VERSION
         return hash
-
-    last_commit = (
-        os.popen("git rev-parse HEAD 2> /dev/null").read().strip()
-    )  # strip needed to remove newlines
     return os.popen(f"git describe --exact-match {last_commit} 2> /dev/null").read()
 
 