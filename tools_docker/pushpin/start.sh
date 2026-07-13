54d03fedb02456f0d81205358eb1f1f3aec2a9c7
Pierre Goiffon <pierre.goiffon@algoo.fr>
2026-07-13 15:39:13 +0200

    docker pusphin: remove zurl

--- tools_docker/pushpin/start.sh
+++ tools_docker/pushpin/start.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-teardown () {
-    if [ -n "$ZURL_PID" ]; then kill "$ZURL_PID"; fi
-    if [ -n "$PUSHPIN_PID" ]; then kill "$PUSHPIN_PID"; fi
-    exit 0
-}
-
-# Starting zurl separately to use /etc/zurl.conf instead of /var/run/pushpin/zurl.conf
-# So that zurl shares the same parameters with mongrel2.
-mkdir -p /var/run/zurl
-trap teardown HUP INT TERM
-zurl --config=/etc/zurl.conf &
-ZURL_PID=$!
+trap 'kill $PUSHPIN_PID; exit 0' HUP INT TERM
 pushpin --merge-output &
 PUSHPIN_PID=$!
 wait
