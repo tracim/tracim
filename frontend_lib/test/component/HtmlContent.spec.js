8f23f962eb587a96c696cf12cc7b56e043c14e31
Mergepath <mergepath@local>
2026-08-02 03:29:30 +0000

    fix: address #6666 - 🐛 Bug: setup_default_backend. fails to install node and npm
    Closes #6666

--- frontend_lib/test/component/HtmlContent.spec.js
+++ frontend_lib/test/component/HtmlContent.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
 import HTMLContent from '../../src/component/HTMLContent/HTMLContent.jsx'
+import { CUSTOM_EVENT } from '../../src/customEvent.js'
 
 describe('<HTMLContent />', () => {
   const props = {
@@ -17,5 +18,48 @@ describe('<HTMLContent />', () => {
 
   it('should display the content of the text', () => {
     expect(wrapper.render().text()).to.contains(props.htmlValue)
+  })
+
+  it('should redirect same-origin links on plain left-click', () => {
+    global.GLOBAL_dispatchEvent.resetHistory()
+    const linkHref = '/ui/workspaces/1/contents/2'
+    const wrapper = mount(
+      <HTMLContent {...props} htmlValue={`<a href="${linkHref}">internal link</a>`} />
+    )
+    const anchor = wrapper.getDOMNode().querySelector('a')
+    let isDefaultPrevented = false
+
+    wrapper.find('article').simulate('click', {
+      target: anchor,
+      button: 0,
+      preventDefault: () => { isDefaultPrevented = true }
+    })
+
+    expect(global.GLOBAL_dispatchEvent.calledOnce).to.equal(true)
+    expect(global.GLOBAL_dispatchEvent.firstCall.args[0]).to.deep.equal({
+      type: CUSTOM_EVENT.REDIRECT,
+      data: { url: linkHref }
+    })
+    expect(isDefaultPrevented).to.equal(true)
+  })
+
+  it('should let the browser handle same-origin links on Ctrl+Click', () => {
+    global.GLOBAL_dispatchEvent.resetHistory()
+    const linkHref = '/ui/workspaces/1/contents/2'
+    const wrapper = mount(
+      <HTMLContent {...props} htmlValue={`<a href="${linkHref}">internal link</a>`} />
+    )
+    const anchor = wrapper.getDOMNode().querySelector('a')
+    let isDefaultPrevented = false
+
+    wrapper.find('article').simulate('click', {
+      target: anchor,
+      button: 0,
+      ctrlKey: true,
+      preventDefault: () => { isDefaultPrevented = true }
+    })
+
+    expect(global.GLOBAL_dispatchEvent.called).to.equal(false)
+    expect(isDefaultPrevented).to.equal(false)
   })
 })
