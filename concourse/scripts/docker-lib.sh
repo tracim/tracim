a822fd80db3d8cb64b920b0a944ef2b4e4852b90
Pierre Goiffon <pierre.goiffon@algoo.fr>
2026-08-05 16:40:59 +0200

    Remove cgroup v1 code
    Was causing following error:
    mount: /sys/fs/cgroup/cpuset: fsconfig() failed: cgroup: Unknown subsys name 'cpuset'.
           dmesg(1) may have more information after failed mount system call.
    
    Since Trixie we have cgroup v2 unified hierarchy only => cpuset is already bound into the single unified /sys/fs/cgroup tree

--- concourse/scripts/docker-lib.sh
+++ concourse/scripts/docker-lib.sh
@@ -9,48 +9,13 @@ DOCKER_DATA_ROOT=${DOCKER_DATA_ROOT:-/scratch/docker}
 IP_FOR_WAN_INTERFACE=8.8.8.8
 
 sanitize_cgroups() {
+  # cgroup v2 unified hierarchy only (the only mode on Debian 13/Trixie):
+  # dockerd handles it natively, we just need it mounted read-write.
   mkdir -p /sys/fs/cgroup
   mountpoint -q /sys/fs/cgroup || \
-    mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
+    mount -t cgroup2 -o nsdelegate cgroup2 /sys/fs/cgroup
 
   mount -o remount,rw /sys/fs/cgroup
-
-  sed -e 1d /proc/cgroups | while read sys hierarchy num enabled; do
-    if [ "$enabled" != "1" ]; then
-      # subsystem disabled; skip
-      continue
-    fi
-
-    grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<$sys\\>")" || true
-    if [ -z "$grouping" ]; then
-      # subsystem not mounted anywhere; mount it on its own
-      grouping="$sys"
-    fi
-
-    mountpoint="/sys/fs/cgroup/$grouping"
-
-    mkdir -p "$mountpoint"
-
-    # clear out existing mount to make sure new one is read-write
-    if mountpoint -q "$mountpoint"; then
-      umount "$mountpoint"
-    fi
-
-    mount -n -t cgroup -o "$grouping" cgroup "$mountpoint"
-
-    if [ "$grouping" != "$sys" ]; then
-      if [ -L "/sys/fs/cgroup/$sys" ]; then
-        rm "/sys/fs/cgroup/$sys"
-      fi
-
-      ln -s "$mountpoint" "/sys/fs/cgroup/$sys"
-    fi
-  done
-
-  if ! test -e /sys/fs/cgroup/systemd ; then
-    mkdir /sys/fs/cgroup/systemd
-    mount -t cgroup -o none,name=systemd none /sys/fs/cgroup/systemd
-  fi
 }
 
 start_docker() {
