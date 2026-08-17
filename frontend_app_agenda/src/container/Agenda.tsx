tracim/tracim

commit 3ff9d714bafa63a2bf5355dcff806d52d8c6f918

feat(front): add custom event for page change triggered by useLocation and setHeadTitle
tracim/tracim!12

frontend_app_agenda/src/container/Agenda.tsx
@@ -165,6 +165,8 @@ export class Agenda extends React.Component<any, any> {
   }
 
   setHeadTitle = (title: string): void => {
+    if (!title) return
+
     // @ts-expect-error global variable
     GLOBAL_dispatchEvent({
       type: CUSTOM_EVENT.SET_HEAD_TITLE,

frontend_app_logbook/src/container/Logbook.jsx
@@ -472,6 +472,8 @@ export class Logbook extends React.Component {
   setHeadTitle = (contentName) => {
     const { state } = this
 
+    if (!contentName) return
+
     if (state.config && state.config.workspace && state.isVisible) {
       GLOBAL_dispatchEvent({
         type: CUSTOM_EVENT.SET_HEAD_TITLE,

backend/tracim_backend/views/frontend.py
@@ -16,6 +16,7 @@ from tracim_backend.lib.utils.utils import FRONTEND_UI_SUBPATH
 from tracim_backend.views.controllers import Controller
 
 INDEX_PAGE_NAME = "index.mak"
+ANALYTICS_JS_FILE_NAME = "analytics.js"
 APP_FRONTEND_PATH = "app/{minislug}.app.optimized.js"
 # INFO S.G - 2020-12-10 - minimum recommended size is 128bits = 16bytes, doubling this
 CSP_NONCE_SIZE = 32
@@ -71,6 +72,15 @@ class FrontendController(Controller):
             if entry.name.endswith(".js") and entry.is_file():
                 custom_toolbox_files.append(entry)
         return custom_toolbox_files
+
+    def _get_analytics_file_path(self) -> str:
+        analytics_file_path = os.path.join(
+            self.dist_folder_path, "assets", "branding", ANALYTICS_JS_FILE_NAME
+        )
+        analytics_file_web_path = f"/assets/branding/{ANALYTICS_JS_FILE_NAME}"
+        if not os.path.exists(analytics_file_path):
+            return ""
+        return analytics_file_web_path
 
     def ui(self, context, request: TracimRequest):
         return self.index(context, request)
@@ -139,6 +149,7 @@ class FrontendController(Controller):
                 )
             csp_headers.append((csp_header_key, csp_header_value))
             base_response = Response(headerlist=[("Content-Type", "text/html")] + csp_headers)
+            analytics_file_path = self._get_analytics_file_path()
         return render_to_response(
             self._get_index_file_path(),
             {
@@ -158,6 +169,7 @@ class FrontendController(Controller):
                 "excluded_notifications": app_config.WEB__NOTIFICATIONS__EXCLUDED,
                 "csp_nonce": csp_nonce,
                 "glob": self.glob,
+                "analytics_file_path": analytics_file_path,
             },
             request=request,
             response=base_response,

frontend/dist/assets/branding.sample/analytics.js
@@ -1,0 +1,24 @@
+// Bellow is an example of analytics script using Matomo
+// Replace the content of this file with the code from your analytics software
+
+// var _paq = window._paq = window._paq || [];
+// /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+// _paq.push(['trackPageView']);
+// _paq.push(['enableLinkTracking']);
+// (function() {
+//   var u="https://matomo.mydomain.fr/";
+//   _paq.push(['setTrackerUrl', u+'matomo.php']);
+//   _paq.push(['setSiteId', '1']);
+//   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+//   g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+// })();
+
+// Custom event "newPageViewed" is fired by Tracim on page change once the new page title has been set.
+// document.addEventListener('newPageViewed', function (e) {
+//   // Custom code goes here
+//   _paq.push(['setCustomUrl', window.location.href]);
+//   // The page title includes the number of unread mention. The event data e.detail.newTitle is the page title
+//   // without the unread mention count to make the title stable.
+//   _paq.push(['setDocumentTitle', e.detail.newTitle]);
+//   _paq.push(['trackPageView']);
+// })

frontend/dist/index.mak
@@ -108,6 +108,10 @@
       GLOBAL_primaryColor = '${primary_color_str}'
       GLOBAL_excludedNotifications = '${excluded_notifications}'.replace(/\s/g, '').split(',')
     </script>
+
+    % if analytics_file_path:
+      <script src="${analytics_file_path}" nonce="${csp_nonce}"></script>
+    % endif
   </head>
 
   <body>

frontend/src/action-creator.sync.js
@@ -181,7 +181,8 @@ export const LANG = 'Lang'
 export const updateLangList = langList => ({ type: `${UPDATE}/${LANG}`, langList })
 
 export const HEAD_TITLE = 'HeadTitle'
-export const setHeadTitle = (headTitle, titlePrefix = '') => ({ type: `${SET}/${HEAD_TITLE}`, headTitle, titlePrefix })
+export const SET_HEAD_TITLE = `${SET}/${HEAD_TITLE}`
+export const setHeadTitle = (headTitle, titlePrefix = '') => ({ type: SET_HEAD_TITLE, headTitle, titlePrefix })
 
 export const BREADCRUMBS = 'Breadcrumbs'
 export const setBreadcrumbs = newBreadcrumbs => ({ type: `${SET}/${BREADCRUMBS}`, newBreadcrumbs })

frontend/src/component/TriggerPageView.jsx
@@ -1,0 +1,36 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import {
+  TracimComponent,
+  CUSTOM_EVENT
+} from 'tracim_frontend_lib'
+
+export const TriggerPageView = props => {
+  const location = useLocation()
+
+  // INFO - CH - 2025-09-15 - Tracim.jsx fire CUSTOM_EVENT.HEAD_TITLE_CHANGED on every page title change.
+  // This component listen to this event but deregister the listener right after triggering it.
+  // It register it again on location change (using useLocation hook).
+  // This makes CUSTOM_EVENT.NEW_PAGE_VIEWED to be fired only when both the location and the page title
+  // have changed. Which allow to fire a "Page View" event that include the up-to-date page title.
+  useEffect(() => {
+    props.registerCustomEventHandlerList([
+      {
+        name: CUSTOM_EVENT.HEAD_TITLE_CHANGED,
+        handler: (data) => {
+          const customEvent = new globalThis.CustomEvent(CUSTOM_EVENT.NEW_PAGE_VIEWED, {
+            detail: { newTitle: data }
+          })
+
+          document.dispatchEvent(customEvent)
+
+          props.unregisterCustomEventHandlerList([{ name: CUSTOM_EVENT.HEAD_TITLE_CHANGED }])
+        }
+      }
+    ])
+  }, [location])
+
+  return null
+}
+
+export default TracimComponent(TriggerPageView)

frontend/src/container/Tracim.jsx
@@ -97,6 +97,7 @@ import Favorites from './Favorites.jsx'
 import ContentRedirection from './ContentRedirection.jsx'
 import WorkspacePage from './WorkspacePage.jsx'
 import ToDo from './ToDo.jsx'
+import TriggerPageView from '../component/TriggerPageView.jsx'
 
 const CONNECTION_MESSAGE_DISPLAY_DELAY_MS = 4000
 
@@ -374,11 +375,18 @@ export class Tracim extends React.Component {
     const hasUnreadNotificationCountChanged = unreadNotificationCount !== prevUnreadNotificationCount
 
     if ((hasHeadTitleChanged || hasUnreadMentionCountChanged) && props.system.titleArgs?.length > 0) {
-      let newHeadTitle = buildHeadTitle(props.system.titleArgs)
+      const newHeadTitle = buildHeadTitle(props.system.titleArgs)
+      let newHeadTitleWithNotifCount = newHeadTitle
+
       if (unreadMentionCount > 0) {
-        newHeadTitle = `(${unreadMentionCount > 99 ? '99+' : unreadMentionCount}) ${newHeadTitle}`
+        newHeadTitleWithNotifCount = `(${unreadMentionCount > 99 ? '99+' : unreadMentionCount}) ${newHeadTitle}`
       }
-      document.title = newHeadTitle
+
+      document.title = newHeadTitleWithNotifCount
+      GLOBAL_dispatchEvent({
+        type: CUSTOM_EVENT.HEAD_TITLE_CHANGED,
+        data: newHeadTitle
+      })
     }
 
     if (hasUnreadMentionCountChanged || hasUnreadNotificationCountChanged) {
@@ -420,6 +428,8 @@ export class Tracim extends React.Component {
 
     return (
       <div className='tracim fullWidthFullHeight' dir={i18next.dir()}>
+        <TriggerPageView />
+
         <Header />
 
         {state.displayConnectionError && (

commit 9253ce4e16d450b071fcfcbaaf5e0cb46d2741c4

Fix frontend unit tests errors (#6936)
- OpenContentApp: new prop added in the component in 48293aae, but this prop was absent from the corresponding test mock
- frontend_app_agenda Agenda linter error: introduced by 3ff9d714b
- Agenda import error in test: introduced in ac2e69898
- Agenda.spec state update: regression introduced in 1e3f66837
- frontend_app_logbook lint error

frontend_app_agenda/src/container/Agenda.tsx
@@ -165,7 +165,7 @@ export class Agenda extends React.Component<any, any> {
   }
 
   setHeadTitle = (title: string): void => {
-    if (!title) return
+    if (title === '') return
 
     // @ts-expect-error global variable
     GLOBAL_dispatchEvent({

frontend_app_logbook/src/component/Logbook.jsx
@@ -193,8 +193,6 @@ export class Logbook extends React.Component {
   }
 
   handleConfirmRemoveEntry = (entry) => {
-    const { props } = this
-
     const newLogbook = removeEntryFromLogbook(this.state.logbook, entry)
 
     this.save(newLogbook, entry.id)

frontend/test/src/component/OpenContentApp.spec.js
@@ -18,6 +18,7 @@ describe('<OpenContentApp />', () => {
   const dispatchCustomEventSpy = sinon.spy()
   const onUpdateAppOpenedTypeSpy = sinon.spy()
   const renderAppFeatureSpy = sinon.spy()
+  const dispatchBroadcastChannelCustomEventSpy = sinon.spy()
 
   const dispatchMock = p => {
     if (isFunction(p)) {
@@ -41,6 +42,7 @@ describe('<OpenContentApp />', () => {
     },
     onUpdateAppOpenedType: onUpdateAppOpenedTypeSpy,
     dispatch: dispatchMock,
+    liveMessageManager: { dispatchBroadcastChannelCustomEvent: dispatchBroadcastChannelCustomEventSpy },
     t: s => s
   }
 

frontend_app_agenda/test/.mocharc.js
@@ -1,9 +1,12 @@
 'use strict'
 
+require('@babel/register')({
+  extensions: ['.js', '.jsx', '.ts', '.tsx']
+})
+
 module.exports = {
   require: [
     'regenerator-runtime',
-    '@babel/register',
     'ignore-styles',
     'test/setup',
     'isomorphic-fetch' // INFO - GB - 2020-06-18- Isomorphic-fetch provide the fetch API because Node does not contain it, and unit test run in Node context

frontend_app_agenda/test/Agenda.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
-import { Agenda } from '../src/container/Agenda.jsx'
+import { Agenda } from '../src/container/Agenda'
 import { user, workspace } from 'tracim_frontend_lib/dist/tracim_frontend_lib.test_utils.standalone.js'
 import { debug } from './fixture/defaultProps.js' // INFO - CH - 2021-12-30 - This file is generated by build_app.sh
 import {
@@ -85,7 +85,7 @@ describe('<Agenda />', () => {
               }
             }
           }
-          wrapper.setState({ userWorkspaceList: userWorkspaceList })
+          wrapper.setState({ agendaList: userWorkspaceList })
           wrapper.instance().handleSharedspaceModified(tlmData)
           expect(wrapper.state('content').workspaceLabel).to.equal(tlmData.fields.workspace.label)
         })